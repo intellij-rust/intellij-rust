17b6680c741441188838442c09dd9779039fca8e
Alexey Kudinkin <alexey.kudinkin@gmail.com>
2015-09-17 02:21:06 +0300

    (T): Pushed common test utilities

---
+++ test/org/rust/lang/RustTestUtils.kt
@@ -0,0 +1,13 @@
+package org.rust.lang
+
+import com.intellij.openapi.application.PathManager
+import kotlin.platform.platformStatic
+
+public object RustTestUtils {
+
+    @platformStatic
+    public fun setTestDataPath() {
+        System.setProperty(PathManager.PROPERTY_HOME_PATH, "test/testData")
+    }
+
+}
