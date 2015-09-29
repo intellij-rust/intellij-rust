f5a615c412b2912d29eab9990dba6c1b33d0a323
Aleksey Kladov <aleksey.kladov@gmail.com>
2015-09-29 21:18:50 +0300

    (PAR): minor cleanup

--- src/org/rust/lang/core/parser/RustParserUtil.kt
+++ src/org/rust/lang/core/parser/RustParserUtil.kt
@@ -5,15 +5,14 @@ import com.intellij.lang.parser.GeneratedParserUtilBase
 import com.intellij.psi.TokenType
 import org.rust.lang.core.lexer.RustTokenElementTypes
 import org.rust.lang.core.lexer.containsEOL
-import kotlin.platform.platformStatic
 
 public object RustParserUtil : GeneratedParserUtilBase() {
 
-    @platformStatic
-    public fun skipUntilEOL(b: PsiBuilder, level: Int) : Boolean {
+    @JvmStatic
+    public fun skipUntilEOL(b: PsiBuilder, level: Int): Boolean {
         while (!b.eof()) {
-            if (b.getTokenType() == TokenType.WHITE_SPACE
-            ||  b.getTokenText()?.containsEOL() != null) return true;
+            if (b.tokenType == TokenType.WHITE_SPACE
+                    || b.tokenText?.containsEOL() != null) return true;
 
             b.advanceLexer();
         }
