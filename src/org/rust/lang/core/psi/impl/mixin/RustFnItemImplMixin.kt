5ad0850083fc3e3c5916bd05ed8c1650b3631c30
Aleksey Kladov <aleksey.kladov@gmail.com>
2015-10-13 02:57:24 +0300

    (PAR): consolidate all paths in one place

--- src/org/rust/lang/core/psi/impl/mixin/RustFnItemImplMixin.kt
+++ src/org/rust/lang/core/psi/impl/mixin/RustFnItemImplMixin.kt
@@ -1,10 +1,7 @@
 package org.rust.lang.core.psi.impl.mixin
 
 import com.intellij.lang.ASTNode
-import org.rust.lang.core.psi.RustPathExpr
 import org.rust.lang.core.psi.RustFnItem
-import org.rust.lang.core.psi.RustNamedElement
-import org.rust.lang.core.psi.RustPathExprPart
 import org.rust.lang.core.psi.impl.RustCompositeElementImpl
 import org.rust.lang.core.resolve.scope.RustResolveScope
 
--- src/org/rust/lang/core/resolve/scope/RustResolveScope.kt
+++ src/org/rust/lang/core/resolve/scope/RustResolveScope.kt
@@ -4,30 +4,29 @@ import com.intellij.psi.PsiElement
 import org.rust.lang.core.psi.RustCompositeElement
 import org.rust.lang.core.psi.RustPathExpr
 import org.rust.lang.core.psi.RustNamedElement
-import org.rust.lang.core.psi.RustPathExprPart
 import org.rust.lang.core.psi.util.match
 import org.rust.lang.core.resolve.RustResolveEngine
 
 public interface RustResolveScope : RustCompositeElement {
 
-    public fun lookup(pathPart: RustPathExprPart): RustNamedElement? {
-        for (c in getChildren()) {
-            if (c is RustNamedElement && pathPart.getIdentifier().match(c.getName())) {
-                return pathPart.getPathExprPart().let {
-                    tail ->
-                        when (tail) {
-                            null -> return c
-                            else -> when (c) {
-                                        is RustResolveScope -> c.lookup(tail)
-                                        else -> null
-                                    }
-                        }
-                }
-            }
-        }
-
-        return null;
-    }
+//    public fun lookup(pathPart: RustPathExprPart): RustNamedElement? {
+//        for (c in getChildren()) {
+//            if (c is RustNamedElement && pathPart.getIdentifier().match(c.getName())) {
+//                return pathPart.getPathExprPart().let {
+//                    tail ->
+//                        when (tail) {
+//                            null -> return c
+//                            else -> when (c) {
+//                                        is RustResolveScope -> c.lookup(tail)
+//                                        else -> null
+//                                    }
+//                        }
+//                }
+//            }
+//        }
+//
+//        return null;
+//    }
 
 }
 
