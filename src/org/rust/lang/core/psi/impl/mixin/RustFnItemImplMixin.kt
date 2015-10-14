e2339673c4ead9d3d091fd7e006a3cd82bf64aff
Aleksey Kladov <aleksey.kladov@gmail.com>
2015-10-15 02:45:51 +0300

    (NAV): add simple FileStructure

--- src/org/rust/lang/core/psi/impl/mixin/RustFnItemImplMixin.kt
+++ src/org/rust/lang/core/psi/impl/mixin/RustFnItemImplMixin.kt
@@ -3,6 +3,7 @@ package org.rust.lang.core.psi.impl.mixin
 import com.intellij.lang.ASTNode
 import org.rust.lang.core.psi.RustFnItem
 import org.rust.lang.core.psi.impl.RustCompositeElementImpl
+import org.rust.lang.core.psi.impl.RustNamedElementImpl
 import org.rust.lang.core.resolve.scope.RustResolveScope
 
 public abstract class RustFnItemImplMixin(node: ASTNode)
