fcb35d31f46c31c149be4d7753203e0ba4b8799b
Aleksey Kladov <aleksey.kladov@gmail.com>
2015-10-15 12:07:06 +0300

    (PSI): fn item is a named element

--- src/org/rust/lang/core/psi/impl/mixin/RustFnItemImplMixin.kt
+++ src/org/rust/lang/core/psi/impl/mixin/RustFnItemImplMixin.kt
@@ -2,12 +2,11 @@ package org.rust.lang.core.psi.impl.mixin
 
 import com.intellij.lang.ASTNode
 import org.rust.lang.core.psi.RustFnItem
-import org.rust.lang.core.psi.impl.RustCompositeElementImpl
 import org.rust.lang.core.psi.impl.RustNamedElementImpl
 import org.rust.lang.core.resolve.scope.RustResolveScope
 
 public abstract class RustFnItemImplMixin(node: ASTNode)
-    : RustCompositeElementImpl(node)
+    : RustNamedElementImpl(node)
     , RustFnItem
     , RustResolveScope {
 
