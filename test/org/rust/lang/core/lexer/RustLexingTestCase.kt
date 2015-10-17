a8ac6af76e31d8c4a52fbd5c5ca8639a6a660efe
Aleksey Kladov <aleksey.kladov@gmail.com>
2015-10-18 02:55:23 +0300

    (T): do not corrupt PROPERTY_HOME_PATH in LexingTestCase
    LexerTestCase is looking for the fixture files relative to IDEA home dir,
    but they are situated in project source dir.
    
    The solution to change IDEA home to project is not satisfying. If we
    don't revert the change, then further tests will write some data to test
    data dir, and we can't revert the change even with setUp / tearDown,
    because PathManager caches System properties.
    
    We can reimplement `doFileTest` method of `LexerTestCase`, but that's
    quite a bit of copy paste.
    
    So let's hack around the issue by cooking up dirPath relative to IDEA home.

--- test/org/rust/lang/core/lexer/RustLexingTestCase.kt
+++ test/org/rust/lang/core/lexer/RustLexingTestCase.kt
@@ -1,25 +1,28 @@
 package org.rust.lang.core.lexer
 
-import com.intellij.lang.ParserDefinition
 import com.intellij.lexer.Lexer
 import com.intellij.openapi.application.PathManager
 import com.intellij.testFramework.LexerTestCase
 import org.rust.lang.RustFileType
-import org.rust.lang.RustTestUtils
-import org.rust.lang.core.RustParserDefinition
+import java.nio.file.Paths
+
 
 public class RustLexingTestCase : LexerTestCase() {
 
-    init {
-        RustTestUtils.setTestDataPath()
-    }
+    override fun getDirPath(): String {
+        val home = Paths.get(PathManager.getHomePath()).toAbsolutePath()
+        val testData = Paths.get("testData", "lexer").toAbsolutePath()
 
-    override fun getDirPath(): String? = "lexer"
+        // XXX: unfortunately doFileTest will look for the file relative to the home directory of
+        // the test instance of IDEA, so let's cook a dirPath starting with several ../../
+        return home.relativize(testData).toString()
+    }
 
     override fun createLexer(): Lexer? = RustLexer()
 
     val ext = RustFileType.DEFAULTS.EXTENSION
 
+    // @formatter:off
     fun testComments()          { doFileTest(ext) }
     fun testShebang()           { doFileTest(ext) }
     fun testFloat()             { doFileTest(ext) }
@@ -27,4 +30,5 @@ public class RustLexingTestCase : LexerTestCase() {
     fun testCharLiterals()      { doFileTest(ext) }
     fun testStringLiterals()    { doFileTest(ext) }
     fun testByteLiterals()      { doFileTest(ext) }
+    // @formatter:on
 }
