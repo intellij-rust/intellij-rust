8850c40396486fb21a586fbf315338268bcb8193
Aleksey Kladov <aleksey.kladov@gmail.com>
2015-10-20 13:09:39 +0300

    (PAR): implement `no_struct_literal_expr`
    When parsing condition in `if/when/match`, certain kinds of
    expressions are forbidden. Namely, struct literals (`Foo {i: 92}`) and
    block-ending ranges (`1..{42}`). This helps in such situations:
    
    ```
    if i < Foo {
       ...
    }
    
    for x in 1.. {
        ...
    }
    
    ```
    
    This commit adds a boolean flag to the parser, which keeps track of the
    context and allows to impose the restrictions without duplicating the
    grammar for expressions. Rustc's `libsyntax/parser/parser.rs` handles
    the situation in a similar way.
    
    Reference: https://github.com/rust-lang/rfcs/blob/master/text/0092-struct-grammar.md

--- src/org/rust/lang/core/parser/RustParserUtil.kt
+++ src/org/rust/lang/core/parser/RustParserUtil.kt
@@ -2,11 +2,49 @@ package org.rust.lang.core.parser
 
 import com.intellij.lang.PsiBuilder
 import com.intellij.lang.parser.GeneratedParserUtilBase
+import com.intellij.openapi.util.Key
 import com.intellij.psi.TokenType
 import org.rust.lang.core.lexer.RustTokenElementTypes
 import org.rust.lang.core.lexer.containsEOL
 
 public object RustParserUtil : GeneratedParserUtilBase() {
+
+    private val STRUCT_ALLOWED: Key<Boolean> = Key("org.rust.STRUCT_ALLOWED")
+
+    private fun PsiBuilder.isStructAllowed(): Boolean {
+        return getUserData(STRUCT_ALLOWED) ?: true
+    }
+
+    private fun PsiBuilder.setStructAllowed(value: Boolean) {
+        return putUserData(STRUCT_ALLOWED, value)
+    }
+
+    @JvmStatic
+    public fun checkStructAllowed(b: PsiBuilder, level: Int) : Boolean = b.isStructAllowed()
+
+    @JvmStatic
+    public fun checkBraceAllowed(b: PsiBuilder, level: Int) : Boolean {
+        return b.isStructAllowed() || b.tokenType != RustTokenElementTypes.LBRACE
+    }
+
+    @JvmStatic
+    public fun withoutStructLiterals(b: PsiBuilder, level: Int, parser: GeneratedParserUtilBase.Parser): Boolean {
+        val old = b.isStructAllowed()
+        b.setStructAllowed(false)
+        val result = parser.parse(b, level)
+        b.setStructAllowed(old)
+        return result
+    }
+
+    @JvmStatic
+    public fun withStructLiterals(b: PsiBuilder, level: Int, parser: GeneratedParserUtilBase.Parser): Boolean {
+        val old = b.isStructAllowed()
+        b.setStructAllowed(true)
+        val result = parser.parse(b, level)
+        b.setStructAllowed(old)
+        return result
+    }
+
 
     @JvmStatic
     public fun skipUntilEOL(b: PsiBuilder, level: Int): Boolean {
--- test/org/rust/lang/core/parser/RustParserCtrsTestCase.kt
+++ test/org/rust/lang/core/parser/RustParserCtrsTestCase.kt
@@ -51,11 +51,7 @@ public class RustParserCtrsTestCase : ParsingTestCase("ctrs", ".rs", RustParserD
     private val expectedErrors = setOf(
             "testData/ctrs/test/1.1.0/run-pass/utf8-bom.rs",
             "testData/ctrs/test/1.1.0/run-pass/macro-interpolation.rs",
-            "testData/ctrs/test/1.1.0/run-pass/ranges-precedence.rs",
-            "testData/ctrs/test/1.1.0/run-pass/small-enums-with-fields.rs",
-            "testData/ctrs/test/1.1.0/run-pass/struct-lit-functional-no-fields.rs",
-            "testData/ctrs/test/1.1.0/doc-core/libcore_macros_rs_0006.rs",
-            "testData/ctrs/test/1.2.0/run-pass/ranges-precedence.rs"
+            "testData/ctrs/test/1.1.0/run-pass/small-enums-with-fields.rs"
     )
 }
 
