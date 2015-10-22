d8bdf7614607e2385dbc89908285ea38765a4178
Aleksey Kladov <aleksey.kladov@gmail.com>
2015-10-22 03:10:36 +0300

    (T): explicitly register BraceMatcher in tests
    This fixes test order dependency. BraceMatcher affects parser output for
    ill formed input. Previously, parser tests used the brace matcher only
    if some LightCodeInsightFixtureTestCase was exercised before. Now brace
    matcher is always used.

--- test/org/rust/lang/core/parser/RustParsingTestCaseBase.kt
+++ test/org/rust/lang/core/parser/RustParsingTestCaseBase.kt
@@ -1,8 +1,11 @@
 package org.rust.lang.core.parser
 
+import com.intellij.lang.LanguageBraceMatching
 import com.intellij.testFramework.ParsingTestCase
 import org.jetbrains.annotations.NonNls
+import org.rust.lang.RustLanguage
 import org.rust.lang.core.RustParserDefinition
+import org.rust.lang.highlight.RustBraceMatcher
 
 abstract class RustParsingTestCaseBase(@NonNls dataPath: String)
  : ParsingTestCase("psi/" + dataPath, "rs", true /*lowerCaseFirstLetter*/, RustParserDefinition()) {
@@ -11,6 +14,7 @@ abstract class RustParsingTestCaseBase(@NonNls dataPath: String)
 
     override fun setUp() {
         super.setUp()
+        addExplicitExtension(LanguageBraceMatching.INSTANCE, RustLanguage.INSTANCE, RustBraceMatcher())
     }
 
     override fun tearDown() {
