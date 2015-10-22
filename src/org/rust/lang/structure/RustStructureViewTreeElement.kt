c7baafc0870671d06df9fcb99eb51d415d7d7918
Aleksey Kladov <aleksey.kladov@gmail.com>
2015-10-22 17:15:22 +0300

    (NAV): use native recursive visitor

--- src/org/rust/lang/structure/RustStructureViewTreeElement.kt
+++ src/org/rust/lang/structure/RustStructureViewTreeElement.kt
@@ -2,7 +2,7 @@ package org.rust.lang.structure
 
 import com.intellij.ide.structureView.impl.common.PsiTreeElementBase
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiElementVisitor
+import com.intellij.psi.PsiRecursiveElementWalkingVisitor
 import org.rust.lang.core.psi.impl.RustFileImpl
 import org.rust.lang.core.psi.impl.RustFnItemImpl
 import org.rust.lang.core.psi.impl.RustImplMethodImpl
@@ -13,16 +13,14 @@ class RustStructureViewTreeElement(file: RustFileImpl) : PsiTreeElementBase<Rust
 
     override fun getChildrenBase(): Collection<RustStructureViewElement> {
         var result = listOf<RustStructureViewElement>()
-        element?.accept(object :
-                PsiElementVisitor() {
-            override fun visitElement(element: PsiElement?) {
+        element?.accept(object : PsiRecursiveElementWalkingVisitor() {
+            override fun elementFinished(element: PsiElement?) {
                 when (element) {
                     is RustFnItemImpl ->
                         result += RustStructureViewElement(element)
                     is RustImplMethodImpl ->
                         result += RustStructureViewElement(element)
                 }
-                element?.children?.forEach { it.accept(this) }
             }
         })
 
