4a66b0ae75b26ba440deca3a40a3a64ea859e3d5
Aleksey Kladov <aleksey.kladov@gmail.com>
2015-10-23 17:25:00 +0300

    (PAR): fix impl_item grammar
    The fix is simply to get rid of `type_prim`, it is unnecessary in LL setting.

--- testData/psi/parser/well-formed/impls.rs
+++ testData/psi/parser/well-formed/impls.rs
@@ -1,3 +1,32 @@
+struct S;
+
+trait A {
+    type B;
+}
+
+impl A for S {
+    type B = S;
+}
+
+
+trait T { }
+trait P<X> { }
+
+
+impl T  { }
+impl (T) { }
+impl T for S { }
+// Syntactically invalid
+//impl (T) for S { }
+
+impl<U> P<U> { }
+impl<U> (P<U>) { }
+impl<U> P<U> for S { }
+impl T for <S as A>::B { }
+
+// Semantically invalid
+impl (<S as A>::B) { }
+
 impl<'a, T> Iterator for Iter<'a, T> + 'a {
     type Item = &'a T;
 
