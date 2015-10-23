bc27b9a0c2c5ab811999a352e7aaa86d76303a92
Aleksey Kladov <aleksey.kladov@gmail.com>
2015-10-23 15:20:19 +0300

    (T): resurrect CTRS tests
    Also make sure that CTRS test fails if for some reason no files were found.

--- test/org/rust/lang/core/parser/RustCTRSTestCase.kt
+++ test/org/rust/lang/core/parser/RustCTRSTestCase.kt
@@ -11,9 +11,7 @@ import org.junit.Assert
 import org.rust.lang.core.RustParserDefinition
 import java.io.File
 
-public class RustCTRSTestCase : RustParsingTestCaseBase("ctrs") {
-
-    override fun getTestDataPath() = "testData"
+public class RustCTRSTestCase : RustParsingTestCaseBase("parser/ctrs") {
 
     public fun hasError(file: PsiFile): Boolean {
         var hasErrors = false
@@ -30,8 +28,10 @@ public class RustCTRSTestCase : RustParsingTestCaseBase("ctrs") {
     }
 
     fun testCtrs() {
+        var nFilesVisited = 0
         FileUtil.visitFiles(File(myFullDataPath, "test"), {
             if (it.isFile && it.extension == myFileExt.trimStart('.')) {
+                nFilesVisited++;
                 val text = FileUtil.loadFile(it)
                 val psi = createPsiFile(it.name, text)
                 val expectedError = expectedErrors.contains(it.path)
@@ -46,12 +46,13 @@ public class RustCTRSTestCase : RustParsingTestCaseBase("ctrs") {
             }
             true
         })
+        Assert.assertTrue("CTRS tests were not run.", nFilesVisited > 3000)
     }
 
     private val expectedErrors = setOf(
-            "testData/ctrs/test/1.1.0/run-pass/utf8-bom.rs",
-            "testData/ctrs/test/1.1.0/run-pass/macro-interpolation.rs",
-            "testData/ctrs/test/1.1.0/run-pass/small-enums-with-fields.rs"
+            "testData/psi/parser/ctrs/test/1.1.0/run-pass/utf8-bom.rs",
+            "testData/psi/parser/ctrs/test/1.1.0/run-pass/macro-interpolation.rs",
+            "testData/psi/parser/ctrs/test/1.1.0/run-pass/small-enums-with-fields.rs"
     )
 }
 
