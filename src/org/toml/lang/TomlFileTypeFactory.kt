a73d0e5ea14b57afc7dd088247e0694afca3b73d
winger <isenbaev@gmail.com>
2015-10-24 16:49:47 -0700

    Fix Toml lexer (https://github.com/alexeykudinkin/intellij-rust/issues/20)

--- src/org/toml/lang/TomlFileTypeFactory.kt
+++ src/org/toml/lang/TomlFileTypeFactory.kt
@@ -10,7 +10,7 @@ import com.intellij.openapi.fileTypes.FileTypeFactory
 public class TomlFileTypeFactory : FileTypeFactory() {
 
     override fun createFileTypes(consumer: FileTypeConsumer) {
-        consumer.consume(TomlFileType.INSTANCE, TomlFileType.DEFAULTS.EXTENSION);
+        consumer.consume(TomlFileType.INSTANCE, TomlFileType.INSTANCE.defaultExtension);
     }
 
 }
