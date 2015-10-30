f1b6d1d5ec561d8d308d8f9da0253a258f37f574
Alexey Kudinkin <alexey.kudinkin@gmail.com>
2015-10-30 16:51:42 +0300

    (PSI): Made `RustNamedElement` explicitly (!) `NavigatablePsiElement`

--- src/org/rust/lang/core/psi/RustNamedElement.kt
+++ src/org/rust/lang/core/psi/RustNamedElement.kt
@@ -1,6 +1,8 @@
 package org.rust.lang.core.psi
 
+import com.intellij.psi.NavigatablePsiElement
 import com.intellij.psi.PsiNamedElement
 
-public interface RustNamedElement : PsiNamedElement {}
+public interface RustNamedElement : PsiNamedElement,
+                                    NavigatablePsiElement {}
 
