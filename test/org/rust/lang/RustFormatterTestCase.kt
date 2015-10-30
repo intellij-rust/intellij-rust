242e7a68762d6da454d13368b8b71cb1438a99fe
Aleksey Kladov <aleksey.kladov@gmail.com>
2015-10-31 02:18:39 +0300

    (FMT): implement the simplest formatter possible

---
+++ test/org/rust/lang/RustFormatterTestCase.kt
@@ -0,0 +1,24 @@
+package org.rust.lang
+
+import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.psi.codeStyle.CodeStyleManager
+
+class RustFormatterTestCase : RustTestCase() {
+    override fun getTestDataPath() = "testData/formatter"
+
+    private fun doTest() {
+        myFixture.configureByFile(fileName)
+
+        object : WriteCommandAction.Simple<Any>(project) {
+            override fun run() {
+                CodeStyleManager.getInstance(project).reformat(myFixture.file)
+            }
+        }.execute()
+
+        myFixture.checkResultByFile(goldFileName)
+    }
+
+    //@formatter:off
+    fun testBlocks()                { doTest() }
+    //@formatter:on
+}
--- test/org/rust/lang/RustTestCase.kt
+++ test/org/rust/lang/RustTestCase.kt
@@ -6,6 +6,9 @@ abstract class RustTestCase : LightCodeInsightFixtureTestCase() {
     final protected val fileName: String
         get() = "${camelToSnake(getTestName(true))}.rs"
 
+    final protected val goldFileName: String
+        get() = "${camelToSnake(getTestName(true))}_gold.rs"
+
     private fun camelToSnake(camelCaseName: String): String =
             camelCaseName.split("(?=[A-Z])".toRegex())
                     .map { it.toLowerCase() }
