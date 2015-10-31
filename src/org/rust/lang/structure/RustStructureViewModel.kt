205f893bb8d16ab28ae76c2eb1c34a338ef74999
Tobias Bieniek <tobias.bieniek@gmx.de>
2015-10-31 13:21:59 +0100

    StructureViewModel: Implement ElementInfoProvider

--- src/org/rust/lang/structure/RustStructureViewModel.kt
+++ src/org/rust/lang/structure/RustStructureViewModel.kt
@@ -1,12 +1,15 @@
 package org.rust.lang.structure
 
+import com.intellij.ide.structureView.StructureViewModel
 import com.intellij.ide.structureView.StructureViewTreeElement
 import com.intellij.ide.structureView.TextEditorBasedStructureViewModel
 import com.intellij.openapi.editor.Editor
+import org.rust.lang.core.psi.RustFnItem
+import org.rust.lang.core.psi.RustImplMethod
 import org.rust.lang.core.psi.impl.RustFileImpl
 
 class RustStructureViewModel(editor: Editor?, file: RustFileImpl) :
-        TextEditorBasedStructureViewModel(editor, file) {
+        TextEditorBasedStructureViewModel(editor, file), StructureViewModel.ElementInfoProvider {
 
     override fun getRoot(): StructureViewTreeElement {
         return RustFileTreeElement(psiFile)
@@ -14,6 +17,21 @@ class RustStructureViewModel(editor: Editor?, file: RustFileImpl) :
 
     override fun getPsiFile(): RustFileImpl {
         return super.getPsiFile() as RustFileImpl
+    }
+
+    override fun isAlwaysShowsPlus(element: StructureViewTreeElement): Boolean {
+        return when(element.value) {
+            is RustFileImpl -> true
+            else -> false
+        }
+    }
+
+    override fun isAlwaysLeaf(element: StructureViewTreeElement): Boolean {
+        return when(element.value) {
+            is RustFnItem -> true
+            is RustImplMethod -> true
+            else -> false
+        }
     }
 }
 
