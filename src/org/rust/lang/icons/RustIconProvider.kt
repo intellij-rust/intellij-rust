5aa37d3c80b4a3170b3d9b10c8452b203f06e8e1
Tobias Bieniek <tobias.bieniek@gmx.de>
2015-10-31 15:48:34 +0100

    Structure View: Add visibility icons to enums

--- src/org/rust/lang/icons/RustIconProvider.kt
+++ src/org/rust/lang/icons/RustIconProvider.kt
@@ -8,6 +8,7 @@ import com.intellij.ui.RowIcon
 import com.intellij.util.PlatformIcons
 import com.intellij.util.VisibilityIcons
 import com.intellij.util.ui.EmptyIcon
+import org.rust.lang.core.psi.RustEnumItem
 import org.rust.lang.core.psi.RustItem
 import org.rust.lang.core.psi.RustStructDeclField
 import org.rust.lang.core.psi.RustStructItem
@@ -17,13 +18,25 @@ import javax.swing.Icon
 class RustIconProvider: IconProvider() {
     override fun getIcon(element: PsiElement, flags: Int): Icon? {
         return when (element) {
-            is RustEnumItemImpl -> RustIcons.ENUM
+            is RustEnumItemImpl -> getEnumIcon(element, flags)
             is RustStructItemImpl -> getStructIcon(element, flags)
             is RustStructDeclField -> getStructDeclFieldIcon(element, flags)
             is RustFnItemImpl -> RustIcons.FUNCTION
             is RustImplMethodImpl -> RustIcons.METHOD
             else -> null
         }
+    }
+
+    private fun getEnumIcon(element: RustEnumItem, flags: Int): Icon? {
+        if ((flags and Iconable.ICON_FLAG_VISIBILITY) == 0)
+            return RustIcons.ENUM;
+
+        val parent = element.parent?.parent as RustItem?
+
+        val icon = RowIcon(RustIcons.ENUM, EmptyIcon.create(PlatformIcons.PUBLIC_ICON))
+        val visibility = if (parent?.pub == null) PsiUtil.ACCESS_LEVEL_PRIVATE else PsiUtil.ACCESS_LEVEL_PUBLIC
+        VisibilityIcons.setVisibilityIcon(visibility, icon);
+        return icon;
     }
 
     private fun getStructIcon(element: RustStructItem, flags: Int): Icon? {
