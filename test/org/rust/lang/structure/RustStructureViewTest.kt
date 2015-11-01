76369174b5c23abd9b5fc8ba1b61dbd502ecf8ef
Tobias Bieniek <tobias.bieniek@gmx.de>
2015-11-01 17:22:59 +0100

    Structure View: Use testStructureView() and assertTreeEqual() for unit tests

--- test/org/rust/lang/structure/RustStructureViewTest.kt
+++ test/org/rust/lang/structure/RustStructureViewTest.kt
@@ -1,39 +1,24 @@
 package org.rust.lang.structure
 
-import com.intellij.ide.actions.ViewStructureAction
-import com.intellij.ide.util.FileStructurePopup
-import com.intellij.openapi.fileEditor.impl.text.TextEditorProvider
-import com.intellij.openapi.util.Disposer
-import com.intellij.testFramework.PlatformTestUtil
-import org.assertj.core.api.Assertions.assertThat
+import com.intellij.openapi.util.io.FileUtil
+import com.intellij.testFramework.PlatformTestUtil.assertTreeEqual
 import org.rust.lang.RustTestCase
-import javax.swing.tree.TreePath
+import java.io.File
 
 class RustStructureViewTest : RustTestCase() {
     override fun getTestDataPath() = "testData/structure"
 
-    private fun structureContainsElements(vararg expected: String) {
-        myFixture.configureByFile(fileName)
-        val editor = TextEditorProvider.getInstance()!!.getTextEditor(myFixture.editor)
-        val popup = ViewStructureAction.createPopup(myFixture.project, editor)
-                ?: throw AssertionError("popup mustn't be null")
-
-        try {
-            popup.createCenterPanel()
-            popup.shallowExpand()
-            val text = PlatformTestUtil.print(popup.tree)
-
-            assertThat(text).contains(*expected)
-        } finally {
-            Disposer.dispose(popup)
-        }
+    private fun doTest(expected: String) {
+        myFixture.configureByFile(fileName);
+        myFixture.testStructureView { assertTreeEqual(it.tree, expected) }
     }
 
-    private fun FileStructurePopup.shallowExpand() {
-        tree.expandPath(TreePath(tree.model.root))
+    private fun doFileTest() {
+        val text = FileUtil.loadFile(File(testDataPath + "/" + fileName.replace(".rs", ".txt")))
+        doTest(text)
     }
 
-    fun testFunctions() {
-        structureContainsElements("fn_foo", "method_bar")
-    }
+    //@formatter:off
+    fun testFunctions() { doFileTest() }
+    //@formatter:on
 }
