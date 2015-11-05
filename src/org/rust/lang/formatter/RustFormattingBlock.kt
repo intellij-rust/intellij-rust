e3bce10dae66bc85af7cc9113459536c1a57dfbf
Aleksey Kladov <aleksey.kladov@gmail.com>
2015-11-05 03:08:08 +0300

    (FMT): refactoring

--- src/org/rust/lang/formatter/RustFormattingBlock.kt
+++ src/org/rust/lang/formatter/RustFormattingBlock.kt
@@ -3,15 +3,15 @@ package org.rust.lang.formatter
 import com.intellij.formatting.*
 import com.intellij.lang.ASTNode
 import com.intellij.psi.TokenType.WHITE_SPACE
+import com.intellij.psi.tree.IElementType
 import com.intellij.psi.tree.TokenSet
-import org.rust.lang.core.lexer.RustTokenElementTypes.LBRACE
-import org.rust.lang.core.lexer.RustTokenElementTypes.RBRACE
+import org.rust.lang.core.lexer.RustTokenElementTypes.*
 import org.rust.lang.core.psi.RustCompositeElementTypes.*
 
 class RustFormattingBlock(private val node: ASTNode, private val indent: Indent?) : ASTBlock {
 
     override fun getChildAttributes(newChildIndex: Int): ChildAttributes {
-        val childIndent = if (BLOCKS_TOKEN_SET.contains(node.elementType)) {
+        val childIndent = if (node.elementType isIn BLOCKS_TOKEN_SET) {
             Indent.getNormalIndent()
         } else {
             Indent.getNoneIndent()
@@ -34,30 +34,37 @@ class RustFormattingBlock(private val node: ASTNode, private val indent: Indent?
     override fun getSubBlocks(): List<Block> = mySubBlock
 
     private val mySubBlock: List<Block> by lazy {
-        val blockStart = node.findChildByType(LBRACE)?.startOffset ?: 0
         node.getChildren(null)
                 .filter { it.textRange.length > 0 && it.elementType != WHITE_SPACE }
                 .map {
-                    RustFormattingBlock(it, calcIndent(it, blockStart))
+                    RustFormattingBlock(it, calcChildIndent(it))
                 }
     }
 
-    private fun calcIndent(child: ASTNode, blockStart: Int): Indent {
-        if (!BLOCKS_TOKEN_SET.contains(node.elementType)) {
-            return Indent.getNoneIndent()
-        }
-        if (BRACES_TOKEN_SET.contains(child.elementType)) {
-            return Indent.getNoneIndent()
+    private fun calcChildIndent(child: ASTNode): Indent {
+        val parentType = node.elementType
+        val childType = child.elementType
+
+        if (parentType isIn BLOCKS_TOKEN_SET) {
+            if (childType isIn BLOCK_START_TOKEN_SET || childType isIn BRACES_TOKEN_SET) {
+                return Indent.getNoneIndent()
+            }
+            return Indent.getNormalIndent()
         }
 
-        // TODO(matklad): change PSI structure and get rid of this abomination
-        if (child.startOffset < blockStart) {
-            return Indent.getNoneIndent()
-        }
-
-        return Indent.getNormalIndent()
+        return Indent.getNoneIndent()
     }
 }
+
+private val BLOCK_START_TOKEN_SET = TokenSet.create(
+        PUB,
+        MOD,
+        STRUCT,
+        ENUM,
+        IMPL,
+        TRAIT,
+        MATCH
+)
 
 private val BLOCKS_TOKEN_SET = TokenSet.create(
         BLOCK,
@@ -73,3 +80,5 @@ private val BLOCKS_TOKEN_SET = TokenSet.create(
 private val BRACES_TOKEN_SET = TokenSet.create(
         LBRACE, RBRACE
 )
+
+infix fun IElementType.isIn(set: TokenSet) = set.contains(this)
