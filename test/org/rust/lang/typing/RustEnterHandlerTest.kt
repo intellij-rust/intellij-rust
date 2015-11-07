4033e2415abb20eeb04bb233844e983ff606ab7b
Tobias Bieniek <tobias.bieniek@gmx.de>
2015-11-07 19:41:05 +0100

    Add EnterHandler unit tests

---
+++ test/org/rust/lang/typing/RustEnterHandlerTest.kt
@@ -0,0 +1,19 @@
+package org.rust.lang.typing
+
+import org.rust.lang.RustTestCase
+
+class RustEnterHandlerTest : RustTestCase() {
+    override fun getTestDataPath() = "testData/typing"
+
+    private fun doTest() {
+        myFixture.configureByFile(fileName)
+        myFixture.type('\n')
+        myFixture.checkResultByFile(fileName.replace(".rs", "_after.rs"), true)
+    }
+
+    fun testInLineComment() = doTest()
+    fun testAfterLineComment() = doTest()
+    fun testInBlockComment() = doTest()
+    fun testInOuterDocComment() = doTest()
+    fun testInInnerDocComment() = doTest()
+}
---
+++ testData/typing/in_inner_doc_comment.rs
@@ -0,0 +1,4 @@
+fn double(x: i32) -> i32 {
+    //! multi<caret>ply by two
+    x * 2
+}
