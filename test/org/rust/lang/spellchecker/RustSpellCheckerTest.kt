90929851f3b7cb0994e3b64f51f63f919ae6acdb
Aleksey Kladov <aleksey.kladov@gmail.com>
2015-11-07 14:54:09 +0300

    (T): add spellchecker tests

---
+++ test/org/rust/lang/spellchecker/RustSpellCheckerTest.kt
@@ -0,0 +1,21 @@
+package org.rust.lang.spellchecker
+
+import com.intellij.spellchecker.inspections.SpellCheckingInspection
+import org.rust.lang.RustTestCase
+
+class RustSpellCheckerTest : RustTestCase() {
+    override fun getTestDataPath() = "testData/spellchecker"
+    private fun doTest(processComments: Boolean = true, processLiterals: Boolean = true) {
+        val inspection = SpellCheckingInspection()
+        inspection.processLiterals = processLiterals
+        inspection.processComments = processComments
+
+        myFixture.enableInspections(inspection)
+        myFixture.testHighlighting(false, false, true, fileName)
+    }
+
+    fun testComments() = doTest()
+    fun testStringLiterals() = doTest()
+    fun testCommentsSuppressed() = doTest(processComments = false)
+    fun testStringLiteralsSuppressed() = doTest(processLiterals = false)
+}
