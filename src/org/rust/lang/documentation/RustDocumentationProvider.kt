9ce04995c3824f922c68d12541354cef9e6bf531
Tobias Bieniek <tobias.bieniek@gmx.de>
2015-11-08 13:13:35 +0100

    DocumentationProvider: Implement quick navigate info for functions

--- src/org/rust/lang/documentation/RustDocumentationProvider.kt
+++ src/org/rust/lang/documentation/RustDocumentationProvider.kt
@@ -2,12 +2,14 @@ package org.rust.lang.documentation
 
 import com.intellij.lang.documentation.AbstractDocumentationProvider
 import com.intellij.psi.PsiElement
+import org.rust.lang.core.psi.RustFnItem
 import org.rust.lang.core.psi.RustPatIdent
 
 class RustDocumentationProvider : AbstractDocumentationProvider() {
 
     override fun getQuickNavigateInfo(element: PsiElement?, originalElement: PsiElement?) = when (element) {
         is RustPatIdent -> getQuickNavigateInfo(element)
+        is RustFnItem -> getQuickNavigateInfo(element)
         else -> null
     }
 
@@ -16,6 +18,22 @@ class RustDocumentationProvider : AbstractDocumentationProvider() {
         val bindingMode = element.bindingMode?.mut?.let { "mut " }.orEmpty()
 
         return "let $bindingMode<b>${element.identifier.text}</b>$location"
+    }
+
+    private fun getQuickNavigateInfo(element: RustFnItem): String {
+        val text = element.text
+
+        val identifier = element.identifier
+        val identStart = identifier.startOffsetInParent
+        val identEnd = identStart + identifier.textLength
+        val signatureLength = element.block?.startOffsetInParent ?: element.textLength
+
+        val beforeIdent = text.subSequence(0, identStart)
+        val identText = text.subSequence(identStart, identEnd)
+        val afterIdent = text.subSequence(identEnd, signatureLength).toString().trimEnd()
+
+        val location = getLocationString(element)
+        return "$beforeIdent<b>$identText</b>$afterIdent$location"
     }
 
     private fun getLocationString(element: PsiElement?): String {
