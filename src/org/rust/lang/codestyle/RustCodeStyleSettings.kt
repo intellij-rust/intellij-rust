1863a93bd777880cd35f2637c9a9ef73ee916b1b
Tobias Bieniek <tobias.bieniek@gmx.de>
2015-11-08 11:04:02 +0100

    CodeStyle: Remove unnecessary constructor keyword

--- src/org/rust/lang/codestyle/RustCodeStyleSettings.kt
+++ src/org/rust/lang/codestyle/RustCodeStyleSettings.kt
@@ -3,5 +3,5 @@ package org.rust.lang.codestyle
 import com.intellij.psi.codeStyle.CodeStyleSettings
 import com.intellij.psi.codeStyle.CustomCodeStyleSettings
 
-class RustCodeStyleSettings constructor(container: CodeStyleSettings) :
+class RustCodeStyleSettings(container: CodeStyleSettings) :
         CustomCodeStyleSettings(RustCodeStyleSettings::class.java.simpleName, container)
