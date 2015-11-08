303de6b2cfe9475dcd1a472335de3453d03d5851
Tobias Bieniek <tobias.bieniek@gmx.de>
2015-11-08 10:01:17 +0100

    Move user-facing strings into RustBundle.properties file
    Extracted from #38 and reduced to the currently used strings

--- src/org/rust/lang/module/RustModuleType.kt
+++ src/org/rust/lang/module/RustModuleType.kt
@@ -1,6 +1,7 @@
 package org.rust.lang.module
 
 import com.intellij.openapi.module.ModuleType
+import org.rust.lang.i18n.RustBundle
 import org.rust.lang.icons.RustIcons
 import javax.swing.Icon
 
@@ -11,13 +12,8 @@ public class RustModuleType() : ModuleType<RustModuleBuilder>(RustModuleType.RUS
         return RustModuleBuilder()
     }
 
-    override fun getName(): String {
-        return "Rust Module"
-    }
-
-    override fun getDescription(): String {
-        return "Rust Module"
-    }
+    override fun getName() = RustBundle.message("rust.module.display_name")
+    override fun getDescription() = RustBundle.message("rust.module.display_name")
 
     override fun getBigIcon(): Icon {
         return RustIcons.FILE_BIG
