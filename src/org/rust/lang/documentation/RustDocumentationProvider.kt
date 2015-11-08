09604ede4446248f0dcb6007f78820893189410e
Tobias Bieniek <tobias.bieniek@gmx.de>
2015-11-08 12:39:29 +0100

    DocumentationProvider: Import cleanup

--- src/org/rust/lang/documentation/RustDocumentationProvider.kt
+++ src/org/rust/lang/documentation/RustDocumentationProvider.kt
@@ -2,8 +2,6 @@ package org.rust.lang.documentation
 
 import com.intellij.lang.documentation.AbstractDocumentationProvider
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiFile
-import org.rust.lang.core.psi.RustBindingMode
 import org.rust.lang.core.psi.RustPatIdent
 
 class RustDocumentationProvider : AbstractDocumentationProvider() {
