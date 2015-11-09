04dd03e9a844f96ed54e916e6d903d6631b28563
Tobias Bieniek <tobias.bieniek@gmx.de>
2015-11-09 09:34:34 +0100

    Formatter: Move test data into org.rust.lang.formatter.testdata

--- test/org/rust/lang/formatter/RustFormatterTestCase.kt
+++ test/org/rust/lang/formatter/RustFormatterTestCase.kt
@@ -6,7 +6,7 @@ import org.rust.lang.RustTestCase
 class RustFormatterTestCase : FormatterTestCase() {
     override fun getTestDataPath() = "testData"
 
-    override fun getBasePath() = "formatter"
+    override fun getBasePath() = "org/rust/lang/formatter/testdata"
 
     override fun getFileExtension() = "rs"
 
