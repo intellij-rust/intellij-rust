2e04e1344023cfca47e2670ee9371e938309c477
Tobias Bieniek <tobias.bieniek@gmx.de>
2015-11-09 09:43:52 +0100

    TOML Annotator: Move test data into org.toml.lang.annotator.testdata

--- test/org/toml/lang/annotator/TomlAnnotatorTest.kt
+++ test/org/toml/lang/annotator/TomlAnnotatorTest.kt
@@ -3,7 +3,7 @@ package org.toml.lang.annotator
 import org.toml.lang.TomlTestCase
 
 class TomlAnnotatorTest : TomlTestCase() {
-    override fun getTestDataPath() = "testData/toml/annotator/"
+    override fun getTestDataPath() = "testData/org/toml/lang/annotator/testdata"
     private fun doTestInfo() = myFixture.testHighlighting(true, true, true, fileName)
 
     fun testInlineTables() {
