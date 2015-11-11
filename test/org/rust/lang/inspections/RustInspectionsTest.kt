ede4ec4707da197e2f009166525c40589467ac9d
Tobias Bieniek <tobias.bieniek@gmx.de>
2015-11-11 15:45:27 +0100

    Inspections: Add "Approximate Constants" inspection
    inspired by https://github.com/Manishearth/rust-clippy/wiki#approx_constant

---
+++ test/org/rust/lang/inspections/RustInspectionsTest.kt
@@ -0,0 +1,15 @@
+package org.rust.lang.inspections
+
+import com.intellij.codeInspection.LocalInspectionTool
+import org.rust.lang.RustTestCase
+
+class RustInspectionsTest : RustTestCase() {
+    override fun getTestDataPath() = "testData/org/rust/lang/inspections/fixtures"
+
+    private inline  fun <reified T: LocalInspectionTool>doTest() {
+        myFixture.enableInspections(T::class.java)
+        myFixture.testHighlighting(true, false, true, fileName)
+    }
+
+    fun testApproxConstant() = doTest<ApproxConstantInspection>()
+}
