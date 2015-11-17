5fda040a2ddf04a526e40902dc4418c46727c88c
Pavel Chernykh <pavel.v.chernykh@gmail.com>
2015-11-17 22:51:17 +0200

    Implement nonexistent module declaration inspection.
    Add inspection and appropriate test.
    Register inspection as local inspection with error highlighting level.
    Add extended inspection description.
    Add some words about inspection under Features section in Usage.md.
    
    Resolves #142

--- test/org/rust/lang/inspections/RustInspectionsTest.kt
+++ test/org/rust/lang/inspections/RustInspectionsTest.kt
@@ -4,13 +4,23 @@ import com.intellij.codeInspection.LocalInspectionTool
 import org.rust.lang.RustTestCase
 
 class RustInspectionsTest : RustTestCase() {
+    val NONEXISTENT_MODULE_DECLARATION_HINT = "Create module file"
+
     override fun getTestDataPath() = "testData/org/rust/lang/inspections/fixtures"
 
     private inline  fun <reified T: LocalInspectionTool>doTest() {
         myFixture.enableInspections(T::class.java)
         myFixture.testHighlighting(true, false, true, fileName)
+
+    }
+
+    private inline  fun <reified T: LocalInspectionTool>doTestWithIntention(hint: String) {
+        doTest<T>()
+        myFixture.findSingleIntention(hint)
     }
 
     fun testApproxConstant() = doTest<ApproxConstantInspection>()
     fun testSelfConvention() = doTest<SelfConventionInspection>()
+    fun testNonexistentModuleDeclaration() =
+        doTestWithIntention<NonexistentModuleDeclarationInspection>(NONEXISTENT_MODULE_DECLARATION_HINT)
 }
---
+++ testData/org/rust/lang/inspections/fixtures/nonexistent_module_declaration.rs
@@ -0,0 +1,5 @@
+mod <error>nonex<caret>istent</error>;
+
+fn main() {
+    println!("Hello, nonexistent module declaration inspection");
+}
