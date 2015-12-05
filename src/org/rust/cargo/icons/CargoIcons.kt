237065a8b811ee75088e2f399cfdf67a0d9b13cf
Tobias Bieniek <tobias.bieniek@gmx.de>
2015-12-05 01:04:57 +0100

    CargoIcons: Align values

--- src/org/rust/cargo/icons/CargoIcons.kt
+++ src/org/rust/cargo/icons/CargoIcons.kt
@@ -3,6 +3,6 @@ package org.rust.cargo.icons
 import com.intellij.openapi.util.IconLoader
 
 public object CargoIcons {
-    public val ICON = IconLoader.getIcon("/org/rust/lang/icons/cargo.png")
+    public val ICON      = IconLoader.getIcon("/org/rust/lang/icons/cargo.png")
     public val LOCK_ICON = IconLoader.getIcon("/org/rust/lang/icons/cargo-lock.png")
 }
