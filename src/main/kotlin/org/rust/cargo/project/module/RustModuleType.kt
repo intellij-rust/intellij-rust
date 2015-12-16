f008b5c2ca114e72e013d377835b7a97bacd26c3
Alexey Kudinkin <alexey.kudinkin@gmail.com>
2015-12-16 12:34:37 +0300

    (MISC): Tidying up

--- src/main/kotlin/org/rust/cargo/project/module/RustModuleType.kt
+++ src/main/kotlin/org/rust/cargo/project/module/RustModuleType.kt
@@ -2,8 +2,7 @@ package org.rust.cargo.project.module
 
 import com.intellij.openapi.module.ModuleType
 
-
 /**
  * Abstract Rust's module-type (a-la Crate)
  */
-abstract class RustModuleType(modTypeId: String) : ModuleType<RustModuleBuilder>(modTypeId)
+abstract class RustModuleType(typeId: String) : ModuleType<RustModuleBuilder>(typeId)
