cf2995d33933a903223a63289bb6886523d319a7
Aleksey Kladov <aleksey.kladov@gmail.com>
2016-02-11 04:02:28 +0300

    (T): add a fuzz test for StringLiteralLexer

--- src/test/kotlin/org/rust/lang/core/lexer/RustStringLiteralLexingTestCase.kt
+++ src/test/kotlin/org/rust/lang/core/lexer/RustStringLiteralLexingTestCase.kt
@@ -1,6 +1,7 @@
 package org.rust.lang.core.lexer
 
 import com.intellij.lexer.Lexer
+import java.util.*
 
 public class RustStringLiteralLexingTestCase : RustLexingTestCaseBase() {
     override fun getTestDataPath(): String = "org/rust/lang/core/lexer/fixtures/escapes"
@@ -16,4 +17,36 @@ public class RustStringLiteralLexingTestCase : RustLexingTestCaseBase() {
     fun testInvalidUnicodeEscapes() = doTest()
     fun testMixed() = doTest()
     fun testMixedQuoted() = doTest()
+
+    fun testFuzzy() {
+        val lexers = listOf(
+            RustStringLiteralLexer.forByteLiterals(),
+            RustStringLiteralLexer.forByteStringLiterals(),
+            RustStringLiteralLexer.forCharLiterals(),
+            RustStringLiteralLexer.forStringLiterals()
+        )
+
+        for (lexer in lexers) {
+            repeat(10000) {
+                execute(lexer, randomLiteral())
+            }
+        }
+    }
+
+    private fun execute(lexer: RustStringLiteralLexer, input: CharSequence)  {
+        lexer.start(input)
+        while (lexer.tokenType != null) {
+            lexer.advance()
+        }
+    }
+
+    private fun randomLiteral(): String {
+        val random = Random()
+        val length = random.nextInt(10)
+        val chars = """\xu"vnrt0'"""
+        val xs = CharArray(length, {
+            chars[random.nextInt(chars.length)]
+        })
+        return String(xs)
+    }
 }
