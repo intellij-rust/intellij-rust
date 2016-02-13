753d94dd1718388da4b1e19728c89eb2b3556153
Marek Kaput <marek.kaput@outlook.com>
2016-02-13 18:07:58 +0100

    (ANNOTATOR): implement some annotations with stub methods in RustLiteralImpl

--- src/main/kotlin/org/rust/lang/core/psi/RustLiteral.kt
+++ src/main/kotlin/org/rust/lang/core/psi/RustLiteral.kt
@@ -6,4 +6,9 @@ import com.intellij.psi.tree.IElementType
 
 interface RustLiteral : PsiElement, NavigationItem {
     val tokenType: IElementType
+    val value: Any?
+    val valueString: String
+    val suffix: String
+    val possibleSuffixes: Collection<String>
+    val hasPairedDelimiters: Boolean
 }
--- src/main/kotlin/org/rust/lang/core/psi/impl/RustLiteralImpl.kt
+++ src/main/kotlin/org/rust/lang/core/psi/impl/RustLiteralImpl.kt
@@ -8,5 +8,20 @@ class RustLiteralImpl(type: IElementType, text: CharSequence) : LeafPsiElement(t
     override val tokenType: IElementType
         get() = node.elementType
 
+    override val value: Any?
+        get() = throw UnsupportedOperationException("not implemented yet")
+
+    override val valueString: String
+        get() = throw UnsupportedOperationException("not implemented yet")
+
+    override val suffix: String
+        get() = throw UnsupportedOperationException("not implemented yet")
+
+    override val possibleSuffixes: Collection<String>
+        get() = throw UnsupportedOperationException("not implemented yet")
+
+    override val hasPairedDelimiters: Boolean
+        get() = throw UnsupportedOperationException("not implemented yet")
+
     override fun toString(): String = "RustLiteralImpl($tokenType)"
 }
