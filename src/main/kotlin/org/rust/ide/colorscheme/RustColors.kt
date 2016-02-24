846866f268c59efd9d0775a713736ef14802ef3a
Aleksey Kladov <aleksey.kladov@gmail.com>
2016-02-24 16:00:39 +0300

    (MISC): minor renaming

--- src/main/kotlin/org/rust/ide/colorscheme/RustColors.kt
+++ src/main/kotlin/org/rust/ide/colorscheme/RustColors.kt
@@ -1,41 +1,41 @@
 package org.rust.ide.colorscheme
 
-import com.intellij.openapi.editor.DefaultLanguageHighlighterColors as DLHC
+import com.intellij.openapi.editor.DefaultLanguageHighlighterColors as Default
 import com.intellij.openapi.editor.colors.TextAttributesKey.createTextAttributesKey as r
 
 object RustColors {
-    val IDENTIFIER      = r("org.rust.IDENTIFIER",      DLHC.IDENTIFIER)
+    val IDENTIFIER      = r("org.rust.IDENTIFIER",      Default.IDENTIFIER)
 
-    val LIFETIME        = r("org.rust.LIFETIME",        DLHC.IDENTIFIER)
+    val LIFETIME        = r("org.rust.LIFETIME",        Default.IDENTIFIER)
 
-    val CHAR            = r("org.rust.CHAR",            DLHC.STRING)
-    val STRING          = r("org.rust.STRING",          DLHC.STRING)
-    val NUMBER          = r("org.rust.NUMBER",          DLHC.NUMBER)
+    val CHAR            = r("org.rust.CHAR",            Default.STRING)
+    val STRING          = r("org.rust.STRING",          Default.STRING)
+    val NUMBER          = r("org.rust.NUMBER",          Default.NUMBER)
 
-    val KEYWORD         = r("org.rust.KEYWORD",         DLHC.KEYWORD)
+    val KEYWORD         = r("org.rust.KEYWORD",         Default.KEYWORD)
 
-    val BLOCK_COMMENT   = r("org.rust.BLOCK_COMMENT",   DLHC.BLOCK_COMMENT)
-    val EOL_COMMENT     = r("org.rust.EOL_COMMENT",     DLHC.LINE_COMMENT)
-    val DOC_COMMENT     = r("org.rust.DOC_COMMENT",     DLHC.DOC_COMMENT)
+    val BLOCK_COMMENT   = r("org.rust.BLOCK_COMMENT",   Default.BLOCK_COMMENT)
+    val EOL_COMMENT     = r("org.rust.EOL_COMMENT",     Default.LINE_COMMENT)
+    val DOC_COMMENT     = r("org.rust.DOC_COMMENT",     Default.DOC_COMMENT)
 
-    val PARENTHESIS     = r("org.rust.PARENTHESIS",     DLHC.PARENTHESES)
-    val BRACKETS        = r("org.rust.BRACKETS",        DLHC.BRACKETS)
-    val BRACES          = r("org.rust.BRACES",          DLHC.BRACES)
+    val PARENTHESIS     = r("org.rust.PARENTHESIS",     Default.PARENTHESES)
+    val BRACKETS        = r("org.rust.BRACKETS",        Default.BRACKETS)
+    val BRACES          = r("org.rust.BRACES",          Default.BRACES)
 
-    val OPERATORS       = r("org.rust.OPERATORS",       DLHC.OPERATION_SIGN)
+    val OPERATORS       = r("org.rust.OPERATORS",       Default.OPERATION_SIGN)
 
-    val SEMICOLON       = r("org.rust.SEMICOLON",       DLHC.SEMICOLON)
-    val DOT             = r("org.rust.DOT",             DLHC.DOT)
-    val COMMA           = r("org.rust.COMMA",           DLHC.COMMA)
+    val SEMICOLON       = r("org.rust.SEMICOLON",       Default.SEMICOLON)
+    val DOT             = r("org.rust.DOT",             Default.DOT)
+    val COMMA           = r("org.rust.COMMA",           Default.COMMA)
 
-    val ATTRIBUTE       = r("org.rust.ATTRIBUTE",       DLHC.METADATA)
+    val ATTRIBUTE       = r("org.rust.ATTRIBUTE",       Default.METADATA)
 
-    val MACRO           = r("org.rust.MACRO",           DLHC.IDENTIFIER)
+    val MACRO           = r("org.rust.MACRO",           Default.IDENTIFIER)
 
-    val TYPE_PARAMETER  = r("org.rust.TYPE_PARAMETER",  DLHC.IDENTIFIER)
+    val TYPE_PARAMETER  = r("org.rust.TYPE_PARAMETER",  Default.IDENTIFIER)
 
-    val MUT_BINDING     = r("org.rust.MUT_BINDING",     DLHC.IDENTIFIER)
+    val MUT_BINDING     = r("org.rust.MUT_BINDING",     Default.IDENTIFIER)
 
-    val VALID_STRING_ESCAPE   = r("org.rust.VALID_STRING_ESCAPE",   DLHC.VALID_STRING_ESCAPE)
-    val INVALID_STRING_ESCAPE = r("org.rust.INVALID_STRING_ESCAPE", DLHC.INVALID_STRING_ESCAPE)
+    val VALID_STRING_ESCAPE   = r("org.rust.VALID_STRING_ESCAPE",   Default.VALID_STRING_ESCAPE)
+    val INVALID_STRING_ESCAPE = r("org.rust.INVALID_STRING_ESCAPE", Default.INVALID_STRING_ESCAPE)
 }
