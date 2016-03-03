644c521e59d368e110c109067c5e7b33009baa29
Aleksey Kladov <aleksey.kladov@gmail.com>
2016-03-03 16:15:38 +0300

    (GRAM): parse tuple expressions faster

--- src/main/kotlin/org/rust/lang/core/parser/RustParserUtil.kt
+++ src/main/kotlin/org/rust/lang/core/parser/RustParserUtil.kt
@@ -7,6 +7,7 @@ import com.intellij.psi.TokenType
 import com.intellij.psi.tree.IElementType
 import org.rust.lang.core.psi.RustTokenElementTypes
 import org.rust.lang.core.lexer.containsEOL
+import org.rust.lang.core.psi.RustCompositeElementTypes
 import org.rust.lang.utils.Cookie
 import org.rust.lang.utils.using
 
@@ -58,6 +59,31 @@ object RustParserUtil : GeneratedParserUtilBase() {
 
         drop(m)
         return false;
+    }
+
+    // Parses either a paren_expr (92) or a tuple_expr (92, ) by postponing the decision of
+    // what exactly is parsed until `,` is (not) seen.
+    @JvmStatic fun tupleOrParenExpr(builder: PsiBuilder, level: Int,
+                                    anyExpr: Parser,
+                                    tupleExprEnd: Parser,
+                                    parenExprEnd: Parser): Boolean {
+        if (!recursion_guard_(builder, level, "tupleOrParenExpr")) return false
+        if (!nextTokenIsFast(builder, RustTokenElementTypes.LPAREN)) return false
+        val marker = enter_section_(builder)
+        var result = consumeTokenFast(builder, RustTokenElementTypes.LPAREN)
+        result = result && anyExpr.parse(builder, level + 1)
+
+        var hasComma = false
+        result = result && if (nextTokenIsFast(builder, RustTokenElementTypes.COMMA)) {
+            hasComma = true
+            tupleExprEnd.parse(builder, level + 1)
+        } else {
+            parenExprEnd.parse(builder, level + 1)
+        }
+        val elementType = if (hasComma) RustCompositeElementTypes.TUPLE_EXPR else RustCompositeElementTypes.PAREN_EXPR
+
+        exit_section_(builder, marker, elementType, result)
+        return result
     }
 
     @JvmStatic fun checkStructAllowed(b: PsiBuilder, level: Int) : Boolean = b.getStructAllowed()
