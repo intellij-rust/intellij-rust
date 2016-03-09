48ae750ea70756aa74ac1c9c01336cfd24563d67
zjhmale <zjhmale@gmail.com>
2016-03-09 08:57:17 +0800

    (INSPECTION) add quick fix

--- src/main/kotlin/org/rust/ide/inspections/UnnecessaryParenthesisInspection.kt
+++ src/main/kotlin/org/rust/ide/inspections/UnnecessaryParenthesisInspection.kt
@@ -1,9 +1,13 @@
 package org.rust.ide.inspections
 
+import com.intellij.codeInspection.LocalQuickFixBase
+import com.intellij.codeInspection.ProblemDescriptor
 import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElementVisitor
 import org.rust.lang.core.psi.RustIfExpr
 import org.rust.lang.core.psi.RustVisitor
+import org.rust.lang.core.psi.impl.RustParenExprImpl
 
 /**
  * Created by zjh on 16/3/8.
@@ -13,11 +17,19 @@ class UnnecessaryParenthesisInspection : RustLocalInspectionTool() {
 
     override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor =
         object : RustVisitor() {
-            override fun visitIfExpr(o: RustIfExpr) {
-                val exprText = o.expr?.text
+            override fun visitIfExpr(expr: RustIfExpr) {
+                val exprText = expr.expr?.text
                 if (exprText?.startsWith("(")!! && exprText?.endsWith(")")!!) {
-                    holder.registerProblem(o.expr!!, "Unnecessary Parenthesis")
+                    holder.registerProblem(expr.expr!!, "Unnecessary Parenthesis", RemoveParenthesis);
                 }
             }
         }
+
+    object RemoveParenthesis : LocalQuickFixBase("Remove parenthesis") {
+        override fun applyFix(project: Project, descriptor: ProblemDescriptor) {
+            val expr = descriptor.psiElement as RustParenExprImpl
+            expr.lparen.delete()
+            expr.rparen.delete()
+        }
+    }
 }
