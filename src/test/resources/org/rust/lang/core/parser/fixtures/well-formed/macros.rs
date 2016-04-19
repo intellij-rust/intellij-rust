d5e39f58346675700b5b6bcff7d6ee77367556be
Marek Kaput <marek.kaput@outlook.com>
2016-04-19 18:52:50 +0200

    (GRAM, PSI): support various parens for try! and format! macros  http://is.gd/84owrf

--- src/test/resources/org/rust/lang/core/parser/fixtures/well-formed/macros.rs
+++ src/test/resources/org/rust/lang/core/parser/fixtures/well-formed/macros.rs
@@ -17,7 +17,18 @@ fn foo() {
     let v1 = vec![1, 2, 3];
     let v2 = vec![1; 10];
     try!(bar());
-    format!("{argument}", argument = "test");   // => "test"
-    format_args!("{name} {}", 1, name = 2);        // => "2 1"
+    format!("{argument}", argument = "test");  // => "test"
+    format_args!("{name} {}", 1, name = 2);    // => "2 1"
     debug!("{a} {c} {b}", a="a", b='b', c=3);  // => "a 3 b"
+
+    try![bar()];
+    try! {
+        bar()
+    }
+
+    format!["hello {}", "world!"];
+    format! {
+        "x = {}, y = {y}",
+        10, y = 30
+    }
 }
