d45caefedf83bce14cf1c5a0065fdd87f3e383f4
Aleksey Kladov <aleksey.kladov@gmail.com>
2016-04-26 16:56:32 +0300

    (INSPECTIONS): improve unresolved mod inspection
    Don't show false positive warning if integration with cargo is not
    configured yet.

--- src/main/kotlin/org/rust/ide/inspections/UnresolvedModuleDeclarationInspection.kt
+++ src/main/kotlin/org/rust/ide/inspections/UnresolvedModuleDeclarationInspection.kt
@@ -6,12 +6,14 @@ import com.intellij.codeInspection.ProblemDescriptor
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElementVisitor
+import org.rust.cargo.util.cargoProject
 import org.rust.lang.core.psi.RustModDeclItem
 import org.rust.lang.core.psi.RustVisitor
 import org.rust.lang.core.psi.containingMod
 import org.rust.lang.core.psi.impl.mixin.explicitPath
 import org.rust.lang.core.psi.impl.mixin.getOrCreateModuleFile
 import org.rust.lang.core.psi.impl.mixin.isPathAttributeRequired
+import org.rust.lang.core.psi.util.module
 import org.rust.lang.core.psi.util.ownsDirectory
 
 class UnresolvedModuleDeclarationInspection : RustLocalInspectionTool() {
@@ -29,7 +31,12 @@ class UnresolvedModuleDeclarationInspection : RustLocalInspectionTool() {
 
                 val containingMod = modDecl.containingMod ?: return
                 if (!containingMod.ownsDirectory) {
-                    holder.registerProblem(modDecl, "Cannot declare a new module at this location")
+                    // We don't want to show the warning if there is no cargo project
+                    // associated with the current module. Without it we can't know for
+                    // sure that a mod is not a directory owner.
+                    if (modDecl.module?.cargoProject != null) {
+                        holder.registerProblem(modDecl, "Cannot declare a new module at this location")
+                    }
                     return
                 }
 
