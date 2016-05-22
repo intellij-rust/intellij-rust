f714f56dada7101973aec5b50bf4640d656625a2
Alexey Kudinkin <alexey.kudinkin@gmail.com>
2016-05-23 00:49:08 +0300

    (MISC): Made code compilable by Kotlin's `1.0.2` compiler;         Tidying up;

--- src/main/kotlin/org/rust/ide/utils/PlatformUtils.kt
+++ src/main/kotlin/org/rust/ide/utils/PlatformUtils.kt
@@ -5,7 +5,14 @@ import com.intellij.openapi.application.WriteAction
 import com.intellij.openapi.components.ServiceManager
 import com.intellij.openapi.project.Project
 
-inline fun <T> runWriteAction(runnable: () -> T): T {
+
+/**
+ * Wrapper util to run [runnable] under WL
+ *
+ * NOTA BENE:   Both of the following utils purposefully DE-INLINED to make
+ *              it compilable by the Kotlin 1.0.2 (due to internal compiler bug, see https://youtrack.jetbrains.com/issue/KT-12458)
+ */
+/* inline */ fun <T> runWriteAction(runnable: () -> T): T {
     val token = WriteAction.start()
     try {
         return runnable()
@@ -15,7 +22,10 @@ inline fun <T> runWriteAction(runnable: () -> T): T {
     }
 }
 
-inline fun <T> runReadAction(runnable: () -> T): T {
+/**
+ * Wrapper util to run [runnable] under RL
+ */
+/* inline */ fun <T> runReadAction(runnable: () -> T): T {
     val token = ReadAction.start()
     try {
         return runnable()
@@ -25,6 +35,12 @@ inline fun <T> runReadAction(runnable: () -> T): T {
     }
 }
 
+/**
+ * Util to extract application-level services
+ */
 inline fun <reified T : Any> service(): T = ServiceManager.getService(T::class.java)
 
+/**
+ * Util to extract project-level services
+ */
 inline fun <reified T : Any> Project.service(): T = ServiceManager.getService(this, T::class.java)
