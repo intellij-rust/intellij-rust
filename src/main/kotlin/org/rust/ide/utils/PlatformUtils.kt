8cf1c82b17527e2c9fbf998f0f378d0312f515cb
Alexey.Kudinkin <alexey.kudinkin@gmail.com>
2016-05-23 12:50:45 +0300

    (MISC): Workarounded KT-12458

--- src/main/kotlin/org/rust/ide/utils/PlatformUtils.kt
+++ src/main/kotlin/org/rust/ide/utils/PlatformUtils.kt
@@ -8,11 +8,8 @@ import com.intellij.openapi.project.Project
 
 /**
  * Wrapper util to run [runnable] under WL
- *
- * NOTA BENE:   Both of the following utils purposefully DE-INLINED to make
- *              it compilable by the Kotlin 1.0.2 (due to internal compiler bug, see https://youtrack.jetbrains.com/issue/KT-12458)
  */
-/* inline */ fun <T> runWriteAction(runnable: () -> T): T {
+inline fun <T> runWriteAction(runnable: () -> T): T {
     val token = WriteAction.start()
     try {
         return runnable()
@@ -25,7 +22,7 @@ import com.intellij.openapi.project.Project
 /**
  * Wrapper util to run [runnable] under RL
  */
-/* inline */ fun <T> runReadAction(runnable: () -> T): T {
+inline fun <T> runReadAction(runnable: () -> T): T {
     val token = ReadAction.start()
     try {
         return runnable()
