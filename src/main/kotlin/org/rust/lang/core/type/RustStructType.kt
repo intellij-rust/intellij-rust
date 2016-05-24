266ea4253d2fc9c083b93f6e7ba003165bb4a0c6
Aleksey Kladov <aleksey.kladov@gmail.com>
2016-05-24 17:39:12 +0300

    (TYPE): simplify

--- src/main/kotlin/org/rust/lang/core/type/RustStructType.kt
+++ src/main/kotlin/org/rust/lang/core/type/RustStructType.kt
@@ -5,28 +5,14 @@ import org.rust.lang.core.psi.RustMod
 import org.rust.lang.core.psi.RustStructItem
 import org.rust.lang.core.psi.util.parentOfType
 
-class RustStructType(val struct: RustStructItem) : RustResolvedTypeBase() {
-    override val inheritedImplsInner: Collection<RustImplItem> get() {
+data class RustStructType(val struct: RustStructItem) : RustResolvedType {
+    override val inheritedImpls: Collection<RustImplItem> by lazy {
         val potentialImpls = struct.parentOfType<RustMod>()?.items.orEmpty().filterIsInstance<RustImplItem>()
-        return potentialImpls.filter { impl ->
+        potentialImpls.filter { impl ->
             impl.traitRef == null && (impl.type?.resolvedType == this)
         }
     }
 
     override fun toString(): String = "<struct type $struct>"
 
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other?.javaClass != javaClass) return false
-
-        other as RustStructType
-
-        if (struct != other.struct) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return struct.hashCode()
-    }
 }
--- src/main/kotlin/org/rust/lang/core/type/RustUnknownType.kt
+++ src/main/kotlin/org/rust/lang/core/type/RustUnknownType.kt
@@ -2,7 +2,7 @@ package org.rust.lang.core.type
 
 import org.rust.lang.core.psi.RustImplItem
 
-object RustUnknownType : RustResolvedTypeBase() {
-    override val inheritedImplsInner: Collection<RustImplItem> = emptyList()
+object RustUnknownType : RustResolvedType {
+    override val inheritedImpls: Collection<RustImplItem> = emptyList()
     override fun toString(): String = "<unknown type>"
 }
