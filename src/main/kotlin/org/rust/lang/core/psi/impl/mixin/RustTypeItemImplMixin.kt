d3360e68eb79d5c339631b83ed10f074dfa08261
Nikita Ofitserov <himikof@gmail.com>
2016-05-28 17:53:51 +0300

    (RES): resolve type parameters in type items

--- src/main/kotlin/org/rust/lang/core/psi/impl/mixin/RustTypeItemImplMixin.kt
+++ src/main/kotlin/org/rust/lang/core/psi/impl/mixin/RustTypeItemImplMixin.kt
@@ -3,6 +3,7 @@ package org.rust.lang.core.psi.impl.mixin
 import com.intellij.lang.ASTNode
 import com.intellij.psi.stubs.IStubElementType
 import org.rust.ide.icons.RustIcons
+import org.rust.lang.core.psi.RustDeclaringElement
 import org.rust.lang.core.psi.RustTypeItem
 import org.rust.lang.core.psi.iconWithVisibility
 import org.rust.lang.core.psi.impl.RustItemImpl
@@ -14,6 +15,8 @@ abstract class RustTypeItemImplMixin : RustItemImpl, RustTypeItem {
     constructor(node: ASTNode) : super(node)
 
     constructor(stub: RustItemStub, nodeType: IStubElementType<*, *>) : super(stub, nodeType)
+
+    override val declarations: Collection<RustDeclaringElement> get() = genericParams?.typeParamList.orEmpty()
 
     override fun getIcon(flags: Int): Icon? {
         return iconWithVisibility(flags, RustIcons.TYPE)
