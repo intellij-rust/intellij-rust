0b39a5c58aee110bc509e6418a979e9b307a4092
Ronald Kinard <furyhunter600@gmail.com>
2016-05-31 19:24:25 -0500

    Cleanup folding.

--- src/main/kotlin/org/rust/ide/folding/RustFoldingBuilder.kt
+++ src/main/kotlin/org/rust/ide/folding/RustFoldingBuilder.kt
@@ -6,71 +6,69 @@ import com.intellij.lang.folding.FoldingDescriptor
 import com.intellij.openapi.editor.Document
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
-import com.intellij.psi.util.PsiTreeUtil
 import org.rust.lang.core.psi.*
 import org.rust.lang.core.psi.impl.RustFile
+import org.rust.lang.core.psi.util.descendentsOfType
 import java.util.*
 
 class RustFoldingBuilder() : FoldingBuilderEx() {
-    override fun getPlaceholderText(node: ASTNode): String? {
-        return "{...}"
-    }
+    override fun getPlaceholderText(node: ASTNode): String = "{...}"
 
     override fun buildFoldRegions(root: PsiElement, document: Document, quick: Boolean): Array<out FoldingDescriptor> {
         if (root !is RustFile) return emptyArray()
 
         val descriptors: MutableList<FoldingDescriptor> = ArrayList()
 
-        PsiTreeUtil.findChildrenOfType(root, RustBlockExpr::class.java).forEach {
+        root.descendentsOfType<RustBlockExpr>().forEach {
             val block = it.block
             if (block != null) {
                 descriptors += FoldingDescriptor(it.node, block.textRange)
             }
         }
-        PsiTreeUtil.findChildrenOfType(root, RustImplItem::class.java).forEach {
+        root.descendentsOfType<RustImplItem>().forEach {
             val implBody = it.implBody
             if (implBody != null) {
                 descriptors += FoldingDescriptor(it.node, implBody.textRange)
             }
         }
-        PsiTreeUtil.findChildrenOfType(root, RustStructItem::class.java).forEach {
+        root.descendentsOfType<RustStructItem>().forEach {
             val structDeclArgs = it.structDeclArgs
             if (structDeclArgs != null) {
                 descriptors += FoldingDescriptor(it.node, structDeclArgs.textRange)
             }
         }
-        PsiTreeUtil.findChildrenOfType(root, RustStructExpr::class.java).forEach {
+        root.descendentsOfType<RustStructExpr>().forEach {
             descriptors += FoldingDescriptor(it.node, it.structExprBody.textRange)
         }
-        PsiTreeUtil.findChildrenOfType(root, RustEnumItem::class.java).forEach {
+        root.descendentsOfType<RustEnumItem>().forEach {
             val enumBody = it.enumBody
             descriptors += FoldingDescriptor(it.node, enumBody.textRange)
         }
-        PsiTreeUtil.findChildrenOfType(root, RustEnumVariant::class.java).forEach {
+        root.descendentsOfType<RustEnumVariant>().forEach {
             val structDeclArgs = it.enumStructArgs
             if (structDeclArgs != null) {
                 descriptors += FoldingDescriptor(it.node, structDeclArgs.textRange)
             }
         }
-        PsiTreeUtil.findChildrenOfType(root, RustFnItem::class.java).forEach {
+        root.descendentsOfType<RustFnItem>().forEach {
             val fnBody = it.block
             if (fnBody != null) {
                 descriptors += FoldingDescriptor(it.node, fnBody.textRange)
             }
         }
-        PsiTreeUtil.findChildrenOfType(root, RustImplMethodMember::class.java).forEach {
+        root.descendentsOfType<RustImplMethodMember>().forEach {
             val methodBody = it.block
             if (methodBody != null) {
                 descriptors += FoldingDescriptor(it.node, methodBody.textRange)
             }
         }
-        PsiTreeUtil.findChildrenOfType(root, RustTraitMethodMember::class.java).forEach {
+        root.descendentsOfType<RustTraitMethodMember>().forEach {
             val methodBody = it.block
             if (methodBody != null) {
                 descriptors += FoldingDescriptor(it.node, methodBody.textRange)
             }
         }
-        PsiTreeUtil.findChildrenOfType(root, RustModItem::class.java).forEach {
+        root.descendentsOfType<RustModItem>().forEach {
             val rbrace = it.rbrace;
             if (rbrace != null) {
                 descriptors += FoldingDescriptor(it.node, TextRange(it.lbrace.textOffset, rbrace.textOffset))
@@ -79,7 +77,5 @@ class RustFoldingBuilder() : FoldingBuilderEx() {
         return descriptors.toTypedArray()
     }
 
-    override fun isCollapsedByDefault(node: ASTNode): Boolean {
-        return false
-    }
+    override fun isCollapsedByDefault(node: ASTNode): Boolean = false
 }
