61aa85681d03de0a996f7612c8418e21662ad6f4
Alexey Kudinkin <alexey.kudinkin@gmail.com>
2016-06-05 21:52:05 +0300

    (REF): Re-purposed `RustFieldExprReference` to refer to `fieldId` instead        of the whole expr

--- src/main/kotlin/org/rust/lang/core/resolve/ref/RustFieldExprReferenceImpl.kt
+++ src/main/kotlin/org/rust/lang/core/resolve/ref/RustFieldExprReferenceImpl.kt
@@ -1,17 +1,15 @@
 package org.rust.lang.core.resolve.ref
 
 import org.rust.lang.core.psi.RustFieldExprElement
-import org.rust.lang.core.psi.RustFieldIdElement
-import org.rust.lang.core.psi.util.parentOfType
 import org.rust.lang.core.resolve.RustResolveEngine
 
 class RustFieldExprReferenceImpl(
     fieldExpr: RustFieldExprElement
-) : RustReferenceBase<RustFieldIdElement>(fieldExpr.fieldId!!, fieldExpr.fieldId!!)
+) : RustReferenceBase<RustFieldExprElement>(fieldExpr, fieldExpr.fieldId!!)
   , RustReference {
 
     override fun getVariants(): Array<out Any> = emptyArray()
 
     override fun resolveVerbose(): RustResolveEngine.ResolveResult =
-        RustResolveEngine.resolveFieldExpr(element.parentOfType<RustFieldExprElement>()!!)
+        RustResolveEngine.resolveFieldExpr(element)
 }
