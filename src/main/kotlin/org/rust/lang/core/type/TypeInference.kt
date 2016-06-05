510514075196db2b2fa2fde50a9c346c3ddcf6ab
Alexey Kudinkin <alexey.kudinkin@gmail.com>
2016-06-05 20:07:12 +0300

    (MISC): Dismantled `psiCached` hook

--- src/main/kotlin/org/rust/lang/core/type/TypeInference.kt
+++ src/main/kotlin/org/rust/lang/core/type/TypeInference.kt
@@ -1,47 +1,55 @@
 package org.rust.lang.core.type
 
 import com.intellij.psi.util.CachedValueProvider
+import com.intellij.psi.util.CachedValuesManager
 import com.intellij.psi.util.PsiModificationTracker
 import org.rust.lang.core.psi.RustExprElement
 import org.rust.lang.core.psi.RustTypeElement
 import org.rust.lang.core.psi.visitors.RustTypificationVisitor
 import org.rust.lang.core.type.unresolved.RustUnresolvedType
 import org.rust.lang.core.type.visitors.RustTypeResolvingVisitor
-import org.rust.lang.utils.psiCached
 
-val RustExprElement.type: RustUnresolvedType by psiCached {
-    CachedValueProvider {
-        CachedValueProvider.Result.create(
-            RustTypificationVisitor().let {
-                accept(it)
-                it.inferred
-            },
-            PsiModificationTracker.MODIFICATION_COUNT
+val RustExprElement.type: RustUnresolvedType
+    get() =
+        CachedValuesManager.getCachedValue(this,
+            CachedValueProvider {
+                CachedValueProvider.Result.create(
+                    RustTypificationVisitor().let {
+                        accept(it)
+                        it.inferred
+                    },
+                    PsiModificationTracker.MODIFICATION_COUNT
+                )
+            }
         )
-    }
-}
 
-val RustExprElement.resolvedType: RustType by psiCached {
-    CachedValueProvider {
-        CachedValueProvider.Result.create(type.accept(RustTypeResolvingVisitor()), PsiModificationTracker.MODIFICATION_COUNT)
-    }
-}
-
-val RustTypeElement.type: RustUnresolvedType by psiCached {
-    CachedValueProvider {
-        CachedValueProvider.Result.create(
-            RustTypificationVisitor().let {
-                accept(it)
-                it.inferred
-            },
-            PsiModificationTracker.MODIFICATION_COUNT
+val RustExprElement.resolvedType: RustType
+    get() =
+        CachedValuesManager.getCachedValue(this,
+            CachedValueProvider {
+                CachedValueProvider.Result.create(type.accept(RustTypeResolvingVisitor()), PsiModificationTracker.MODIFICATION_COUNT)
+            }
         )
-    }
-}
 
-val RustTypeElement.resolvedType: RustType by psiCached {
-    CachedValueProvider {
-        CachedValueProvider.Result.create(type.accept(RustTypeResolvingVisitor()), PsiModificationTracker.MODIFICATION_COUNT)
-    }
-}
+val RustTypeElement.type: RustUnresolvedType
+    get() =
+        CachedValuesManager.getCachedValue(this,
+            CachedValueProvider {
+                CachedValueProvider.Result.create(
+                    RustTypificationVisitor().let {
+                        accept(it)
+                        it.inferred
+                    },
+                    PsiModificationTracker.MODIFICATION_COUNT
+                )
+            }
+        )
+
+val RustTypeElement.resolvedType: RustType
+    get() =
+        CachedValuesManager.getCachedValue(this,
+            CachedValueProvider {
+                CachedValueProvider.Result.create(type.accept(RustTypeResolvingVisitor()), PsiModificationTracker.MODIFICATION_COUNT)
+            }
+        )
 
