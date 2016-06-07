e76bb0aa87ab434db7bc3369ffcd5b573e8e2833
Aleksey Kladov <aleksey.kladov@gmail.com>
2016-06-07 17:11:01 +0300

    (GRAM): kill trivial mixins

--- src/main/kotlin/org/rust/lang/core/psi/impl/mixin/RustTraitConstMemberImplMixin.kt
+++
@@ -1,9 +0,0 @@
-package org.rust.lang.core.psi.impl.mixin
-
-import com.intellij.lang.ASTNode
-import org.rust.lang.core.psi.RustTraitConstMemberElement
-import org.rust.lang.core.psi.impl.RustNamedElementImpl
-
-abstract class RustTraitConstMemberImplMixin(node: ASTNode) : RustNamedElementImpl(node)
-                                                            , RustTraitConstMemberElement {
-}
--- src/main/kotlin/org/rust/lang/core/psi/impl/mixin/RustTraitTypeMemberImplMixin.kt
+++
@@ -1,9 +0,0 @@
-package org.rust.lang.core.psi.impl.mixin
-
-import com.intellij.lang.ASTNode
-import org.rust.lang.core.psi.RustTraitTypeMemberElement
-import org.rust.lang.core.psi.impl.RustNamedElementImpl
-
-abstract class RustTraitTypeMemberImplMixin(node: ASTNode) : RustNamedElementImpl(node)
-                                                           , RustTraitTypeMemberElement {
-}
