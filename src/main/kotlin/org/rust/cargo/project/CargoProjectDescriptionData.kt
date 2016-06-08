0f964393f3bb9afaf6b3e9c1c023a3b482c1fe8f
Aleksey Kladov <aleksey.kladov@gmail.com>
2016-06-08 17:46:05 +0300

    (MISC): var -> val

--- src/main/kotlin/org/rust/cargo/project/CargoProjectDescriptionData.kt
+++ src/main/kotlin/org/rust/cargo/project/CargoProjectDescriptionData.kt
@@ -4,30 +4,30 @@ package org.rust.cargo.project
  * A POD-style representation of [CargoProjectDescription] used as intermediate representation
  * between `cargo metadata` JSON and [CargoProjectDescription] object graph.
  *
- * Dependency graph is via adjacency list, where `Index` is the order of a particular
+ * Dependency graph is represented via adjacency list, where `Index` is the order of a particular
  * package in `packages` list.
  */
 data class CargoProjectDescriptionData(
-    var rootPackageIndex: Int,
+    val rootPackageIndex: Int,
     val packages: List<Package>,
     val dependencies: Collection<DependencyNode>
 ) {
     data class DependencyNode(
-        var packageIndex: Int,
-        var dependenciesIndexes: Collection<Int>
+        val packageIndex: Int,
+        val dependenciesIndexes: Collection<Int>
     )
 
     data class Package(
-        var contentRootUrl: String,
-        var name: String,
-        var version: String,
-        var targets: Collection<Target>,
-        var source: String?
+        val contentRootUrl: String,
+        val name: String,
+        val version: String,
+        val targets: Collection<Target>,
+        val source: String?
     )
 
     data class Target(
-        var url: String,
-        var kind: CargoProjectDescription.TargetKind
+        val url: String,
+        val kind: CargoProjectDescription.TargetKind
     )
 }
 
