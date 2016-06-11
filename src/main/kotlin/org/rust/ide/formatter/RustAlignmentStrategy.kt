458f49f1db2bc529dca49594bae09bc29de52436
Marek Kaput <marek.kaput@outlook.com>
2016-06-11 17:04:45 +0200

    (FMT): refactor RustAlignmentStrategy

--- src/main/kotlin/org/rust/ide/formatter/RustAlignmentStrategy.kt
+++ src/main/kotlin/org/rust/ide/formatter/RustAlignmentStrategy.kt
@@ -2,57 +2,79 @@ package org.rust.ide.formatter
 
 import com.intellij.formatting.Alignment
 import com.intellij.lang.ASTNode
+import com.intellij.psi.tree.IElementType
 import com.intellij.psi.tree.TokenSet
 
-/**
- * Mimics [com.intellij.formatting.alignment.AlignmentStrategy], but offers more flexible API
- */
 interface RustAlignmentStrategy {
     /**
      * Requests current strategy for alignment to use for given child.
      */
     fun getAlignment(child: ASTNode, parent: ASTNode?, childCtx: RustFmtContext): Alignment?
 
-    companion object {
-        /**
-         * Always returns `null`.
-         */
-        val NULL_STRATEGY = object : RustAlignmentStrategy {
-            override fun getAlignment(child: ASTNode, parent: ASTNode?, childCtx: RustFmtContext): Alignment? = null
+    /**
+     * Always returns `null`.
+     */
+    object NullStrategy : RustAlignmentStrategy {
+        override fun getAlignment(child: ASTNode, parent: ASTNode?, childCtx: RustFmtContext): Alignment? = null
+    }
+
+    /**
+     * Apply this strategy only when child element is in [tt].
+     *
+     * @param isBlackList   denotes how filter set is interpreted. If set to `false`, the filter is a white list:
+     *                      only children which element types are in filter set will get wrapped alignment;
+     *                      otherwise, the filter works as a black list: these children will be ignored.
+     */
+    fun filter(vararg tt: IElementType, isBlackList: Boolean = false): RustAlignmentStrategy =
+        filter(TokenSet.create(*tt), isBlackList)
+
+    /**
+     * Apply this strategy only when child element type matches [filterSet].
+     *
+     * @param isBlackList   denotes how [filterSet] is interpreted. If set to `false`, the filter is a white list:
+     *                      only children which element types are in [filterSet] will get wrapped alignment;
+     *                      otherwise, the filter works as a black list: these children will be ignored.
+     */
+    fun filter(filterSet: TokenSet, isBlackList: Boolean = false): RustAlignmentStrategy =
+        object : RustAlignmentStrategy {
+            override fun getAlignment(child: ASTNode, parent: ASTNode?, childCtx: RustFmtContext): Alignment? =
+                if ((child.elementType in filterSet) xor isBlackList) {
+                    this@RustAlignmentStrategy.getAlignment(child, parent, childCtx)
+                } else {
+                    null
+                }
         }
 
+    /**
+     * Apply this strategy only when [predicate] passes.
+     */
+    fun cond(predicate: (child: ASTNode, parent: ASTNode?, ctx: RustFmtContext) -> Boolean): RustAlignmentStrategy =
+        object : RustAlignmentStrategy {
+            override fun getAlignment(child: ASTNode, parent: ASTNode?, childCtx: RustFmtContext): Alignment? =
+                if (predicate(child, parent, childCtx)) {
+                    this@RustAlignmentStrategy.getAlignment(child, parent, childCtx)
+                } else {
+                    null
+                }
+        }
+
+    companion object {
         /**
-         * Returns [alignment] when [predicate] passes; otherwise, `null`.
+         * Always returns [alignment].
          */
-        fun wrapCond(alignment: Alignment = Alignment.createAlignment(),
-                     predicate: (child: ASTNode, parent: ASTNode?, ctx: RustFmtContext) -> Boolean): RustAlignmentStrategy =
+        fun wrap(alignment: Alignment = Alignment.createAlignment()): RustAlignmentStrategy =
             object : RustAlignmentStrategy {
                 override fun getAlignment(child: ASTNode, parent: ASTNode?, childCtx: RustFmtContext): Alignment? =
-                    if (predicate(child, parent, childCtx)) alignment else null
+                    alignment
             }
 
         /**
-         * Returns alignment returned by [alignmentGetter] when [predicate] passes; otherwise, `null`.
+         * Always returns [RustFmtContext.sharedAlignment]
          */
-        fun lazyWrapCond(alignmentGetter: (ctx: RustFmtContext) -> Alignment?,
-                         predicate: (child: ASTNode, parent: ASTNode?, ctx: RustFmtContext) -> Boolean): RustAlignmentStrategy =
+        fun shared(): RustAlignmentStrategy =
             object : RustAlignmentStrategy {
                 override fun getAlignment(child: ASTNode, parent: ASTNode?, childCtx: RustFmtContext): Alignment? =
-                    if (predicate(child, parent, childCtx)) alignmentGetter(childCtx) else null
-            }
-
-        /**
-         * Returns alignment returned by [alignmentGetter] when child element type matches [filterSet].
-         *
-         * @param isBlackList   denotes how [filterSet] is interpreted. If set to `false`, the filter is a white list:
-         *                      only children which element types are in [filterSet] will get wrapped alignment;
-         *                      otherwise, the filter works as a black list: these children will be ignored.
-         */
-        fun lazyWrapFiltered(alignmentGetter: (ctx: RustFmtContext) -> Alignment?,
-                             filterSet: TokenSet, isBlackList: Boolean = false): RustAlignmentStrategy =
-            object : RustAlignmentStrategy {
-                override fun getAlignment(child: ASTNode, parent: ASTNode?, childCtx: RustFmtContext): Alignment? =
-                    if ((child.elementType in filterSet) xor isBlackList) alignmentGetter(childCtx) else null
+                    childCtx.sharedAlignment
             }
     }
 }
--- src/main/kotlin/org/rust/ide/formatter/impl/alignment.kt
+++ src/main/kotlin/org/rust/ide/formatter/impl/alignment.kt
@@ -1,26 +1,25 @@
 package org.rust.ide.formatter.impl
 
-import com.intellij.psi.tree.TokenSet
 import org.rust.ide.formatter.RustAlignmentStrategy
 import org.rust.ide.formatter.blocks.RustFmtBlock
 import org.rust.lang.core.psi.RustCompositeElementTypes.*
 
 fun RustFmtBlock.getAlignmentStrategy(): RustAlignmentStrategy = when (node.elementType) {
     TUPLE_EXPR, TUPLE_TYPE, ARG_LIST, ENUM_TUPLE_ARGS ->
-        RustAlignmentStrategy.wrapCond { c, p, x -> !c.isBlockDelim(p) }
+        RustAlignmentStrategy.wrap()
+            .cond { c, p, x -> !c.isBlockDelim(p) }
 
     in PARAMS_LIKE ->
-        RustAlignmentStrategy.lazyWrapCond(
-            { it.sharedAlignment },
-            { c, p, x -> !c.isBlockDelim(p) })
+        RustAlignmentStrategy.shared()
+            .cond { c, p, x -> !c.isBlockDelim(p) }
 
     in FN_DECLS ->
-        RustAlignmentStrategy.lazyWrapFiltered(
-            { it.sharedAlignment },
-            TokenSet.create(RET_TYPE, WHERE_CLAUSE))
+        RustAlignmentStrategy.shared()
+            .filter(RET_TYPE, WHERE_CLAUSE)
 
     PAT_ENUM ->
-        RustAlignmentStrategy.wrapCond { c, p, x -> x.metLBrace && !c.isBlockDelim(p) }
+        RustAlignmentStrategy.wrap()
+            .cond { c, p, x -> x.metLBrace && !c.isBlockDelim(p) }
 
-    else -> RustAlignmentStrategy.NULL_STRATEGY
+    else -> RustAlignmentStrategy.NullStrategy
 }
