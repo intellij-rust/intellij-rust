bbc430cb4a319f307bbbbcd15c0ea1111c0c4ca7
Aleksey Kladov <aleksey.kladov@gmail.com>
2016-06-13 14:05:37 +0300

    (T): add test for prelude completion

--- src/test/kotlin/org/rust/lang/core/completion/RustCompletionTest.kt
+++ src/test/kotlin/org/rust/lang/core/completion/RustCompletionTest.kt
@@ -1,9 +1,8 @@
 package org.rust.lang.core.completion
 
-import org.assertj.core.api.Assertions.assertThat
 import org.rust.lang.RustTestCaseBase
 
-class RustCompletionTest : RustTestCaseBase() {
+class RustCompletionTest : RustCompletionTestBase() {
 
     override val dataPath = "org/rust/lang/core/completion/fixtures"
 
@@ -33,21 +32,4 @@ class RustCompletionTest : RustTestCaseBase() {
         executeSoloCompletion()
     }
 
-    private fun checkSoleCompletion() = checkByFile {
-        executeSoloCompletion()
-    }
-
-    private fun checkNoCompletion() {
-        myFixture.configureByFile(fileName)
-        val variants = myFixture.completeBasic()
-        assertThat(variants).isNotNull()
-        assertThat(variants.size).isZero()
-    }
-
-    private fun executeSoloCompletion() {
-        val variants = myFixture.completeBasic()
-        assertThat(variants)
-            .withFailMessage("Expected a single completion, but got ${variants?.size}")
-            .isNull()
-    }
 }
