9febfacaed16273365b4474b780f0d658cdbb8fa
Alexey.Kudinkin <alexey.kudinkin@gmail.com>
2016-06-28 19:46:36 +0300

    (TYP): Replaced all type methods to return `Sequence`s instead of `Collection`s

--- src/main/kotlin/org/rust/lang/core/resolve/RustResolveEngine.kt
+++ src/main/kotlin/org/rust/lang/core/resolve/RustResolveEngine.kt
@@ -129,7 +129,7 @@ object RustResolveEngine {
         val receiverType = call.expr.resolvedType
         val name = call.identifier.text
         val matching = receiverType.nonStaticMethods.filter { it.name == name }
-        return ResolveResult.buildFrom(matching)
+        return ResolveResult.buildFrom(matching.asIterable())
     }
 
     //
@@ -496,7 +496,10 @@ private fun RustUseItemElement.nonWildcardEntries(): Sequence<ScopeEntry> {
 
 
 private val Collection<RustNamedElement>.scopeEntries: Sequence<ScopeEntry>
-    get() = asSequence().mapNotNull { ScopeEntry.of(it) }
+    get() = asSequence().scopeEntries
+
+private val Sequence<RustNamedElement>.scopeEntries: Sequence<ScopeEntry>
+    get() = mapNotNull { ScopeEntry.of(it) }
 
 
 private fun RustNamedElement?.asResolveResult(): RustResolveEngine.ResolveResult =
