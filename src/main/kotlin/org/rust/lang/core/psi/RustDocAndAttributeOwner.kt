7620e3aaad604c9f5c4d1d4b6f6fe1d168d5e092
Marek Kaput <marek.kaput@outlook.com>
2016-06-30 13:31:24 +0200

    (PSI): cleanup

--- src/main/kotlin/org/rust/lang/core/psi/RustDocAndAttributeOwner.kt
+++ src/main/kotlin/org/rust/lang/core/psi/RustDocAndAttributeOwner.kt
@@ -10,19 +10,24 @@ import org.rust.lang.core.psi.RustTokenElementTypes.OUTER_EOL_DOC_COMMENT
 
 interface RustDocAndAttributeOwner : RustCompositeElement, NavigatablePsiElement
 
-val RustDocAndAttributeOwner.documentation: String? get() {
-    val lines = mutableListOf<String>()
-    if (this is RustOuterAttributeOwner) {
-        lines += outerDocs
-    }
-    if (this is RustInnerAttributeOwner) {
-        lines += innerDocs
-    }
-    return lines.joinToString("\n")
-}
+/**
+ * Get list of all item's inner and outer attributes.
+ */
+val RustDocAndAttributeOwner.allAttributes: List<RustAttrElement>
+    get() = (this as? RustOuterAttributeOwner)?.outerAttrList.orEmpty() +
+        (this as? RustInnerAttributeOwner)?.innerAttrList.orEmpty()
 
-val RustDocAndAttributeOwner.queryAttributes: QueryAttributes get() = QueryAttributes(allAttributes)
+/**
+ * Returns [QueryAttributes] for given PSI element.
+ */
+val RustDocAndAttributeOwner.queryAttributes: QueryAttributes
+    get() = QueryAttributes(allAttributes)
 
+/**
+ * Allows for easy querying [RustDocAndAttributeOwner] for specific attributes.
+ *
+ * **Do not instantiate directly**, use [RustDocAndAttributeOwner.queryAttributes] instead.
+ */
 class QueryAttributes(private val attributes: List<RustAttrElement>) {
     fun hasAtomAttribute(name: String): Boolean =
         metaItems
@@ -35,13 +40,22 @@ class QueryAttributes(private val attributes: List<RustAttrElement>) {
             .mapNotNull { it.litExpr?.stringLiteralValue }
             .singleOrNull()
 
-
-    private val metaItems: List<RustMetaItemElement> get() = attributes.mapNotNull { it.metaItem }
+    private val metaItems: List<RustMetaItemElement>
+        get() = attributes.mapNotNull { it.metaItem }
 }
 
-private val RustDocAndAttributeOwner.allAttributes: List<RustAttrElement> get() {
-    return (this as? RustOuterAttributeOwner)?.outerAttrList.orEmpty() +
-        (this as? RustInnerAttributeOwner)?.innerAttrList.orEmpty()
+/**
+ * Get documentation text of given [RustDocAndAttributeOwner].
+ */
+val RustDocAndAttributeOwner.documentation: String? get() {
+    val lines = mutableListOf<String>()
+    if (this is RustOuterAttributeOwner) {
+        lines += outerDocs
+    }
+    if (this is RustInnerAttributeOwner) {
+        lines += innerDocs
+    }
+    return lines.joinToString("\n")
 }
 
 private val RustOuterAttributeOwner.outerDocs: List<String> get() {
