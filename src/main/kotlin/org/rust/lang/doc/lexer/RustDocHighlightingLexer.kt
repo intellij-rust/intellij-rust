69f2ed7bd44f29e3370ef8e360f9433a38839a2f
Aleksey Kladov <aleksey.kladov@gmail.com>
2016-07-04 19:57:12 +0300

    (LEX): fix O(N^2) lexer performance

--- src/main/kotlin/org/rust/lang/doc/lexer/RustDocHighlightingLexer.kt
+++ src/main/kotlin/org/rust/lang/doc/lexer/RustDocHighlightingLexer.kt
@@ -1,19 +1,39 @@
 package org.rust.lang.doc.lexer
 
-import com.intellij.lexer.FlexAdapter
-import com.intellij.lexer.Lexer
-import com.intellij.lexer.MergeFunction
-import com.intellij.lexer.MergingLexerAdapter
+import com.intellij.lexer.*
 import com.intellij.openapi.util.text.StringUtil
 import com.intellij.psi.TokenType.WHITE_SPACE
 import com.intellij.psi.tree.TokenSet
+import com.intellij.util.text.CharSequenceSubSequence
 import org.rust.lang.doc.psi.RustDocElementTypes.*
 import org.rust.lang.doc.psi.RustDocKind
 
+
+/**
+ * Default lexer copies the whole `buf` in the [reset] method into an array.
+ *
+ * As we use this lexer as a layered lexer, [reset] is called a lot with `buf` containing the whole
+ * file, and `start` and `end` pointing to the range of the documentation comment.
+ *
+ * This wrapper avoids coping the whole file.
+ *
+ * TODO: switch to the patched jFlex version from `intellij-community/tools/lexer`
+ */
+private class FlexWrapper(private val delegate: FlexLexer) : FlexLexer by delegate {
+    private var myStart: Int = 0
+    override fun getTokenStart(): Int = delegate.tokenStart + myStart
+    override fun getTokenEnd(): Int = delegate.tokenEnd + myStart
+    override fun reset(buf: CharSequence, start: Int, end: Int, initialState: Int) {
+        myStart = start
+        delegate.reset(CharSequenceSubSequence(buf, start, end), 0, end - start, initialState)
+    }
+}
+
 class RustDocHighlightingLexer(kind: RustDocKind) :
     MergingLexerAdapter(
-        FlexAdapter(_RustDocHighlightingLexer(null, kind.isBlock)),
-        TOKENS_TO_MERGE) {
+        FlexAdapter(FlexWrapper(_RustDocHighlightingLexer(null, kind.isBlock))),
+        TOKENS_TO_MERGE
+    ) {
 
     override fun getMergeFunction() = MergeFunction { type, lexer ->
         if (type == DOC_TEXT) {
