50760f56b7316cec147bb7be8c1052ba1df3ebcc
Aleksey Kladov <aleksey.kladov@gmail.com>
2016-07-19 18:02:16 +0300

    (MISC): reduce duplication in RustElementFactory

--- src/main/kotlin/org/rust/lang/core/psi/RustElementFactory.kt
+++ src/main/kotlin/org/rust/lang/core/psi/RustElementFactory.kt
@@ -3,36 +3,28 @@ package org.rust.lang.core.psi
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiFileFactory
 import org.rust.lang.RustLanguage
-import org.rust.lang.core.psi.impl.RustFile
 import org.rust.lang.core.psi.util.childOfType
 
 object RustElementFactory {
-    fun createFileFromText(project: Project, text: String): RustFile? =
-        PsiFileFactory.getInstance(project).createFileFromText("DUMMY.rs", RustLanguage, text) as RustFile?
+    fun createExpression(project: Project, expression: String): RustExprElement? =
+        createFromText(project, "fn main() {$expression;}")
 
-    fun createExpression(project: Project, expression: String): RustExprElement? {
-        val file = createFileFromText(project, "fn main() {$expression;}")
-        return file?.childOfType<RustExprElement>()
-    }
+    fun createModDeclItem(project: Project, modName: String): RustModDeclItemElement? =
+        createFromText(project, "mod $modName;")
 
-    fun createModDeclItem(project: Project, modName: String): RustModDeclItemElement? {
-        val file = createFileFromText(project, "mod $modName;")
-        return file?.childOfType<RustModDeclItemElement>()
-    }
+    fun createOuterAttr(project: Project, attrContents: String): RustOuterAttrElement? =
+        createFromText(project, "#[$attrContents] struct Dummy;")
 
-    fun createOuterAttr(project: Project, attrContents: String): RustOuterAttrElement? {
-        val file = createFileFromText(project, "#[$attrContents] struct Dummy;")
-        return file?.childOfType<RustOuterAttrElement>()
-    }
-
-    fun createUseItem(project: Project, path: String): RustUseItemElement? {
-        val file = createFileFromText(project, "use $path;")
-        return file?.childOfType<RustUseItemElement>()
-    }
+    fun createUseItem(project: Project, path: String): RustUseItemElement? =
+        createFromText(project, "use $path;")
 
     fun createStructExprBody(project: Project, fieldNames: List<String>): RustStructExprBodyElement? {
         val fields = fieldNames.map { "$it: ()," }.joinToString("\n")
-        val file = createFileFromText(project, "fn main() { S { $fields }; }")
-        return file?.childOfType<RustStructExprBodyElement>()
+        return createFromText(project, "fn main() { S { $fields }; }")
     }
+
+    private inline fun <reified T : RustCompositeElement> createFromText(project: Project, code: String): T? =
+        PsiFileFactory.getInstance(project)
+            .createFileFromText("DUMMY.rs", RustLanguage, code)
+            ?.childOfType<T>()
 }
