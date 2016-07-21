6ac8ae292d2a7dc4873e8f9966dae8d322cacac0
Aleksey Kladov <aleksey.kladov@gmail.com>
2016-07-21 17:04:28 +0300

    (RUN): add target when running test from context
    Running `cargo test my_unique_test_name` is not enough if your project
    includes several integration test crates (like cargo does), because each
    of this crates will be executed (running zero test). To avoid this
    problem, we add the necessary target kind dependent argument, like
    
    `cargo test --bin main my_unique_test_name`

--- src/main/kotlin/org/rust/cargo/runconfig/producers/CargoTestRunConfigurationProducer.kt
+++ src/main/kotlin/org/rust/cargo/runconfig/producers/CargoTestRunConfigurationProducer.kt
@@ -6,13 +6,14 @@ import com.intellij.execution.actions.RunConfigurationProducer
 import com.intellij.openapi.util.Ref
 import com.intellij.psi.PsiElement
 import org.rust.cargo.CargoConstants
+import org.rust.cargo.project.CargoProjectDescription
 import org.rust.cargo.runconfig.CargoCommandConfiguration
 import org.rust.cargo.runconfig.CargoCommandRunConfigurationType
-import org.rust.lang.core.psi.RustFnItemElement
-import org.rust.lang.core.psi.RustMod
-import org.rust.lang.core.psi.canonicalCratePath
-import org.rust.lang.core.psi.functions
+import org.rust.cargo.util.cargoProject
+import org.rust.lang.core.psi.*
+import org.rust.lang.core.psi.util.module
 import org.rust.lang.core.psi.util.parentOfType
+import org.rust.lang.core.resolve.util.RustResolveUtil
 
 class CargoTestRunConfigurationProducer : RunConfigurationProducer<CargoCommandConfiguration>(CargoCommandRunConfigurationType()) {
 
@@ -24,7 +25,7 @@ class CargoTestRunConfigurationProducer : RunConfigurationProducer<CargoCommandC
 
         return configuration.command == CargoConstants.Commands.TEST &&
             configuration.name == test.configurationName &&
-            configuration.additionalArguments == test.testPath
+            configuration.additionalArguments == test.commandLineParameters
     }
 
     override fun setupConfigurationFromContext(
@@ -36,7 +37,7 @@ class CargoTestRunConfigurationProducer : RunConfigurationProducer<CargoCommandC
 
         configuration.command = CargoConstants.Commands.TEST
         configuration.name = test.configurationName
-        configuration.additionalArguments = test.testPath
+        configuration.additionalArguments = test.commandLineParameters
         return true
     }
 
@@ -45,8 +46,22 @@ class CargoTestRunConfigurationProducer : RunConfigurationProducer<CargoCommandC
 
     private data class TestConfig(
         val configurationName: String,
-        val testPath: String
-    )
+        val testPath: String,
+        val target: CargoProjectDescription.Target
+    ) {
+        val commandLineParameters: String get() {
+            val targetKind = when (target.kind) {
+                CargoProjectDescription.TargetKind.BIN     -> "bin"
+                CargoProjectDescription.TargetKind.LIB     -> "lib"
+                CargoProjectDescription.TargetKind.TEST    -> "test"
+                CargoProjectDescription.TargetKind.EXAMPLE -> "example"
+                CargoProjectDescription.TargetKind.BENCH   -> "bench"
+                CargoProjectDescription.TargetKind.UNKNOWN -> return testPath
+            }
+
+            return "--$targetKind ${target.name} $testPath"
+        }
+    }
 
     private fun findTest(context: ConfigurationContext): TestConfig? = listOfNotNull(
         findTestFunction(context),
@@ -56,7 +71,8 @@ class CargoTestRunConfigurationProducer : RunConfigurationProducer<CargoCommandC
     private fun findTestFunction(context: ConfigurationContext): TestConfig? {
         val fn = context.psiLocation?.parentOfType<RustFnItemElement>() ?: return null
         val name = fn.name ?: return null
-        return if (fn.isTest) TestConfig("Test $name", name) else null
+        val target = cargoTargetForElement(fn) ?: return null
+        return if (fn.isTest) TestConfig("Test $name", name, target) else null
     }
 
     private fun findTestMod(context: ConfigurationContext): TestConfig? {
@@ -67,7 +83,13 @@ class CargoTestRunConfigurationProducer : RunConfigurationProducer<CargoCommandC
             "Test ${mod.modName}"
 
         val testPath = mod.canonicalCratePath ?: return null
-        return if (mod.functions.any { it.isTest }) TestConfig(testName, testPath) else null
+        val target = cargoTargetForElement(mod) ?: return null
+        return if (mod.functions.any { it.isTest }) TestConfig(testName, testPath, target) else null
+    }
+
+    private fun cargoTargetForElement(element: RustCompositeElement): CargoProjectDescription.Target? {
+        val rootMod = RustResolveUtil.getCrateRootModFor(element) ?: return null
+        return element.module?.cargoProject?.findTargetForFile(rootMod.containingFile.virtualFile)
     }
 }
 
