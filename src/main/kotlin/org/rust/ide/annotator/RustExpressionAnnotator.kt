4cb763ceb2df6827252c566197101e8f8e8a05bf
Aleksey Kladov <aleksey.kladov@gmail.com>
2016-07-29 17:08:57 +0300

    (ANN): tweak highlighting

--- src/main/kotlin/org/rust/ide/annotator/RustExpressionAnnotator.kt
+++ src/main/kotlin/org/rust/ide/annotator/RustExpressionAnnotator.kt
@@ -1,6 +1,7 @@
 package org.rust.ide.annotator
 
 import com.intellij.codeInspection.LocalQuickFixAndIntentionActionOnPsiElement
+import com.intellij.codeInspection.ProblemHighlightType
 import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.Annotator
 import com.intellij.openapi.editor.Editor
@@ -30,6 +31,7 @@ class RustExpressionAnnotator : Annotator {
         for (field in expr.structExprFieldList) {
             if (field.reference.resolve() == null) {
                 holder.createErrorAnnotation(field.identifier, "No such field")
+                    .highlightType = ProblemHighlightType.LIKE_UNKNOWN_SYMBOL
             }
         }
 
