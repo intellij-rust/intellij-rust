c13279f2f0663aed7820bb62ec317086ba984eae
Alexey Kudinkin <alexey.kudinkin@gmail.com>
2016-08-06 00:47:16 +0200

    (IDX): Reset `RustImplIndex` on every test run

--- src/test/kotlin/org/rust/ide/annotator/RustHighlightingPerformanceTest.kt
+++ src/test/kotlin/org/rust/ide/annotator/RustHighlightingPerformanceTest.kt
@@ -1,6 +1,8 @@
 package org.rust.ide.annotator
 
+import com.intellij.psi.stubs.StubUpdatingIndex
 import com.intellij.testFramework.LightProjectDescriptor
+import com.intellij.util.indexing.FileBasedIndex
 import org.junit.experimental.categories.Category
 import org.rust.Performance
 import org.rust.lang.RustTestCaseBase
@@ -8,6 +10,12 @@ import org.rust.lang.RustTestCaseBase
 @Category(Performance::class)
 class RustHighlightingPerformanceTest : RustTestCaseBase() {
     override val dataPath = "org/rust/ide/annotator/fixtures/performance"
+
+    override fun setUp() {
+        super.setUp()
+
+        FileBasedIndex.getInstance().requestRebuild(StubUpdatingIndex.INDEX_ID)
+    }
 
     override fun getProjectDescriptor(): LightProjectDescriptor = WithStdlibRustProjectDescriptor()
 
