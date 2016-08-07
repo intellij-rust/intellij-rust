73926678b2109080cd07cf98f71dafe9cc3a48d1
Alexey Kudinkin <alexey.kudinkin@gmail.com>
2016-08-07 04:32:47 +0200

    (MISC): Tidying up

--- src/main/kotlin/org/rust/lang/core/types/RustCharacterType.kt
+++ src/main/kotlin/org/rust/lang/core/types/RustCharacterType.kt
@@ -1,6 +1,5 @@
 package org.rust.lang.core.types
 
-import org.rust.lang.core.types.unresolved.RustUnresolvedTypeBase
 import org.rust.lang.core.types.visitors.RustTypeVisitor
 import org.rust.lang.core.types.visitors.RustUnresolvedTypeVisitor
 
