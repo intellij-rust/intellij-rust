3b8c7154cb03c39f0c6ac3eb6f79a5f52c479013
Vladimir Kharitonov <ujpv@mail.ru>
2016-09-26 03:33:18 +0300

    (SURROUND) Test by text

---
+++ src/test/kotlin/org/rust/ide/surroundWith/RustSurrounderTestByStringCaseBase.kt
@@ -0,0 +1,56 @@
+package org.rust.ide.surroundWith
+
+import com.intellij.codeInsight.generation.surroundWith.SurroundWithHandler
+import com.intellij.lang.LanguageSurrounders
+import com.intellij.lang.surroundWith.Surrounder
+import com.intellij.openapi.command.WriteCommandAction
+import org.assertj.core.api.Assertions
+import org.rust.lang.RustFileType
+import org.rust.lang.RustLanguage
+import org.rust.lang.RustTestCaseBase
+import java.util.*
+
+abstract class RustSurrounderTestByStringCaseBase(val surrounder: Surrounder) : RustTestCaseBase() {
+    override val dataPath: String  = ""
+
+    protected fun doTest(before: String, after: String) {
+        myFixture.configureByText(RustFileType, before)
+
+        checkApplicability(fileName, true)
+        WriteCommandAction.runWriteCommandAction(myFixture.project) {
+            SurroundWithHandler.invoke(myFixture.project, myFixture.editor, myFixture.file, surrounder)
+        }
+
+        myFixture.checkResult(after)
+    }
+
+    protected fun doTestNotApplicable(before: String) {
+        myFixture.configureByText(RustFileType, before)
+        checkApplicability(before, false)
+    }
+
+    private fun checkApplicability(testCase: String, isApplicable: Boolean) {
+        val descriptor = LanguageSurrounders.INSTANCE.allForLanguage(RustLanguage)
+            .first { descriptor ->
+                descriptor.surrounders.any { surrounder ->
+                    surrounder.javaClass == this.surrounder.javaClass
+                }
+            }
+
+        val selectionModer = myFixture.editor.selectionModel
+        if (!selectionModer.hasSelection())
+            selectionModer.selectLineAtCaret()
+
+        val elements = descriptor.getElementsToSurround(
+            myFixture.file, selectionModer.selectionStart, selectionModer.selectionEnd)
+
+        Assertions.assertThat(surrounder.isApplicable(elements))
+            .withFailMessage(
+                "surrounder %s be applicable to given selection:\n\n%s\nElements: %s",
+                if (isApplicable) "shouldn't" else "should",
+                testCase,
+                Arrays.toString(elements)
+            )
+            .isEqualTo(isApplicable)
+    }
+}
