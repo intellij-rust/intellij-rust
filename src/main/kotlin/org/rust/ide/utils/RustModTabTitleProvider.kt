a225eb45cf3097dd91721a8072518a6f41fdc881
Matthew Stavola <mstavola9@gmail.com>
2016-10-02 10:50:57 -0700

    (MISC): Provide unique titles for mod files

--- src/main/kotlin/org/rust/ide/utils/RustModTabTitleProvider.kt
+++ src/main/kotlin/org/rust/ide/utils/RustModTabTitleProvider.kt
@@ -1,16 +1,17 @@
 package org.rust.ide.utils
 
-import com.intellij.openapi.fileEditor.impl.EditorTabTitleProvider
+import com.intellij.openapi.fileEditor.impl.UniqueNameEditorTabTitleProvider
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import org.rust.lang.core.psi.impl.isRustFile
 
-class RustModTabTitleProvider : EditorTabTitleProvider {
+class RustModTabTitleProvider : UniqueNameEditorTabTitleProvider() {
     val MOD_FILE_NAME = "mod.rs"
 
     override fun getEditorTabTitle(project: Project, file: VirtualFile): String? {
         if (file.isRustFile && MOD_FILE_NAME == file.name) {
-            return "${file.parent.name}/${file.name}"
+            return  super.getEditorTabTitle(project, file) ?:
+                "${file.parent.name}/${file.name}"
         }
 
         return null
