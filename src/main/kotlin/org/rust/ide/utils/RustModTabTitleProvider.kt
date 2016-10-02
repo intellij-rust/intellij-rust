772b2b2c0087bcc0dd6ec90af202bac8ba39db20
Matthew Stavola <mstavola9@gmail.com>
2016-10-01 21:59:18 -0700

    (MISC): Qualify mod.rs files with parent dir name

---
+++ src/main/kotlin/org/rust/ide/utils/RustModTabTitleProvider.kt
@@ -0,0 +1,18 @@
+package org.rust.ide.utils
+
+import com.intellij.openapi.fileEditor.impl.EditorTabTitleProvider
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import org.rust.lang.core.psi.impl.isRustFile
+
+class RustModTabTitleProvider : EditorTabTitleProvider {
+    val MOD_FILE_NAME = "mod.rs"
+
+    override fun getEditorTabTitle(project: Project, file: VirtualFile): String? {
+        if (file.isRustFile && MOD_FILE_NAME == file.name) {
+            return "${file.parent.name}/${file.name}"
+        }
+
+        return null
+    }
+}
