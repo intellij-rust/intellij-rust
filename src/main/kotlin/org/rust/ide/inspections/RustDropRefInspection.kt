b9e5ed91718af37ac2d5b1d8b6e5c8318c0287c9
Andrew Lygin <alygin@gmail.com>
2016-10-04 10:02:35 +0300

    (MISC) Implemented Drop Reference inspection

--- src/main/kotlin/org/rust/ide/inspections/RustDropRefInspection.kt
+++ src/main/kotlin/org/rust/ide/inspections/RustDropRefInspection.kt
@@ -8,7 +8,6 @@ import org.rust.lang.core.psi.RustElementVisitor
 import org.rust.lang.core.psi.RustPathExprElement
 import org.rust.lang.core.psi.RustUnaryExprElement
 import org.rust.lang.core.psi.impl.RustFnItemElementImpl
-import org.rust.lang.core.psi.impl.RustPathExprElementImpl
 
 /**
  * Checks for calls to std::mem::drop with a reference instead of an owned value.
@@ -34,13 +33,11 @@ class RustDropRefInspection : RustLocalInspectionTool() {
                 val args = expr.argList.exprList
                 if (args.size == 1) {
                     val arg = args[0]
-                    if (arg is RustUnaryExprElement) {
-                        if (arg.expr is RustPathExprElement) {
-                            holder.registerProblem(
-                                expr,
-                                "Call to std::mem::drop with a reference argument. Dropping a reference does nothing",
-                                RemoveRefFix(arg, "Call with owned value"))
-                        }
+                    if (arg is RustUnaryExprElement && arg.expr is RustPathExprElement) {
+                        holder.registerProblem(
+                            expr,
+                            "Call to std::mem::drop with a reference argument. Dropping a reference does nothing",
+                            RemoveRefFix(arg, "Call with owned value"))
                     }
                 }
             }
