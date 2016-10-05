e6e9f6f0464f3319bf772f2ee947ea993e7dcb53
Andrew Lygin <alygin@gmail.com>
2016-10-05 17:09:35 +0300

    (MISC) Double Negation inspection (#713)
    * (MISC) Double Negation inspection
    
    * (MISC) Negation check refactoring in the Double Negation inspection

---
+++ src/main/kotlin/org/rust/ide/inspections/RustDoubleNegInspection.kt
@@ -0,0 +1,30 @@
+package org.rust.ide.inspections
+
+import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.psi.PsiElementVisitor
+import org.rust.lang.core.psi.RustElementVisitor
+import org.rust.lang.core.psi.RustExprElement
+import org.rust.lang.core.psi.RustUnaryExprElement
+
+/**
+ * Checks for usage of double negation, which is a no-op in Rust but might be misleading for
+ * programmers with background in languages that have prefix operators.
+ *
+ * Analogue of Clippy's double_neg.
+ */
+class RustDoubleNegInspection : RustLocalInspectionTool() {
+    override fun getDisplayName(): String = "Double negation"
+
+    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
+        return object : RustElementVisitor() {
+            override fun visitUnaryExpr(expr: RustUnaryExprElement) {
+                if (expr.isNegation && expr.expr.isNegation) {
+                    holder.registerProblem(expr, "--x could be misinterpreted as a pre-decrement, but effectively is a no-op")
+                }
+            }
+        }
+    }
+
+    private val RustExprElement?.isNegation: Boolean
+        get() = this is RustUnaryExprElement && minus != null
+}
