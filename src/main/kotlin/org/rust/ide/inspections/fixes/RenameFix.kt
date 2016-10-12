6a78cae5f629d45608bd9dc79af6ee1605c51a72
Andrew Lygin <alygin@gmail.com>
2016-10-12 15:40:10 +0300

    (INSP): Added Rename fix to Naming inspection (#728)

---
+++ src/main/kotlin/org/rust/ide/inspections/fixes/RenameFix.kt
@@ -0,0 +1,28 @@
+package org.rust.ide.inspections.fixes
+
+import com.intellij.codeInspection.LocalQuickFix
+import com.intellij.codeInspection.ProblemDescriptor
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiNamedElement
+import com.intellij.refactoring.RefactoringFactory
+
+/**
+ * Fix that renames the given element.
+ * @param element The element to be renamed.
+ * @param newName The new name for the element.
+ * @param fixName The name to use for the fix instead of the default one to better fit the inspection.
+ */
+class RenameFix(
+    val element: PsiNamedElement,
+    val newName: String,
+    val fixName: String = "Rename to `$newName`"
+) : LocalQuickFix {
+    override fun getName() = fixName
+    override fun getFamilyName() = "Rename element"
+
+    override fun applyFix(project: Project, descriptor: ProblemDescriptor) =
+        ApplicationManager.getApplication().invokeLater {
+            RefactoringFactory.getInstance(project).createRename(element, newName).run()
+        }
+}
