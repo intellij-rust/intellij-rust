91f5e9e2b70b5d887a0318d585467692f5bcfada
Aleksey Kladov <aleksey.kladov@gmail.com>
2016-10-14 12:55:32 +0300

    (GRAM): do support .. in tuple patterns

--- src/test/resources/org/rust/lang/core/parser/fixtures/complete/patterns.rs
+++ src/test/resources/org/rust/lang/core/parser/fixtures/complete/patterns.rs
@@ -4,6 +4,8 @@ fn patterns() {
     let S {field,} = x;
     let S {field, ..} = x;
     let T(field, ..) = x;
+    let T(.., field) = x;
+    let (x, .., y) = (1, 2, 3, 4, 5);
 
     let ref a @ _ = value;
 
