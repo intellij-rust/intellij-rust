89119c5145f08cad628365c7e0bbbc11ea30b68f
Aleksey Kladov <aleksey.kladov@gmail.com>
2016-10-18 14:25:17 +0300

    (MISC): sipmlify

--- src/main/kotlin/org/rust/ide/annotator/RustTraitMethodImplLineMarkerProvider.kt
+++ src/main/kotlin/org/rust/ide/annotator/RustTraitMethodImplLineMarkerProvider.kt
@@ -5,9 +5,9 @@ import com.intellij.codeInsight.daemon.RelatedItemLineMarkerProvider
 import com.intellij.codeInsight.navigation.NavigationGutterIconBuilder
 import com.intellij.psi.PsiElement
 import org.rust.ide.icons.RustIcons
-import org.rust.lang.core.psi.RustImplItemElement
 import org.rust.lang.core.psi.RustImplMethodMemberElement
 import org.rust.lang.core.psi.RustTraitItemElement
+import org.rust.lang.core.psi.impl.mixin.superMethod
 import org.rust.lang.core.psi.util.parentOfType
 import javax.swing.Icon
 
@@ -15,12 +15,12 @@ import javax.swing.Icon
  * Annotates the implementation of a trait method with an icon on the gutter.
  */
 class RustTraitMethodImplLineMarkerProvider : RelatedItemLineMarkerProvider() {
-    override fun collectNavigationMarkers(el: PsiElement, result: MutableCollection<in RelatedItemLineMarkerInfo<PsiElement>>?) {
-        if (result == null || el !is RustImplMethodMemberElement) return
-        val implBlock = el.parentOfType<RustImplItemElement>() ?: return
-        val trait = implBlock.traitRef?.path?.reference?.resolve() ?: return
-        if (trait !is RustTraitItemElement) return
-        val traitMethod = trait.traitMethodMemberList.find { it.name == el.name } ?: return
+    override fun collectNavigationMarkers(el: PsiElement, result: MutableCollection<in RelatedItemLineMarkerInfo<PsiElement>>) {
+        if (el !is RustImplMethodMemberElement) return
+
+        val traitMethod = el.superMethod ?: return
+        val trait = traitMethod.parentOfType<RustTraitItemElement>() ?: return
+
         val action: String
         val icon: Icon
         if (traitMethod.isAbstract) {
@@ -30,10 +30,12 @@ class RustTraitMethodImplLineMarkerProvider : RelatedItemLineMarkerProvider() {
             action = "Overrides"
             icon = RustIcons.OVERRIDING_METHOD
         }
+
         val builder = NavigationGutterIconBuilder
             .create(icon)
             .setTargets(listOf(traitMethod))
             .setTooltipText("$action method in `${trait.name}`")
+
         result.add(builder.createLineMarkerInfo(el))
     }
 }
--- src/main/kotlin/org/rust/lang/core/psi/impl/mixin/RustImplMethodMemberImplMixin.kt
+++ src/main/kotlin/org/rust/lang/core/psi/impl/mixin/RustImplMethodMemberImplMixin.kt
@@ -6,7 +6,7 @@ import com.intellij.psi.stubs.IStubElementType
 import org.rust.ide.icons.RustIcons
 import org.rust.lang.core.psi.RustImplItemElement
 import org.rust.lang.core.psi.RustImplMethodMemberElement
-import org.rust.lang.core.psi.RustNamedElement
+import org.rust.lang.core.psi.RustTraitMethodMemberElement
 import org.rust.lang.core.psi.iconWithVisibility
 import org.rust.lang.core.psi.impl.RustPsiImplUtil
 import org.rust.lang.core.psi.util.parentOfType
@@ -30,10 +30,9 @@ abstract class RustImplMethodMemberImplMixin : RustFnImplMixin<RustImplMethodMem
     override val isPublic: Boolean get() = RustPsiImplUtil.isPublic(this)
 }
 
-val RustImplMethodMemberElement.superMethod: RustNamedElement?
-    get() {
-        val rustImplItem = parentOfType<RustImplItemElement>() ?: return null
-        val superTrait = rustImplItem.traitRef?.trait ?: return null
+val RustImplMethodMemberElement.superMethod: RustTraitMethodMemberElement? get() {
+    val rustImplItem = parentOfType<RustImplItemElement>() ?: return null
+    val superTrait = rustImplItem.traitRef?.trait ?: return null
 
-        return superTrait.traitMethodMemberList.find { it.name == this.name }
-    }
+    return superTrait.traitMethodMemberList.find { it.name == this.name }
+}
