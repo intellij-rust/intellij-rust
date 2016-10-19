bf9dad5300081f4d3b979cb2570594ef87b4e41b
Andrew Lygin <alygin@gmail.com>
2016-10-19 12:00:21 +0300

    (INSP): Inspections refactoring

--- src/main/kotlin/org/rust/ide/inspections/RustUnresolvedReferenceInspection.kt
+++ src/main/kotlin/org/rust/ide/inspections/RustUnresolvedReferenceInspection.kt
@@ -1,35 +1,29 @@
 package org.rust.ide.inspections
 
 import com.intellij.codeInspection.ProblemsHolder
-import com.intellij.psi.PsiElementVisitor
 import org.rust.lang.core.psi.*
 import org.rust.lang.core.types.util.isPrimitive
 import org.rust.lang.core.types.visitors.impl.RustTypificationEngine
 
 class RustUnresolvedReferenceInspection : RustLocalInspectionTool() {
-    override fun getDisplayName(): String = "Unresolved reference"
+    override fun getDisplayName() = "Unresolved reference"
 
-    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
-        return object : RustElementVisitor() {
+    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean) =
+        object : RustElementVisitor() {
             override fun visitPath(o: RustPathElement) {
                 val p = o.parent
                 val isPrimitiveType = o is RustPathElement &&
                     p is RustPathTypeElement &&
                     RustTypificationEngine.typifyType(p).isPrimitive
 
-                if (isPrimitiveType)
-                    return
+                if (isPrimitiveType || o.reference.resolve() != null) return
 
-                if (o.reference.resolve() != null)
-                    return
-
-                val parent = o.path
-                val parentRes = parent?.reference?.resolve()
-                if (parent == null || parentRes is RustMod || parentRes is RustEnumItemElement) {
+                val parent = o.path ?: return
+                val parentRes = parent.reference.resolve()
+                if (parentRes is RustMod || parentRes is RustEnumItemElement) {
                     holder.registerProblem(o.navigationElement, "Unresolved reference")
                 }
             }
-        }
     }
 }
 
