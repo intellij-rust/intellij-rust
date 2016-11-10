c3ddbd3eb816dfa819d33b9bfe590ac480eb21b5
Kharitonov Vladimir <ujpv@mail.ru>
2016-11-10 22:10:34 +0300

    (INT): Match to if let

---
+++ src/main/kotlin/org/rust/ide/intentions/MatchToIfLetIntention.kt
@@ -0,0 +1,54 @@
+package org.rust.ide.intentions
+
+import com.intellij.codeInsight.intention.PsiElementBaseIntentionAction
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiElement
+import org.rust.lang.core.psi.*
+import org.rust.lang.core.psi.util.getNextNonCommentSibling
+import org.rust.lang.core.psi.util.parentOfType
+
+class MatchToIfLetIntention : PsiElementBaseIntentionAction() {
+    override fun getText() = "Convert math statement to if let"
+    override fun getFamilyName(): String = text
+
+    override fun isAvailable(project: Project, editor: Editor?, element: PsiElement): Boolean =
+        findMathExpr(element) != null
+
+    override fun invoke(project: Project, editor: Editor?, element: PsiElement) {
+        val matchExpr = checkNotNull(findMathExpr(element))
+        val matchBody = checkNotNull(matchExpr.matchBody)
+        val arm =  matchBody.matchArmList.find { it.expr?.isVoid == false } ?: return
+        var bodyText = arm.expr?.text ?: return
+        if (arm.expr !is RustBlockExprElement) {
+            bodyText = "{\n$bodyText\n}"
+        }
+
+        val rustIfLetExprElement =
+            RustElementFactory.createExpression(project, "if let ${arm.matchPat.text} = ${matchExpr.expr.text} $bodyText")
+                as RustIfLetExprElement
+        matchExpr.replace(rustIfLetExprElement)
+    }
+
+    private fun findMathExpr(element: PsiElement): RustMatchExprElement? {
+        if (!element.isWritable) return null
+
+        val matchExpr = element.parentOfType<RustMatchExprElement>() ?: return null
+        val matchBody = matchExpr.matchBody
+        val matchArmList = matchBody?.matchArmList ?: return null
+
+        val notVoidArms: List<RustMatchArmElement> = matchArmList.filter { it.expr?.isVoid == false }
+        if (notVoidArms.size == 1 &&
+            notVoidArms[0].matchPat.patList.size == 1 &&
+            notVoidArms[0].matchPat.patList[0].text != "_") {
+            return matchExpr
+        } else {
+            return null
+        }
+    }
+
+    val RustExprElement.isVoid: Boolean get() =
+    (this is RustBlockExprElement &&
+        (block?.lbrace.getNextNonCommentSibling()?.equals(this.block?.rbrace) ?: true)) ||
+        this is RustUnitExprElement
+}
---
+++ src/test/kotlin/org/rust/ide/intentions/MatchToIfLetIntentionTest.kt
@@ -0,0 +1,128 @@
+package org.rust.ide.intentions
+
+class MatchToIfLetIntentionTest : RustIntentionTestBase(MatchToIfLetIntention()) {
+    fun testUnavailableAllVoidArms() = doUnavailableTest(
+        """
+        enum MyOption {
+            Nothing,
+            Some(x),
+        }
+
+        fn main() {
+            let a = MyOption::Some(52);
+
+            match a {
+                MyOption::Some(x) => {},/*caret*/
+                Nothing => {}
+            }
+        }
+        """
+    )
+
+    fun testUnavailableAllNotVoidArms() = doUnavailableTest(
+        """
+        enum MyOption {
+            Nothing,
+            Some(x),
+        }
+
+        fn main() {
+            let a = MyOption::Some(52);
+
+            match a {
+                MyOption::Some(x) => {42},
+                Nothing => {43}/*caret*/
+            }
+        }
+        """
+    )
+
+    fun testUnavailable_pattern() = doUnavailableTest(
+        """
+        enum OptionColor {
+            NoColor,
+            Color(i32, i32, i32),
+        }
+
+        fn main() {
+            let color = OptionColor::Color(255, 255, 255);
+
+            match color {/*caret*/
+                OptionColor::Color(_, _, _) => {},
+                _ => {print!("No color")},
+            };
+        }
+        """
+    )
+
+    fun testSimple1() = doAvailableTest(
+        """
+        enum MyOption {
+            Nothing,
+            Some(x),
+        }
+
+        fn main() {
+            let color = MyOption::Some(52);
+
+            match color {
+                MyOption::Some(x) => {
+                    let a = x + 1;
+                    let b = x + 2;
+                    let c = a + b;
+                },/*caret*/
+                Nothing => {}
+            }
+        }
+        """
+        ,
+        """
+        enum MyOption {
+            Nothing,
+            Some(x),
+        }
+
+        fn main() {
+            let color = MyOption::Some(52);
+
+            if let MyOption::Some(x) = color {
+                let a = x + 1;
+                let b = x + 2;
+                let c = a + b;
+            }
+        }
+        """
+    )    fun testSimple2() = doAvailableTest(
+        """
+        enum OptionColor {
+            NoColor,
+            Color(i32, i32, i32),
+        }
+
+        fn main() {
+            let color = OptionColor::Color(255, 255, 255);
+
+            match color {/*caret*/
+                OptionColor::Color(255, 255, 255) => print!("White"),
+                OptionColor::Color(_,   _,   _  ) => {},
+                OptionColor::NoColor => {},
+            };
+        }
+        """
+        ,
+        """
+        enum OptionColor {
+            NoColor,
+            Color(i32, i32, i32),
+        }
+
+        fn main() {
+            let color = OptionColor::Color(255, 255, 255);
+
+            if let OptionColor::Color(255, 255, 255) = color {
+                print!("White")
+            };
+        }
+        """
+    )
+}
