243bd601f05d4a693adcc392c87f5293b48402c6
David Herman <d9n.coding@gmail.com>
2016-11-23 07:11:44 -0800

    (MISC) Fix explain filter hyperlink behavior
    In IntelliJ console code, there's special case handling for the
    'BrowserHyperlinkInfo' navigatable, so that setting focus to it
    (when using the next occurrence / prev occurrence action) won't
    automatically navigate to it (See
    ConsoleViewImpl#calcNextOccurrence).
    
    Therefore, we switch the class used to that one, so that automatically
    opening web pages in the backgroud won't happen anymore as the user
    jumps between occurrences in the console log. However, the user can
    still click on the link directly.
    
    Fixes #611

--- src/main/kotlin/org/rust/cargo/runconfig/RustExplainFilter.kt
+++ src/main/kotlin/org/rust/cargo/runconfig/RustExplainFilter.kt
@@ -1,5 +1,6 @@
 package org.rust.cargo.runconfig
 
+import com.intellij.execution.filters.BrowserHyperlinkInfo
 import com.intellij.execution.filters.Filter
 import com.intellij.execution.filters.Filter.Result
 import com.intellij.ide.browsers.OpenUrlHyperlinkInfo
@@ -23,7 +24,7 @@ class RustExplainFilter : Filter, DumbAware {
 
         val eNumber = matcher.group(1)
         val url = "https://doc.rust-lang.org/error-index.html#E$eNumber"
-        val info = OpenUrlHyperlinkInfo(url)
+        val info = BrowserHyperlinkInfo(url)
 
         val highlightStartOffset = entireLength - line.length + matcher.start()
         val highlightEndOffset = highlightStartOffset + link_length
