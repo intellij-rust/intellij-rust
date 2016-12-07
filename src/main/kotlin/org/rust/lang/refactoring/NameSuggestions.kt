59562d79a2d4330231a07d73ea0c77caf93006f1
Liam Clark <liam.alsitair.clark@gmail.com>
2016-12-07 11:44:21 +0100

    (Misc) suggest the function name if we are introducing directly for a call.
    If the type has been resolved, introduce the first letter of the type name.
    Bugfix for expressions that are part of an argument but not the entire arugment themselves.

--- src/main/kotlin/org/rust/lang/refactoring/NameSuggestions.kt
+++ src/main/kotlin/org/rust/lang/refactoring/NameSuggestions.kt
@@ -8,37 +8,49 @@ import org.rust.lang.core.psi.RustCallExprElement
 import org.rust.lang.core.psi.RustExprElement
 import org.rust.lang.core.psi.RustFnItemElement
 import org.rust.lang.core.psi.util.parentOfType
+import org.rust.lang.core.types.util.resolvedType
 import java.util.*
 
 fun suggestedNames(project: Project, expr: RustExprElement): LinkedHashSet<String> {
     val names = LinkedHashSet<String>()
 
+    val typeString = expr.resolvedType.toString()
+    //doesn't really seem to do that much.
+    if (typeString != "unkown") {
+        names.addAll(RustNameUtil(typeString.take(1)))
+    }
+
     if (expr.isArgument()) {
         val name = nameForArgument(project, expr)
-        val suggestionsByName = NameUtil.getSuggestionsByName(name, "", "", false, false, false).map(String::toSnakeCase)
+        val suggestionsByName = RustNameUtil(name)
         names.addAll(suggestionsByName)
+    } else if (expr is RustCallExprElement) {
+        names.addAll(RustNameUtil(expr.expr.text))
     }
 
     return names
 }
 
-
 fun nameForArgument(project: Project, expr: RustExprElement): String {
     val call = expr.parentOfType<RustCallExprElement>(strict = false) ?: return ""
 
     val parameterIndex = call.argList.children.indexOf(expr)
+    val fn = findFnImpl(project, call)
 
+    return fn?.parameters?.parameterList?.get(parameterIndex)?.pat?.text ?: ""
+}
+
+fun findFnImpl(project: Project, callExpr: RustCallExprElement): RustFnItemElement? {
     val navigator = RustSymbolNavigationContributor()
-    val items = navigator.getItemsByName(call.firstChild.text, null, project, false)
-    val fn = items[0] as RustFnItemElement
+    val items = navigator.getItemsByName(callExpr.firstChild.text, null, project, false)
 
-    return fn.parameters?.parameterList?.get(parameterIndex)?.pat?.text ?: ""
+    return items.firstOrNull() as? RustFnItemElement
 }
 
 fun String.toSnakeCase(): String {
     val builder = StringBuilder()
     for (char in this.toCharArray()) {
-        if(char.isUpperCase()) {
+        if (char.isUpperCase()) {
             builder
                 .append('_')
                 .append(char.toLowerCase())
@@ -50,4 +62,6 @@ fun String.toSnakeCase(): String {
     return builder.toString()
 }
 
-fun RustExprElement.isArgument() = this.parentOfType<RustArgListElement>(strict = false) != null
+fun RustExprElement.isArgument() = this.parent is RustArgListElement
+
+fun RustNameUtil(name: String) = NameUtil.getSuggestionsByName(name, "", "", false, false, false).map(String::toSnakeCase)
--- src/test/kotlin/org/rust/lang/refactoring/NameSuggestionsKtTest.kt
+++ src/test/kotlin/org/rust/lang/refactoring/NameSuggestionsKtTest.kt
@@ -9,7 +9,7 @@ import org.rust.lang.core.psi.impl.RustFile
 class NameSuggestionsKtTest : RustTestCaseBase() {
     override val dataPath = "org/rust/lang/refactoring/fixtures/introduce_variable/"
 
-    fun testSuggestedNames() = doTest("""
+    fun testArgumentNames() = doTest("""
 fn foo(a: i32, veryCoolVariableName: i32) {
     a + b
 }
@@ -17,13 +17,46 @@ fn foo(a: i32, veryCoolVariableName: i32) {
 fn bar() {
     foo(4, 10/*caret*/ + 2)
 }
-
 """) {
         val refactoring = RustIntroduceVariableRefactoring(myFixture.project, myFixture.editor, myFixture.file as RustFile)
         val expr = refactoring.possibleTargets().first()
 
         assertThat(nameForArgument(project, expr)).isEqualTo("veryCoolVariableName")
-        assertThat(suggestedNames(project, expr)).contains("name", "variable_name", "cool_variable_name" ,"very_cool_variable_name")
+        assertThat(suggestedNames(project, expr)).containsExactly("name", "variable_name", "cool_variable_name" ,"very_cool_variable_name")
+    }
+
+    fun testNonDirectArgumentNames() = doTest("""
+fn foo(a: i32, veryCoolVariableName: i32) {
+    a + b
+}
+
+fn bar() {
+    foo(4, 1/*caret*/0 + 2)
+}
+""") {
+        val refactoring = RustIntroduceVariableRefactoring(myFixture.project, myFixture.editor, myFixture.file as RustFile)
+        val expr = refactoring.possibleTargets().first()
+
+        assertThat(suggestedNames(project, expr)).containsExactly("i")
+    }
+
+
+
+    fun testFunctionNames() = doTest("""
+fn foo(a: i32, veryCoolVariableName: i32) -> i32 {
+    a + b
+}
+
+fn bar() {
+    f/*caret*/oo(4, 10 + 2)
+}
+"""
+    ) {
+        val refactoring = RustIntroduceVariableRefactoring(myFixture.project, myFixture.editor, myFixture.file as RustFile)
+        val expr = refactoring.possibleTargets().first()
+
+        val names = suggestedNames(project, expr)
+        assertThat(names).containsExactly("i", "foo")
     }
 
     private fun doTest(@Language("Rust") before: String, action: () -> Unit) {
