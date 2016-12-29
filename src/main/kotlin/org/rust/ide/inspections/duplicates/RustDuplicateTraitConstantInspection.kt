b338de8505a66bfa05b992eb62379d5e23b785a4
Aleksey Kladov <aleksey.kladov@gmail.com>
2016-12-29 14:54:05 +0300

    (GRAM): trait_const_member is constant

--- src/main/kotlin/org/rust/ide/inspections/duplicates/RustDuplicateTraitConstantInspection.kt
+++ src/main/kotlin/org/rust/ide/inspections/duplicates/RustDuplicateTraitConstantInspection.kt
@@ -6,7 +6,7 @@ import org.rust.lang.core.psi.RustTraitItemElement
 
 class RustDuplicateTraitConstantInspection : RustDuplicateInspectionTool() {
     override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean) =
-        createInspection(RustTraitItemElement::getTraitConstMemberList) {
+        createInspection(RustTraitItemElement::getConstantList) {
             holder.registerProblem(it.identifier, "Duplicate trait constant <code>#ref</code>", ProblemHighlightType.GENERIC_ERROR_OR_WARNING)
         }
 }
