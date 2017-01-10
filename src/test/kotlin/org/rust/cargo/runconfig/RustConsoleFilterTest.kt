7714c984668912a7f3609a6d709c89ca55121d93
Aleksey Kladov <aleksey.kladov@gmail.com>
2017-01-10 16:12:27 +0300

    (RUN): fix regex for new-style errors
    closes #909

--- src/test/kotlin/org/rust/cargo/runconfig/RustConsoleFilterTest.kt
+++ src/test/kotlin/org/rust/cargo/runconfig/RustConsoleFilterTest.kt
@@ -21,4 +21,18 @@ src/main.rs:25:26: 25:40 error: no method named `read_to_string` found for type 
             """error: the trait bound `std::string::String: std::ops::Index<_>` is not satisfied [--explain E0277]
  --> src/main.rs:4:5""",
             " --> [src/main.rs -> main.rs]:4:5", 1)
+
+    fun testNewErrorFormatMoreSpaces() =
+        checkHighlights(filter,
+            """error: the trait bound `std::string::String: std::ops::Index<_>` is not satisfied [--explain E0277]
+   --> src/main.rs:4:5""",
+            "   --> [src/main.rs -> main.rs]:4:5", 1)
+
+    fun testNewErrorFormatNewLine() =
+        checkHighlights(filter,
+            """error: the trait bound `std::string::String: std::ops::Index<_>` is not satisfied [--explain E0277]
+ --> src/main.rs:4:5
+""",
+            " --> [src/main.rs -> main.rs]:4:5\n", 1)
+
 }
