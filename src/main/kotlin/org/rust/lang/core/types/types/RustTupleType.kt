faf4e1ac8e8cfcb9f0df076089addf9bb1421b2b
Aleksey Kladov <aleksey.kladov@gmail.com>
2017-01-19 16:01:16 +0300

    TY: simplify

--- src/main/kotlin/org/rust/lang/core/types/types/RustTupleType.kt
+++ src/main/kotlin/org/rust/lang/core/types/types/RustTupleType.kt
@@ -3,18 +3,9 @@ package org.rust.lang.core.types.types
 import org.rust.lang.core.types.RustType
 
 data class RustTupleType(val types: List<RustType>) : RustType {
-
-    operator fun get(i: Int): RustType {
-        require(i >= 0)
-        return types.getOrElse(i, { RustUnknownType })
-    }
-
-    val size: Int = types.size
-
     override fun substitute(map: Map<RustTypeParameterType, RustType>): RustTupleType =
         RustTupleType(types.map { it.substitute(map) })
 
     override fun toString(): String = types.joinToString(", ", "(", ")")
-
 }
 
