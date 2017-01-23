91ecb7e99e992fa25ed557d0c802e34cf60ad942
Aleksey Kladov <aleksey.kladov@gmail.com>
2017-01-24 00:48:44 +0300

    T: test inspection descriptions

--- src/test/kotlin/org/rust/ide/inspections/RsInspectionsTestBase.kt
+++ src/test/kotlin/org/rust/ide/inspections/RsInspectionsTestBase.kt
@@ -17,7 +17,13 @@ abstract class RsInspectionsTestBase(val useStdLib: Boolean = false) : RsTestBas
         myFixture.testHighlighting(true, false, true, fileName)
     }
 
-    protected inline fun <reified T : LocalInspectionTool> checkByText(text: String, checkWarn: Boolean = true, checkInfo: Boolean = false, checkWeakWarn: Boolean = false) {
+    protected inline fun <reified T : LocalInspectionTool> checkByText(
+        text: String,
+        checkWarn: Boolean = true, checkInfo: Boolean = false, checkWeakWarn: Boolean = false
+    ) {
+        val description = "inspectionDescriptions/${T::class.simpleName?.dropLast("Inspection".length)}.html"
+        T::class.java.classLoader.getResource(description)
+            ?: error("No inspection description for ${T::class} ($description)")
         myFixture.configureByText("main.rs", text)
         enableInspection<T>()
         myFixture.checkHighlighting(checkWarn, checkInfo, checkWeakWarn)
