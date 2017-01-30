13951fac38e188e9ad1f7e3dde8506ebdf07b5b5
Aleksey Kladov <aleksey.kladov@gmail.com>
2017-01-30 14:20:23 +0300

    T: test descriptions once per class

--- src/test/kotlin/org/rust/ide/intentions/RsIntentionTestBase.kt
+++ src/test/kotlin/org/rust/ide/intentions/RsIntentionTestBase.kt
@@ -7,8 +7,17 @@ import org.rust.lang.RsTestBase
 abstract class RsIntentionTestBase(val intention: IntentionAction) : RsTestBase() {
     final override val dataPath: String get() = ""
 
+    fun testIntentionHasDocumentation() {
+        val directory = "intentionDescriptions/${intention.javaClass.simpleName}"
+        val files = listOf("before.rs.template", "after.rs.template", "description.html")
+        for (file in files) {
+            intention.javaClass.classLoader.getResource("$directory/$file")
+                ?: error("No inspection description for ${intention.javaClass}.\n" +
+                "Add ${files.joinToString()} to src/main/resources/$directory")
+        }
+    }
+
     protected fun doAvailableTest(@Language("Rust") before: String, @Language("Rust") after: String) {
-        checkDocumentation()
         InlineFile(before).withCaret()
         myFixture.launchAction(intention)
         myFixture.checkResult(replaceCaretMarker(after))
@@ -18,16 +27,6 @@ abstract class RsIntentionTestBase(val intention: IntentionAction) : RsTestBase(
         InlineFile(before).withCaret()
         check(intention.familyName !in myFixture.availableIntentions.mapNotNull { it.familyName }) {
             "\"$intention\" intention should not be available"
-        }
-    }
-
-    private fun checkDocumentation() {
-        val directory = "intentionDescriptions/${intention.javaClass.simpleName}"
-        val files = listOf("before.rs.template", "after.rs.template", "description.html")
-        for (file in files) {
-            intention.javaClass.classLoader.getResource("$directory/$file")
-                ?: error("No inspection description for ${intention.javaClass}.\n" +
-                "Add ${files.joinToString()} to src/main/resources/$directory")
         }
     }
 }
