1e04f1d6ed42754e6cfdca14401296a7eb541700
Aleksey Kladov <aleksey.kladov@gmail.com>
2017-01-30 14:36:28 +0300

    T: test intention description style

--- src/test/kotlin/org/rust/ide/intentions/RsIntentionTestBase.kt
+++ src/test/kotlin/org/rust/ide/intentions/RsIntentionTestBase.kt
@@ -1,7 +1,9 @@
 package org.rust.ide.intentions
 
 import com.intellij.codeInsight.intention.IntentionAction
+import com.intellij.openapi.util.io.StreamUtil
 import org.intellij.lang.annotations.Language
+import org.rust.ide.utils.loadCodeSampleResource
 import org.rust.lang.RsTestBase
 
 abstract class RsIntentionTestBase(val intention: IntentionAction) : RsTestBase() {
@@ -11,9 +13,14 @@ abstract class RsIntentionTestBase(val intention: IntentionAction) : RsTestBase(
         val directory = "intentionDescriptions/${intention.javaClass.simpleName}"
         val files = listOf("before.rs.template", "after.rs.template", "description.html")
         for (file in files) {
-            intention.javaClass.classLoader.getResource("$directory/$file")
+            val stream = intention.javaClass.classLoader.getResourceAsStream("$directory/$file")
                 ?: error("No inspection description for ${intention.javaClass}.\n" +
                 "Add ${files.joinToString()} to src/main/resources/$directory")
+
+            if (file.endsWith(".html")) {
+                val text = StreamUtil.readText(stream, Charsets.UTF_8)
+                checkHtmlStyle(text)
+            }
         }
     }
 
@@ -27,6 +34,19 @@ abstract class RsIntentionTestBase(val intention: IntentionAction) : RsTestBase(
         InlineFile(before).withCaret()
         check(intention.familyName !in myFixture.availableIntentions.mapNotNull { it.familyName }) {
             "\"$intention\" intention should not be available"
+        }
+    }
+
+    private fun checkHtmlStyle(html: String) {
+        // http://stackoverflow.com/a/1732454
+        val re = "<body>(.*)</body>".toRegex(RegexOption.DOT_MATCHES_ALL)
+        val body = re.find(html)!!.groups[1]!!.value.trim()
+        check(body[0].isUpperCase()) {
+            "Please start description with the capital latter"
+        }
+
+        check(body.last() == '.') {
+            "Please end description with a period"
         }
     }
 }
