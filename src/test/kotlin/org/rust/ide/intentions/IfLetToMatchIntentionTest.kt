51ad3d0d766026989fd48cc1c6e3625db371b359
Antoine Wacheux <awacheux.utc@gmail.com>
2017-01-30 09:47:11 +0100

    INT: Add IfLetToMatchIntention (#973)
    * INT: Add IfLetToMatchIntention
    
    * INT : Add IfLetToMatchIntentionTest

---
+++ src/test/kotlin/org/rust/ide/intentions/IfLetToMatchIntentionTest.kt
@@ -0,0 +1,103 @@
+package org.rust.ide.intentions
+
+class IfLetToMatchIntentionTest : RsIntentionTestBase(IfLetToMatchIntention()) {
+    fun testSimple() = doAvailableTest("""
+    fn main() {
+        if let Some(value) = x {/*caret*/
+
+        }
+    }
+    """, """
+    fn main() {
+        match x {
+            Some(value) => {}
+        }
+    }
+    """)
+
+    fun testSimpleElse() = doAvailableTest("""
+    fn main() {
+        if let Some(val) = x {
+
+        } else {
+            /*caret*/
+        }
+    }
+    """, """
+    fn main() {
+        match x {
+            Some(val) => {},
+            _ => {}
+        }
+    }
+    """)
+
+    fun testElseIf() = doAvailableTest("""
+    fn main() {
+        if let A(value) = x {
+
+        } else if let B(value) = x {
+            /*caret*/
+        }
+    }
+    """, """
+    fn main() {
+        match x {
+            A(value) => {},
+            B(value) => {}
+        }
+    }
+    """)
+
+    fun testElseIfElse() = doAvailableTest("""
+    fn main() {
+        if let A(value) = x {
+            /*caret*/
+        } else if let B(value) = x {
+
+        } else {
+
+        }
+    }
+    """, """
+    fn main() {
+        match x {
+            A(value) => {},
+            B(value) => {},
+            _ => {}
+        }
+    }
+    """
+    )
+
+    fun testTrackbackIf() = doAvailableTest("""
+    fn main() {
+        if let A(value) = x {
+
+        } else if let B(value) = x {
+            /*caret*/
+        } else {
+
+        }
+    }
+    """, """
+    fn main() {
+        match x {
+            A(value) => {},
+            B(value) => {},
+            _ => {}
+        }
+    }
+    """
+    )
+
+    fun testApplyOnSametarget() = doUnavailableTest("""
+    fn main() {
+        if let A(value) = x {
+
+        } else if let B(value) = y {
+            /*caret*/
+        }
+    }
+    """)
+}
