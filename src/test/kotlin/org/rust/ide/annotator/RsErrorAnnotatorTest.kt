0cde90fa88b69d79cb643134fa9f6ca10bed308a
Andrew Lygin <alygin@gmail.com>
2017-01-31 15:53:14 +0300

    (ANNOT): Detect E0061 for tuple-like structs

--- src/test/kotlin/org/rust/ide/annotator/RsErrorAnnotatorTest.kt
+++ src/test/kotlin/org/rust/ide/annotator/RsErrorAnnotatorTest.kt
@@ -338,6 +338,18 @@ class RsErrorAnnotatorTest : RsAnnotatorTestBase() {
         }
     """)
 
+    fun testE0061_InvalidParametersNumberInTupleStructs() = checkErrors("""
+        struct Foo0();
+        struct Foo1(u8);
+        fn main() {
+            let _ = Foo0();
+            let _ = Foo1(1);
+
+            let _ = Foo0<error descr="This function takes 0 parameters but 1 parameter was supplied [E0061]">(4)</error>;
+            let _ = Foo1<error descr="This function takes 1 parameter but 2 parameters were supplied [E0061]">(10, false)</error>;
+        }
+    """)
+
     fun testE0061_RespectsCfgAttribute() = checkErrors("""
         struct Foo;
         impl Foo {
