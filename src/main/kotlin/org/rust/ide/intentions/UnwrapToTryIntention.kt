04a77a7dd19fb97163c188884eb6c7e25aa8996a
Вячеслав Моклев <slavam2605@bk.ru>
2017-02-10 12:47:32 +0300

    INT: Add UnwrapToTryIntention (#995)
    * INT: Add UnwrapToTryIntention
    
    * Review fixes

---
+++ src/main/kotlin/org/rust/ide/intentions/UnwrapToTryIntention.kt
@@ -0,0 +1,29 @@
+package org.rust.ide.intentions
+
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiElement
+import org.rust.lang.core.psi.RsMethodCallExpr
+import org.rust.lang.core.psi.RsPsiFactory
+import org.rust.lang.core.psi.util.parentOfType
+
+class UnwrapToTryIntention : RsElementBaseIntentionAction<RsMethodCallExpr>() {
+    override fun getText() = "Replace .unwrap() with try"
+    override fun getFamilyName() = text
+
+    override fun findApplicableContext(project: Project, editor: Editor, element: PsiElement): RsMethodCallExpr? {
+        val methodCall = element
+            .parentOfType<RsMethodCallExpr>(strict = false) ?: return null
+        if (methodCall.identifier.textMatches("unwrap") &&
+            methodCall.typeArgumentList == null &&
+            methodCall.valueArgumentList.exprList.isEmpty()) {
+            return methodCall
+        }
+        return null
+    }
+
+    override fun invoke(project: Project, editor: Editor, ctx: RsMethodCallExpr) {
+        val tryElement = RsPsiFactory(project).createTryExpression(ctx.expr)
+        ctx.replace(tryElement)
+    }
+}
---
+++ src/test/kotlin/org/rust/ide/intentions/UnwrapToTryIntentionTest.kt
@@ -0,0 +1,113 @@
+package org.rust.ide.intentions
+
+class UnwrapToTryIntentionTest : RsIntentionTestBase(UnwrapToTryIntention()) {
+    fun testAvailable1() = doAvailableTest("""
+        fn main() {
+            let a = a.unwrap/*caret*/();
+        }
+    """, """
+        fn main() {
+            let a = a?;
+        }
+    """)
+
+    fun testAvailable2() = doAvailableTest("""
+        fn main() {
+            let a = Ok(12).unwrap/*caret*/();
+        }
+    """, """
+        fn main() {
+            let a = Ok(12)?;
+        }
+    """)
+
+    fun testAvailable3() = doAvailableTest("""
+        fn main() {
+            let a = (a + b).unwrap/*caret*/();
+        }
+    """, """
+        fn main() {
+            let a = (a + b)?;
+        }
+    """)
+
+    fun testAvailable4() = doAvailableTest("""
+        fn main() {
+            let a = a + b.unwrap/*caret*/();
+        }
+    """, """
+        fn main() {
+            let a = a + b?;
+        }
+    """)
+
+    fun testAvailable5() = doAvailableTest("""
+        fn main() {
+            let a = a.unwrap/*caret*/().to_string();
+        }
+    """, """
+        fn main() {
+            let a = a?.to_string();
+        }
+    """)
+
+    fun testAvailable6() = doAvailableTest("""
+        fn main() {
+            let a = a.unwrap().unwrap/*caret*/().unwrap();
+        }
+    """, """
+        fn main() {
+            let a = a.unwrap()?.unwrap();
+        }
+    """)
+
+    fun testAvailable7() = doAvailableTest("""
+        fn main() {
+            let a = a.unwrap  /*caret*/  ();
+        }
+    """, """
+        fn main() {
+            let a = a?;
+        }
+    """)
+
+    fun testAvailable8() = doAvailableTest("""
+        fn main() {
+            let a = a.unwrap(b.unwrap(/*caret*/));
+        }
+    """, """
+        fn main() {
+            let a = a.unwrap(b?);
+        }
+    """)
+
+    fun testUnavailable1() = doUnavailableTest("""
+        fn main() {
+            let a = a.foo/*caret*/();
+        }
+    """)
+
+    fun testUnavailable2() = doUnavailableTest("""
+        fn main() {
+            let a = a.unwrap::<>/*caret*/();
+        }
+    """)
+
+    fun testUnavailable3() = doUnavailableTest("""
+        fn main() {
+            let a = a.unwrap::<i32>/*caret*/();
+        }
+    """)
+
+    fun testUnavailable4() = doUnavailableTest("""
+        fn main() {
+            let a = a.unwrap/*caret*/(12);
+        }
+    """)
+
+    fun testUnavailable5() = doUnavailableTest("""
+        fn main() {
+            let a = a.unwrap/*caret*/;
+        }
+    """)
+}
