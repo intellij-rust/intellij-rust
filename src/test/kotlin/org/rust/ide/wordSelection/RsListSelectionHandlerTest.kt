10f8ab466b7f6c95ad4b6e6e2e7e0965bb82194b
Andrew Lygin <alygin@gmail.com>
2017-02-13 15:37:22 +0400

    SEL: Select args and params lists without parentheses (#998)
    * SEL: Select args and params lists without parentheses
    
    * SEL: Move list limiter token sets to RsTokenType

---
+++ src/test/kotlin/org/rust/ide/wordSelection/RsListSelectionHandlerTest.kt
@@ -0,0 +1,48 @@
+package org.rust.ide.wordSelection
+
+class RsListSelectionHandlerTest : RsSelectionHandlerTestBase() {
+
+    fun `test function value parameters list`() = doTest("""
+        fn foo(a: u3<caret>2, b: bool) {}
+    """, """
+        fn foo(a: <selection>u3<caret>2</selection>, b: bool) {}
+    """, """
+        fn foo(<selection>a: u3<caret>2</selection>, b: bool) {}
+    """, """
+        fn foo(<selection>a: u3<caret>2, b: bool</selection>) {}
+    """, """
+        fn foo<selection>(a: u3<caret>2, b: bool)</selection> {}
+    """)
+
+    fun `test function type parameters list`() = doTest("""
+        fn add<'a, T<caret>: 'a>(a: &'a str, b: T) {}
+    """, """
+        fn add<'a, <selection>T<caret></selection>: 'a>(a: &'a str, b: T) {}
+    """, """
+        fn add<'a, <selection>T<caret>: 'a</selection>>(a: &'a str, b: T) {}
+    """, """
+        fn add<<selection>'a, T<caret>: 'a</selection>>(a: &'a str, b: T) {}
+    """, """
+        fn add<selection><'a, T<caret>: 'a></selection>(a: &'a str, b: T) {}
+    """)
+
+    fun `test function value arguments list`() = doTest("""
+        fn foo() { u32::pow(1<caret>2, 4); }
+    """, """
+        fn foo() { u32::pow(<selection>1<caret>2</selection>, 4); }
+    """, """
+        fn foo() { u32::pow(<selection>1<caret>2, 4</selection>); }
+    """, """
+        fn foo() { u32::pow<selection>(1<caret>2, 4)</selection>; }
+    """)
+
+    fun `test function type arguments list`() = doTest("""
+        fn foo() -> Result<u32, bo<caret>ol> { Ok(0) }
+    """, """
+        fn foo() -> Result<u32, <selection>bo<caret>ol</selection>> { Ok(0) }
+    """, """
+        fn foo() -> Result<<selection>u32, bo<caret>ol</selection>> { Ok(0) }
+    """, """
+        fn foo() -> Result<selection><u32, bo<caret>ol></selection> { Ok(0) }
+    """)
+}
