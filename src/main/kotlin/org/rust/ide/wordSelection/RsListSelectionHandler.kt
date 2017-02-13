fbb87fc6c86ebc33f281f966560de95973af461b
Andrew Lygin <alygin@gmail.com>
2017-02-13 14:20:32 +0300

    SEL: Move list limiter token sets to RsTokenType

--- src/main/kotlin/org/rust/ide/wordSelection/RsListSelectionHandler.kt
+++ src/main/kotlin/org/rust/ide/wordSelection/RsListSelectionHandler.kt
@@ -4,7 +4,6 @@ import com.intellij.codeInsight.editorActions.ExtendWordSelectionHandlerBase
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
-import com.intellij.psi.tree.TokenSet
 import org.rust.lang.core.psi.*
 
 class RsListSelectionHandler : ExtendWordSelectionHandlerBase() {
@@ -13,8 +12,8 @@ class RsListSelectionHandler : ExtendWordSelectionHandlerBase() {
 
     override fun select(e: PsiElement, editorText: CharSequence, cursorOffset: Int, editor: Editor): List<TextRange>? {
         val node = e.node!!
-        val startNode = node.findChildByType(TokenSet.create(RsElementTypes.LPAREN,RsElementTypes.LT)) ?: return null
-        val endNode = node.findChildByType(TokenSet.create(RsElementTypes.RPAREN,RsElementTypes.GT)) ?: return null
+        val startNode = node.findChildByType(RS_LIST_OPEN_SYMBOLS) ?: return null
+        val endNode = node.findChildByType(RS_LIST_CLOSE_SYMBOLS) ?: return null
         val range = TextRange(startNode.startOffset + 1, endNode.startOffset)
         return listOf(range)
     }
