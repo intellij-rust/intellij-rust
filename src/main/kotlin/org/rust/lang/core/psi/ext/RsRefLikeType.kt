89e17e171310298a1ede26399b2b822cf0174601
Peter Oberndorfer <kumbayo84@arcor.de>
2017-02-14 20:46:38 +0100

    STUB: Fix a copy/paste mistake in RsRefLikeType.isRef
    The code for RsRefLikeType.isRef accidentally checked isMut val
    instead of isRef val on the stub.

--- src/main/kotlin/org/rust/lang/core/psi/ext/RsRefLikeType.kt
+++ src/main/kotlin/org/rust/lang/core/psi/ext/RsRefLikeType.kt
@@ -3,4 +3,4 @@ package org.rust.lang.core.psi.ext
 import org.rust.lang.core.psi.RsRefLikeType
 
 val RsRefLikeType.isMut: Boolean get() = stub?.isMut ?: (mut != null)
-val RsRefLikeType.isRef: Boolean get() = stub?.isMut ?: (and != null)
+val RsRefLikeType.isRef: Boolean get() = stub?.isRef ?: (and != null)
