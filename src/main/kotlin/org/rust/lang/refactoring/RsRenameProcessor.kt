8771f3d08d6ffea886d41b867952c6aed4f15a4d
Andrew Lygin <alygin@gmail.com>
2017-02-24 23:11:05 +0300

    REN: Don’t fail on wrong id type while renaming

---
+++ src/main/kotlin/org/rust/lang/refactoring/RsRenameProcessor.kt
@@ -0,0 +1,26 @@
+package org.rust.lang.refactoring
+
+import com.intellij.psi.PsiElement
+import com.intellij.psi.search.SearchScope
+import com.intellij.refactoring.rename.RenamePsiElementProcessor
+import org.rust.lang.core.psi.RsLabel
+import org.rust.lang.core.psi.RsLabelDecl
+import org.rust.lang.core.psi.RsLifetime
+import org.rust.lang.core.psi.RsLifetimeDecl
+
+class RsRenameProcessor : RenamePsiElementProcessor() {
+
+    override fun canProcessElement(element: PsiElement): Boolean = true
+
+    override fun prepareRenaming(element: PsiElement, newName: String?, allRenames: MutableMap<PsiElement, String>, scope: SearchScope) {
+        if (newName == null) return
+        if (element is RsLifetime || element is RsLifetimeDecl || element is RsLabel || element is RsLabelDecl) {
+            allRenames.put(element, newName.ensureQuote())
+        } else {
+            allRenames.put(element, newName.trimStart('\''))
+        }
+    }
+
+    private fun String.ensureQuote(): String = if (startsWith('\'')) { this } else { "'$this" }
+
+}
