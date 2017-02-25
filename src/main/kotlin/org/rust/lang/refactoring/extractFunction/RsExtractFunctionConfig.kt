f28959f399e0858199dd6c0d53ed635d5f2c14d8
Jansen Jan <farodin91@sek-server.de>
2017-02-25 14:08:01 +0100

    TYPE: Implement extract method without support of parameter

---
+++ src/main/kotlin/org/rust/lang/refactoring/extractFunction/RsExtractFunctionConfig.kt
@@ -0,0 +1,59 @@
+package org.rust.lang.refactoring.extractFunction
+
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import org.rust.ide.utils.findStatementsInRange
+import org.rust.lang.core.psi.RsFunction
+import org.rust.lang.core.psi.RsImplItem
+import org.rust.lang.core.psi.ext.parentOfType
+import org.rust.lang.core.psi.ext.selfParameter
+
+class RsExtractFunctionConfig(val file: PsiFile, start: Int, end: Int) {
+    var implType = RsWrapperType.Function
+    var anchor: RsFunction? = null
+    var elements: List<PsiElement> = emptyList()
+    var name = ""
+    var visibilityLevelPublic = false
+
+    init {
+        init(start, end)
+    }
+
+    private fun init(start: Int, end: Int) {
+        elements = findStatementsInRange(file, start, end).asList()
+        if (elements.isEmpty()) return
+        val first = elements.first()
+        val last = elements.last()
+
+        // check element should be a part of one block
+        val parentOfFirst = first.parentOfType<RsFunction>() ?: return
+        anchor = last.parentOfType<RsFunction>() ?: return
+        if (parentOfFirst != anchor) {
+            return
+        }
+
+        // find wrapper parent type of selection: fun / method / trait impl method
+        val impl = parentOfFirst.parent as? RsImplItem?
+        if (impl != null) {
+            if (impl.traitRef != null) {
+                if (parentOfFirst.selfParameter == null) {
+                    implType = RsWrapperType.TraitFunction
+                } else {
+                    implType = RsWrapperType.TraitMethod
+                }
+            } else {
+                if (parentOfFirst.selfParameter == null) {
+                    implType = RsWrapperType.ImplFunction
+                } else {
+                    implType = RsWrapperType.ImplMethod
+                }
+            }
+        }
+
+        //TODO: Find possible input and output parameter
+    }
+
+    fun isMethod() =
+        implType == RsWrapperType.TraitMethod || implType == RsWrapperType.ImplMethod
+
+}
