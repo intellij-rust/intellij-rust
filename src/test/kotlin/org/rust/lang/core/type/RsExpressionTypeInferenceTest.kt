2945108ccbe21214d9f4ef13d5070edf3234db69
Aleksey Kladov <aleksey.kladov@gmail.com>
2017-02-27 18:28:50 +0300

    T: reformat

--- src/test/kotlin/org/rust/lang/core/type/RsExpressionTypeInferenceTest.kt
+++ src/test/kotlin/org/rust/lang/core/type/RsExpressionTypeInferenceTest.kt
@@ -365,52 +365,47 @@ class RsExpressionTypeInferenceTest : RsTypificationTestBase() {
         }
     }
 
-    fun `test const pointer`() {
-        testExpr("""
+    fun `test const pointer`() = testExpr("""
         fn main() {
             let y = 42;
             let x = &y as *const i32;
             x
           //^ *const i32
-        }""")
-    }
+        }
+    """)
 
-    fun `test cast`() {
-        testExpr("""
+    fun `test cast`() = testExpr("""
         fn main() {
             let y = 42;
             let x = &y as i64;
             x
           //^ i64
-        }""")
-    }
+        }
+    """)
 
-    fun `test pointer deref`() {
-        testExpr("""
+    fun `test pointer deref`() = testExpr("""
         fn main() {
             let x : *const i32;
             let y = *x; // it should be in unsafe
             y
           //^ i32
-        }""")
-    }
+        }
+    """)
 
-    fun `test slice type`() {
-        testExpr("""
+    fun `test slice type`() = testExpr("""
         fn main() {
             let x : [i32];
             x
           //^ [i32]
-        }""")
-    }
+        }
+    """)
 
-    fun `test array type`() {
-        testExpr("""
+    fun `test array type`() = testExpr("""
         fn main() {
             let x : [i32; 8];
             x
           //^ [i32; 8]
-        }""")
-    }
+        }
+    """)
 }
 
