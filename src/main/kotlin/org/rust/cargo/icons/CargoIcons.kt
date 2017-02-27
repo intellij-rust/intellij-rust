41bf65fdfa0e669e4299e1dac50ac4b9eb5fb1b2
Marek Kaput <marek.kaput@outlook.com>
2017-02-27 21:26:11 +0100

    more icons

--- src/main/kotlin/org/rust/cargo/icons/CargoIcons.kt
+++ src/main/kotlin/org/rust/cargo/icons/CargoIcons.kt
@@ -5,4 +5,5 @@ import com.intellij.openapi.util.IconLoader
 object CargoIcons {
     val ICON = IconLoader.getIcon("/icons/cargo.png")
     val LOCK_ICON = IconLoader.getIcon("/icons/cargo-lock.png")
+    val BUILD_RS_ICON = IconLoader.getIcon("/icons/build-rs.png")
 }
---
+++ src/main/kotlin/org/rust/ide/RsConstants.kt
@@ -0,0 +1,7 @@
+package org.rust.ide
+
+object RsConstants {
+    const val MOD_RS_FILE = "mod.rs"
+    const val MAIN_RS_FILE = "main.rs"
+    const val LIB_RS_FILE = "lib.rs"
+}
