ae3733d253acb515a3bfbc3dc169f98bc1c5aa99
Aleksey Kladov <aleksey.kladov@gmail.com>
2017-03-10 04:42:21 +0300

    T: fix test

--- src/test/kotlin/org/rust/ide/template/postfix/NotPostfixTemplateTest.kt
+++ src/test/kotlin/org/rust/ide/template/postfix/NotPostfixTemplateTest.kt
@@ -3,11 +3,11 @@ package org.rust.ide.template.postfix
 class NotPostfixTemplateTest : PostfixTemplateTest(NotPostfixTemplate()) {
     fun `test simple`() = doTest("""
         fn foo() {
-            assert!(self.nodes.is_empty().not/*caret*/);
+            assert!(nodes.is_empty().not/*caret*/);
         }
     """, """
         fn foo() {
-            assert!(!self.nodes.is_empty()/*caret*/);
+            assert!(!nodes.is_empty()/*caret*/);
         }
     """)
 }
