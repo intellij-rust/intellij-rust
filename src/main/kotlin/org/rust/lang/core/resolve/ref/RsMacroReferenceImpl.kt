2f93f844423ffe82b81c79ac6d0ed6c2d3be70d5
Aleksey Kladov <aleksey.kladov@gmail.com>
2017-03-16 15:49:25 +0300

    QuickDocumentaion works for macros
    closes #630

--- src/main/kotlin/org/rust/lang/core/resolve/ref/RsMacroReferenceImpl.kt
+++ src/main/kotlin/org/rust/lang/core/resolve/ref/RsMacroReferenceImpl.kt
@@ -5,17 +5,19 @@ import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.stubs.StubIndex
 import org.rust.lang.core.psi.RsMacroDefinition
 import org.rust.lang.core.psi.RsMacroInvocation
+import org.rust.lang.core.psi.RsMacroItem
+import org.rust.lang.core.psi.ext.parentOfType
 import org.rust.lang.core.stubs.index.RsMacroDefinitionIndex
 
 class RsMacroReferenceImpl(macroInvocation: RsMacroInvocation) : RsReferenceBase<RsMacroInvocation>(macroInvocation) {
-    override fun resolveInner(): List<RsMacroDefinition> =
+    override fun resolveInner(): List<RsMacroItem> =
         StubIndex.getElements(
             RsMacroDefinitionIndex.KEY,
             element.referenceName,
             element.project,
             GlobalSearchScope.allScope(element.project),
             RsMacroDefinition::class.java
-        ).toList()
+        ).mapNotNull{ it.parentOfType<RsMacroItem>() }.toList()
 
     override val RsMacroInvocation.referenceAnchor: PsiElement
         get() = referenceNameElement
