80f360c70cd468fd854e9bec39edab428aac8bb5
Peter Oberndorfer <kumbayo84@arcor.de>
2017-03-19 18:28:26 +0100

    TY: Support type parameter substitutions for *const and *mut pointer types

--- src/main/kotlin/org/rust/lang/core/types/types/RustPointerType.kt
+++ src/main/kotlin/org/rust/lang/core/types/types/RustPointerType.kt
@@ -4,4 +4,7 @@ import org.rust.lang.core.types.RustType
 
 data class RustPointerType(val referenced: RustType, val mutable: Boolean = false) : RustType {
     override fun toString() = "*${if (mutable) "mut" else "const"} $referenced"
+
+    override fun substitute(map: Map<RustTypeParameterType, RustType>): RustType =
+        RustPointerType(referenced.substitute(map), mutable)
 }
