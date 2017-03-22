28fe2210dc4b0be341b0f87023e0a380565fb9db
Aleksey Kladov <aleksey.kladov@gmail.com>
2017-03-22 17:43:57 +0300

    DBG: show console when building project for debug

--- debugger/src/main/kotlin/org/rust/debugger/runconfig/RsDebugRunner.kt
+++ debugger/src/main/kotlin/org/rust/debugger/runconfig/RsDebugRunner.kt
@@ -3,22 +3,21 @@ package org.rust.debugger.runconfig
 import com.google.gson.Gson
 import com.google.gson.JsonParser
 import com.google.gson.JsonSyntaxException
+import com.intellij.execution.RunContentExecutor
 import com.intellij.execution.RunProfileStarter
 import com.intellij.execution.configurations.GeneralCommandLine
 import com.intellij.execution.configurations.RunProfile
 import com.intellij.execution.configurations.RunProfileState
 import com.intellij.execution.configurations.RunnerSettings
 import com.intellij.execution.executors.DefaultDebugExecutor
-import com.intellij.execution.process.CapturingProcessHandler
-import com.intellij.execution.process.ProcessTerminatedListener
+import com.intellij.execution.process.*
 import com.intellij.execution.runners.AsyncGenericProgramRunner
 import com.intellij.execution.runners.ExecutionEnvironment
 import com.intellij.execution.ui.RunContentDescriptor
-import com.intellij.openapi.application.runWriteAction
-import com.intellij.openapi.fileEditor.FileDocumentManager
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.module.Module
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.Task
-import com.intellij.openapi.project.Project
 import com.intellij.xdebugger.XDebugProcess
 import com.intellij.xdebugger.XDebugSession
 import com.intellij.xdebugger.XDebuggerManager
@@ -29,6 +28,8 @@ import org.jetbrains.concurrency.Promise
 import org.rust.cargo.project.settings.toolchain
 import org.rust.cargo.runconfig.CargoRunState
 import org.rust.cargo.runconfig.command.CargoCommandConfiguration
+import org.rust.cargo.toolchain.Cargo
+import org.rust.cargo.toolchain.CargoCommandLine
 import org.rust.cargo.toolchain.impl.CargoMetadata
 import org.rust.cargo.util.cargoProjectRoot
 
@@ -52,20 +53,20 @@ class RsDebugRunner : AsyncGenericProgramRunner<RunnerSettings>() {
         val cargo = project.toolchain!!.cargo(config.configurationModule.module!!.cargoProjectRoot!!.path)
 
         val buildCommand = if (config.cargoCommandLine.command == "run") {
-            config.cargoCommandLine.copy(additionalArguments = listOf("--message-format=json"), command = "build")
+            config.cargoCommandLine.copy(command = "build")
         } else {
             check(config.cargoCommandLine.command == "test")
-            config.cargoCommandLine.copy(additionalArguments = listOf("--no-run", "--message-format=json")
-                + config.cargoCommandLine.additionalArguments)
+            config.cargoCommandLine.prependArgument("--no-run")
         }
 
-        val task = BuildCargoBinaryTask(project, cargo.generalCommand(buildCommand))
-        runWriteAction {
-            FileDocumentManager.getInstance().saveAllDocuments()
-        }
-
-        return task.queueAsync()
-            .then { binary -> RsRunProfileStarter(GeneralCommandLine(binary)) }
+        return buildProjectAndGetBinaryArtifactPath(config.configurationModule.module!!, buildCommand, cargo)
+            .then { result ->
+                when (result) {
+                    is CargoBuildResult.BuildFailed -> null
+                    is CargoBuildResult.WithoutBinary -> error("Can't find a binary to debug")
+                    is CargoBuildResult.Binary -> RsRunProfileStarter(GeneralCommandLine(result.path))
+                }
+            }
     }
 }
 
@@ -90,52 +91,72 @@ private class RsRunProfileStarter(val commandLine: GeneralCommandLine) : RunProf
 }
 
 
-private class BuildCargoBinaryTask(
-    project: Project,
-    val cmd: GeneralCommandLine
-) : Task.Backgroundable(project, "Building Cargo project") {
-    private val promise: AsyncPromise<String> = AsyncPromise()
-
-    override fun run(indicator: ProgressIndicator) {
-        indicator.isIndeterminate = true
-        val process = CapturingProcessHandler(cmd)
-
-        val output = process.runProcessWithProgressIndicator(indicator)
-        if (output.isCancelled && output.exitCode != 0) {
-            error("Failed to build crate: ${cmd.commandLineString}")
-        }
-
-        val parser = JsonParser()
-        val gson = Gson()
-        val binary = output.stdoutLines
-            .mapNotNull {
-                try {
-                    parser.parse(it)
-                } catch(e: JsonSyntaxException) {
-                    null
-                }
-            }
-            .mapNotNull { if (it.isJsonObject) it.asJsonObject else null }
-            .filter { it.getAsJsonPrimitive("reason").asString == "compiler-artifact" }
-            .filter { "bin" in gson.fromJson(it.getAsJsonObject("target"), CargoMetadata.Target::class.java).kind }
-            .flatMap { it.getAsJsonArray("filenames").map { it.asString } }
-            .singleOrNull()
-
-        if (binary != null) {
-            promise.setResult(binary)
-        } else {
-            promise.setError("Can't find a single binary to debug")
-        }
-    }
-
-    override fun onSuccess() {}
-
-    override fun onError(error: Exception) {
-        promise.setError(error)
-    }
-
-    fun queueAsync(): Promise<String> {
-        queue()
-        return promise
-    }
+private sealed class CargoBuildResult {
+    object BuildFailed : CargoBuildResult()
+    object WithoutBinary : CargoBuildResult()
+    class Binary(val path: String) : CargoBuildResult()
 }
+
+/**
+ * Runs `command` twice:
+ *   * once to show console with errors
+ *   * the second time to get json output
+ * See https://github.com/rust-lang/cargo/issues/3855
+ */
+private fun buildProjectAndGetBinaryArtifactPath(module: Module, command: CargoCommandLine, cargo: Cargo): Promise<CargoBuildResult> {
+    val result = AsyncPromise<CargoBuildResult>()
+
+    val processForUserOutput = ProcessOutput()
+    val processForUser = KillableColoredProcessHandler(cargo.generalCommand(command))
+    val processForJson = CapturingProcessHandler(cargo.generalCommand(command.prependArgument("--message-format=json")))
+
+    processForUser.addProcessListener(CapturingProcessAdapter(processForUserOutput))
+
+    ApplicationManager.getApplication().invokeLater {
+        RunContentExecutor(module.project, processForUser)
+            .withAfterCompletion {
+                if (processForUserOutput.exitCode != 0) {
+                    result.setResult(CargoBuildResult.BuildFailed)
+                    return@withAfterCompletion
+                }
+
+                object : Task.Backgroundable(module.project, "Building Cargo project") {
+                    override fun run(indicator: ProgressIndicator) {
+                        indicator.isIndeterminate = true
+                        val output = processForJson.runProcessWithProgressIndicator(indicator)
+                        if (output.isCancelled || output.exitCode != 0) {
+                            result.setResult(CargoBuildResult.BuildFailed)
+                            return
+                        }
+
+                        val parser = JsonParser()
+                        val gson = Gson()
+                        val binary = output.stdoutLines
+                            .mapNotNull {
+                                try {
+                                    parser.parse(it)
+                                } catch(e: JsonSyntaxException) {
+                                    null
+                                }
+                            }
+                            .mapNotNull { if (it.isJsonObject) it.asJsonObject else null }
+                            .filter { it.getAsJsonPrimitive("reason").asString == "compiler-artifact" }
+                            .filter { "bin" in gson.fromJson(it.getAsJsonObject("target"), CargoMetadata.Target::class.java).kind }
+                            .flatMap { it.getAsJsonArray("filenames").map { it.asString } }
+                            .singleOrNull()
+
+                        result.setResult(
+                            if (binary != null) CargoBuildResult.Binary(binary) else CargoBuildResult.WithoutBinary
+                        )
+
+                    }
+                }.queue()
+            }
+            .run()
+    }
+
+    return result
+}
+
+private fun CargoCommandLine.prependArgument(arg: String): CargoCommandLine =
+    copy(additionalArguments = listOf(arg) + additionalArguments)
