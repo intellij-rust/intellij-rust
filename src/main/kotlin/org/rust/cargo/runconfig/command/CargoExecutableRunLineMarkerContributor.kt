003e8f7f7bd44e1ad715bbdfb2ca163b66158e36
Aleksey Kladov <aleksey.kladov@gmail.com>
2017-03-27 17:08:03 +0300

    Fix compat with 2016.3.5

--- src/main/kotlin/org/rust/cargo/runconfig/command/CargoExecutableRunLineMarkerContributor.kt
+++ src/main/kotlin/org/rust/cargo/runconfig/command/CargoExecutableRunLineMarkerContributor.kt
@@ -3,6 +3,7 @@ package org.rust.cargo.runconfig.command
 import com.intellij.execution.lineMarker.ExecutorAction
 import com.intellij.execution.lineMarker.RunLineMarkerContributor
 import com.intellij.icons.AllIcons
+import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.psi.PsiElement
 import com.intellij.util.Function
 import org.rust.lang.core.psi.RsElementTypes.IDENTIFIER
@@ -15,7 +16,11 @@ class CargoExecutableRunLineMarkerContributor : RunLineMarkerContributor() {
         val fn = element.parent as? RsFunction ?: return null
         if (!CargoExecutableRunConfigurationProducer.isMainFunction(fn)) return null
 
-        val actions = ExecutorAction.getActions(0)
+        // BACKCOMPAT: 2016.3
+        // See https://youtrack.jetbrains.com/issue/KT-17090
+        @Suppress("UNCHECKED_CAST")
+        val actions: Array<AnAction> = ExecutorAction::class.java.getMethod("getActions", Integer.TYPE)
+            .invoke(null, 0) as Array<AnAction>
 
         return Info(
             AllIcons.RunConfigurations.TestState.Run,
