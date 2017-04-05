a9d68944373c9bf3dce6db1e70fe7a794e1c1dc1
Andrew Lygin <alygin@gmail.com>
2017-04-05 12:30:40 +0300

    FOLD: Fold one liner functions

---
+++ src/test/resources/org/rust/ide/folding/fixtures/one_liner_function.rs
@@ -0,0 +1,10 @@
+fn foo()<fold text=' { '> {
+    </fold>println!("Hello World");<fold text=' }'>
+}</fold>
+
+fn bar()<fold text=' { '>
+{
+
+    </fold>println!("Hello World");<fold text=' }'>
+
+}</fold>
