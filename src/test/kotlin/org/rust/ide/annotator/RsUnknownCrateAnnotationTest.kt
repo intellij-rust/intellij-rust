298098d289c40977b2e795d52a28c6cd4a377eb2
Adrien Navratil <adrien1975@live.fr>
2017-04-20 00:35:29 +0200

    Tests fix

--- src/test/kotlin/org/rust/ide/annotator/RsUnknownCrateAnnotationTest.kt
+++ src/test/kotlin/org/rust/ide/annotator/RsUnknownCrateAnnotationTest.kt
@@ -5,10 +5,6 @@ import com.intellij.testFramework.LightProjectDescriptor
 class RsUnknownCrateAnnotationTest : RsAnnotatorTestBase() {
     override fun getProjectDescriptor(): LightProjectDescriptor = WithStdlibAndDependencyRustProjectDescriptor
 
-    fun testValidCrate() = checkErrors("""
-        extern crate dep_lib_target;
-    """)
-
     fun testInvalidCrate() = checkErrors("""
         <error descr="Unknown crate 'litarvan' [E0463]">extern crate litarvan;</error>
     """)
