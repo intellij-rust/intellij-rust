c97e33a89a17410b589650d31ff23c5408643f6f
Adrien Navratil <adrien1975@live.fr>
2017-04-19 18:25:10 +0200

    Unknown crate inspection, and renforced unresolved reference inspection, now enabled by default but as warning

--- src/main/kotlin/org/rust/ide/inspections/RsUnresolvedReferenceInspection.kt
+++ src/main/kotlin/org/rust/ide/inspections/RsUnresolvedReferenceInspection.kt
@@ -2,7 +2,6 @@ package org.rust.ide.inspections
 
 import com.intellij.codeInspection.ProblemsHolder
 import org.rust.lang.core.psi.*
-import org.rust.lang.core.psi.ext.RsMod
 import org.rust.lang.core.types.isPrimitive
 import org.rust.lang.core.types.type
 
@@ -19,10 +18,16 @@ class RsUnresolvedReferenceInspection : RsLocalInspectionTool() {
 
                 if (isPrimitiveType || o.reference.resolve() != null) return
 
-                val parent = o.path ?: return
-                val parentRes = parent.reference.resolve()
-                if (parentRes is RsMod || parentRes is RsEnumItem) {
+                if (o.path != null) {
                     holder.registerProblem(o.navigationElement, "Unresolved reference")
+                }
+            }
+
+            override fun visitMethodCallExpr(o: RsMethodCallExpr) {
+                if (o.reference.resolve() == null &&
+                    !(o.firstChild is RsPathExpr && (o.firstChild as RsPathExpr).path.reference.resolve() is RsPatBinding) &&
+                    !(o.firstChild is RsMethodCallExpr && o.firstChild.reference!!.resolve() == null)) {
+                    holder.registerProblem(o.identifier.navigationElement, "Unresolved reference")
                 }
             }
         }
