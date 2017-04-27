43e9074a636751214319320cedaaf530707ed634
Aleksey Kladov <aleksey.kladov@gmail.com>
2017-04-27 19:05:53 +0300

    REF: convert tuple struct to block struct refactoring

---
+++ src/test/kotlin/org/rust/ide/refactoring/RsConvertToNamedFieldsActionTest.kt
@@ -0,0 +1,31 @@
+package org.rust.ide.refactoring
+
+import org.intellij.lang.annotations.Language
+import org.rust.lang.RsTestBase
+import org.rust.lang.refactoring.RsConvertToNamedFieldsAction
+
+class RsConvertToNamedFieldsActionTest : RsTestBase() {
+    fun `test works for structs`() = doTest("""
+        struct/*caret*/ S(i32, pub String);
+    """, """
+        struct S {
+            _0: i32,
+            pub _1: String
+        }
+    """)
+
+    fun `test works for an enum variant`() = doTest("""
+        enum E { S1(i32, /*caret*/String), S3 }
+    """, """
+        enum E { S1 {
+            _0: i32,
+            _1: String
+        }, S3 }
+    """)
+
+    private fun doTest(@Language("Rust") before: String, @Language("Rust") after: String) {
+        InlineFile(before).withCaret()
+        myFixture.testAction(RsConvertToNamedFieldsAction())
+        myFixture.checkResult(after)
+    }
+}
