ecaa8783d353d8442f1431d8934d2766cee86073
Arseniy Pendryak <a.pendryak@yandex.ru>
2017-04-29 01:45:10 +0800

    COMP: Suggest associated types

--- src/main/kotlin/org/rust/lang/core/resolve/NameResolution.kt
+++ src/main/kotlin/org/rust/lang/core/resolve/NameResolution.kt
@@ -186,6 +186,9 @@ fun processPathResolveVariants(path: RsPath, isCompletion: Boolean, processor: R
             val s = base.`super`
             if (s != null && processor("super", s)) return true
         }
+        if (base is RsTraitItem && qualifier.cself != null) {
+            if (processAll(base.typeAliasList, processor)) return true
+        }
         if (processItemOrEnumVariantDeclarations(base, ns, processor, isSuperChain(qualifier))) return true
         if (base is RsTypeBearingItemElement && parent !is RsUseItem) {
             if (processAssociatedFunctionsDeclarations(base.project, base.type, processor)) return true
