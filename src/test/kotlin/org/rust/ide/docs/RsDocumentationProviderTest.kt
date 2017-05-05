cb533751a938be7f28012352dc362450740aa7ba
Arseniy Pendryak <a.pendryak@yandex.ru>
2017-05-05 11:15:32 +0800

    T: create base method of quick docimentation tests

--- src/test/kotlin/org/rust/ide/docs/RsDocumentationProviderTest.kt
+++ src/test/kotlin/org/rust/ide/docs/RsDocumentationProviderTest.kt
@@ -3,10 +3,24 @@ package org.rust.ide.docs
 import com.intellij.codeInsight.documentation.DocumentationManager
 import com.intellij.openapi.util.io.FileUtil
 import com.intellij.psi.PsiElement
+import org.intellij.lang.annotations.Language
 import org.rust.lang.RsTestBase
 import java.io.File
 
 abstract class RsDocumentationProviderTest : RsTestBase() {
+
+    protected inline fun doTest(@Language("Rust") code: String,
+                                @Language("Html") expected: String,
+                                block: RsDocumentationProvider.(PsiElement, PsiElement?) -> String?) {
+        InlineFile(code)
+
+        val (originalElement, _, offset) = findElementWithDataAndOffsetInEditor<PsiElement>()
+        val element = DocumentationManager.getInstance(project)
+            .findTargetElement(myFixture.editor, offset, myFixture.file, originalElement)!!
+
+        val actual = RsDocumentationProvider().block(element, originalElement)?.trim()
+        assertSameLines(expected.trimIndent(), actual)
+    }
 
     protected fun compareByHtml(block: (PsiElement, PsiElement?) -> String?) {
         val expectedFile = File("$testDataPath/${fileName.replace(".rs", ".html")}")
