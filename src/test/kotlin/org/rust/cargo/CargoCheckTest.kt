0c931cfdc79c626286075e5224a62622be1a68dd
Vasily Kirichenko <vaskir@gmail.com>
2017-06-06 22:00:36 +0300

    add a cargo check test

--- src/test/kotlin/org/rust/cargo/CargoCheckTest.kt
+++ src/test/kotlin/org/rust/cargo/CargoCheckTest.kt
@@ -7,9 +7,15 @@ import org.rust.cargo.project.settings.toolchain
 class CargoCheckTest : RustWithToolchainTestBase() {
     override val dataPath = "src/test/resources/org/rust/cargo/check/fixtures"
 
-    fun `returns zero error code if project has no errors`() = withProject("hello") {
+    fun `test returns zero error code if project has no errors`() = withProject("hello") {
         val moduleDirectory = PathUtil.getParentPath(module.moduleFilePath)
         val result = module.project.toolchain!!.cargo(moduleDirectory).checkProject(testRootDisposable)
         assertThat(result.exitCode).isEqualTo(0)
+    }
+
+    fun `test returns non zero error code if project has errors`() = withProject("errors") {
+        val moduleDirectory = PathUtil.getParentPath(module.moduleFilePath)
+        val result = module.project.toolchain!!.cargo(moduleDirectory).checkProject(testRootDisposable)
+        assertThat(result.exitCode).isNotEqualTo(0)
     }
 }
