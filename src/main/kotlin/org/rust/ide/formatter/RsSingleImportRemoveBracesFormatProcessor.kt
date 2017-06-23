84be915914b5ab61424a4503df37c9528b701ea2
Mike Carlin <mikefcarlin@gmail.com>
2017-06-23 09:03:04 -0700

    (FMT) Remove redundant braces around single item imports

---
+++ src/main/kotlin/org/rust/ide/formatter/RsSingleImportRemoveBracesFormatProcessor.kt
@@ -0,0 +1,60 @@
+package org.rust.ide.formatter
+
+import com.intellij.lang.ASTNode
+import com.intellij.openapi.util.TextRange
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiRecursiveElementVisitor
+import com.intellij.psi.impl.source.codeStyle.PreFormatProcessor
+import org.rust.lang.core.psi.RsElementTypes
+import org.rust.lang.core.psi.RsUseGlob
+import org.rust.lang.core.psi.ext.elementType
+import org.rust.lang.core.psi.ext.getNextNonCommentSibling
+import org.rust.lang.core.psi.ext.getPrevNonCommentSibling
+
+/**
+ * Pre format processor ensuring that if an import statement only contains a single import from a crate that
+ * there are no curly braces surrounding it.
+ *
+ * For example the following would change like so:
+ *
+ * `use getopts::{optopt};` --> `use getopts::optopt;`
+ *
+ * While this wouldn't change at all:
+ *
+ * `use getopts::{optopt, optarg};`
+ *
+ */
+class RsSingleImportRemoveBracesFormatProcessor : PreFormatProcessor {
+    override fun process(element: ASTNode, range: TextRange): TextRange {
+
+        var numRemovedBraces = 0
+        element.psi.accept(object : PsiRecursiveElementVisitor() {
+            override fun visitElement(element: PsiElement) {
+                if (range.contains(element.textRange)) {
+                    super.visitElement(element)
+                }
+
+                if (element is RsUseGlob && removeBracesAroundSingleImport(element)) {
+                    numRemovedBraces += 2
+                }
+            }
+        })
+        return range.grown(-numRemovedBraces)
+    }
+
+    fun removeBracesAroundSingleImport(element: RsUseGlob): Boolean {
+        if (element.elementType != RsElementTypes.USE_GLOB) return false
+        if (element.children.size > 1) return false
+
+        val leftBrace = element.getPrevNonCommentSibling() ?: return false
+        val rightBrace = element.getNextNonCommentSibling() ?: return false
+        if (leftBrace.elementType == RsElementTypes.LBRACE &&
+            rightBrace.elementType == RsElementTypes.RBRACE) {
+            leftBrace.delete()
+            rightBrace.delete()
+            return true
+        }
+
+        return false
+    }
+}
---
+++ src/test/kotlin/org/rust/ide/formatter/RsFormatterImportBracesTest.kt
@@ -0,0 +1,21 @@
+package org.rust.ide.formatter
+
+class RsFormatterImportBracesTest : RsFormatterTestBase() {
+
+    fun testRemoveBracesIfSingleImport() = doTextTest("use getopts::{optopt};", "use getopts::optopt;")
+
+    fun testWontRemoveBracesIfMultiImport() = doTextTest("use getopts::{optopt, optarg};",
+        "use getopts::{optopt, optarg};")
+
+    fun testRemoveBracesWithMultipleImports() = doTextTest(
+        """
+        use getopts::{optopt};
+        use std::io::{self, Read, Write};
+        use std::Vec::{Vec};
+        """,
+        """
+        use getopts::optopt;
+        use std::io::{self, Read, Write};
+        use std::Vec::Vec;
+        """)
+}
