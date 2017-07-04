0a5655427ed57270a581bb9e6dc6f9ff5be53dca
Aleksey Kladov <aleksey.kladov@gmail.com>
2017-07-04 12:43:31 +0300

    Minor, better example

--- exampleProject/src/main.rs
+++ exampleProject/src/main.rs
@@ -1,12 +1,3 @@
-struct S1;
-pub struct S2;
-pub(crate) struct S3;
-pub(self) struct S4;
-mod a {
-    pub (super) struct S5;
-    mod b {
-        pub(in super::super) struct S6;
-    }
+fn main() {
+    println!("Hello, W0rld!");
 }
-
-fn main() {}
