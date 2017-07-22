172bc25befc2cec583b293a0b75024cb5501ff44
Arseniy Pendryak <a.pendryak@yandex.ru>
2017-07-23 01:09:25 +0300

    ANN: don't try to annotate non project files

--- src/test/kotlin/org/rustSlowTests/RsCargoCheckAnnotatorTest.kt
+++ src/test/kotlin/org/rustSlowTests/RsCargoCheckAnnotatorTest.kt
@@ -8,6 +8,7 @@ package org.rustSlowTests
 import com.intellij.lang.annotation.HighlightSeverity
 import org.rust.cargo.RustWithToolchainTestBase
 import org.rust.cargo.project.settings.rustSettings
+import org.rust.cargo.project.workspace.cargoWorkspace
 import org.rust.fileTree
 
 class RsCargoCheckAnnotatorTest : RustWithToolchainTestBase() {
@@ -57,6 +58,44 @@ class RsCargoCheckAnnotatorTest : RustWithToolchainTestBase() {
         }.create()
         myFixture.openFileInEditor(cargoProjectDirectory.findFileByRelativePath("src/main.rs")!!)
         val highlights = myFixture.doHighlighting(HighlightSeverity.WEAK_WARNING)
+        check(highlights.isEmpty(), {
+            "Did not expect any highlights, got:\n$highlights"
+        })
+    }
+
+    // https://github.com/intellij-rust/intellij-rust/issues/1497
+    fun `test don't try to highlight non project files`() {
+        fileTree {
+            toml("Cargo.toml", """
+                [package]
+                name = "hello"
+                version = "0.1.0"
+                authors = []
+
+                [dependencies]
+                rand = "0.3.15"
+            """)
+
+            dir("src") {
+                rust("lib.rs", """
+                    extern crate rand;
+
+                    fn foo() {
+                        let a: i32 = "
+                        ";
+                    }
+                """)
+            }
+        }.create()
+
+        val path = myModule.cargoWorkspace
+            ?.findPackage("rand")
+            ?.contentRoot
+            ?.findFileByRelativePath("src/lib.rs")
+            ?: error("Can't find 'src/lib.rs' in 'rand' library")
+        myFixture.openFileInEditor(path)
+
+        val highlights = myFixture.doHighlighting(HighlightSeverity.ERROR)
         check(highlights.isEmpty(), {
             "Did not expect any highlights, got:\n$highlights"
         })
