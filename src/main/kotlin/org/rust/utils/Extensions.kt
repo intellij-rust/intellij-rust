03c50b80dd111be5d06778f011b2ca8ccdecf586
vlad20012 <beskvlad@yandex.ru>
2017-07-27 17:24:19 +0300

    Iterate over psi children effectively
    PsiElement.children allocates an ArrayList and an array, so we should
    iterate over children manually

--- src/main/kotlin/org/rust/utils/Extensions.kt
+++ src/main/kotlin/org/rust/utils/Extensions.kt
@@ -7,6 +7,8 @@ package org.rust.utils
 
 import com.intellij.execution.configurations.GeneralCommandLine
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiElement
+import com.intellij.psi.impl.source.tree.CompositeElement
 import com.intellij.util.io.systemIndependentPath
 import java.nio.file.Path
 import java.nio.file.Paths
@@ -20,3 +22,17 @@ val Int.seconds: Int
 fun GeneralCommandLine(path: Path, vararg args: String) = GeneralCommandLine(path.systemIndependentPath, *args)
 fun GeneralCommandLine.withWorkDirectory(path: Path?) = withWorkDirectory(path?.systemIndependentPath)
 val VirtualFile.pathAsPath: Path get() = Paths.get(path)
+
+/**
+ * Iterates all children of the `PsiElement` and invokes `action` for each one.
+ */
+inline fun PsiElement.forEachChild(action: (PsiElement) -> Unit) {
+    var psiChild: PsiElement? = firstChild
+
+    while (psiChild != null) {
+        if (psiChild.node is CompositeElement) {
+            action(psiChild)
+        }
+        psiChild = psiChild.nextSibling
+    }
+}
