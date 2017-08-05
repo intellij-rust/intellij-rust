4f891ff96c3e2d5d9b747ee33a0bfdf2801cc0e3
Jansen Jan <farodin91@sek-server.de>
2017-08-05 12:05:49 +0200

    TYPE: Classify usage type

---
+++ src/main/kotlin/org/rust/ide/search/RsUsageTypeProvider.kt
@@ -0,0 +1,70 @@
+/*
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
+package org.rust.ide.search
+
+import com.intellij.psi.PsiElement
+import com.intellij.usages.UsageTarget
+import com.intellij.usages.impl.rules.UsageType
+import com.intellij.usages.impl.rules.UsageTypeProviderEx
+import org.rust.lang.core.psi.*
+
+object RsUsageTypeProvider : UsageTypeProviderEx {
+    override fun getUsageType(element: PsiElement?): UsageType? = getUsageType(element, UsageTarget.EMPTY_ARRAY)
+
+    override fun getUsageType(element: PsiElement?, targets: Array<out UsageTarget>): UsageType? {
+        if (element is RsUseGlob) {
+            return UsageType("use")
+        }
+        val parent = element?.goUp<RsPath>() ?: return null
+        if (parent is RsBaseType) {
+            val context = parent.goUp<RsBaseType>()
+            return when (context) {
+                is RsTypeReference -> {
+                    when (context.parent) {
+                        is RsImplItem -> UsageType("impl")
+                        else -> UsageType("type reference")
+                    }
+                }
+                else -> null
+            }
+        }
+        if (parent is RsPathExpr) {
+            val context = parent.goUp<RsPathExpr>()
+            return when (context) {
+                is RsDotExpr -> UsageType("dot expr")
+                is RsCallExpr -> UsageType("function call")
+                is RsValueArgumentList -> UsageType("argument")
+                is RsFormatMacroArg -> UsageType("macro argument")
+                is RsExpr -> UsageType("expr")
+                else -> null
+            }
+        }
+        return when (parent) {
+            is RsUseItem -> UsageType("use")
+            is RsStructLiteral -> UsageType("init struct")
+            is RsStructLiteralField -> UsageType("init field")
+            is RsTraitRef -> UsageType("trait ref")
+            is RsMethodCall -> UsageType("method call")
+            is RsMetaItem -> UsageType("meta item")
+            is RsFieldLookup -> UsageType("field")
+            else -> {
+                val context = parent.parent
+                when (context) {
+                    is RsModDeclItem -> UsageType("mod")
+                    else -> null
+                }
+            }
+        }
+    }
+
+    inline fun <reified T : PsiElement> PsiElement.goUp(): PsiElement {
+        var context = this
+        while (context is T) {
+            context = context.parent
+        }
+        return context
+    }
+}
--- src/test/kotlin/org/rust/ide/search/RsFindUsagesTest.kt
+++ src/test/kotlin/org/rust/ide/search/RsFindUsagesTest.kt
@@ -7,36 +7,174 @@ package org.rust.ide.search
 
 import org.assertj.core.api.Assertions.assertThat
 import org.intellij.lang.annotations.Language
+import org.rust.ide.actions.line
 import org.rust.lang.RsTestBase
 import org.rust.lang.core.psi.ext.RsNamedElement
 
 class RsFindUsagesTest : RsTestBase() {
-    fun testFindLocalVar() = doTest("""
+    fun `test variable usages`() = doTestByText("""
         fn foo(x: i32) -> i32 {
              //^
-            let y = x * 2;
-            let x = x * 3 + y;
+            let y = x * 2;// - expr
+            let x = x * 3 + y;// - expr
             x
         }
-    """, 2)
+    """)
 
-    fun testFunction() = doTest("""
+    fun `test function usages`() = doTestByText("""
          fn foo() {}
            //^
 
-         fn bar() { foo() }
+         fn bar() { foo() } // - function call
 
          mod a {
-             use super::foo;
+             use super::foo;// - use
 
-             fn baz() { foo() }
+             fn baz() { foo() } // - function call
          }
 
          mod b {
              fn foo() {}
              fn bar() { foo() }
          }
-    """, 3)
+    """)
+
+    fun `test mod usages`() = doTestByText("""
+        mod b {
+          //^
+            fn bar() { foo() }
+        }
+
+        mod a {
+            use super::b;// - use
+        }
+    """)
+
+    fun `test struct usages`() = doTestByText("""
+        struct B;
+             //^
+
+        impl B {// - impl
+            fn new() -> B {// - type reference
+                B// - null
+            }
+        }
+
+        fn test() {
+            let b = B::new();// - function call
+        }
+        mod a {
+            use super::B;// - use
+        }
+    """)
+
+    fun `test struct with fields usages`() = doTestByText("""
+        struct B {
+             //^
+            test: u32
+        }
+        trait A {}
+
+        impl B {// - impl
+            fn new() -> B {// - type reference
+                B {// - init struct
+                    test: 0
+                }
+            }
+        }
+        impl A for B {}// - impl
+
+        fn bar<T: A>() {}
+
+        fn test() {
+            let b = B::new();// - function call
+            bar::<B>();// - type reference
+        }
+        mod a {
+            use super::B;// - use
+        }
+    """)
+
+    fun `test trait usages`() = doTestByText("""
+        trait B {}
+            //^
+        struct A;
+        impl B for A {}// - trait ref
+
+        fn bar<T: B>() {}// - trait ref
+
+        mod a {
+            use super::B;// - use
+        }
+    """)
+
+    fun `test struct field usages`() = doTestByText("""
+        struct B {
+            test: u32
+            //^
+        }
+
+        impl B {
+            fn new() -> B {
+                B {
+                    test: 0// - init field
+                }
+            }
+        }
+
+        fn test() {
+            let mut b = B::new();
+            println!("{}", b.test);// - field
+            b.test = 10;// - field
+        }
+    """)
+
+    fun `test variable 2 usages`() = doTestByText("""
+        struct B;
+
+        impl B {
+            fn new() -> B {
+                B
+            }
+            fn foo(&self) {}
+        }
+        fn bar(b: B) {
+
+        }
+
+        fn test() {
+            let b = B::new();
+              //^
+            b.foo();// - dot expr
+            println!("{:?}", b);// - macro argument
+            bar(b);// - argument
+        }
+    """)
+
+    private fun doTestByText(@Language("Rust") code: String) {
+        InlineFile(code)
+        val source = findElementInEditor<RsNamedElement>()
+
+        val actual = markersActual(source)
+        val expected = markersFrom(code)
+        assertEquals(expected.joinToString(COMPARE_SEPARATOR), actual.joinToString(COMPARE_SEPARATOR))
+    }
+
+    private fun markersActual(source: RsNamedElement) =
+        myFixture.findUsages(source)
+            .filter { it.element != null }
+            .map { Pair(it.element?.line ?: -1, RsUsageTypeProvider.getUsageType(it.element).toString()) }
+
+    private fun markersFrom(text: String) =
+        text.split('\n')
+            .withIndex()
+            .filter { it.value.contains(MARKER) }
+            .map { Pair(it.index, it.value.substring(it.value.indexOf(MARKER) + MARKER.length).trim()) }
+
+    private companion object {
+        val MARKER = "// - "
+        val COMPARE_SEPARATOR = " | "
+    }
 
     private fun doTest(@Language("Rust") code: String, expectedUsages: Int) {
         InlineFile(code)
