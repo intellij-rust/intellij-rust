09f1d92f6c0add6c37ff7becbd56df3ec26f809b
Jansen Jan <farodin91@sek-server.de>
2017-08-12 18:36:49 +0200

    Short with ignore ref as level
    Fixes #1613

--- src/main/kotlin/org/rust/ide/presentation/Ty.kt
+++ src/main/kotlin/org/rust/ide/presentation/Ty.kt
@@ -42,7 +42,7 @@ private fun render(ty: Ty, level: Int): String {
 
         is TyTuple -> ty.types.map(r).joinToString(", ", "(", ")")
         is TyArray -> "[${r(ty.base)}; ${ty.size}]"
-        is TyReference -> "${if (ty.mutable) "&mut " else "&"}${r(ty.referenced)}"
+        is TyReference -> "${if (ty.mutable) "&mut " else "&"}${render(ty.referenced, level)}"
         is TyPointer -> "*${if (ty.mutable) "mut" else "const"} ${r(ty.referenced)}"
         is TyTraitObject -> ty.trait.name ?: anonymous
         is TyTypeParameter -> ty.name ?: anonymous
