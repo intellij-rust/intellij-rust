4084c314e753a05604d3f6edb162cfbdcf018ddc
Arseniy Pendryak <a.pendryak@yandex.ru>
2017-09-22 13:37:42 +0300

    T: test coercion while index expr type inference

--- src/test/kotlin/org/rust/lang/core/type/RsIndexTypeInferenceTest.kt
+++ src/test/kotlin/org/rust/lang/core/type/RsIndexTypeInferenceTest.kt
@@ -5,15 +5,11 @@
 
 package org.rust.lang.core.type
 
+import org.intellij.lang.annotations.Language
+
 class RsIndexTypeInferenceTest : RsTypificationTestBase() {
 
-    fun testIndex() = testExpr("""
-        #[lang = "index"]
-        pub trait Index<Idx: ?Sized> {
-            type Output: ?Sized;
-            fn index(&self, index: Idx) -> &Self::Output;
-        }
-
+    fun `test index`() = doTest("""
         struct S;
 
         impl Index<i32> for S {
@@ -30,13 +26,7 @@ class RsIndexTypeInferenceTest : RsTypificationTestBase() {
         }
     """)
 
-    fun testGenericOutput() = testExpr("""
-        #[lang = "index"]
-        pub trait Index<Idx: ?Sized> {
-            type Output: ?Sized;
-            fn index(&self, index: Idx) -> &Self::Output;
-        }
-
+    fun `test generic output`() = doTest("""
         struct S<T>(T);
 
         impl<T> Index<i32> for S<T> {
@@ -53,13 +43,7 @@ class RsIndexTypeInferenceTest : RsTypificationTestBase() {
         }
     """)
 
-    fun testGenericIndexAndOutput() = testExpr("""
-        #[lang = "index"]
-        pub trait Index<Idx: ?Sized> {
-            type Output: ?Sized;
-            fn index(&self, index: Idx) -> &Self::Output;
-        }
-
+    fun `test generic index and output`() = doTest("""
         struct Key<K>(K);
         struct S<K, V>(Key<K>, V);
 
@@ -77,13 +61,7 @@ class RsIndexTypeInferenceTest : RsTypificationTestBase() {
         }
     """)
 
-    fun testMultipleImplementation() = testExpr("""
-        #[lang = "index"]
-        pub trait Index<Idx: ?Sized> {
-            type Output: ?Sized;
-            fn index(&self, index: Idx) -> &Self::Output;
-        }
-
+    fun `test multiple implementation`() = doTest("""
         struct S;
 
         impl Index<f64> for S {
@@ -107,13 +85,7 @@ class RsIndexTypeInferenceTest : RsTypificationTestBase() {
         }
     """)
 
-    fun testImplicitUsize() = testExpr("""
-        #[lang = "index"]
-        pub trait Index<Idx: ?Sized> {
-            type Output: ?Sized;
-            fn index(&self, index: Idx) -> &Self::Output;
-        }
-
+    fun `test implicit usize`() = doTest("""
         struct Vec<T>;
 
         impl<T> Index<usize> for Vec<T> {
@@ -129,4 +101,60 @@ class RsIndexTypeInferenceTest : RsTypificationTestBase() {
           //^ i32
         }
     """)
+
+    fun `test dereference`() = doTest("""
+        struct Vec<T>;
+
+        impl<T> Index<usize> for Vec<T> {
+            type Output = T;
+            fn index(&self, index: usize) -> &T { unimplemented!() }
+        }
+
+        fn foo(v: Vec<u32>) -> u32 {
+            let p = &v;
+            let m = p[0];
+            m
+          //^ u32
+        }
+    """)
+
+    fun `test transitive coercion`() = doTest("""
+        #[lang = "deref"]
+        pub trait Deref {
+            type Target: ?Sized;
+            fn deref(&self) -> &Self::Target;
+        }
+
+        struct A;
+        struct B;
+
+        impl Index<usize> for A {
+            type Output = i32;
+            fn index(&self, index: usize) -> &Self::Output { unimplemented!() }
+        }
+
+        impl Deref for B {
+            type Target = A;
+            fn deref(&self) -> &Self::Target { unimplemented!() }
+        }
+
+        fn main() {
+            let b = B;
+            let r = &b;
+            let v = r[0];
+            v;
+          //^ i32
+        }
+    """)
+
+    private fun doTest(@Language("Rust") code: String) {
+        val indexLangItem = """
+            #[lang = "index"]
+            pub trait Index<Idx: ?Sized> {
+                type Output: ?Sized;
+                fn index(&self, index: Idx) -> &Self::Output;
+            }
+        """
+        testExpr("$indexLangItem\n\n$code")
+    }
 }
