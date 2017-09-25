3ac80e8e6775167b3223863b2f7508e6826668a4
Peter Oberndorfer <kumbayo84@arcor.de>
2017-09-25 21:34:03 +0200

    TY: Coerce references to pointers
    In places that expect a pointer it is possible to pass a reference
    due to coercion.
    Coercion can also remove mutability.
    See https://doc.rust-lang.org/nomicon/coercions.html
    
    &T     -> *const T
    &mut T -> *mut T
    &mut T -> *const T
    
    The test in RsErrorAnnotatorTest.rs is not affected by this
    because in TypeInference.coerceResolved() we currently ignore the error
    where references are involved.
    
    CC: #1753

--- src/test/kotlin/org/rust/lang/core/type/RsNumericLiteralTypeInferenceTest.kt
+++ src/test/kotlin/org/rust/lang/core/type/RsNumericLiteralTypeInferenceTest.kt
@@ -174,13 +174,11 @@ class RsNumericLiteralTypeInferenceTest : RsTypificationTestBase() {
         }                   //^ u8
     """)
 
-    fun `test infer coerce reference to ptr`() = expect<IllegalStateException> {
-        testExpr("""
+    fun `test infer coerce reference to ptr`() = testExpr("""
         fn main() {
             let a: *const u8 = &0;
         }                     //^ u8
     """)
-    }
 
     fun `test infer unary minus rvalue from lvalue`() = testExpr("""
         fn main() {
