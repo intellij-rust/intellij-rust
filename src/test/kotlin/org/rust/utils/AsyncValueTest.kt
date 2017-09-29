3f4e35d33e967f038c91f36dae7f3caa5d176c98
Aleksey Kladov <aleksey.kladov@gmail.com>
2017-09-29 14:42:33 +0300

    T: add a test for AsyncValue

---
+++ src/test/kotlin/org/rust/utils/AsyncValueTest.kt
@@ -0,0 +1,44 @@
+/*
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
+package org.rust.utils
+
+import junit.framework.TestCase
+import org.jetbrains.concurrency.AsyncPromise
+import java.util.concurrent.ForkJoinPool
+import java.util.concurrent.RecursiveTask
+import java.util.concurrent.TimeUnit
+
+
+class AsyncValueTest : TestCase() {
+    fun `test async value is thread-safe`() = repeat(32) {
+        val value = AsyncValue(0)
+        val pool = ForkJoinPool(16)
+        val n = 16
+        val m = 16
+        for (i in 0 until n) {
+            pool.execute {
+                for (j in 0 until m) {
+                    value.updateAsync { v ->
+                        val p = AsyncPromise<Int>()
+                        object : RecursiveTask<Unit>() {
+                            override fun compute() {
+                                p.setResult(v + 1)
+                            }
+                        }.fork()
+                        p
+                    }
+                }
+            }
+        }
+        pool.shutdown()
+        pool.awaitTermination(1, TimeUnit.MINUTES)
+        val result = value.updateSync { v -> v }.blockingGet(1, TimeUnit.MINUTES)
+        check(value.currentState == n * m)
+        check(result == n * m) {
+            "Impossible: expected ${n * m}, got $result"
+        }
+    }
+}
