a54c27cd81a9fa916dab1ecd3b008a5acc1e60b3
vlad20012 <beskvlad@yandex.ru>
2017-10-09 15:37:57 +0300

    ANN: typecheck is now a disabled by default inspection

---
+++ src/main/kotlin/org/rust/ide/inspections/RsExperimentalChecksInspection.kt
@@ -0,0 +1,22 @@
+/*
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
+package org.rust.ide.inspections
+
+import com.intellij.codeInspection.ProblemsHolder
+import org.rust.lang.core.psi.RsFunction
+import org.rust.lang.core.psi.RsVisitor
+import org.rust.lang.core.types.addToHolder
+import org.rust.lang.core.types.inference
+
+class RsExperimentalChecksInspection : RsLocalInspectionTool() {
+    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean) = object : RsVisitor() {
+        override fun visitFunction(fn: RsFunction) {
+            for (it in fn.inference.diagnostics) {
+                if (it.experimental) it.addToHolder(holder)
+            }
+        }
+    }
+}
