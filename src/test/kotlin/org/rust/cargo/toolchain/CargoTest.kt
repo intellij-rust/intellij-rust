0f7f27b4182620fe0536e9552b59806a10b8dec4
Aleksey Kladov <aleksey.kladov@gmail.com>
2017-10-09 15:03:31 +0300

    T: fix tests for windows

--- src/test/kotlin/org/rust/cargo/toolchain/CargoTest.kt
+++ src/test/kotlin/org/rust/cargo/toolchain/CargoTest.kt
@@ -13,12 +13,13 @@ import java.nio.file.Paths
 
 class CargoTest : RsTestBase() {
     fun `test basic command`() = checkCommandLine(
-        cargo.toGeneralCommandLine(CargoCommandLine("test", listOf("--all"))),
-        """
+        cargo.toGeneralCommandLine(CargoCommandLine("test", listOf("--all"))), """
         cmd: /usr/bin/cargo test --all -- --nocapture
-        env: TERM=ansi, RUST_BACKTRACE=full, RUSTC=/usr/bin/rustc
-        """
-    )
+        env: RUSTC=/usr/bin/rustc, RUST_BACKTRACE=full, TERM=ansi
+        """, """
+        cmd: C:/usr/bin/cargo.exe test --all -- --nocapture
+        env: RUSTC=C:/usr/bin/rustc.exe, RUST_BACKTRACE=full, TERM=ansi
+    """)
 
     fun `test propagates proxy settings`() {
         val http = HttpConfigurable().apply {
@@ -31,36 +32,35 @@ class CargoTest : RsTestBase() {
         }
         val cargo = cargo.apply { setHttp(http) }
         checkCommandLine(
-            cargo.toGeneralCommandLine(CargoCommandLine("check")),
-            """
+            cargo.toGeneralCommandLine(CargoCommandLine("check")), """
             cmd: /usr/bin/cargo check
-            env: TERM=ansi, http_proxy=http://user:pwd@host:3268/, RUST_BACKTRACE=full, RUSTC=/usr/bin/rustc
-            """
-        )
+            env: RUSTC=/usr/bin/rustc, RUST_BACKTRACE=full, TERM=ansi, http_proxy=http://user:pwd@host:3268/
+            """, """
+            cmd: C:/usr/bin/cargo.exe check
+            env: RUSTC=C:/usr/bin/rustc.exe, RUST_BACKTRACE=full, TERM=ansi, http_proxy=http://user:pwd@host:3268/
+        """)
     }
 
     fun `test adds colors for common commands`() = checkCommandLine(
-        cargo.toColoredCommandLine(CargoCommandLine("run", listOf("--release", "--", "foo"))),
-        """
+        cargo.toColoredCommandLine(CargoCommandLine("run", listOf("--release", "--", "foo"))), """
         cmd: /usr/bin/cargo run --color=always --release -- foo
-        env: TERM=ansi, RUST_BACKTRACE=full, RUSTC=/usr/bin/rustc
-        """,
-        """
-        cmd: /usr/bin/cargo run --release -- foo
-        env: TERM=ansi, RUST_BACKTRACE=full, RUSTC=/usr/bin/rustc
-        """
-    )
+        env: RUSTC=/usr/bin/rustc, RUST_BACKTRACE=full, TERM=ansi
+        """, """
+        cmd: C:/usr/bin/cargo.exe run --release -- foo
+        env: RUSTC=C:/usr/bin/rustc.exe, RUST_BACKTRACE=full, TERM=ansi
+    """)
 
     fun `test don't add color for unknown command`() = checkCommandLine(
-        cargo.toColoredCommandLine(CargoCommandLine("tree")),
-        """
+        cargo.toColoredCommandLine(CargoCommandLine("tree")), """
         cmd: /usr/bin/cargo tree
-        env: TERM=ansi, RUST_BACKTRACE=full, RUSTC=/usr/bin/rustc
-        """
-    )
+        env: RUSTC=/usr/bin/rustc, RUST_BACKTRACE=full, TERM=ansi
+        """, """
+        cmd: C:/usr/bin/cargo.exe tree
+        env: RUSTC=C:/usr/bin/rustc.exe, RUST_BACKTRACE=full, TERM=ansi
+    """)
 
-    private fun checkCommandLine(cmd: GeneralCommandLine, expected: String, expectedWin: String? = null) {
-        val cleaned = (if (expectedWin != null && SystemInfo.isWindows) expectedWin else expected).trimIndent()
+    private fun checkCommandLine(cmd: GeneralCommandLine, expected: String, expectedWin: String) {
+        val cleaned = (if (SystemInfo.isWindows) expectedWin else expected).trimIndent()
         val actual = cmd.debug().trim()
         check(cleaned == actual) {
             "Expected:\n$cleaned\nActual:\n$actual"
@@ -68,10 +68,11 @@ class CargoTest : RsTestBase() {
     }
 
     private fun GeneralCommandLine.debug(): String {
+        val env = environment.entries.sortedBy { it.key }
         return buildString {
             append("cmd: $commandLineString")
             append("\n")
-            append("env: ${environment.entries.joinToString { (key, value) -> "$key=$value" }}")
+            append("env: ${env.joinToString { (key, value) -> "$key=$value" }}")
         }.replace("\\", "/")
     }
 
