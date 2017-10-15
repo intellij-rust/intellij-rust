cecfb919c4d4c15a6508bf22d1cff22a8b1fed35
Brent <bgardner@squarelabs.net>
2017-10-15 17:29:35 -0600

    RES: 1587 (It is not possible to debug applications with command line parameters)
    Add parsing to pass correct arguments to cargo for run and debug.

--- debugger/src/main/kotlin/org/rust/debugger/runconfig/RsDebugRunner.kt
+++ debugger/src/main/kotlin/org/rust/debugger/runconfig/RsDebugRunner.kt
@@ -63,7 +63,7 @@ class RsDebugRunner : com.intellij.execution.runners.AsyncGenericProgramRunner<R
         val cargo = env.project.toolchain!!.cargo(cargoProjectDirectory)
 
         val buildCommand = if (cleaned.cmd.command == "run") {
-            cleaned.cmd.copy(command = "build")
+            cleaned.cmd.copy(command = "build", additionalArguments = cleaned.cmd.getBuildArguments())
         } else {
             check(cleaned.cmd.command == "test")
             cleaned.cmd.prependArgument("--no-run")
@@ -73,6 +73,7 @@ class RsDebugRunner : com.intellij.execution.runners.AsyncGenericProgramRunner<R
             .then { result ->
                 result?.path?.let {
                     val commandLine = GeneralCommandLine(it).withWorkDirectory(cargoProjectDirectory)
+                    commandLine.withParameters(cleaned.cmd.getRunArguments())
                     RsRunProfileStarter(commandLine)
                 }
             }
--- src/main/kotlin/org/rust/cargo/toolchain/CargoCommandLine.kt
+++ src/main/kotlin/org/rust/cargo/toolchain/CargoCommandLine.kt
@@ -6,6 +6,7 @@
 package org.rust.cargo.toolchain
 
 import com.intellij.execution.configuration.EnvironmentVariablesData
+import com.intellij.util.execution.ParametersListUtil
 import org.rust.cargo.project.model.CargoProject
 import java.nio.file.Path
 
@@ -33,5 +34,23 @@ data class CargoCommandLine(
             listOf("--manifest-path", project.manifest.toString()) + additionalArguments,
             backtraceMode, channel, workingDirectory, environmentVariables, nocapture
         )
+    }
+
+    /**
+     * Returns the list of arguments after the "--". If there is no "--" returns an empty list.
+     */
+    fun getRunArguments() : List<String> {
+        val idx = additionalArguments.indexOf("--")
+        val cnt = if(idx >= 0) idx else additionalArguments.size-1
+        return additionalArguments.takeLast(Math.max(0, additionalArguments.size - cnt - 1))
+    }
+
+    /**
+     * Returns the arguments before any "--" argument, intended for the "cargo build" command.
+     */
+    fun getBuildArguments() : List<String> {
+        val idx = additionalArguments.indexOf("--")
+        val cnt = if(idx >= 0) idx else additionalArguments.size
+        return additionalArguments.take(cnt)
     }
 }
