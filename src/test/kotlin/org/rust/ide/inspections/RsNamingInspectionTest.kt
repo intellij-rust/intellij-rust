5d21646a434fe6986e25c883c9fbe51581de217b
Peter Oberndorfer <kumbayo84@arcor.de>
2017-10-29 19:55:19 +0100

    INSP: Enable automatic fixing of macro naming conventions
    Renaming of macros works just fine so enable automatic fixing
    of their naming convention.

--- src/test/kotlin/org/rust/ide/inspections/RsNamingInspectionTest.kt
+++ src/test/kotlin/org/rust/ide/inspections/RsNamingInspectionTest.kt
@@ -302,6 +302,14 @@ abstract class RsNamingInspectionTest(inspection: RsNamingInspection) : RsInspec
             #[allow(non_snake_case)]
             macro_rules! MacroFoo { () => {}; }
         """)
+
+        fun testMacrosFix() = checkFixByText("Rename to `macro_foo`", """
+            macro_rules! <warning descr="Macro `MacroFoo` should have a snake case name such as `macro_foo`">Macro<caret>Foo</warning> { () => {}; }
+            MacroFoo!();
+        """, """
+            macro_rules! macro_foo { () => {}; }
+            macro_foo!();
+        """)
     }
 
     class RsMethodNamingInspectionTest: RsNamingInspectionTest(RsMethodNamingInspection()) {
