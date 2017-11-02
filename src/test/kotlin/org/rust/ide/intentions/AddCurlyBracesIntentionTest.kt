d9951c95871b94234f86b2051a8837d14b02cb2f
Aleksey Kladov <aleksey.kladov@gmail.com>
2017-11-02 17:40:54 +0300

    Rename tests

--- src/test/kotlin/org/rust/ide/intentions/AddCurlyBracesIntentionTest.kt
+++ src/test/kotlin/org/rust/ide/intentions/AddCurlyBracesIntentionTest.kt
@@ -7,32 +7,32 @@ package org.rust.ide.intentions
 
 class AddCurlyBracesIntentionTest : RsIntentionTestBase(AddCurlyBracesIntention()) {
 
-    fun testAddCurlyBracesSimple() = doAvailableTest(
+    fun `test add curly braces simple`() = doAvailableTest(
         "use std::m/*caret*/em;",
         "use std::{m/*caret*/em};"
     )
 
-    fun testAddCurlyBraceSuper() = doAvailableTest(
+    fun `test add curly brace super`() = doAvailableTest(
         "use super/*caret*/::qux;",
         "use super::{qux};"
     )
 
-    fun testAddCurlyBracesLonger() = doAvailableTest(
+    fun `test add curly braces longer`() = doAvailableTest(
         "use foo::bar::/*caret*/baz::qux;",
         "use foo::bar::/*caret*/baz::{qux};"
     )
 
-    fun testAddCurlyBracesAlias() = doAvailableTest(
+    fun `test add curly braces alias`() = doAvailableTest(
         "use std::mem as mem/*caret*/ory;",
         "use std::{mem as mem/*caret*/ory};"
     )
 
-    fun testAddCurlyBracesExtra() = doAvailableTest(
+    fun `test add curly braces extra`() = doAvailableTest(
         "#[macro_use] pub use /*comment*/ std::me/*caret*/m;",
         "#[macro_use] pub use /*comment*/ std::{me/*caret*/m};"
     )
 
-    fun testNotAvailableForStarImports() = doUnavailableTest("""
+    fun `test not available for star imports`() = doUnavailableTest("""
         use foo::*/*caret*/;
     """)
 }
