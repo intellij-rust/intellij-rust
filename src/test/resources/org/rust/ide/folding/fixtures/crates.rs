9778d307c0e3c36b98713335e0d929fdf2b1d244
Filippok <filippok@filippok.codes>
2017-12-05 02:18:53 +0000

    added tests for new foldings

---
+++ src/test/resources/org/rust/ide/folding/fixtures/crates.rs
@@ -0,0 +1,11 @@
+<fold text='/* crates */'>#[macro_use]
+extern crate lazy_static;
+extern crate bitflags;
+extern crate dbus_macros;
+#[cfg(not(test))]
+extern crate getopts;
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+extern crate rustc_serialize;
+extern crate json_macro;</fold>
---
+++ src/test/resources/org/rust/ide/folding/fixtures/mods.rs
@@ -0,0 +1,14 @@
+<fold text='/* mods */'>#[macro_use]
+mod macros;
+mod convert;
+mod callbacks;
+mod keys;
+mod lua;
+mod registry;
+mod commands;
+mod ipc;
+mod layout;
+mod render;
+mod wayland;
+mod modes;
+mod awesome;</fold>
