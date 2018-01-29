580366d267a7fee8d9eb30eb213fba14f0388e6d
Toshiaki Kameyama <kmymtsak@gmail.com>
2018-01-29 10:20:24 +0900

    INT/INSP: sort method in impl according to the order in the trait

--- src/main/kotlin/org/rust/ide/inspections/RsSortImplTraitMembersInspection.kt
+++ src/main/kotlin/org/rust/ide/inspections/RsSortImplTraitMembersInspection.kt
@@ -5,21 +5,54 @@
 
 package org.rust.ide.inspections
 
-import com.intellij.codeInspection.IntentionWrapper
+import com.intellij.codeInspection.LocalQuickFix
+import com.intellij.codeInspection.ProblemDescriptor
 import com.intellij.codeInspection.ProblemsHolder
-import org.rust.ide.intentions.SortImplTraitMembersIntention
-import org.rust.lang.core.psi.RsImplItem
-import org.rust.lang.core.psi.RsVisitor
+import com.intellij.openapi.project.Project
+import org.rust.lang.core.psi.*
+import org.rust.lang.core.psi.ext.RsItemElement
+import org.rust.lang.core.psi.ext.RsTraitOrImpl
+import org.rust.lang.core.psi.ext.elementType
+import org.rust.lang.core.psi.ext.resolveToTrait
 
 class RsSortImplTraitMembersInspection : RsLocalInspectionTool() {
     override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean) = object : RsVisitor() {
-        override fun visitImplItem(o: RsImplItem) {
-            if (SortImplTraitMembersIntention.isApplicableTo(o) == null) return
+        override fun visitImplItem(implItem: RsImplItem) {
+            val traitItems = targetItems(implItem.traitRef?.resolveToTrait).takeIf { it.isNotEmpty() } ?: return
+            val implItems = targetItems(implItem).takeIf { it.isNotEmpty() } ?: return
+            if (traitItems.size != implItems.size) return
+            if (traitItems.zip(implItems).all { it.first.isSameItem(it.second) }) return
             holder.registerProblem(
-                o,
+                implItem,
                 "Different impl member order from the trait",
-                IntentionWrapper(SortImplTraitMembersIntention(), o.containingFile)
+                SortImplTraitMembersFix(traitItems, implItems)
             )
         }
     }
+
+    private fun targetItems(item: RsTraitOrImpl?): List<RsItemElement> {
+        val children = item?.members?.children
+        return children?.mapNotNull {
+            if (it is RsFunction || it is RsConstant || it is RsTypeAlias) it as? RsItemElement else null
+        }?.takeIf { it.size == children.size } ?: emptyList()
+    }
+
+    private class SortImplTraitMembersFix(
+        private val traitItems: List<RsItemElement>,
+        private val implItems: List<RsItemElement>
+    ) : LocalQuickFix {
+        override fun getName() = "Apply same member order"
+
+        override fun getFamilyName() = name
+
+        override fun applyFix(project: Project, descriptor: ProblemDescriptor) {
+            val implItemMap = implItems.associate { it.name + it.elementType to it }
+            val sortedImplItems = traitItems.mapNotNull { implItemMap[it.name + it.elementType]?.copy() }
+            traitItems.zip(implItems).forEachIndexed { index, (traitItem, implItem) ->
+                if (!traitItem.isSameItem(implItem)) implItem.replace(sortedImplItems[index])
+            }
+        }
+    }
 }
+
+private fun RsItemElement.isSameItem(other: RsItemElement): Boolean = name == other.name && elementType == other.elementType
--- src/test/kotlin/org/rust/ide/inspections/RsSortImplTraitMembersInspectionTest.kt
+++ src/test/kotlin/org/rust/ide/inspections/RsSortImplTraitMembersInspectionTest.kt
@@ -7,7 +7,7 @@ package org.rust.ide.inspections
 
 class RsSortImplTraitMembersInspectionTest : RsInspectionsTestBase(RsSortImplTraitMembersInspection()) {
 
-    fun `test different order`() = checkByText("""
+    fun `test same order`() = checkFixIsUnavailable("Apply same member order", """
         struct Struct {
             i: i32
         }
@@ -24,7 +24,41 @@ class RsSortImplTraitMembersInspectionTest : RsInspectionsTestBase(RsSortImplTra
             fn test3(&self) -> i32;
         }
 
-        <weak_warning descr="Different impl member order from the trait">impl Trait for Struct {
+        <caret>impl Trait for Struct {
+            const ID1: i32 = 1;
+            const ID2: i32 = 2;
+            type T1 = T;
+            type T2 = T;
+            fn test1(&self) -> i32 {
+                self.i
+            }
+            fn test2(&self) -> i32 {
+                self.i * 2
+            }
+            fn test3(&self) -> i32 {
+                self.i * 3
+            }
+        }
+    """)
+
+    fun `test different order`() = checkFixByText("Apply same member order", """
+        struct Struct {
+            i: i32
+        }
+
+        struct T;
+
+        trait Trait {
+            const ID1: i32;
+            const ID2: i32;
+            type T1;
+            type T2;
+            fn test1(&self) -> i32;
+            fn test2(&self) -> i32;
+            fn test3(&self) -> i32;
+        }
+
+        <weak_warning descr="Different impl member order from the trait"><caret>impl Trait for Struct {
             type T2 = T;
             const ID2: i32 = 2;
             fn test3(&self) -> i32 {
@@ -39,9 +73,7 @@ class RsSortImplTraitMembersInspectionTest : RsInspectionsTestBase(RsSortImplTra
             type T1 = T;
             const ID1: i32 = 1;
         }</weak_warning>
-    """)
-
-    fun `test same order`() = checkByText("""
+    """, """
         struct Struct {
             i: i32
         }
@@ -58,7 +90,7 @@ class RsSortImplTraitMembersInspectionTest : RsInspectionsTestBase(RsSortImplTra
             fn test3(&self) -> i32;
         }
 
-        impl Trait for Struct {
+        <caret>impl Trait for Struct {
             const ID1: i32 = 1;
             const ID2: i32 = 2;
             type T1 = T;
@@ -75,5 +107,28 @@ class RsSortImplTraitMembersInspectionTest : RsInspectionsTestBase(RsSortImplTra
         }
     """)
 
-}
 
+    fun `test different order with same name`() = checkFixByText("Apply same member order", """
+        trait Foo {
+            type bar;
+            fn bar();
+        }
+
+        <weak_warning descr="Different impl member order from the trait"><caret>impl Foo for () {
+            fn bar() {
+            }
+            type bar = ();
+        }</weak_warning>
+    """, """
+        trait Foo {
+            type bar;
+            fn bar();
+        }
+
+        impl Foo for () {
+            type bar = ();
+            fn bar() {
+            }
+        }
+    """)
+}
