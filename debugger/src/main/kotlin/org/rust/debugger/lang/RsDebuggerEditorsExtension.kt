0ee74fefb740c9fadf3c2e3b5ed02bbbe9b89d2e
Vyacheslav Karpukhin <vyacheslav@karpukhin.com>
2018-02-26 17:11:19 +0100

    Adapting to new debugger API

---
+++ debugger/src/main/kotlin/org/rust/debugger/lang/RsDebuggerEditorsExtension.kt
@@ -0,0 +1,13 @@
+/*
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
+package org.rust.debugger.lang
+
+import com.jetbrains.cidr.execution.debugger.CidrDebuggerEditorsExtensionBase
+import org.rust.lang.RsLanguage
+
+class RsDebuggerEditorsExtension : CidrDebuggerEditorsExtensionBase() {
+    override fun getSupportedLanguage() = RsLanguage
+}
