4cc66ea45b664bb86f61f2bed288274e6a05262c
oleg-semenov <olegsa@gmail.com>
2018-03-26 02:05:44 +0300

    ANN: add type mismatch quick fix using TryFrom
    This commit adds quick-fix for the types mismatch when
    `impl TryFrom<A> for B` exists where A is actual type and B is expected
    type, which fulfills part of the forth bullet-point of the issue:
    https://github.com/intellij-rust/intellij-rust/issues/1730.
    
    The Fix skips suggesting the "TryFrom" when it's implemented through
    `impl<T, U> TryFrom<U> for T where T: From<U> {...}`,
    since the regular "From" should is already suggested in those cases.
    
    (cherry picked from commit 3880d7d)

---
+++ src/test/kotlin/org/rust/ide/annotator/fixes/ConvertToTyUsingTryFromTraitFixTest.kt
@@ -0,0 +1,226 @@
+/*
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
+package org.rust.ide.annotator.fixes
+
+import org.rust.ide.inspections.RsExperimentalChecksInspection
+import org.rust.ide.inspections.RsInspectionsTestBase
+
+class ConvertToTyUsingTryFromTraitFixTest : RsInspectionsTestBase(RsExperimentalChecksInspection()) {
+    override fun getProjectDescriptor() = WithStdlibRustProjectDescriptor
+
+    fun `test B from A when impl TryFrom A for B is available`() = checkFixByText("Convert to Bb using `TryFrom` trait", """
+        #![feature(try_from)]
+        use std::convert::TryFrom;
+
+        struct Aa;
+        struct Bb;
+        #[derive(Debug)] struct Ee;
+
+        impl TryFrom<Aa> for Bb { type Error = Ee; fn try_from(a: Aa) -> Result<Self, Self::Error> {Ok(Bb)} }
+
+        fn main () {
+            let b: Bb = <error>Aa<caret></error>;
+        }
+    """, """
+        #![feature(try_from)]
+        use std::convert::TryFrom;
+
+        struct Aa;
+        struct Bb;
+        #[derive(Debug)] struct Ee;
+
+        impl TryFrom<Aa> for Bb { type Error = Ee; fn try_from(a: Aa) -> Result<Self, Self::Error> {Ok(Bb)} }
+
+        fn main () {
+            let b: Bb = Bb::try_from(Aa).unwrap();
+        }
+    """)
+
+    fun `test B from A when impl TryFrom A for B is available and fn ret Result with Err match`() = checkFixByText("Convert to Bb using `TryFrom` trait", """
+        #![feature(try_from)]
+        use std::convert::TryFrom;
+
+        struct Aa;
+        struct Bb;
+        #[derive(Debug)] struct Ee;
+
+        impl TryFrom<Aa> for Bb { type Error = Ee; fn try_from(a: Aa) -> Result<Self, Self::Error> {Ok(Bb)} }
+
+        fn foo() -> Result<B, Ee> {
+            let b: Bb = <error>Aa<caret></error>;
+            return Ok(b);
+        }
+    """, """
+        #![feature(try_from)]
+        use std::convert::TryFrom;
+
+        struct Aa;
+        struct Bb;
+        #[derive(Debug)] struct Ee;
+
+        impl TryFrom<Aa> for Bb { type Error = Ee; fn try_from(a: Aa) -> Result<Self, Self::Error> {Ok(Bb)} }
+
+        fn foo() -> Result<B, Ee> {
+            let b: Bb = Bb::try_from(Aa)?;
+            return Ok(b);
+        }
+    """)
+
+    fun `test B from A when impl TryFrom A for B is available and fn ret Result with Err mismatch`() = checkFixByText("Convert to Bb using `TryFrom` trait", """
+        #![feature(try_from)]
+        use std::convert::TryFrom;
+
+        struct Aa;
+        struct Bb;
+        #[derive(Debug)] struct Ee;
+        #[derive(Debug)] struct Ee2;
+
+        impl TryFrom<Aa> for Bb { type Error = Ee; fn try_from(a: Aa) -> Result<Self, Self::Error> {Ok(Bb)} }
+
+        fn foo() -> Result<B, Ee2> {
+            let b: Bb = <error>Aa<caret></error>;
+            return Ok(b);
+        }
+    """, """
+        #![feature(try_from)]
+        use std::convert::TryFrom;
+
+        struct Aa;
+        struct Bb;
+        #[derive(Debug)] struct Ee;
+        #[derive(Debug)] struct Ee2;
+
+        impl TryFrom<Aa> for Bb { type Error = Ee; fn try_from(a: Aa) -> Result<Self, Self::Error> {Ok(Bb)} }
+
+        fn foo() -> Result<B, Ee2> {
+            let b: Bb = Bb::try_from(Aa).unwrap();
+            return Ok(b);
+        }
+    """)
+
+    fun `test B from A when impl TryFrom A for B is available and fn ret Result with Err match through From trait`() = checkFixByText("Convert to Bb using `TryFrom` trait", """
+        #![feature(try_from)]
+        use std::convert::TryFrom;
+
+        struct Aa;
+        struct Bb;
+        #[derive(Debug)] struct Ee;
+        #[derive(Debug)] struct Ee2;
+
+        impl TryFrom<Aa> for Bb { type Error = Ee; fn try_from(a: Aa) -> Result<Self, Self::Error> {Ok(Bb)} }
+        impl From<Ee> for Ee2 { fn from(e: Ee) -> Self {Ee2} }
+
+        fn foo() -> Result<B, Ee2> {
+            let b: Bb = <error>Aa<caret></error>;
+            return Ok(b);
+        }
+    """, """
+        #![feature(try_from)]
+        use std::convert::TryFrom;
+
+        struct Aa;
+        struct Bb;
+        #[derive(Debug)] struct Ee;
+        #[derive(Debug)] struct Ee2;
+
+        impl TryFrom<Aa> for Bb { type Error = Ee; fn try_from(a: Aa) -> Result<Self, Self::Error> {Ok(Bb)} }
+        impl From<Ee> for Ee2 { fn from(e: Ee) -> Self {Ee2} }
+
+        fn foo() -> Result<B, Ee2> {
+            let b: Bb = Bb::try_from(Aa)?;
+            return Ok(b);
+        }
+    """)
+
+    fun `test B from A when impl TryFrom A for B is available and lambda ret Result with Err match`() = checkFixByText("Convert to Bb using `TryFrom` trait", """
+        #![feature(try_from)]
+        use std::convert::TryFrom;
+
+        struct Aa;
+        struct Bb;
+        #[derive(Debug)] struct Ee;
+
+        impl TryFrom<Aa> for Bb { type Error = Ee; fn try_from(a: Aa) -> Result<Self, Self::Error> {Ok(Bb)} }
+
+        fn main() {
+            let f = || -> Result<B, Ee> { let b: Bb = <error>Aa<caret></error>; return Ok(b);};
+        }
+    """, """
+        #![feature(try_from)]
+        use std::convert::TryFrom;
+
+        struct Aa;
+        struct Bb;
+        #[derive(Debug)] struct Ee;
+
+        impl TryFrom<Aa> for Bb { type Error = Ee; fn try_from(a: Aa) -> Result<Self, Self::Error> {Ok(Bb)} }
+
+        fn main() {
+            let f = || -> Result<B, Ee> { let b: Bb = Bb::try_from(Aa)?; return Ok(b);};
+        }
+    """)
+
+    fun `test Result of B from A when impl TryFrom A for B is available`() = checkFixByText("Convert to Bb using `TryFrom` trait", """
+        #![feature(try_from)]
+        use std::convert::TryFrom;
+
+        struct Aa;
+        struct Bb;
+        #[derive(Debug)] struct Ee;
+
+        impl TryFrom<Aa> for Bb { type Error = Ee; fn try_from(a: Aa) -> Result<Self, Self::Error> {Ok(Bb)} }
+
+        fn main () {
+            let b: Result<Bb, Ee> = <error>Aa<caret></error>;
+        }
+    """, """
+        #![feature(try_from)]
+        use std::convert::TryFrom;
+
+        struct Aa;
+        struct Bb;
+        #[derive(Debug)] struct Ee;
+
+        impl TryFrom<Aa> for Bb { type Error = Ee; fn try_from(a: Aa) -> Result<Self, Self::Error> {Ok(Bb)} }
+
+        fn main () {
+            let b: Result<Bb, Ee> = Bb::try_from(Aa);
+        }
+    """)
+
+    fun `test no fix when impl TryFrom A for B has wrong Err type`() = checkFixIsUnavailable("Convert to Bb using `TryFrom` trait", """
+        #![feature(try_from)]
+        use std::convert::TryFrom;
+
+        struct Aa;
+        struct Bb;
+        #[derive(Debug)] struct Ee;
+        #[derive(Debug)] struct Ee2;
+
+        impl TryFrom<Aa> for Bb { type Error = Ee; fn try_from(a: Aa) -> Result<Self, Self::Error> {Ok(Bb)} }
+
+        fn main () {
+            let b: Result<Bb, Ee2> = <error>Aa<caret></error>;
+        }
+    """)
+
+    fun `test no fix when impl TryFrom A for B is not available`() = checkFixIsUnavailable("Convert to Bb using `TryFrom` trait", """
+        #![feature(try_from)]
+        use std::convert::TryFrom;
+        struct Aa;
+        struct Bb;
+        struct Cc;
+        struct Dd;
+        #[derive(Debug)] struct Ee;
+
+        impl TryFrom<Cc> for Bb { type Error = Ee; fn try_from(c: Cc) -> Result<Self, Self::Error> {Ok(Bb)} }
+        impl TryFrom<Aa> for Dd { type Error = Ee; fn try_from(a: Aa) -> Result<Self, Self::Error> {Ok(Dd)} }
+
+        fn main () {
+            let b: Bb = <error>Aa<caret></error>;
+        }
+    """)
+}
