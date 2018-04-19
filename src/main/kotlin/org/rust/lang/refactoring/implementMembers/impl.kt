c5bca37b6faf6586ac4688da10f4e6760f9d1f5a
Jakub Wieczorek <jakub.adam@yandex.ru>
2018-04-19 07:52:46 +0300

    ACT: "Implement members" should honor the order of trait members in the definition
    (cherry picked from commit 0d90b91)

--- src/main/kotlin/org/rust/lang/refactoring/implementMembers/impl.kt
+++ src/main/kotlin/org/rust/lang/refactoring/implementMembers/impl.kt
@@ -9,13 +9,15 @@ import com.intellij.codeInsight.hint.HintManager
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.runWriteAction
 import com.intellij.openapi.editor.Editor
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiWhiteSpace
+import com.intellij.psi.tree.IElementType
+import com.intellij.psi.util.PsiTreeUtil
+import org.rust.lang.core.leftSiblings
 import org.rust.lang.core.psi.*
-import org.rust.lang.core.psi.ext.RsNamedElement
-import org.rust.lang.core.psi.ext.TraitImplementationInfo
-import org.rust.lang.core.psi.ext.resolveToBoundTrait
-import org.rust.lang.core.psi.ext.resolveToTrait
+import org.rust.lang.core.psi.ext.*
+import org.rust.lang.core.rightSiblings
 import org.rust.lang.core.types.BoundElement
-import org.rust.lang.core.types.ty.Substitution
 import org.rust.openapiext.checkReadAccessAllowed
 import org.rust.openapiext.checkWriteAccessAllowed
 
@@ -32,7 +34,7 @@ fun generateTraitMembers(impl: RsImplItem, editor: Editor?) {
     if (chosen.isEmpty()) return
     runWriteAction {
         // Non-null was checked by `findMembersToImplement`.
-        insertNewTraitMembers(chosen, impl.members!!, trait.subst)
+        insertNewTraitMembers(chosen, impl.members!!, trait)
     }
 }
 
@@ -45,20 +47,63 @@ private fun findMembersToImplement(impl: RsImplItem): Pair<TraitImplementationIn
     return implInfo to trait
 }
 
-private fun insertNewTraitMembers(selected: Collection<RsNamedElement>, members: RsMembers, subst: Substitution) {
+private fun insertNewTraitMembers(selected: Collection<RsAbstractable>, existingMembers: RsMembers, trait: BoundElement<RsTraitItem>) {
     checkWriteAccessAllowed()
     if (selected.isEmpty()) return
 
-    val templateImpl = RsPsiFactory(members.project).createMembers(
-        selected.filterIsInstance<RsFunction>(),
-        selected.filterIsInstance<RsTypeAlias>(),
-        selected.filterIsInstance<RsConstant>(),
-        subst
-    )
-    val lastMethodOrBrace = members.functionList.lastOrNull() ?: members.lbrace ?: return
-    members.addRangeAfter(
-        templateImpl.lbrace.nextSibling,
-        templateImpl.rbrace?.prevSibling,
-        lastMethodOrBrace
-    )
+    val templateImpl = RsPsiFactory(existingMembers.project).createMembers(selected, trait.subst)
+
+    val traitMembers = trait.element.members!!.childrenOfType<RsAbstractable>()
+    val newMembers = templateImpl.childrenOfType<RsAbstractable>()
+
+    // [1] First, check if the order of the existingMembers already implemented
+    // matches the order of existingMembers in the trait declaration.
+    val existingMembersOrder = existingMembers.childrenOfType<RsAbstractable>().map {
+        existingMember -> traitMembers.indexOfFirst { it.name == existingMember.name }
+    }
+    val areExistingMembersInTheRightOrder = existingMembersOrder == existingMembersOrder.sorted()
+
+    for ((index, newMember) in newMembers.withIndex()) {
+        val posInTrait = traitMembers.indexOfFirst { it.name == newMember.name }
+
+        // If [1] does not hold, the first new member we will append at the end of the implementation.
+        // All the other ones will consequently be inserted at the right position in relation to that very first one.
+        val anchor = if (!areExistingMembersInTheRightOrder && index == 0) {
+            existingMembers.childrenOfType<RsAbstractable>().lastOrNull()
+        } else {
+            existingMembers.childrenOfType<RsAbstractable>().findLast {
+                existingMember -> traitMembers.indexOfFirst { it.name == existingMember.name } < posInTrait
+            }
+        } ?: existingMembers.lbrace ?: return
+
+        // If the newly added item is a function, we add an extra line between it and each of its siblings.
+        val addedMember = existingMembers.addAfter(newMember, anchor)
+
+        val previousAbstractable = addedMember.leftSiblings.find { it is RsAbstractable }
+        if (previousAbstractable != null) {
+            if (previousAbstractable is RsFunction || addedMember is RsFunction) {
+                val whitespaces = createExtraWhitespacesAroundFunction(previousAbstractable, addedMember)
+                existingMembers.addBefore(whitespaces, addedMember)
+            }
+        }
+
+        val nextAbstractable = addedMember.rightSiblings.find { it is RsAbstractable }
+        if (nextAbstractable != null) {
+            if (nextAbstractable is RsFunction || addedMember is RsFunction) {
+                val whitespaces = createExtraWhitespacesAroundFunction(addedMember, nextAbstractable)
+                existingMembers.addAfter(whitespaces, addedMember)
+            }
+        }
+    }
+}
+
+private fun createExtraWhitespacesAroundFunction(left: PsiElement, right: PsiElement): PsiElement {
+    val lineCount = left
+        .rightSiblings
+        .takeWhile { it != right }
+        .filterIsInstance<PsiWhiteSpace>()
+        .map { it.text.count { c -> c == '\n' } }
+        .sum()
+    val extraLineCount = Math.max(0, 2 - lineCount)
+    return RsPsiFactory(left.project).createWhitespace("\n".repeat(extraLineCount))
 }
