f03841a6832351d5f664c1dab2e28686fd56ddd3
vlad20012 <beskvlad@gmail.com>
2018-05-02 03:36:02 +0300

    LIN: provided RegExp language injection into string literals
    (cherry picked from commit 13e6d74d58ef7c8950779de6719f732d5452d9ff)

--- src/main/kotlin/org/rust/lang/core/psi/ext/RsLitExpr.kt
+++ src/main/kotlin/org/rust/lang/core/psi/ext/RsLitExpr.kt
@@ -5,10 +5,23 @@
 
 package org.rust.lang.core.psi.ext
 
+import com.intellij.lang.ASTNode
+import com.intellij.psi.LiteralTextEscaper
+import com.intellij.psi.PsiLanguageInjectionHost
+import com.intellij.psi.impl.source.tree.LeafElement
+import com.intellij.psi.stubs.IStubElementType
+import org.intellij.lang.regexp.DefaultRegExpPropertiesProvider
+import org.intellij.lang.regexp.RegExpLanguageHost
+import org.intellij.lang.regexp.psi.RegExpChar
+import org.intellij.lang.regexp.psi.RegExpGroup
+import org.intellij.lang.regexp.psi.RegExpNamedGroupRef
+import org.rust.ide.injected.escaperForLiteral
 import org.rust.lang.core.psi.RsLitExpr
 import org.rust.lang.core.psi.RsLiteralKind
+import org.rust.lang.core.psi.impl.RsExprImpl
 import org.rust.lang.core.psi.kind
 import org.rust.lang.core.stubs.RsLitExprStub
+import org.rust.lang.core.stubs.RsPlaceholderStub
 import org.rust.lang.core.stubs.RsStubLiteralType
 import org.rust.lang.core.types.ty.TyFloat
 import org.rust.lang.core.types.ty.TyInteger
@@ -29,3 +42,46 @@ val RsLitExpr.stubType: RsStubLiteralType? get() {
 
 val RsLitExpr.integerLiteralValue: String? get() =
     (stub as? RsLitExprStub)?.integerLiteralValue ?: integerLiteral?.text
+
+abstract class RsLitExprMixin : RsExprImpl, RsLitExpr, RegExpLanguageHost {
+
+    constructor(node: ASTNode) : super(node)
+    constructor(stub: RsPlaceholderStub, nodeType: IStubElementType<*, *>) : super(stub, nodeType)
+
+    override fun isValidHost(): Boolean {
+        return true
+    }
+
+    override fun updateText(text: String): PsiLanguageInjectionHost {
+        val valueNode = node.firstChildNode
+        assert(valueNode is LeafElement)
+        (valueNode as LeafElement).replaceWithText(text)
+        return this
+    }
+
+    override fun createLiteralTextEscaper(): LiteralTextEscaper<RsLitExpr> =
+        escaperForLiteral(this)
+
+    override fun characterNeedsEscaping(c: Char): Boolean = false
+    override fun supportsPerl5EmbeddedComments(): Boolean = false
+    override fun supportsPossessiveQuantifiers(): Boolean = true
+    override fun supportsPythonConditionalRefs(): Boolean = false
+    override fun supportsNamedGroupSyntax(group: RegExpGroup): Boolean = true
+
+    override fun supportsNamedGroupRefSyntax(ref: RegExpNamedGroupRef): Boolean =
+        ref.isNamedGroupRef
+
+    override fun supportsExtendedHexCharacter(regExpChar: RegExpChar): Boolean = true
+
+    override fun isValidCategory(category: String): Boolean =
+        DefaultRegExpPropertiesProvider.getInstance().isValidCategory(category)
+
+    override fun getAllKnownProperties(): Array<Array<String>> =
+        DefaultRegExpPropertiesProvider.getInstance().allKnownProperties
+
+    override fun getPropertyDescription(name: String?): String? =
+        DefaultRegExpPropertiesProvider.getInstance().getPropertyDescription(name)
+
+    override fun getKnownCharacterClasses(): Array<Array<String>> =
+        DefaultRegExpPropertiesProvider.getInstance().knownCharacterClasses
+}
