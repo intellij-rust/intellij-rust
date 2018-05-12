d2c238196235960189fbf9293b3e448b3896896d
oleg-semenov <olegsa@gmail.com>
2018-05-12 19:16:30 +0300

    ANN: Only allow BororwMut and AsMut quick-fixes on mutable expressions
    This commit makes the quick-fixes that use BororwMut and AsMut traits
    to be applicable only for truly mutable expressions. This fixes part of
    the: https://github.com/intellij-rust/intellij-rust/issues/2472
    
    (cherry picked from commit 1bbcc775dc82cfc54ca2d61ab673f9c3312922d7)

--- src/test/kotlin/org/rust/ide/annotator/fixes/ConvertToBorrowedTyWithMutFixTest.kt
+++ src/test/kotlin/org/rust/ide/annotator/fixes/ConvertToBorrowedTyWithMutFixTest.kt
@@ -6,5 +6,34 @@
 package org.rust.ide.annotator.fixes
 
 
-internal class ConvertToBorrowedTyWithMutFixTest : ConvertToTyUsingTraitFixTestBase(
-    true, "BorrowMut", "borrow_mut", "use std::borrow::BorrowMut;")
+class ConvertToBorrowedTyWithMutFixTest : ConvertToTyUsingTraitFixTestBase(
+    true, "BorrowMut", "borrow_mut", "use std::borrow::BorrowMut;") {
+
+    fun `test &String to &mut String`() = checkFixIsUnavailable("Convert to &mut String using `BorrowMut` trait", """
+        $imports
+
+        fn main() {
+            let mut s = String::from("hello");
+
+            change(<error>&s<caret></error>);
+        }
+
+        fn change(some_string: &mut String) {
+            some_string.push_str(", world");
+        }
+    """)
+
+    fun `test String to &mut String`() = checkFixIsUnavailable("Convert to &mut String using `BorrowMut` trait", """
+        $imports
+
+        fn main() {
+            let s = String::from("hello");
+
+            change(<error>s<caret></error>);
+        }
+
+        fn change(some_string: &mut String) {
+            some_string.push_str(", world");
+        }
+    """)
+}
