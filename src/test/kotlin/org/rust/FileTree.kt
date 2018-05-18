c6732c956c4184a69cdeec1a158a3a4f93547756
Arseniy Pendryak <a.pendryak@yandex.ru>
2018-05-18 03:04:17 +0300

    T: don't use 'TreeUtil.bindStubsToTree' in stub tests.
    'TreeUtil.bindStubsToTree' method was removed in 2018.2
    so this change modifies stub tests to avoid compatibility issue while 2018.2 supporting

--- src/test/kotlin/org/rust/FileTree.kt
+++ src/test/kotlin/org/rust/FileTree.kt
@@ -25,7 +25,7 @@ fun fileTree(builder: FileTreeBuilder.() -> Unit): FileTree =
     FileTree(FileTreeBuilderImpl().apply { builder() }.intoDirectory())
 
 fun fileTreeFromText(@Language("Rust") text: String): FileTree {
-    val fileSeparator = """^\s* //- (\S+)\s*$""".toRegex(RegexOption.MULTILINE)
+    val fileSeparator = """^\s*//- (\S+)\s*$""".toRegex(RegexOption.MULTILINE)
     val fileNames = fileSeparator.findAll(text).map { it.groupValues[1] }.toList()
     val fileTexts = fileSeparator.split(text).filter(String::isNotBlank).map { it.trimIndent() }
 
