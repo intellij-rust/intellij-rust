757ca8954bce89c1ee8b8823dc3176fdd3c5aa7d
Arseniy Pendryak <a.pendryak@yandex.ru>
2018-05-21 12:56:41 +0300

    T: make tests of inspections with weak warnings check warning description

--- src/test/kotlin/org/rust/ide/inspections/RsFieldInitShorthandInspectionTest.kt
+++ src/test/kotlin/org/rust/ide/inspections/RsFieldInitShorthandInspectionTest.kt
@@ -7,19 +7,19 @@ package org.rust.ide.inspections
 
 class RsFieldInitShorthandInspectionTest : RsInspectionsTestBase(RsFieldInitShorthandInspection()) {
 
-    fun `test not applicable`() = checkByText("""
+    fun `test not applicable`() = checkFixIsUnavailable("Use initialization shorthand", """
         fn main() {
-            let _ = S { foo: bar, baz: &baz };
+            let _ = S { foo: bar/*caret*/, baz: &baz };
         }
-    """)
+    """, checkWeakWarn = true)
 
     fun `test fix`() = checkFixByText("Use initialization shorthand", """
         fn main() {
-            let _ = S { <weak_warning descr="Expression can be simplified">foo: foo<caret></weak_warning>, baz: quux };
+            let _ = S { <weak_warning descr="Expression can be simplified">foo: foo/*caret*/</weak_warning>, baz: quux };
         }
     """, """
         fn main() {
-            let _ = S { foo<caret>, baz: quux };
+            let _ = S { foo/*caret*/, baz: quux };
         }
-    """)
+    """, checkWeakWarn = true)
 }
