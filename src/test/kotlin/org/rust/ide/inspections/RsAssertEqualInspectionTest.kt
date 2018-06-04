9b87280fc262e7073661dd3a34fa65b258b0ac70
Arseniy Pendryak <a.pendryak@yandex.ru>
2018-06-04 21:47:43 +0300

    INSP: improve `RsAssertEqualInspection`:
    * minor refactoring
    * don't show warning if types don't implement `PartialEq` trait
    * add testmarks

--- src/test/kotlin/org/rust/ide/inspections/RsAssertEqualInspectionTest.kt
+++ src/test/kotlin/org/rust/ide/inspections/RsAssertEqualInspectionTest.kt
@@ -5,12 +5,12 @@
 
 package org.rust.ide.inspections
 
-class RsAssertEqualInspectionTest : RsInspectionsTestBase(RsAssertEqualInspection(), useStdLib = true) {
+class RsAssertEqualInspectionTest : RsInspectionsTestBase(RsAssertEqualInspection(), true) {
     fun `test simple assert_eq fix`() = checkFixByText("Convert to assert_eq!", """
         fn main() {
             let x = 10;
             let y = 10;
-            <weak_warning descr="assert!(a == b) can be assert_eq!(a, b)">assert!(x == y<caret>)</weak_warning>;
+            <weak_warning descr="assert!(a == b) can be assert_eq!(a, b)">assert!(x == y/*caret*/)</weak_warning>;
         }
     """, """
         fn main() {
@@ -26,7 +26,7 @@ class RsAssertEqualInspectionTest : RsInspectionsTestBase(RsAssertEqualInspectio
         }
 
         fn main() {
-            <weak_warning descr="assert!(a == b) can be assert_eq!(a, b)">assert!(answer() == 42<caret>)</weak_warning>;
+            <weak_warning descr="assert!(a == b) can be assert_eq!(a, b)">assert!(answer() == 42/*caret*/)</weak_warning>;
         }
     """, """
         fn answer() -> i32 {
@@ -42,7 +42,7 @@ class RsAssertEqualInspectionTest : RsInspectionsTestBase(RsAssertEqualInspectio
         fn main() {
             let x = 10;
             let y = 10;
-            <weak_warning descr="assert!(a == b) can be assert_eq!(a, b)">assert!(x == y, "format {}", 0<caret>)</weak_warning>;
+            <weak_warning descr="assert!(a == b) can be assert_eq!(a, b)">assert!(x == y, "format {}", 0/*caret*/)</weak_warning>;
         }
     """, """
         fn main() {
@@ -56,7 +56,7 @@ class RsAssertEqualInspectionTest : RsInspectionsTestBase(RsAssertEqualInspectio
         fn main() {
             let x = 10;
             let y = 42;
-            <weak_warning descr="assert!(a != b) can be assert_ne!(a, b)">assert!(x != y<caret>)</weak_warning>;
+            <weak_warning descr="assert!(a != b) can be assert_ne!(a, b)">assert!(x != y/*caret*/)</weak_warning>;
         }
     """, """
         fn main() {
@@ -68,15 +68,15 @@ class RsAssertEqualInspectionTest : RsInspectionsTestBase(RsAssertEqualInspectio
 
     fun `test expr assert_ne fix`() = checkFixByText("Convert to assert_ne!", """
         fn answer() -> i32 {
-            return 42
+            return 42;
         }
 
         fn main() {
-            <weak_warning descr="assert!(a != b) can be assert_ne!(a, b)">assert!(answer() != 50<caret>)</weak_warning>;
+            <weak_warning descr="assert!(a != b) can be assert_ne!(a, b)">assert!(answer() != 50/*caret*/)</weak_warning>;
         }
     """, """
         fn answer() -> i32 {
-            return 42
+            return 42;
         }
 
         fn main() {
@@ -88,7 +88,7 @@ class RsAssertEqualInspectionTest : RsInspectionsTestBase(RsAssertEqualInspectio
         fn main() {
             let x = 10;
             let y = 10;
-            <weak_warning descr="assert!(a != b) can be assert_ne!(a, b)">assert!(x != y, "format {}", 0<caret>)</weak_warning>;
+            <weak_warning descr="assert!(a != b) can be assert_ne!(a, b)">assert!(x != y, "format {}", 0/*caret*/)</weak_warning>;
         }
     """, """
         fn main() {
@@ -107,16 +107,27 @@ class RsAssertEqualInspectionTest : RsInspectionsTestBase(RsAssertEqualInspectio
             let y = Number(10);
             assert!(x == y/*caret*/);
         }
-    """)
+    """, checkWeakWarn = true, testmark = RsAssertEqualInspection.Testmarks.debugTraitIsNotImplemented)
 
-    fun `test fix available when arguments derive Debug`() = checkFixByText("Convert to assert_ne!", """
+    fun `test fix unavailable when arguments do not implement PartialEq`() = checkFixIsUnavailable("Convert to assert_eq!", """
+        #[derive(Debug)]
+        struct Number(u32);
+
+        fn main() {
+            let x = Number(10);
+            let y = Number(10);
+            assert!(x == y/*caret*/);
+        }
+    """, checkWeakWarn = true, testmark = RsAssertEqualInspection.Testmarks.partialEqTraitIsNotImplemented)
+
+    fun `test fix available when arguments derive PartialEq & Debug`() = checkFixByText("Convert to assert_ne!", """
         #[derive(Debug, PartialEq)]
         struct Number(u32);
 
         fn main() {
             let x = Number(10);
             let y = Number(10);
-            assert!(x != y/*caret*/);
+            <weak_warning descr="assert!(a != b) can be assert_ne!(a, b)">assert!(x != y/*caret*/)</weak_warning>;
         }
     """, """
         #[derive(Debug, PartialEq)]
@@ -127,5 +138,5 @@ class RsAssertEqualInspectionTest : RsInspectionsTestBase(RsAssertEqualInspectio
             let y = Number(10);
             assert_ne!(x, y);
         }
-    """)
+    """, checkWeakWarn = true)
 }
