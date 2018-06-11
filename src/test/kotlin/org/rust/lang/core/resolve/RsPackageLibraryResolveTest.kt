3556d1c0f9c74ff3242f8beb54b0b66691ec0a36
Arseniy Pendryak <a.pendryak@yandex.ru>
2018-06-11 22:40:10 +0300

    T: slightly refactor rust project descriptors

--- src/test/kotlin/org/rust/lang/core/resolve/RsPackageLibraryResolveTest.kt
+++ src/test/kotlin/org/rust/lang/core/resolve/RsPackageLibraryResolveTest.kt
@@ -5,20 +5,14 @@
 
 package org.rust.lang.core.resolve
 
-import com.intellij.openapi.module.Module
-import com.intellij.testFramework.LightProjectDescriptor
-import org.rust.cargo.project.workspace.CargoWorkspace
-import org.rust.cargo.project.workspace.CargoWorkspaceData
-import java.nio.file.Paths
-
 class RsPackageLibraryResolveTest : RsResolveTestBase() {
 
     fun `test library as crate`() = stubOnlyResolve("""
     //- main.rs
-        extern crate my_lib;
+        extern crate test_package;
 
         fn main() {
-            my_lib::hello();
+            test_package::hello();
         }         //^ lib.rs
 
     //- lib.rs
@@ -27,7 +21,7 @@ class RsPackageLibraryResolveTest : RsResolveTestBase() {
 
     fun `test crate alias`() = stubOnlyResolve("""
     //- main.rs
-        extern crate my_lib as other_name;
+        extern crate test_package as other_name;
 
         fn main() {
             other_name::hello();
@@ -41,7 +35,7 @@ class RsPackageLibraryResolveTest : RsResolveTestBase() {
     fun `test macro rules`() = stubOnlyResolve("""
     //- main.rs
         #[macro_use]
-        extern crate my_lib;
+        extern crate test_package;
 
         fn main() {
             foo_bar!();
@@ -54,7 +48,7 @@ class RsPackageLibraryResolveTest : RsResolveTestBase() {
     fun `test macro rules missing macro_export`() = stubOnlyResolve("""
     //- main.rs
         #[macro_use]
-        extern crate my_lib;
+        extern crate test_package;
 
         fn main() {
             foo_bar!();
@@ -67,7 +61,7 @@ class RsPackageLibraryResolveTest : RsResolveTestBase() {
     fun `test macro rules missing macro_use`() = stubOnlyResolve("""
     //- main.rs
         // Missing #[macro_use] here
-        extern crate my_lib;
+        extern crate test_package;
 
         fn main() {
             foo_bar!();
@@ -80,7 +74,7 @@ class RsPackageLibraryResolveTest : RsResolveTestBase() {
     fun `test macro rules in mod 1`() = stubOnlyResolve("""
     //- main.rs
         #[macro_use]
-        extern crate my_lib;
+        extern crate test_package;
 
         fn main() {
             foo_bar!();
@@ -95,7 +89,7 @@ class RsPackageLibraryResolveTest : RsResolveTestBase() {
     fun `test macro rules in mod 2`() = stubOnlyResolve("""
     //- main.rs
         #[macro_use]
-        extern crate my_lib;
+        extern crate test_package;
 
         fn main() {
             foo_bar!();
@@ -106,14 +100,4 @@ class RsPackageLibraryResolveTest : RsResolveTestBase() {
         #[macro_export]
         macro_rules! foo_bar { () => {} }
     """)
-
-    override fun getProjectDescriptor(): LightProjectDescriptor = WithLibraryProjectDescriptor
-
-    private object WithLibraryProjectDescriptor : RustProjectDescriptorBase() {
-        override fun testCargoProject(module: Module, contentRoot: String): CargoWorkspace {
-            return CargoWorkspaceData(listOf(testCargoPackage(contentRoot, name = "my_lib")), emptyMap()).let {
-                CargoWorkspace.deserialize(Paths.get("/my-crate/Cargo.toml"), it)
-            }
-        }
-    }
 }
