05693d2f67081703b1335cfbe42760e89b651ada
Arseniy Pendryak <a.pendryak@yandex.ru>
2018-06-15 12:50:36 +0300

    Use mod declaration name instead of file system name for `RsFile` to support `#[path]` attribute on mod decl name.

--- src/main/kotlin/org/rust/ide/projectView/RsTreeStructureProvider.kt
+++ src/main/kotlin/org/rust/ide/projectView/RsTreeStructureProvider.kt
@@ -10,15 +10,15 @@ import com.intellij.ide.projectView.ViewSettings
 import com.intellij.ide.projectView.impl.nodes.PsiFileNode
 import com.intellij.ide.util.treeView.AbstractTreeNode
 import com.intellij.openapi.project.DumbAware
+import org.rust.lang.RsConstants
 import org.rust.lang.core.psi.RsFile
-import org.rust.lang.core.psi.ext.RsMod.Companion.MOD_RS
 
 /**
  * Moves `mod.rs` files on top
  */
 class RsTreeStructureProvider : TreeStructureProvider, DumbAware {
     override fun modify(parent: AbstractTreeNode<*>, children: Collection<AbstractTreeNode<*>>, settings: ViewSettings?): Collection<AbstractTreeNode<*>> {
-        if (children.none { it is PsiFileNode && it.value?.name == MOD_RS }) {
+        if (children.none { it is PsiFileNode && it.value?.name == RsConstants.MOD_RS_FILE }) {
             return children
         }
 
@@ -36,5 +36,5 @@ class RsTreeStructureProvider : TreeStructureProvider, DumbAware {
 
 private class RsPsiFileNode(original: PsiFileNode, viewSettings: ViewSettings?)
     : PsiFileNode(original.project, original.value, viewSettings) {
-    override fun getSortKey(): Int = if (value.name == MOD_RS) -1 else 0
+    override fun getSortKey(): Int = if (value.name == RsConstants.MOD_RS_FILE) -1 else 0
 }
