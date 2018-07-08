bf4d8dc73f210c5daeab369ceb298d3eb111db6e
mchernyavsky <chemike47@gmail.com>
2018-07-08 07:48:06 +0300

    PSI: Implement stubs for lifetimes
    (cherry picked from commit d2fe255)

--- src/main/kotlin/org/rust/lang/core/psi/ext/RsLifetimeRef.kt
+++ src/main/kotlin/org/rust/lang/core/psi/ext/RsLifetimeRef.kt
@@ -7,19 +7,25 @@ package org.rust.lang.core.psi.ext
 
 import com.intellij.lang.ASTNode
 import com.intellij.psi.PsiElement
+import com.intellij.psi.stubs.IStubElementType
 import org.rust.lang.core.psi.RsLifetime
 import org.rust.lang.core.resolve.ref.RsLifetimeReferenceImpl
 import org.rust.lang.core.resolve.ref.RsReference
+import org.rust.lang.core.stubs.RsLifetimeStub
 import org.rust.lang.refactoring.RsNamesValidator
 
 
-val RsLifetime.isPredefined : Boolean get() = quoteIdentifier.text in RsNamesValidator.PredefinedLifetimes
+val RsLifetime.isPredefined: Boolean get() = referenceName in RsNamesValidator.PredefinedLifetimes
 
-abstract class RsLifetimeImplMixin (node: ASTNode) : RsElementImpl(node), RsLifetime {
+abstract class RsLifetimeImplMixin : RsStubbedNamedElementImpl<RsLifetimeStub>, RsLifetime {
+
+    constructor(node: ASTNode) : super(node)
+
+    constructor(stub: RsLifetimeStub, nodeType: IStubElementType<*, *>) : super(stub, nodeType)
+
+    override fun getReference(): RsReference = RsLifetimeReferenceImpl(this)
 
     override val referenceNameElement: PsiElement get() = quoteIdentifier
 
-    override val referenceName: String get() = referenceNameElement.text
-
-    override fun getReference(): RsReference = RsLifetimeReferenceImpl(this)
+    override val referenceName: String get() = stub?.name ?: referenceNameElement.text
 }
