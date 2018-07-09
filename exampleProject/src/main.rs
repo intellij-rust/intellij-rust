c624233c3d9f85c3ca3a04e6ee4b38083b466583
ortem <ortem00@gmail.com>
2018-07-09 11:08:16 +0300

    Restore exampleProject

--- exampleProject/src/main.rs
+++ exampleProject/src/main.rs
@@ -1,23 +1,3 @@
-struct S1<A> {
-    field1: A
-}
-
-struct S2<B> {
-    parent: S1<B>,
-    field2: B
-}
-
-impl<T> std::ops::Deref for S2<T> {
-    type Target = S1<T>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.parent
-    }
-}
-
 fn main() {
-    let s1 = S1 { field1: 1u8 };
-    let s2 = S2 { parent: s1, field2: 1u8};
-
-    s2.field1;
+    println!("Hello, World");
 }
