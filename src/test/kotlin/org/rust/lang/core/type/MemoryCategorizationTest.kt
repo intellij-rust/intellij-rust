01a40a9db1330e8612efd2ad7ea42cb611de1b4e
ortem <ortem00@gmail.com>
2018-07-09 11:32:38 +0300

    Add more memory categorization tests

--- src/test/kotlin/org/rust/lang/core/type/MemoryCategorizationTest.kt
+++ src/test/kotlin/org/rust/lang/core/type/MemoryCategorizationTest.kt
@@ -6,6 +6,14 @@
 package org.rust.lang.core.type
 
 class MemoryCategorizationTest : MemoryCategorizationTestBase() {
+    fun `test declared immutable`() = testExpr("""
+        fn main() {
+            let x = 42;
+            x;
+          //^ Immutable
+        }
+    """)
+
     fun `test declared mutable`() = testExpr("""
         fn main() {
             let mut x = 42;
@@ -14,19 +22,84 @@ class MemoryCategorizationTest : MemoryCategorizationTestBase() {
         }
     """)
 
-    fun `test declared immutable`() = testExpr("""
+    fun `test declared immutable with immutable deref`() = testExpr("""
         fn main() {
-            let mut x = 42;
-            x;
-          //^ Immutable
+            let y = 42;
+            let x = &y;
+            (*x);
+             //^ Immutable
         }
     """)
 
-    fun `test array`() = testExpr("""
+    fun `test declared mutable with immutable deref`() = testExpr("""
+        fn main() {
+            let mut y = 42;
+            let x = &y;
+            (*x);
+             //^ Immutable
+        }
+    """)
+
+    fun `test declared mutable with mutable deref`() = testExpr("""
+        fn main() {
+            let mut y = 42;
+            let x = &mut y;
+            (*x);
+             //^ Declared
+        }
+    """)
+
+    fun `test immutable array`() = testExpr("""
+        fn main() {
+            let a: [i32; 3] = [0; 3];
+            a[1];
+             //^ Immutable
+        }
+    """)
+
+    fun `test mutable array`() = testExpr("""
         fn main() {
             let mut a: [i32; 3] = [0; 3];
             a[1];
              //^ Inherited
+        }
+    """)
+
+    fun `test immutable struct`() = testExpr("""
+        struct Foo { a: i32 }
+        fn main() {
+            let x = Foo { a: 1 };
+            (x.a);
+              //^ Immutable
+        }
+    """)
+
+    fun `test mutable struct`() = testExpr("""
+        struct Foo { a: i32 }
+        fn main() {
+            let mut x = Foo { a: 1 };
+            (x.a);
+              //^ Inherited
+        }
+    """)
+
+    fun `test mutable struct with immutable reference`() = testExpr("""
+        struct Foo { a: i32 }
+        fn main() {
+            let mut foo = Foo { a: 1 };
+            let x = &foo;
+            (x.a);
+              //^ Immutable
+        }
+    """)
+
+    fun `test mutable struct with mutable reference`() = testExpr("""
+        struct Foo { a: i32 }
+        fn main() {
+            let mut foo = Foo { a: 1 };
+            let x = &mut foo;
+            (x.a);
+              //^ Declared
         }
     """)
 }
