0fe77f5cdf65e098e7668ba34f9cbd6f130456e5
Peter Oberndorfer <kumbayo84@arcor.de>
2018-07-22 16:09:41 +0200

    UNUSED: FIXUP: Add more cfg values during test
    TODO:
    - Not fully equal to what is set during a run on Linux

--- src/test/kotlin/org/rust/RsTestCase.kt
+++ src/test/kotlin/org/rust/RsTestCase.kt
@@ -18,7 +18,20 @@ interface RsTestCase {
         // See `rustc --print cfg` for some defaults
         // We do not want to set "test" inside Rust stdlib as this disables some important parts
         // TODO: Is this safe for initialization?
-        val testDefaultCfgStdLib = listOf("unix", "target_has_atomic=\"ptr\"", "feature=\"use_std\"")
+        val testDefaultCfgStdLib = listOf("debug_assertions",
+            "target_has_atomic=\"16\"",
+            "target_has_atomic=\"32\"",
+            "target_has_atomic=\"64\"",
+            "target_has_atomic=\"8\"",
+            "target_has_atomic=\"ptr\"",
+            "target_arch=\"x86_64\"",
+            "target_endian=\"little\"",
+            "target_env=\"gnu\"",
+            "target_family=\"unix\"",
+            "target_os=\"linux\"",
+            "target_pointer_width=\"64\"",
+            "unix",
+            "feature=\"use_std\"" /*Not enabled normally*/)
         val testDefaultCfg = testDefaultCfgStdLib.toMutableList() + "test"
     }
 }
