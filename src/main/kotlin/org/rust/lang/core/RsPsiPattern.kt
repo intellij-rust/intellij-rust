147b9e4ff79eb4b8a67459221bfb320923e92815
Arseniy Pendryak <a.pendryak@yandex.ru>
2018-07-25 11:30:59 +0300

    TOML&RES: resolve dependency crate name to the corresponding 'lib.rs'

--- src/main/kotlin/org/rust/lang/core/RsPsiPattern.kt
+++ src/main/kotlin/org/rust/lang/core/RsPsiPattern.kt
@@ -157,7 +157,7 @@ fun <T : PsiElement, Self : PsiElementPattern<T, Self>> PsiElementPattern<T, Sel
     pattern.accepts(sibling)
 }
 
-private fun <T, Self : ObjectPattern<T, Self>> ObjectPattern<T, Self>.with(name: String, cond: (T) -> Boolean): Self =
+fun <T, Self : ObjectPattern<T, Self>> ObjectPattern<T, Self>.with(name: String, cond: (T) -> Boolean): Self =
     with(object : PatternCondition<T>(name) {
         override fun accepts(t: T, context: ProcessingContext?): Boolean = cond(t)
     })
