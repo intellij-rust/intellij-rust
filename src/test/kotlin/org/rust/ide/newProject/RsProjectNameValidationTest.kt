c62b8788274d0377a95df0a5ab98fa2148b0f516
Arseniy Pendryak <a.pendryak@yandex.ru>
2018-08-03 12:03:21 +0300

    PRJ: validate package name while new project creation

---
+++ src/test/kotlin/org/rust/ide/newProject/RsProjectNameValidationTest.kt
@@ -0,0 +1,39 @@
+/*
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
+package org.rust.ide.newProject
+
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+
+@RunWith(Parameterized::class)
+class RsProjectNameValidationTest(
+    private val name: String,
+    private val isBinary: Boolean,
+    private val isValid: Boolean
+) {
+
+    @Test
+    fun test() {
+        val message = RsPackageNameValidator.validate(name, isBinary)
+        val assert: (String?) -> Unit = if (isValid) Assert::assertNull else Assert::assertNotNull
+        assert(message)
+    }
+
+    companion object {
+        @Parameterized.Parameters(name = "{index}: \"{0}\" is valid: {2}")
+        @JvmStatic fun data(): Collection<Array<Any>> = listOf(
+            arrayOf("valid-package_name", false, true),
+            arrayOf("crate", false, false),
+            arrayOf("build", false, true),
+            arrayOf("build", true, false),
+            arrayOf("1name", false, false),
+            arrayOf("package.name", false, false),
+            arrayOf("パッケージ", false , true)
+        )
+    }
+}
