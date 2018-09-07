a8fd0dc0e5a8fca4933c71ff06cc3b67d477c72e
Mikhail Chernyavsky <chemike47@gmail.com>
2018-09-07 06:48:38 +0300

    Search for box item through the RsLangItemIndex

--- src/main/kotlin/org/rust/lang/core/resolve/indexes/RsLangItemIndex.kt
+++ src/main/kotlin/org/rust/lang/core/resolve/indexes/RsLangItemIndex.kt
@@ -13,36 +13,46 @@ import com.intellij.psi.stubs.StubIndexKey
 import com.intellij.util.io.EnumeratorStringDescriptor
 import com.intellij.util.io.KeyDescriptor
 import org.rust.cargo.util.AutoInjectedCrates
+import org.rust.lang.core.psi.RsStructItem
 import org.rust.lang.core.psi.RsTraitItem
+import org.rust.lang.core.psi.ext.RsItemElement
 import org.rust.lang.core.psi.ext.containingCargoPackage
-import org.rust.lang.core.psi.ext.langAttribute
+import org.rust.lang.core.psi.ext.queryAttributes
 import org.rust.lang.core.stubs.RsFileStub
-import org.rust.lang.core.stubs.RsTraitItemStub
 import org.rust.openapiext.getElements
 
-class RsLangItemIndex : AbstractStubIndex<String, RsTraitItem>() {
+class RsLangItemIndex : AbstractStubIndex<String, RsItemElement>() {
     override fun getVersion(): Int = RsFileStub.Type.stubVersion
-    override fun getKey(): StubIndexKey<String, RsTraitItem> = KEY
+    override fun getKey(): StubIndexKey<String, RsItemElement> = KEY
     override fun getKeyDescriptor(): KeyDescriptor<String> = EnumeratorStringDescriptor.INSTANCE
 
     companion object {
         fun findLangItem(project: Project, langAttribute: String, crateName: String = AutoInjectedCrates.CORE): RsTraitItem? {
             val elements = getElements(KEY, langAttribute, project, GlobalSearchScope.allScope(project))
             return if (elements.size < 2) {
-                elements.firstOrNull()
+                elements.firstOrNull() as? RsTraitItem
             } else {
-                elements.find { it.containingCargoPackage?.normName == crateName }
+                elements.find { it.containingCargoPackage?.normName == crateName } as? RsTraitItem
             }
         }
 
-        fun index(stub: RsTraitItemStub, sink: IndexSink) {
-            val key = stub.psi.langAttribute
+        fun findBoxItem(project: Project): RsStructItem? {
+            val elements = getElements(KEY, "owned_box", project, GlobalSearchScope.allScope(project))
+            return if (elements.size < 2) {
+                elements.firstOrNull() as? RsStructItem
+            } else {
+                elements.find { it.containingCargoPackage?.normName == "alloc" } as? RsStructItem
+            }
+        }
+
+        fun index(psi: RsItemElement, sink: IndexSink) {
+            val key = psi.queryAttributes.langAttribute
             if (key != null) {
                 sink.occurrence(KEY, key)
             }
         }
 
-        private val KEY: StubIndexKey<String, RsTraitItem> =
+        private val KEY: StubIndexKey<String, RsItemElement> =
             StubIndexKey.createIndexKey("org.rust.lang.core.resolve.indexes.RsLangItemIndex")
     }
 }
