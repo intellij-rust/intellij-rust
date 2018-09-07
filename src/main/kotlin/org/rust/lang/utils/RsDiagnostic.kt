8f66f2a3cbe1cfc3314919e952ed32a9639ba9ec
vlad20012 <beskvlad@gmail.com>
2018-09-07 19:24:03 +0300

    ANN: E0404 expected trait, found (other item)

--- src/main/kotlin/org/rust/lang/utils/RsDiagnostic.kt
+++ src/main/kotlin/org/rust/lang/utils/RsDiagnostic.kt
@@ -6,7 +6,10 @@
 package org.rust.lang.utils
 
 import com.intellij.codeInsight.intention.IntentionAction
-import com.intellij.codeInspection.*
+import com.intellij.codeInspection.InspectionManager
+import com.intellij.codeInspection.LocalQuickFix
+import com.intellij.codeInspection.ProblemHighlightType
+import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.HighlightSeverity
 import com.intellij.openapi.util.TextRange
@@ -560,6 +563,35 @@ sealed class RsDiagnostic(
         }
     }
 
+    class NotTraitError(
+        element: PsiElement,
+        private val found: RsItemElement
+    ) : RsDiagnostic(element) {
+        override fun prepare() = PreparedAnnotation(
+            ERROR,
+            E0404,
+            errorText()
+        )
+
+        private fun errorText(): String {
+            val itemKind = found.itemKindName
+            val name = escapeString(found.name)
+            return "Expected trait, found $itemKind `$name`"
+        }
+
+        private val RsItemElement.itemKindName: String
+            get() = when (this) {
+                is RsStructItem -> when (kind) {
+                    RsStructKind.STRUCT -> "struct"
+                    RsStructKind.UNION -> "union"
+                }
+                is RsEnumItem -> "enum"
+                is RsTypeAlias -> "type alias"
+                is RsModItem -> "module"
+                else -> error("unknown item")
+            }
+    }
+
     class DuplicateDefinitionError(
         element: PsiElement,
         private val fieldName: String
@@ -732,7 +764,7 @@ enum class RsErrorCode {
     E0121, E0124, E0133, E0185, E0186, E0198, E0199,
     E0200, E0201, E0202, E0261, E0262, E0263, E0277,
     E0308, E0379,
-    E0403, E0407, E0415, E0424, E0426, E0428, E0433, E0449, E0463,
+    E0403, E0404, E0407, E0415, E0424, E0426, E0428, E0433, E0449, E0463,
     E0569,
     E0603, E0614, E0616, E0624, E0658;
 
