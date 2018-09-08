b798181d75bc2d4b37476b60f5571591ea77e6c5
Arseniy Pendryak <a.pendryak@yandex.ru>
2018-09-08 19:56:00 +0300

    RES&COMP: support file references for `#[path]` attribute

--- src/main/kotlin/org/rust/lang/core/resolve/ref/RsLitExprReferenceContributor.kt
+++ src/main/kotlin/org/rust/lang/core/resolve/ref/RsLitExprReferenceContributor.kt
@@ -5,18 +5,22 @@
 
 package org.rust.lang.core.resolve.ref
 
+import com.intellij.openapi.util.Condition
 import com.intellij.psi.*
 import com.intellij.psi.impl.source.resolve.reference.impl.providers.FileReference
 import com.intellij.psi.impl.source.resolve.reference.impl.providers.FileReferenceSet
 import com.intellij.util.ProcessingContext
-import org.rust.lang.core.RsPsiPattern
-import org.rust.lang.core.psi.RsLitExpr
-import org.rust.lang.core.psi.RsLiteralKind
-import org.rust.lang.core.psi.kind
+import org.rust.lang.RsFileType
+import org.rust.lang.core.RsPsiPattern.includeMacroLiteral
+import org.rust.lang.core.RsPsiPattern.pathAttrLiteral
+import org.rust.lang.core.or
+import org.rust.lang.core.psi.*
+import org.rust.lang.core.psi.ext.RsMod
+import org.rust.lang.core.psi.ext.ancestorStrict
 
 class RsLitExprReferenceContributor : PsiReferenceContributor() {
     override fun registerReferenceProviders(registrar: PsiReferenceRegistrar) {
-        registrar.registerReferenceProvider(RsPsiPattern.includeMacroLiteral, RsFileReferenceProvider())
+        registrar.registerReferenceProvider(includeMacroLiteral or pathAttrLiteral, RsFileReferenceProvider())
     }
 }
 
@@ -26,8 +30,36 @@ private class RsFileReferenceProvider : PsiReferenceProvider() {
         if (stringLiteral.isByte) return emptyArray()
         val startOffset = stringLiteral.offsets.value?.startOffset ?: return emptyArray()
         val fs = element.containingFile.originalFile.virtualFile.fileSystem
-        return object : FileReferenceSet(stringLiteral.value ?: "", element, startOffset, null, fs.isCaseSensitive) {
-            override fun getDefaultContexts(): MutableCollection<PsiFileSystemItem> = parentDirectoryContext
-        }.allReferences
+        return RsLiteralFileReferenceSet(stringLiteral.value ?: "", element, startOffset, fs.isCaseSensitive).allReferences
+    }
+}
+
+private class RsLiteralFileReferenceSet(
+    str: String,
+    element: RsLitExpr,
+    startOffset: Int,
+    isCaseSensitive: Boolean
+) : FileReferenceSet(str, element, startOffset, null, isCaseSensitive) {
+
+    override fun getDefaultContexts(): Collection<PsiFileSystemItem> {
+        val parent = element.parent
+        return when (parent) {
+            is RsIncludeMacroArgument -> parentDirectoryContext
+            is RsMetaItem -> {
+                val item = parent.ancestorStrict<RsModDeclItem>() ?: parent.ancestorStrict<RsMod>()
+                listOfNotNull(item?.containingMod?.ownedDirectory)
+            }
+            else -> emptyList()
+        }
+    }
+
+    override fun getReferenceCompletionFilter(): Condition<PsiFileSystemItem> {
+        return when (element.parent) {
+            is RsMetaItem -> Condition { item ->
+                if (item.isDirectory) return@Condition true
+                item.virtualFile.fileType == RsFileType
+            }
+            else -> super.getReferenceCompletionFilter()
+        }
     }
 }
