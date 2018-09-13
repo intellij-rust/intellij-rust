75b2bd3cb5b79017b2bf42c8c762c19c2e31d6b8
Michael Morgan <morgan.michael@me.com>
2018-09-13 14:22:09 -0500

    MACRO: expand multiple `lazy_static` definitions in the same call

--- src/main/kotlin/org/rust/lang/core/macros/LazyStatic.kt
+++ src/main/kotlin/org/rust/lang/core/macros/LazyStatic.kt
@@ -8,51 +8,64 @@ package org.rust.lang.core.macros
 import com.intellij.psi.PsiElement
 import com.intellij.psi.tree.IElementType
 import org.rust.lang.core.psi.*
-import org.rust.lang.core.psi.ext.descendantOfTypeStrict
+import org.rust.lang.core.psi.ext.descendantsOfType
 import org.rust.lang.core.psi.ext.elementType
 
-fun expandLazyStatic(call: RsMacroCall): RsExpandedElement? {
+fun expandLazyStatic(call: RsMacroCall): List<RsExpandedElement>? {
     val arg = call.macroArgument?.compactTT ?: return null
-    val lazyStaticCall = parseLazyStaticCall(arg) ?: return null
-    val text = "${if (lazyStaticCall.pub) "pub " else ""}static ${lazyStaticCall.identifier}: ${lazyStaticCall.type} = &${lazyStaticCall.expr};"
+    val lazyStaticRefs = parseLazyStaticCall(arg)
+    val texts = lazyStaticRefs.map { "${if (it.pub) "pub " else ""}static ${it.identifier}: ${it.type} = &${it.expr};" }
     return RsPsiFactory(call.project)
-        .createFile(text)
-        .descendantOfTypeStrict<RsConstant>()
+        .createFile(texts.joinToString("\n"))
+        .descendantsOfType<RsConstant>()
+        .toList()
 }
 
-private data class LazyStaticCall(
+private data class LazyStaticRef(
     val pub: Boolean,
     val identifier: String,
     val type: String,
     val expr: String
 )
 
-private fun parseLazyStaticCall(tt: RsCompactTT): LazyStaticCall? {
-    // static ref FOO: Foo = Foo::new();
-    val pub = tt.firstToken(RsElementTypes.PUB) != null
-    val ident = tt.firstToken(RsElementTypes.IDENTIFIER) ?: return null
-    val colon = tt.firstToken(RsElementTypes.COLON) ?: return null
-    val eq = tt.firstToken(RsElementTypes.EQ) ?: return null
-    val semi = tt.firstToken(RsElementTypes.SEMICOLON) ?: return null
+private fun parseLazyStaticCall(tt: RsCompactTT): List<LazyStaticRef> {
+    val calls = mutableListOf<LazyStaticRef>()
 
-    val typeStart = colon.textRange.endOffset - tt.textRange.startOffset
-    val typeEnd = eq.textRange.startOffset - tt.textRange.startOffset
-    if (typeStart >= typeEnd) return null
-    val typeText = tt.text.substring(typeStart, typeEnd)
+    var start = tt.firstChild
+    while (start != null) {
+        parseLazyStaticRef(start, tt)?.let { calls.add(it) }
+        start = start.firstSibling(RsElementTypes.SEMICOLON)?.nextSibling
+    }
 
-    val exprStart = eq.textRange.endOffset - tt.textRange.startOffset
-    val exprEnd = semi.textRange.startOffset - tt.textRange.startOffset
-    if (exprStart >= exprEnd) return null
-    val exprText = tt.text.substring(exprStart, exprEnd)
-
-    return LazyStaticCall(pub, ident.text, typeText, exprText)
+    return calls
 }
 
-private fun RsCompactTT.firstToken(type: IElementType): PsiElement? {
-    var child = firstChild
-    while (child != null) {
-        if (child.elementType == type) return child
-        child = child.nextSibling
+private fun parseLazyStaticRef(start: PsiElement, parent: RsCompactTT): LazyStaticRef? {
+    // static ref FOO: Foo = Foo::new();
+    val pub = start.firstSibling(RsElementTypes.PUB) != null
+    val ident = start.firstSibling(RsElementTypes.IDENTIFIER) ?: return null
+    val colon = start.firstSibling(RsElementTypes.COLON) ?: return null
+    val eq = start.firstSibling(RsElementTypes.EQ) ?: return null
+    val semi = start.firstSibling(RsElementTypes.SEMICOLON) ?: return null
+
+    val typeStart = colon.textRange.endOffset - parent.textRange.startOffset
+    val typeEnd = eq.textRange.startOffset - parent.textRange.startOffset
+    if (typeStart >= typeEnd) return null
+    val typeText = parent.text.substring(typeStart, typeEnd)
+
+    val exprStart = eq.textRange.endOffset - parent.textRange.startOffset
+    val exprEnd = semi.textRange.startOffset - parent.textRange.startOffset
+    if (exprStart >= exprEnd) return null
+    val exprText = parent.text.substring(exprStart, exprEnd)
+
+    return LazyStaticRef(pub, ident.text, typeText, exprText)
+}
+
+private fun PsiElement.firstSibling(type: IElementType): PsiElement? {
+    var element: PsiElement? = this
+    while (element != null) {
+        if (element.elementType == type) return element
+        element = element.nextSibling
     }
     return null
 }
