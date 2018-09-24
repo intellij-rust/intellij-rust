18dc9ef1cbc17b70cc62ad1643fbe55d85e19c2e
Arseniy Pendryak <a.pendryak@yandex.ru>
2018-09-24 14:46:22 +0300

    T: tests fqn links with reexports

--- src/test/kotlin/org/rust/ide/docs/RsStdlibResolveLinkTest.kt
+++ src/test/kotlin/org/rust/ide/docs/RsStdlibResolveLinkTest.kt
@@ -14,29 +14,25 @@ import org.rust.lang.core.psi.ext.RsNamedElement
 
 @ProjectDescriptor(WithStdlibRustProjectDescriptor::class)
 class RsStdlibResolveLinkTest : RsTestBase() {
-    fun `test with import`() = doTest("""
+    fun `test with import`() = doTest("Hash", ".../hash/mod.rs", """
         use std::hash::Hash;
 
         fn foo<T: Hash>(t: T) {}
           //^
-    """, "Hash", ".../hash/mod.rs")
+    """)
 
-    fun `test item from prelude`() = doTest("""
+    fun `test item from prelude`() = doTest("String", ".../string.rs", """
         fn foo(s: String) {}
           //^
-    """, "String", ".../string.rs")
+    """)
 
-    fun `test crate fqn link`() = doTest("""
-        fn foo() {}
-          //^
-    """, "std/index.html", ".../libstd/lib.rs")
+    fun `test crate fqn link`() = doTest("std/index.html", ".../libstd/lib.rs")
+    fun `test mod fqn link`() = doTest("std/io/index.html", ".../libstd/io/mod.rs")
+    fun `test fqn link with reexport`() = doTest("std/cmp/trait.Eq.html", ".../libcore/cmp.rs")
+    fun `test mod fqn link with reexport`() = doTest("std/marker/index.html", ".../libcore/marker.rs")
+    fun `test method fqn link with reexport`() = doTest("std/result/enum.Result.html#method.unwrap", ".../libcore/result.rs")
 
-    fun `test mod fqn link`() = doTest("""
-        fn foo() {}
-          //^
-    """, "std/io/index.html", ".../libstd/io/mod.rs")
-
-    private fun doTest(@Language("Rust") code: String, link: String, expectedPath: String) {
+    private fun doTest(link: String, expectedPath: String, @Language("Rust") code: String = DEFAULT_TEXT) {
         check(expectedPath.startsWith("...")) {
             "Expected path should start with '...', but '$expectedPath' was found"
         }
@@ -51,5 +47,13 @@ class RsStdlibResolveLinkTest : RsTestBase() {
         check(actualFile.path.endsWith(expectedPath.drop(3))) {
             "Should resolve to $expectedPath, was ${actualFile.path} instead"
         }
+    }
+
+    companion object {
+        @Language("Rust")
+        private const val DEFAULT_TEXT = """
+            struct Foo;
+                  //^
+        """
     }
 }
