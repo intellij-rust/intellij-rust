e65499964b903f9ec0bc73995bf7f32d42a788f5
Mikhail Chernyavsky <chemike47@gmail.com>
2018-10-11 12:02:10 +0300

    TY: Provide region for TyTraitObject

--- src/main/kotlin/org/rust/lang/core/types/ty/TyTypeParameter.kt
+++ src/main/kotlin/org/rust/lang/core/types/ty/TyTypeParameter.kt
@@ -11,21 +11,28 @@ import org.rust.lang.core.psi.RsTypeParameter
 import org.rust.lang.core.psi.ext.*
 import org.rust.lang.core.types.BoundElement
 import org.rust.lang.core.types.HAS_TY_TYPE_PARAMETER_MASK
+import org.rust.lang.core.types.infer.resolve
+import org.rust.lang.core.types.regions.Region
 import org.rust.lang.core.types.type
 
 class TyTypeParameter private constructor(
     val parameter: TypeParameter,
     val isSized: Boolean,
-    boundsSupplier: () -> Collection<BoundElement<RsTraitItem>>
+    traitBoundsSupplier: () -> Collection<BoundElement<RsTraitItem>>,
+    regionBoundsSupplier: () -> Collection<Region>
 ) : Ty(HAS_TY_TYPE_PARAMETER_MASK) {
 
-    private val bounds: Collection<BoundElement<RsTraitItem>> by lazy(LazyThreadSafetyMode.NONE, boundsSupplier)
+    private val traitBounds: Collection<BoundElement<RsTraitItem>>
+        by lazy(LazyThreadSafetyMode.NONE, traitBoundsSupplier)
+
+    val regionBounds: Collection<Region>
+        by lazy(LazyThreadSafetyMode.NONE, regionBoundsSupplier)
 
     override fun equals(other: Any?): Boolean = other is TyTypeParameter && other.parameter == parameter
     override fun hashCode(): Int = parameter.hashCode()
 
     fun getTraitBoundsTransitively(): Collection<BoundElement<RsTraitItem>> =
-        bounds.flatMap { it.flattenHierarchy }
+        traitBounds.flatMap { it.flattenHierarchy }
 
     val name: String? get() = parameter.name
 
@@ -42,27 +49,40 @@ class TyTypeParameter private constructor(
     }
 
     companion object {
-        private val self = TyTypeParameter(Self, isSized = false) { emptyList() }
+        private val self = TyTypeParameter(Self, false, { emptyList() }, { emptyList() })
 
         fun self(): TyTypeParameter = self
 
         fun self(item: RsTraitOrImpl): TyTypeParameter {
             val isSized = when (item) {
-                is RsTraitItem -> item.implementedTrait?.flattenHierarchy.orEmpty().any { it.element.isSizedTrait  }
+                is RsTraitItem -> item.implementedTrait?.flattenHierarchy.orEmpty().any { it.element.isSizedTrait }
                 is RsImplItem -> item.typeReference?.type?.isSized() == true
                 else -> error("item must be instance of `RsTraitItem` or `RsImplItem`")
             }
-            return TyTypeParameter(Self, isSized) { listOfNotNull(item.implementedTrait) }
+            return TyTypeParameter(
+                Self,
+                isSized,
+                { listOfNotNull(item.implementedTrait) },
+                { emptyList() }
+            )
         }
 
         fun named(parameter: RsTypeParameter): TyTypeParameter =
-            TyTypeParameter(Named(parameter), parameter.isSized) { bounds(parameter) }
+            TyTypeParameter(
+                Named(parameter),
+                parameter.isSized,
+                { traitBounds(parameter) },
+                { regionBounds(parameter) }
+            )
     }
 }
 
-private fun bounds(parameter: RsTypeParameter): List<BoundElement<RsTraitItem>> =
+private fun traitBounds(parameter: RsTypeParameter): List<BoundElement<RsTraitItem>> =
     parameter.bounds.mapNotNull {
         val trait = it.bound.traitRef?.resolveToBoundTrait ?: return@mapNotNull null
         // if `T: ?Sized` then T doesn't have `Sized` bound
         if (!trait.element.isSizedTrait || parameter.isSized) trait else null
     }
+
+private fun regionBounds(parameter: RsTypeParameter): List<Region> =
+    parameter.bounds.mapNotNull { it.bound.lifetime?.resolve() }
