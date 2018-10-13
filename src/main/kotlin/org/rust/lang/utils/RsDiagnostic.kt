b07abb264bffd6a8170dfd4e875095f8234ccf8e
Todd Taomae <ttaomae@gmail.com>
2018-10-12 23:39:10 -1000

    Clean up error annotation handling.

--- src/main/kotlin/org/rust/lang/utils/RsDiagnostic.kt
+++ src/main/kotlin/org/rust/lang/utils/RsDiagnostic.kt
@@ -783,15 +783,38 @@ sealed class RsDiagnostic(
             return "Wrong number of lifetime arguments: expected $expectedLifetimes, found $actualLifetimes"
         }
     }
+
+    class CannotAssignToImmutable(
+        element: PsiElement,
+        private val message: String
+    ) : RsDiagnostic(element) {
+        override fun prepare(): PreparedAnnotation = PreparedAnnotation(
+            ERROR,
+            E0594,
+            "Cannot assign to $message"
+        )
+    }
+
+    class CannotReassignToImmutable(
+        element: PsiElement,
+        private val fix: AddMutableFix?
+    ) : RsDiagnostic(element) {
+        override fun prepare(): PreparedAnnotation = PreparedAnnotation(
+            ERROR,
+            E0384,
+            "Cannot assign twice to immutable variable",
+            fixes = listOfNotNull(fix)
+        )
+    }
 }
 
 enum class RsErrorCode {
     E0046, E0050, E0060, E0061, E0069,
     E0106, E0107, E0121, E0124, E0133, E0185, E0186, E0198, E0199,
     E0200, E0201, E0202, E0261, E0262, E0263, E0277,
-    E0308, E0379,
+    E0308, E0379, E0384,
     E0403, E0404, E0407, E0415, E0424, E0426, E0428, E0433, E0449, E0463,
-    E0569,
+    E0569, E0594,
     E0603, E0614, E0616, E0624, E0658;
 
     val code: String
