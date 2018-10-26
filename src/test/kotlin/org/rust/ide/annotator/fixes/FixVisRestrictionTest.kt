79f86410ed15dc8a0b2dd53fceb858c9e13bf659
Arseniy Pendryak <a.pendryak@yandex.ru>
2018-10-27 01:42:50 +0300

    INSP: annotate visibility restriction item with module path but without `in` keyword.
    Also, add the corresponding quick fix

---
+++ src/test/kotlin/org/rust/ide/annotator/fixes/FixVisRestrictionTest.kt
@@ -0,0 +1,41 @@
+/*
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
+package org.rust.ide.annotator.fixes
+
+import org.rust.ide.annotator.RsAnnotationTestBase
+
+class FixVisRestrictionTest : RsAnnotationTestBase() {
+
+    fun `test fix visibility restriction 1`() = checkFixByText("Fix visibility restriction", """
+        mod foo {
+            pub(/*some comment*/<error descr="Incorrect visibility restriction [E0704]">foo/*caret*/</error>) fn bar() {}
+        }
+    """, """
+        mod foo {
+            pub(/*some comment*/ in foo/*caret*/) fn bar() {}
+        }
+    """)
+
+    fun `test fix visibility restriction 2`() = checkFixByText("Fix visibility restriction", """
+        mod foo {
+            pub(/*some comment*/<error descr="Incorrect visibility restriction [E0704]">super::foo/*caret*/</error>) fn bar() {}
+        }
+    """, """
+        mod foo {
+            pub(/*some comment*/ in super::foo/*caret*/) fn bar() {}
+        }
+    """)
+
+    fun `test do not annotate short version of visibility restriction`() {
+        for (restriction in listOf("crate", "self", "super")) {
+            checkFixIsUnavailable("Fix visibility restriction", """
+                mod foo {
+                    pub($restriction/*caret*/) fn bar() {}
+                }
+            """)
+        }
+    }
+}
