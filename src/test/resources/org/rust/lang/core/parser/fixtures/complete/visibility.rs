aeac55742c798ed62579e371abaf6ae4643ea356
Arseniy Pendryak <a.pendryak@yandex.ru>
2018-10-26 21:30:25 +0300

    GRAM: simplify `VisRestriction` grammar

--- src/test/resources/org/rust/lang/core/parser/fixtures/complete/visibility.rs
+++ src/test/resources/org/rust/lang/core/parser/fixtures/complete/visibility.rs
@@ -4,8 +4,11 @@ pub(crate) struct S3;
 pub(self) struct S4;
 mod a {
     pub (super) struct S5;
+    pub(in a) struct S6;
     mod b {
-        pub(in super::super) struct S6;
+        pub(in super::super) struct S7;
+        // Syntactically invalid
+        pub(a::b) struct S8;
     }
 }
-crate struct S7;
+crate struct S9;
