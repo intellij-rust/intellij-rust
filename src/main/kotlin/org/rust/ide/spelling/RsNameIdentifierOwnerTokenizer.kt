bf978fa60e49180cd06e0d34247da33ac2270f78
Arseniy Pendryak <a.pendryak@yandex.ru>
2018-11-12 18:00:42 +0300

    INSP: adjust spell checker inspection for raw identifiers

---
+++ src/main/kotlin/org/rust/ide/spelling/RsNameIdentifierOwnerTokenizer.kt
@@ -0,0 +1,39 @@
+/*
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
+package org.rust.ide.spelling
+
+import com.intellij.openapi.util.TextRange
+import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.spellchecker.inspections.IdentifierSplitter
+import com.intellij.spellchecker.tokenizer.TokenConsumer
+import com.intellij.spellchecker.tokenizer.Tokenizer
+import org.rust.lang.core.psi.ext.RsNameIdentifierOwner
+import org.rust.lang.core.psi.ext.unescapeIdentifier
+
+// Inspired by `PsiIdentifierOwnerTokenizer`
+object RsNameIdentifierOwnerTokenizer : Tokenizer<RsNameIdentifierOwner>() {
+
+    override fun tokenize(element: RsNameIdentifierOwner, consumer: TokenConsumer) {
+        val identifier = element.nameIdentifier ?: return
+        val range = identifier.textRange
+        if (range.isEmpty) return
+
+        var offset = range.startOffset - element.textRange.startOffset
+        val parent = if (offset < 0) {
+            val commonParent = PsiTreeUtil.findCommonParent(identifier, element) ?: return
+            offset = range.startOffset - commonParent.textRange.startOffset
+            commonParent
+        } else {
+            element
+        }
+        val text = identifier.text
+        val unescapedText = text.unescapeIdentifier()
+        if (text.startsWith("r#")) {
+            offset += 2
+        }
+        consumer.consumeToken(parent, unescapedText, true, offset, TextRange.allOf(unescapedText), IdentifierSplitter.getInstance())
+    }
+}
