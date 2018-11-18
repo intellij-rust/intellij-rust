54e22bc85506a6435f453f89411d29d7eccc738a
Voronchikhin <m.voronchikhin@g.nsu.ru>
2018-11-18 16:57:28 +0700

    reformat tests

--- src/test/kotlin/org/rust/ide/annotator/fixes/ImplementFromTraitFixTest.kt
+++ src/test/kotlin/org/rust/ide/annotator/fixes/ImplementFromTraitFixTest.kt
@@ -14,217 +14,217 @@ import org.rust.ide.annotator.RsAnnotationTestBase
 class ImplementFromTraitFixTest : RsAnnotationTestBase() {
     fun `test non available`() = checkFixIsUnavailable(
         "Create 'From<AError>' for type 'BError'", """
-#![feature(try_trait)]
-use std::ops;
-use std::ops::Try;
-struct A; struct AError;
-struct B; struct BError;
-
-impl From<AError> for BError {
-    fn from(_: A) -> Self {
-        unimplemented!()
+    #![feature(try_trait)]
+    use std::ops;
+    use std::ops::Try;
+    struct A;
+    struct AError;
+    struct B;
+    struct BError;
+    
+    impl From<AError> for BError {
+        fn from(_: A) -> Self {
+            unimplemented!()
+        }
     }
-}
-
-impl ops::Try for B{
-    type Ok = ();
-    type Error = BError;
-
-    fn into_result(self) -> Result<<Self as Try>::Ok, <Self as Try>::Error> {
-        unimplemented!()
+    
+    impl ops::Try for B{
+        type Ok = ();
+        type Error = BError;
+    
+        fn into_result(self) -> Result<<Self as Try>::Ok, <Self as Try>::Error> {
+            unimplemented!()
+        }
+    
+        fn from_error(v: <Self as Try>::Error) -> Self {
+            unimplemented!()
+        }
+    
+        fn from_ok(v: <Self as Try>::Ok) -> Self {
+            unimplemented!()
+        }
     }
-
-    fn from_error(v: <Self as Try>::Error) -> Self {
-        unimplemented!()
+    impl ops::Try for A{
+        type Ok = ();
+        type Error = AError;
+    
+    
+        fn into_result(self) -> Result<<Self as Try>::Ok, <Self as Try>::Error> {
+            unimplemented!()
+        }
+    
+        fn from_error(v: <Self as Try>::Error) -> Self {
+            unimplemented!()
+        }
+    
+        fn from_ok(v: <Self as Try>::Ok) -> Self {
+            unimplemented!()
+        }
     }
-
-    fn from_ok(v: <Self as Try>::Ok) -> Self {
-        unimplemented!()
+    
+    fn foo()->A{ A{} }
+    fn bar()->B{ foo()?<caret>; B{} }
+    fn main(){
+        bar();
     }
-}
-impl ops::Try for A{
-    type Ok = ();
-    type Error = AError;
-
-
-    fn into_result(self) -> Result<<Self as Try>::Ok, <Self as Try>::Error> {
-        unimplemented!()
-    }
-
-    fn from_error(v: <Self as Try>::Error) -> Self {
-        unimplemented!()
-    }
-
-    fn from_ok(v: <Self as Try>::Ok) -> Self {
-        unimplemented!()
-    }
-}
-
-fn foo()->A{A{}}
-fn bar()->B{foo()?<caret>; B{}}
-fn main(){
-    bar();
-}
-
-
-        """
-    )
+    """)
 
     @MockRustcVersion("1.29.0")
     fun `test non available in stable`() = checkFixIsUnavailable(
         "Create 'From<AError>' for type 'BError'", """
-use std::ops;
-use std::ops::Try;
-struct A; struct AError;
-struct B; struct BError;
+    use std::ops;
+    use std::ops::Try;
+    struct A;
+    struct AError;
+    struct B;
+    struct BError;
 
-
-
-impl ops::Try for B{
-    type Ok = ();
-    type Error = BError;
-
-    fn into_result(self) -> Result<<Self as Try>::Ok, <Self as Try>::Error> {
-        unimplemented!()
+    impl ops::Try for B{
+        type Ok = ();
+        type Error = BError;
+    
+        fn into_result(self) -> Result<<Self as Try>::Ok, <Self as Try>::Error> {
+            unimplemented!()
+        }
+    
+        fn from_error(v: <Self as Try>::Error) -> Self {
+            unimplemented!()
+        }
+    
+        fn from_ok(v: <Self as Try>::Ok) -> Self {
+            unimplemented!()
+        }
     }
-
-    fn from_error(v: <Self as Try>::Error) -> Self {
-        unimplemented!()
+    impl ops::Try for A{
+        type Ok = ();
+        type Error = AError;
+    
+    
+        fn into_result(self) -> Result<<Self as Try>::Ok, <Self as Try>::Error> {
+            unimplemented!()
+        }
+    
+        fn from_error(v: <Self as Try>::Error) -> Self {
+            unimplemented!()
+        }
+    
+        fn from_ok(v: <Self as Try>::Ok) -> Self {
+            unimplemented!()
+        }
     }
-
-    fn from_ok(v: <Self as Try>::Ok) -> Self {
-        unimplemented!()
+    
+    fn foo()->A{ A{} }
+    fn bar()->B{ foo()?<caret>; B{} }
+    fn main(){
+        bar();
     }
-}
-impl ops::Try for A{
-    type Ok = ();
-    type Error = AError;
-
-
-    fn into_result(self) -> Result<<Self as Try>::Ok, <Self as Try>::Error> {
-        unimplemented!()
-    }
-
-    fn from_error(v: <Self as Try>::Error) -> Self {
-        unimplemented!()
-    }
-
-    fn from_ok(v: <Self as Try>::Ok) -> Self {
-        unimplemented!()
-    }
-}
-
-fn foo()->A{A{}}
-fn bar()->B{foo()?<caret>; B{}}
-fn main(){
-    bar();
-}
-
-
-        """
-    )
+    """)
 
     @MockRustcVersion("1.29.0-nightly")
     fun testSimple() = checkFixByText("Create 'From<AError>' for type 'BError'", """
-#![feature(try_trait)]
-use std::ops;
-use std::ops::Try;
-struct A; struct AError;
-struct B; struct BError;
-
-
-impl ops::Try for B{
-    type Ok = ();
-    type Error = BError;
-
-    fn into_result(self) -> Result<<Self as Try>::Ok, <Self as Try>::Error> {
-        unimplemented!()
+    #![feature(try_trait)]
+    use std::ops;
+    use std::ops::Try;
+    struct A;
+    struct AError;
+    struct B;
+    struct BError;
+    
+    
+    impl ops::Try for B{
+        type Ok = ();
+        type Error = BError;
+    
+        fn into_result(self) -> Result<<Self as Try>::Ok, <Self as Try>::Error> {
+            unimplemented!()
+        }
+    
+        fn from_error(v: <Self as Try>::Error) -> Self {
+            unimplemented!()
+        }
+    
+        fn from_ok(v: <Self as Try>::Ok) -> Self {
+            unimplemented!()
+        }
     }
-
-    fn from_error(v: <Self as Try>::Error) -> Self {
-        unimplemented!()
+    impl ops::Try for A{
+        type Ok = ();
+        type Error = AError;
+    
+    
+        fn into_result(self) -> Result<<Self as Try>::Ok, <Self as Try>::Error> {
+            unimplemented!()
+        }
+    
+        fn from_error(v: <Self as Try>::Error) -> Self {
+            unimplemented!()
+        }
+    
+        fn from_ok(v: <Self as Try>::Ok) -> Self {
+            unimplemented!()
+        }
     }
-
-    fn from_ok(v: <Self as Try>::Ok) -> Self {
-        unimplemented!()
+    
+    fn foo()->A{ A{} }
+    fn bar()->B{ foo()<error descr="the trait `std::convert::From<AError>` is not implemented for `BError`">?<caret></error>; B{} }
+    fn main(){
+        bar();
     }
-}
-impl ops::Try for A{
-    type Ok = ();
-    type Error = AError;
-
-
-    fn into_result(self) -> Result<<Self as Try>::Ok, <Self as Try>::Error> {
-        unimplemented!()
-    }
-
-    fn from_error(v: <Self as Try>::Error) -> Self {
-        unimplemented!()
-    }
-
-    fn from_ok(v: <Self as Try>::Ok) -> Self {
-        unimplemented!()
-    }
-}
-
-fn foo()->A{A{}}
-fn bar()->B{foo()<error descr="the trait `std::convert::From<AError>` is not implemented for `BError`">?<caret></error>; B{}}
-fn main(){
-    bar();
-}
     """,
         """
-#![feature(try_trait)]
-use std::ops;
-use std::ops::Try;
-struct A; struct AError;
-struct B; struct BError;
+    #![feature(try_trait)]
+    use std::ops;
+    use std::ops::Try;
+    struct A;
+    struct AError;
+    struct B;
+    struct BError;
 
-impl From<AError> for BError {
-    fn from(_: AError) -> Self {
-        unimplemented!()
-    }
-}
-
-
-impl ops::Try for B{
-    type Ok = ();
-    type Error = BError;
-
-    fn into_result(self) -> Result<<Self as Try>::Ok, <Self as Try>::Error> {
-        unimplemented!()
+    impl From<AError> for BError {
+        fn from(_: AError) -> Self {
+            unimplemented!()
+        }
     }
 
-    fn from_error(v: <Self as Try>::Error) -> Self {
-        unimplemented!()
+
+    impl ops::Try for B{
+        type Ok = ();
+        type Error = BError;
+
+        fn into_result(self) -> Result<<Self as Try>::Ok, <Self as Try>::Error> {
+            unimplemented!()
+        }
+
+        fn from_error(v: <Self as Try>::Error) -> Self {
+            unimplemented!()
+        }
+
+        fn from_ok(v: <Self as Try>::Ok) -> Self {
+            unimplemented!()
+        }
+    }
+    impl ops::Try for A{
+        type Ok = ();
+        type Error = AError;
+
+
+        fn into_result(self) -> Result<<Self as Try>::Ok, <Self as Try>::Error> {
+            unimplemented!()
+        }
+
+        fn from_error(v: <Self as Try>::Error) -> Self {
+            unimplemented!()
+        }
+
+        fn from_ok(v: <Self as Try>::Ok) -> Self {
+            unimplemented!()
+        }
     }
 
-    fn from_ok(v: <Self as Try>::Ok) -> Self {
-        unimplemented!()
+    fn foo()->A{ A{} }
+    fn bar()->B{ foo()?<caret>; B{} }
+    fn main(){
+        bar();
     }
-}
-impl ops::Try for A{
-    type Ok = ();
-    type Error = AError;
-
-
-    fn into_result(self) -> Result<<Self as Try>::Ok, <Self as Try>::Error> {
-        unimplemented!()
-    }
-
-    fn from_error(v: <Self as Try>::Error) -> Self {
-        unimplemented!()
-    }
-
-    fn from_ok(v: <Self as Try>::Ok) -> Self {
-        unimplemented!()
-    }
-}
-
-fn foo()->A{A{}}
-fn bar()->B{foo()?<caret>; B{}}
-fn main(){
-    bar();
-}
-        """)
+    """)
 }
