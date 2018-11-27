2a0042d716d18e47490cdc9966cf45dd9e23c14f
mchernyavsky <chemike47@gmail.com>
2018-11-27 22:51:36 +0300

    T: Tests for Cargo `Run Anything`

---
+++ src/183/test/kotlin/org/rust/ide/runAnything/CargoRunAnythingProviderTest.kt
@@ -0,0 +1,53 @@
+/*
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
+package org.rust.ide.runAnything
+
+import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.actionSystem.impl.SimpleDataContext
+import org.rust.EmptyDescriptor
+import org.rust.ProjectDescriptor
+import org.rust.RsTestBase
+
+class CargoRunAnythingProviderTest : RsTestBase() {
+    private val provider: CargoRunAnythingProvider = CargoRunAnythingProvider()
+    private lateinit var dataContext: DataContext
+
+    override fun setUp() {
+        super.setUp()
+        dataContext = SimpleDataContext.getProjectContext(project)
+    }
+
+    @ProjectDescriptor(EmptyDescriptor::class)
+    fun `test do not provide values no rust project linked`() {
+        assertEmpty(provider.getValues(dataContext, "cargo "))
+    }
+
+    fun `test do not provide values for non-cargo command patterns`() {
+        assertEmpty(provider.getValues(dataContext, ""))
+    }
+
+    fun `test command completion`() {
+        val commands = listOf(
+            "build", "check", "clean", "doc", "run", "test", "bench", "update", "search", "publish", "install"
+        ).map { "cargo $it" }
+        assertSameElements(provider.getValues(dataContext, "cargo"), commands)
+        assertSameElements(provider.getValues(dataContext, "cargo "), commands)
+        assertSameElements(provider.getValues(dataContext, "cargo ru"), commands)
+        assertSameElements(provider.getValues(dataContext, "cargo run"), commands)
+    }
+
+    fun `test options completion`() {
+        val runOptions = listOf(
+            "--bin", "--example", "--package", "--jobs", "--release", "--manifest-path", "--verbose", "--quiet",
+            "--features", "--all-features", "--no-default-features"
+        ).map { "cargo run $it" }
+        assertSameElements(provider.getValues(dataContext, "cargo run "), runOptions)
+        assertSameElements(provider.getValues(dataContext, "cargo run -"), runOptions)
+        assertSameElements(provider.getValues(dataContext, "cargo run --"), runOptions)
+        assertSameElements(provider.getValues(dataContext, "cargo run --feat"), runOptions)
+        assertSameElements(provider.getValues(dataContext, "cargo run feat"), runOptions)
+    }
+}
