d40dfb931b177b93415c0ca515ff697a8be1cfa6
mchernyavsky <chemike47@gmail.com>
2018-12-29 18:38:33 +0300

    INT: Add "Put parameters on one line" intention

---
+++ src/main/kotlin/org/rust/ide/intentions/ListIntention.kt
@@ -0,0 +1,46 @@
+/*
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
+package org.rust.ide.intentions
+
+import com.intellij.psi.PsiComment
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiWhiteSpace
+import com.intellij.psi.util.PsiTreeUtil
+import org.rust.lang.core.psi.ext.RsElement
+import org.rust.lang.core.psi.ext.leftSiblings
+import org.rust.lang.core.psi.ext.rightSiblings
+
+abstract class ListIntentionBase<TList : RsElement, TElement : RsElement>(
+    private val listClass: Class<TList>,
+    private val elementClass: Class<TElement>,
+    intentionText: String
+) : RsElementBaseIntentionAction<TList>() {
+
+    init {
+        text = intentionText
+    }
+
+    override fun getFamilyName(): String = text
+
+    protected val PsiElement.listContext: TList?
+        get() = PsiTreeUtil.getParentOfType(this, listClass, true)
+
+    protected val TList.elements: List<TElement>
+        get() = PsiTreeUtil.getChildrenOfTypeAsList(this, elementClass)
+
+    protected fun hasLineBreakAfter(element: TElement): Boolean = nextBreak(element) != null
+
+    protected fun nextBreak(element: TElement): PsiWhiteSpace? = element.rightSiblings.lineBreak()
+
+    protected fun hasLineBreakBefore(element: TElement): Boolean = prevBreak(element) != null
+
+    protected fun prevBreak(element: TElement): PsiWhiteSpace? = element.leftSiblings.lineBreak()
+
+    private fun Sequence<PsiElement>.lineBreak(): PsiWhiteSpace? =
+        dropWhile { it !is PsiWhiteSpace && it !is PsiComment }
+            .takeWhile { it is PsiWhiteSpace || it is PsiComment }
+            .firstOrNull { it is PsiWhiteSpace && it.textContains('\n') } as? PsiWhiteSpace
+}
