500b260ac7e1c4f00ae3d54d4400cade60ae038c
Arseniy Pendryak <a.pendryak@yandex.ru>
2018-12-31 19:36:47 +0300

    GRD: new module structure
    Create separate gradle module for each set of dependencies

--- toml/src/test/kotlin/org/rust/toml/CargoTomlCompletionEditionTest.kt
+++ toml/src/test/kotlin/org/rust/toml/CargoTomlCompletionEditionTest.kt
@@ -21,76 +21,61 @@ class CargoTomlCompletionEditionTest(private val sectionName: String) : CargoTom
     fun after() = tearDown()
 
     @Test
-    fun `key completion`() {
-        doSingleCompletion("""
+    fun `key completion`() = doSingleCompletion("""
         $sectionName
         edi<caret>
     """, """
         $sectionName
         edition<caret>
-    """
-        )
-    }
+    """)
 
     @Test
-    fun `value 2018 completion without quotes`() {
-        doSingleCompletion("""
+    fun `value 2018 completion without quotes`() = doSingleCompletion("""
         $sectionName
         edition = 8<caret>
     """, """
         $sectionName
         edition = "2018"
     """)
-    }
 
     @Test
-    fun `value 2018 completion with quotes`() {
-        doSingleCompletion("""
+    fun `value 2018 completion with quotes`() = doSingleCompletion("""
         $sectionName
         edition = "8<caret>"
     """, """
         $sectionName
         edition = "2018"
     """)
-    }
 
     @Test
-    fun `value 2015 completion without quotes`() {
-        doSingleCompletion("""
+    fun `value 2015 completion without quotes`() = doSingleCompletion("""
         $sectionName
         edition = 5<caret>
     """, """
         $sectionName
         edition = "2015"
     """)
-    }
 
     @Test
-    fun `value 2015 completion with quotes`() {
-        doSingleCompletion("""
+    fun `value 2015 completion with quotes`() = doSingleCompletion("""
         $sectionName
         edition = "5<caret>"
     """, """
         $sectionName
         edition = "2015"
     """)
-    }
 
     @Test
-    fun `other values are not completed`() {
-        checkNoCompletion("""
+    fun `other values are not completed`() = checkNoCompletion("""
         $sectionName
         edition = "3<caret>"
     """)
-    }
 
     @Test
-    fun `values for different keys are not completed`() {
-        checkNoCompletion("""
+    fun `values for different keys are not completed`() = checkNoCompletion("""
         $sectionName
         edidition = "8<caret>"
     """)
-    }
 
     companion object {
         @Parameterized.Parameters(name = "{index}: 'edition' completion in \"{0}\" cargo toml section")
