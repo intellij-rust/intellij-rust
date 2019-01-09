b78e5ebb59604eeb800dcd93fb2177f289d651fb
Kirill Bulatov <mail4score@gmail.com>
2019-01-09 23:54:48 +0200

    Run clippy for with project's channel

--- src/main/kotlin/org/rust/cargo/runconfig/command/RunClippyAction.kt
+++ src/main/kotlin/org/rust/cargo/runconfig/command/RunClippyAction.kt
@@ -7,7 +7,6 @@ package org.rust.cargo.runconfig.command
 
 import com.intellij.openapi.actionSystem.AnActionEvent
 import org.rust.cargo.icons.CargoIcons
-import org.rust.cargo.project.settings.toolchain
 import org.rust.cargo.runconfig.getAppropriateCargoProject
 import org.rust.cargo.toolchain.CargoCommandLine
 import org.rust.cargo.toolchain.RustChannel
@@ -17,9 +16,8 @@ class RunClippyAction : RunCargoCommandActionBase(CargoIcons.CLIPPY) {
 
     override fun actionPerformed(e: AnActionEvent) {
         val project = e.project ?: return
-        val toolchain = project.toolchain ?: return
         val cargoProject = getAppropriateCargoProject(e.dataContext) ?: return
-        val channel = if (toolchain.isRustupAvailable) RustChannel.NIGHTLY else RustChannel.DEFAULT
+        val channel = cargoProject.rustcInfo?.version?.channel ?: RustChannel.DEFAULT
         CargoCommandLine.forProject(cargoProject, "clippy", channel = channel).run(project, cargoProject)
     }
 }
