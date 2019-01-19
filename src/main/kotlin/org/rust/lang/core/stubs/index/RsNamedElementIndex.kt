953c9373534ce3656fa0de7f9a8c68611f2e97f7
vlad20012 <beskvlad@gmail.com>
2019-01-19 15:57:28 +0300

    TY&RES: refactor derive implementation
    Now it's possible to support non-std derives

--- src/main/kotlin/org/rust/lang/core/stubs/index/RsNamedElementIndex.kt
+++ src/main/kotlin/org/rust/lang/core/stubs/index/RsNamedElementIndex.kt
@@ -9,13 +9,8 @@ import com.intellij.openapi.project.Project
 import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.stubs.StringStubIndexExtension
 import com.intellij.psi.stubs.StubIndexKey
-import org.rust.cargo.project.workspace.PackageOrigin
-import org.rust.lang.core.psi.RsTraitItem
 import org.rust.lang.core.psi.ext.RsNamedElement
-import org.rust.lang.core.psi.ext.containingCargoPackage
-import org.rust.lang.core.resolve.STD_DERIVABLE_TRAITS
 import org.rust.lang.core.stubs.RsFileStub
-import org.rust.openapiext.ProjectCache
 import org.rust.openapiext.getElements
 
 class RsNamedElementIndex : StringStubIndexExtension<RsNamedElement>() {
@@ -25,18 +20,6 @@ class RsNamedElementIndex : StringStubIndexExtension<RsNamedElement>() {
     companion object {
         val KEY: StubIndexKey<String, RsNamedElement> =
             StubIndexKey.createIndexKey("org.rust.lang.core.stubs.index.RustNamedElementIndex")
-
-        private val derivableTraitsCache = ProjectCache<String, Collection<RsTraitItem>>("derivableTraitsCache")
-        fun findDerivableTraits(project: Project, target: String): Collection<RsTraitItem> =
-            derivableTraitsCache.getOrPut(project, target) {
-                val stdTrait = STD_DERIVABLE_TRAITS[target]
-                getElements(KEY, target, project, GlobalSearchScope.allScope(project))
-                    .mapNotNull { it as? RsTraitItem }
-                    .filter { e ->
-                        if (stdTrait == null) return@filter true
-                        e.containingCargoPackage?.origin == PackageOrigin.STDLIB && e.containingMod.modName == stdTrait.modName
-                    }
-            }
 
         fun findElementsByName(
             project: Project,
