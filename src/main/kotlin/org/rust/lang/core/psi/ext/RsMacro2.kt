fd49360a73103e24d01b7c3f58bb6f0be24d81db
Arseniy Pendryak <a.pendryak@yandex.ru>
2019-01-24 02:41:31 +0300

    GRAM&STUB: implement common necessary interfaces and stub for macro 2.0

---
+++ src/main/kotlin/org/rust/lang/core/psi/ext/RsMacro2.kt
@@ -0,0 +1,28 @@
+/*
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
+package org.rust.lang.core.psi.ext
+
+import com.intellij.lang.ASTNode
+import com.intellij.psi.stubs.IStubElementType
+import org.rust.ide.icons.RsIcons
+import org.rust.lang.core.psi.RsMacro2
+import org.rust.lang.core.psi.RsPsiImplUtil
+import org.rust.lang.core.stubs.RsMacro2Stub
+import javax.swing.Icon
+
+abstract class RsMacro2ImplMixin : RsStubbedNamedElementImpl<RsMacro2Stub>,
+                                   RsMacro2 {
+
+    constructor(node: ASTNode) : super(node)
+
+    constructor(stub: RsMacro2Stub, elementType: IStubElementType<*, *>) : super(stub, elementType)
+
+    override fun getIcon(flags: Int): Icon? = RsIcons.MACRO
+
+    override val isPublic: Boolean get() = RsPsiImplUtil.isPublic(this, stub)
+
+    override val crateRelativePath: String? get() = RsPsiImplUtil.crateRelativePath(this)
+}
