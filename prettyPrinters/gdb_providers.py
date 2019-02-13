fe2a6a8116e0aa817bf4a47ac32442a99ec7a9f9
ortem <ortem00@gmail.com>
2019-02-13 13:28:19 +0300

    DBG: add bundled printers for gdb

---
+++ prettyPrinters/gdb_providers.py
@@ -0,0 +1,250 @@
+import gdb
+from gdb import lookup_type
+from sys import version_info
+
+if version_info[0] >= 3:
+    xrange = range
+
+ZERO_FIELD = "__0"
+
+
+def unwrap_unique_or_non_null(unique_or_nonnull):
+    # BACKCOMPAT: rust 1.32 https://github.com/rust-lang/rust/commit/7a0911528058e87d22ea305695f4047572c5e067
+    ptr = unique_or_nonnull["pointer"]
+    return ptr if ptr.type.code == gdb.TYPE_CODE_PTR else ptr[ZERO_FIELD]
+
+
+class StructProvider:
+    def __init__(self, valobj, is_variant=False):
+        self.valobj = valobj
+        self.is_variant = is_variant
+        fields = self.valobj.type.fields()
+        self.fields = fields[1:] if is_variant else fields
+
+    def children(self):
+        for i, field in enumerate(self.fields):
+            yield ((field.name, self.valobj[field.name]))
+
+
+class TupleProvider:
+    def __init__(self, valobj, is_variant=False):
+        self.valobj = valobj
+        self.is_variant = is_variant
+        fields = self.valobj.type.fields()
+        self.fields = fields[1:] if is_variant else fields
+
+    def to_string(self):
+        return "size=" + str(len(self.fields))
+
+    def children(self):
+        for i, field in enumerate(self.fields):
+            yield ((str(i), self.valobj[field.name]))
+
+    @staticmethod
+    def display_hint():
+        return "array"
+
+
+class StdStringProvider:
+    def __init__(self, valobj):
+        self.valobj = valobj
+        vec = valobj["vec"]
+        self.length = int(vec["len"])
+        self.data_ptr = vec["buf"]["ptr"]["pointer"][ZERO_FIELD]
+
+    def to_string(self):
+        return self.data_ptr.lazy_string(encoding="utf-8", length=self.length)
+
+    @staticmethod
+    def display_hint():
+        return "string"
+
+
+class StdOsStringProvider:
+    def __init__(self, valobj):
+        self.valobj = valobj
+        buf = self.valobj["inner"]["inner"]
+        is_windows = "Wtf8Buf" in buf.type.name
+        vec = buf[ZERO_FIELD] if is_windows else buf
+
+        self.length = int(vec["len"])
+        self.data_ptr = vec["buf"]["ptr"]["pointer"][ZERO_FIELD]
+
+    def to_string(self):
+        return self.data_ptr.lazy_string(encoding="utf-8", length=self.length)
+
+    def display_hint(self):
+        return "string"
+
+
+class StdStrProvider:
+    def __init__(self, valobj):
+        self.valobj = valobj
+        self.length = int(valobj["length"])
+        self.data_ptr = valobj["data_ptr"]
+
+    def to_string(self):
+        return self.data_ptr.lazy_string(encoding="utf-8", length=self.length)
+
+    @staticmethod
+    def display_hint():
+        return "string"
+
+
+class StdVecProvider:
+    def __init__(self, valobj):
+        self.valobj = valobj
+        self.length = int(valobj["len"])
+        self.data_ptr = unwrap_unique_or_non_null(valobj["buf"]["ptr"])
+
+    def to_string(self):
+        return "size=" + str(self.length)
+
+    def children(self):
+        for index in xrange(0, self.length):
+            yield ("[{}]".format(index), (self.data_ptr + index).dereference())
+
+    @staticmethod
+    def display_hint():
+        return "array"
+
+
+class StdVecDequeProvider:
+    def __init__(self, valobj):
+        self.valobj = valobj
+        self.head = int(valobj["head"])
+        self.tail = int(valobj["tail"])
+        self.cap = int(valobj["buf"]["cap"])
+        self.data_ptr = unwrap_unique_or_non_null(valobj["buf"]["ptr"])
+        self.size = self.head - self.tail if self.head >= self.tail else self.cap + self.head - self.tail
+
+    def to_string(self):
+        return "size=" + str(self.size)
+
+    def children(self):
+        for index in xrange(0, self.size):
+            yield ("[{}]".format(index), (self.data_ptr + ((self.tail + index) % self.cap)).dereference())
+
+    @staticmethod
+    def display_hint():
+        return "array"
+
+
+class StdRcProvider:
+    def __init__(self, valobj, is_atomic=False):
+        self.valobj = valobj
+        self.ptr = unwrap_unique_or_non_null(valobj["ptr"])
+        self.value = self.ptr["data" if is_atomic else "value"]
+        self.strong = self.ptr["strong"]["v" if is_atomic else "value"]["value"]
+        self.weak = self.ptr["weak"]["v" if is_atomic else "value"]["value"] - 1
+
+    def to_string(self):
+        return "strong={}, weak={}".format(int(self.strong), int(self.weak))
+
+    def children(self):
+        yield ("value", self.value)
+        yield ("strong", self.strong)
+        yield ("weak", self.weak)
+
+
+class StdCellProvider:
+    def __init__(self, valobj):
+        self.value = valobj["value"]["value"]
+
+    def children(self):
+        yield ("value", self.value)
+
+
+class StdRefProvider:
+    def __init__(self, valobj):
+        self.value = valobj["value"].dereference()
+        self.borrow = valobj["borrow"]["borrow"]["value"]["value"]
+
+    def to_string(self):
+        borrow = int(self.borrow)
+        return "borrow={}".format(borrow) if borrow >= 0 else "borrow_mut={}".format(-borrow)
+
+    def children(self):
+        yield ("*value", self.value)
+        yield ("borrow", self.borrow)
+
+
+class StdRefCellProvider:
+    def __init__(self, valobj):
+        self.value = valobj["value"]["value"]
+        self.borrow = valobj["borrow"]["value"]["value"]
+
+    def to_string(self):
+        borrow = int(self.borrow)
+        return "borrow={}".format(borrow) if borrow >= 0 else "borrow_mut={}".format(-borrow)
+
+    def children(self):
+        yield ("value", self.value)
+        yield ("borrow", self.borrow)
+
+
+# Yield each key (and optionally value) from a BoxedNode.
+def children_of_node(boxed_node, height, want_values):
+    def cast_to_internal(node):
+        internal_type_name = str(node.type.target()).replace('LeafNode', 'InternalNode')
+        internal_type = lookup_type(internal_type_name)
+        return node.cast(internal_type.pointer())
+
+    ptr = boxed_node['ptr']['pointer']
+    node = ptr[ZERO_FIELD]
+    node = cast_to_internal(node) if height > 0 else node
+    leaf = node['data'] if height > 0 else node.dereference()
+    keys = leaf['keys']['value']['value']
+    values = leaf['vals']['value']['value']
+    length = int(leaf['len'])
+
+    for i in xrange(0, length + 1):
+        if height > 0:
+            for child in children_of_node(node['edges'][i], height - 1, want_values):
+                yield child
+        if i < length:
+            if want_values:
+                yield (keys[i], values[i])
+            else:
+                yield keys[i]
+
+
+class StdBTreeSetProvider:
+    def __init__(self, valobj):
+        self.valobj = valobj
+
+    def to_string(self):
+        return "size=".format(self.valobj["map"]["length"])
+
+    def children(self):
+        root = self.valobj["map"]["root"]
+        node_ptr = root["node"]
+        i = 0
+        for child in children_of_node(node_ptr, root["height"], want_values=False):
+            yield ("[{}]".format(i), child)
+            i = i + 1
+
+    @staticmethod
+    def display_hint():
+        return "array"
+
+
+class StdBTreeMapProvider:
+    def __init__(self, valobj):
+        self.valobj = valobj
+
+    def to_string(self):
+        return "size=".format(self.valobj["length"])
+
+    def children(self):
+        root = self.valobj["root"]
+        node_ptr = root["node"]
+        i = 0
+        for child in children_of_node(node_ptr, root["height"], want_values=True):
+            yield ("key{}".format(i), child[0])
+            yield ("val{}".format(i), child[1])
+            i = i + 1
+
+    @staticmethod
+    def display_hint():
+        return "map"
