237bb93bef71d475690481d7fa9381cabecfa08e
Arseniy Pendryak <a.pendryak@yandex.ru>
2019-03-03 02:33:27 +0300

    T: test diff values for failed comparison tests

---
+++ src/test/kotlin/org/rustSlowTests/cargo/runconfig/test/CargoTestDiffTest.kt
@@ -0,0 +1,76 @@
+/*
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
+package org.rustSlowTests.cargo.runconfig.test
+
+import com.intellij.execution.testframework.stacktrace.DiffHyperlink
+import org.intellij.lang.annotations.Language
+
+class CargoTestDiffTest : CargoTestRunnerTestBase() {
+
+    fun `test int diff`() = doAvailableTest("""
+       assert_eq!(1, 2);
+    """, "1", "2")
+
+    fun `test char diff`() = doAvailableTest("""
+       assert_eq!('a', 'c');
+    """, "a", "c")
+
+    fun `test string diff`() = doAvailableTest("""
+       assert_eq!("aaa", "bbb");
+    """, "aaa", "bbb")
+
+    fun `test multiline string diff`() = doAvailableTest("""
+       assert_eq!("a\naa", "bbb");
+    """, "a\naa", "bbb")
+
+    fun `test assert with additional message`() = doAvailableTest("""
+       assert_eq!(1, 2, "`1` != `2`");
+    """, "1", "2")
+
+    fun `test no diff`() = doUnavailableTest("""
+       assert!("aaa" != "aaa");
+    """)
+
+    fun `test no diff for assert_ne`() = doUnavailableTest("""
+       assert_ne!(123, 123);
+    """)
+
+    private fun doAvailableTest(@Language("Rust") testFnText: String, expected: String, actual: String) {
+        val diff = getDiff(testFnText) ?: error("Diff should be not null")
+        assertEquals("Some text", expected, diff.left)
+        assertEquals(actual, diff.right)
+    }
+
+    private fun doUnavailableTest(@Language("Rust") testFnText: String) {
+        val diff = getDiff(testFnText)
+        assertNull(diff)
+    }
+
+    private fun getDiff(testFnText: String): DiffHyperlink? {
+        val testProject = buildProject {
+            toml("Cargo.toml", """
+                [package]
+                name = "sandbox"
+                version = "0.1.0"
+                authors = []
+            """)
+
+            dir("src") {
+                rust("main.rs", """
+                    #[test]
+                    fn test() {/*caret*/
+                        $testFnText
+                    }
+                """)
+            }
+        }
+        myFixture.configureFromTempProjectFile(testProject.fileWithCaret)
+        val configuration = createTestRunConfigurationFromContext()
+        val testNode = executeAndGetTestRoot(configuration).findTestByName("sandbox::test")
+        assertFalse("Test should fail", testNode.isPassed)
+        return testNode.diffViewerProvider
+    }
+}
---
+++ src/test/kotlin/org/rustSlowTests/cargo/runconfig/test/CargoTestRunnerTestBase.kt
@@ -0,0 +1,52 @@
+/*
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
+package org.rustSlowTests.cargo.runconfig.test
+
+import com.intellij.execution.configurations.RunConfiguration
+import com.intellij.execution.testframework.TestProxyRoot
+import com.intellij.execution.testframework.sm.runner.SMTestProxy
+import com.intellij.execution.testframework.sm.runner.ui.SMTRunnerConsoleView
+import com.intellij.openapi.util.Disposer
+import com.intellij.util.ui.UIUtil
+import org.rust.stdext.removeLast
+import org.rustSlowTests.cargo.runconfig.RunConfigurationTestBase
+
+abstract class CargoTestRunnerTestBase : RunConfigurationTestBase() {
+
+    protected fun executeAndGetTestRoot(configuration: RunConfiguration): SMTestProxy.SMRootTestProxy {
+        val result = execute(configuration)
+        val executionConsole = result.executionConsole as SMTRunnerConsoleView
+        val testsRootNode = executionConsole.resultsViewer.testsRootNode
+        with(result.processHandler) {
+            startNotify()
+            waitFor()
+        }
+        UIUtil.dispatchAllInvocationEvents()
+        Disposer.dispose(executionConsole)
+        return testsRootNode
+    }
+
+    protected fun SMTestProxy.SMRootTestProxy.findTestByName(testFullName: String): SMTestProxy {
+        val fullNameBuffer = mutableListOf<String>()
+
+        fun find(test: SMTestProxy): SMTestProxy? {
+            if (test !is TestProxyRoot) {
+                fullNameBuffer.add(test.name)
+            }
+            if (testFullName == fullNameBuffer.joinToString("::")) return test
+            for (child in test.children) {
+                val result = find(child)
+                if (result != null) return result
+            }
+            if (test !is TestProxyRoot) {
+                fullNameBuffer.removeLast()
+            }
+            return null
+        }
+
+        return checkNotNull(find(this)) { "Could not find the test" }
+    }
+}
