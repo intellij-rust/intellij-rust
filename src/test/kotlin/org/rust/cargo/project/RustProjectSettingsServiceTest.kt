d34bf182229bc85f325ed01591ceb78cfba2afb6
mchernyavsky <chemike47@gmail.com>
2019-03-13 17:52:53 +0300

    T: Tests for clippy annotations and quick fixes

--- src/test/kotlin/org/rust/cargo/project/RustProjectSettingsServiceTest.kt
+++ src/test/kotlin/org/rust/cargo/project/RustProjectSettingsServiceTest.kt
@@ -10,6 +10,7 @@ import com.intellij.configurationStore.serialize
 import com.intellij.testFramework.LightPlatformTestCase
 import org.intellij.lang.annotations.Language
 import org.rust.cargo.project.settings.impl.RustProjectSettingsServiceImpl
+import org.rust.cargo.toolchain.ExternalLinter
 import org.rust.cargo.toolchain.RustToolchain
 import org.rust.openapiext.elementFromXmlString
 import org.rust.openapiext.toXmlString
@@ -23,14 +24,15 @@ class RustProjectSettingsServiceTest : LightPlatformTestCase() {
         val text = """
             <State>
               <option name="autoUpdateEnabled" value="false" />
-              <option name="cargoCheckArguments" value="--no-default-features" />
               <option name="compileAllTargets" value="false" />
               <option name="doctestInjectionEnabled" value="false" />
               <option name="expandMacros" value="false" />
               <option name="explicitPathToStdlib" value="/stdlib" />
+              <option name="externalLinter" value="Clippy" />
+              <option name="externalLinterArguments" value="--no-default-features" />
+              <option name="runExternalLinterOnTheFly" value="true" />
               <option name="showTestToolWindow" value="false" />
               <option name="toolchainHomeDirectory" value="/" />
-              <option name="useCargoCheckAnnotator" value="true" />
               <option name="useOffline" value="true" />
               <option name="useSkipChildren" value="true" />
             </State>
@@ -41,9 +43,10 @@ class RustProjectSettingsServiceTest : LightPlatformTestCase() {
 
         assertEquals(RustToolchain(Paths.get("/")), service.toolchain)
         assertEquals(false, service.autoUpdateEnabled)
+        assertEquals(ExternalLinter.CLIPPY, service.externalLinter)
         assertEquals("/stdlib", service.explicitPathToStdlib)
-        assertEquals(true, service.useCargoCheckAnnotator)
-        assertEquals("--no-default-features", service.cargoCheckArguments)
+        assertEquals(true, service.runExternalLinterOnTheFly)
+        assertEquals("--no-default-features", service.externalLinterArguments)
         assertEquals(false, service.compileAllTargets)
         assertEquals(true, service.useOffline)
         assertEquals(false, service.expandMacros)
