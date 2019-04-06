247b70dedf118d6db0f2dc9666e2c83d2fb675d2
Arseniy Pendryak <a.pendryak@yandex.ru>
2019-04-06 22:55:12 +0300

    Generalize const expr evaluation in RsConstExprEvaluator
    Previously, we need to calculate const integer expression only for array size exprs.
    And it was implemented as `calculateArraySize` method and only for `usize` type.
    Now we need it for `Macth Check` inspection
    So these changes generalize `calculateArraySize` for types

--- src/main/kotlin/org/rust/lang/core/psi/ext/RsArrayType.kt
+++ src/main/kotlin/org/rust/lang/core/psi/ext/RsArrayType.kt
@@ -5,67 +5,15 @@
 
 package org.rust.lang.core.psi.ext
 
-import org.rust.lang.core.psi.*
+import org.rust.lang.core.psi.RsArrayType
 import org.rust.lang.core.types.ty.TyInteger
-import org.rust.lang.core.types.ty.TyPrimitive
+import org.rust.lang.utils.evaluation.ExprValue
+import org.rust.lang.utils.evaluation.RsConstExprEvaluator
 
 val RsArrayType.isSlice: Boolean get() = stub?.isSlice ?: (expr == null)
 
-val RsArrayType.arraySize: Long? get() = calculateArraySize(expr)
-
-private const val MAX_EXPR_DEPTH: Int = 64
-
-private val defaultExprPathResolver: (RsPathExpr) -> RsElement? = { it.path.reference.resolve() }
-
-fun calculateArraySize(expr: RsExpr?, pathExprResolver: ((RsPathExpr) -> RsElement?) = defaultExprPathResolver): Long? {
-
-    fun eval(expr: RsExpr?, depth: Int): Long? {
-        // To prevent SO we restrict max depth of expression
-        if (depth >= MAX_EXPR_DEPTH) return null
-        return when (expr) {
-            is RsLitExpr -> expr.integerLiteralValue
-                ?.removeSuffix(TyInteger.USize.name)
-                ?.toLongOrNull()
-                ?.notNegativeOrNull()
-            is RsPathExpr -> {
-                val const = pathExprResolver(expr) as? RsConstant ?: return null
-                if (!const.isConst) return null
-                val path = (const.typeReference?.typeElement as? RsBaseType)?.path ?: return null
-                val integerType = TyPrimitive.fromPath(path) as? TyInteger ?: return null
-                if (integerType == TyInteger.USize) eval(const.expr, depth + 1) else null
-            }
-            is RsParenExpr -> eval(expr.expr, depth + 1)
-            is RsBinaryExpr -> {
-                val op = expr.operatorType as? ArithmeticOp ?: return null
-                val leftValue = eval(expr.left, depth + 1) ?: return null
-                val rightValue = eval(expr.right, depth + 1) ?: return null
-                // TODO: check overflow
-                val result = when (op) {
-                    ArithmeticOp.ADD -> leftValue + rightValue
-                    ArithmeticOp.SUB -> leftValue - rightValue
-                    ArithmeticOp.MUL -> leftValue * rightValue
-                    ArithmeticOp.DIV -> if (rightValue == 0L) null else leftValue / rightValue
-                    ArithmeticOp.REM -> if (rightValue == 0L) null else leftValue % rightValue
-                    ArithmeticOp.BIT_AND -> leftValue and rightValue
-                    ArithmeticOp.BIT_OR -> leftValue or rightValue
-                    ArithmeticOp.BIT_XOR -> leftValue xor rightValue
-                    // We can't simply convert `rightValue` to Int
-                    // because after conversion of quite large Long values (> 2^31 - 1)
-                    // we can get any Int value including negative one
-                    // so it can lead to incorrect result.
-                    // But if `rightValue` >= `java.lang.Long.BYTES`
-                    // we know result without computation:
-                    // overflow in 'shl' case and 0 in 'shr' case.
-                    ArithmeticOp.SHL -> if (rightValue >= java.lang.Long.BYTES) null else leftValue shl rightValue.toInt()
-                    ArithmeticOp.SHR -> if (rightValue >= java.lang.Long.BYTES) 0 else leftValue shr rightValue.toInt()
-                }
-                result?.notNegativeOrNull()
-            }
-            else -> null
-        }
+val RsArrayType.arraySize: Long?
+    get() {
+        val expr = expr ?: return null
+        return (RsConstExprEvaluator.evaluate(expr, TyInteger.USize) as? ExprValue.Integer)?.value
     }
-
-    return eval(expr, 0)
-}
-
-private fun Long.notNegativeOrNull(): Long? = if (this < 0) null else this
