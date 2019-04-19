fbd7bdd781bb59cbc84ed05d9ffe6fa805d98bfa
mchernyavsky <chemike47@gmail.com>
2019-04-19 17:27:58 +0300

    RES: Fix navigation to files and targets (in test tool window)

--- src/main/kotlin/org/rust/cargo/runconfig/test/CargoTestLocator.kt
+++ src/main/kotlin/org/rust/cargo/runconfig/test/CargoTestLocator.kt
@@ -11,11 +11,18 @@ import com.intellij.execution.testframework.sm.runner.SMTestLocator
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
 import com.intellij.psi.search.GlobalSearchScope
+import org.rust.cargo.project.model.cargoProjects
+import org.rust.cargo.project.model.impl.allTargets
+import org.rust.cargo.project.workspace.PackageOrigin
+import org.rust.lang.core.psi.RsFile
 import org.rust.lang.core.psi.RsFunction
+import org.rust.lang.core.psi.RsModDeclItem
 import org.rust.lang.core.psi.ext.RsMod
 import org.rust.lang.core.psi.ext.RsQualifiedNamedElement
 import org.rust.lang.core.psi.ext.qualifiedName
 import org.rust.lang.core.stubs.index.RsNamedElementIndex
+import org.rust.openapiext.toPsiFile
+import org.rust.stdext.buildList
 
 object CargoTestLocator : SMTestLocator {
     private const val NAME_SEPARATOR: String = "::"
@@ -30,14 +37,31 @@ object CargoTestLocator : SMTestLocator {
     ): List<Location<PsiElement>> {
         if (protocol != TEST_PROTOCOL && protocol != SUITE_PROTOCOL) return emptyList()
         val qualifiedName = toQualifiedName(path)
-        val name = qualifiedName.substringAfterLast(NAME_SEPARATOR)
-        return RsNamedElementIndex.findElementsByName(project, name, scope)
-            .asSequence()
-            .filter { it is RsFunction || it is RsMod }
-            .filterIsInstance<RsQualifiedNamedElement>()
-            .filter { it.qualifiedName == qualifiedName }
-            .map { PsiLocation.fromPsiElement<PsiElement>(it) }
-            .toList()
+
+        // `RsQualifiedNamedElement.qualifiedName` starts with Cargo target name, so if the `qualifiedName` doesn't
+        // contain the name separator, we are looking for a target with the corresponding name
+        if (!qualifiedName.contains(NAME_SEPARATOR)) {
+            return project.cargoProjects.allTargets
+                .filter { it.pkg.origin == PackageOrigin.WORKSPACE }
+                .filter { it.normName == qualifiedName }
+                .mapNotNull { it.crateRoot?.toPsiFile(project) }
+                .map { PsiLocation.fromPsiElement<PsiElement>(it) }
+                .toList()
+        }
+
+        return buildList {
+            val name = qualifiedName.substringAfterLast(NAME_SEPARATOR)
+            for (element in RsNamedElementIndex.findElementsByName(project, name, scope)) {
+                val sourceElement = when (element) {
+                    is RsFunction, is RsMod -> element as? RsQualifiedNamedElement
+                    is RsModDeclItem -> element.reference.resolve() as? RsFile
+                    else -> null
+                }
+                if (sourceElement?.qualifiedName == qualifiedName) {
+                    add(PsiLocation.fromPsiElement(sourceElement))
+                }
+            }
+        }
     }
 
     fun getTestFnUrl(name: String): String = "$TEST_PROTOCOL://$name"
@@ -52,6 +76,7 @@ object CargoTestLocator : SMTestLocator {
 
     private fun toQualifiedName(path: String): String {
         val targetName = path.substringBefore(NAME_SEPARATOR).substringBeforeLast("-")
+        if (!path.contains(NAME_SEPARATOR)) return targetName
         val qualifiedName = path.substringAfter(NAME_SEPARATOR)
         return "$targetName$NAME_SEPARATOR$qualifiedName"
     }
