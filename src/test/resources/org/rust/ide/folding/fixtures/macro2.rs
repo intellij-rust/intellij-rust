bc1b76b2c2adcbab20dfd658b249619d3f99c5ba
ice1000 <ice1000kotlin@foxmail.com>
2019-04-25 09:53:00 -0400

    TYPE: Fold macro2's braces even when there are parenthesises
    Signed-off-by: ice1000 <ice1000kotlin@foxmail.com>

--- src/test/resources/org/rust/ide/folding/fixtures/macro2.rs
+++ src/test/resources/org/rust/ide/folding/fixtures/macro2.rs
@@ -1,10 +1,10 @@
-macro n<fold text='{...}'>($foo:ident, $S:ident, $i:ident, $m:ident)</fold> {
+macro n<fold text='{...}'>($foo:ident, $S:ident, $i:ident, $m:ident)</fold> <fold text='{...}'>{
     mod $foo <fold text='{...}'>{
         #<fold text='{...}'>[derive<fold text='{...}'>(Default)</fold>]</fold>
         pub struct $S <fold text='{...}'>{ $i: u32 }</fold>
         pub macro $m<fold text='{...}'>($e:expr)</fold> <fold text='{...}'>{ $e.$i }</fold>
     }</fold>
-}
+}</fold>
 
 pub macro bar <fold text='{...}'>{
     [$e:expr] => <fold text='{...}'>{
