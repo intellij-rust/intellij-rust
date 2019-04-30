intellij-rust/intellij-rust

commit f0351a46485eef355eff662c2086b311b020be15

PERF&MACRO: optimize `getExpandedFrom`
Now name resolution isn't slower when new macro
expansion engine is enabled

src/main/kotlin/org/rust/lang/core/macros/MacroExpansionManager.kt
@@ -45,10 +45,7 @@ import org.rust.ide.search.RsWithMacrosScope
 import org.rust.lang.RsFileType
 import org.rust.lang.core.psi.*
 import org.rust.lang.core.psi.RsPsiTreeChangeEvent.*
-import org.rust.lang.core.psi.ext.RsElement
-import org.rust.lang.core.psi.ext.containingCargoTarget
-import org.rust.lang.core.psi.ext.resolveToMacro
-import org.rust.lang.core.psi.ext.stubDescendantOfTypeOrSelf
+import org.rust.lang.core.psi.ext.*
 import org.rust.lang.core.resolve.indexes.RsMacroCallIndex
 import org.rust.openapiext.*
 import org.rust.stdext.ThreadLocalDelegate
@@ -586,13 +583,14 @@ private class MacroExpansionServiceImplInner(
 
     fun getExpandedFrom(element: RsExpandedElement): RsMacroCall? {
         checkReadAccessAllowed()
-        if (!element.isValid) return null
-        val file = element.containingFile.virtualFile ?: return null
-        return if (isExpansionFile(file) && element.parent is RsFile /*TODO*/) {
-            storage.getInfoForExpandedFile(file)?.getMacroCall()
-        } else {
-            null
+        if (element.stubParent is RsFile /*TODO*/) {
+            val file = element.containingFile.virtualFile ?: return null
+            if (isExpansionFile(file)) {
+                return storage.getInfoForExpandedFile(file)?.getMacroCall()
+            }
         }
+
+        return null
     }
 
     @TestOnly

src/main/kotlin/org/rust/lang/core/macros/RsExpandedElement.kt
@@ -7,10 +7,10 @@ package org.rust.lang.core.macros
 
 import com.intellij.openapi.util.Key
 import com.intellij.psi.PsiElement
-import com.intellij.psi.StubBasedPsiElement
 import org.rust.lang.core.psi.RsMacroCall
 import org.rust.lang.core.psi.ext.RsElement
 import org.rust.lang.core.psi.ext.ancestors
+import org.rust.lang.core.psi.ext.stubParent
 
 /**
  *  [RsExpandedElement]s are those elements which exist in temporary,
@@ -25,11 +25,7 @@ interface RsExpandedElement : RsElement {
         fun getContextImpl(psi: RsExpandedElement): PsiElement? {
             psi.expandedFrom?.let { return it.context }
             psi.getUserData(RS_EXPANSION_CONTEXT)?.let { return it }
-            if (psi is StubBasedPsiElement<*>) {
-                val stub = psi.stub
-                if (stub != null) return stub.parentStub.psi as RsElement
-            }
-            return psi.parent
+            return psi.stubParent
         }
     }
 }
@@ -47,8 +43,14 @@ fun RsExpandedElement.setExpandedFrom(call: RsMacroCall) {
  * null if this element is not directly produced by a macro
  */
 val RsExpandedElement.expandedFrom: RsMacroCall?
-    get() = project.macroExpansionManager.getExpandedFrom(this)
-        ?: (getUserData(RS_EXPANSION_MACRO_CALL) as? RsMacroCall)
+    get() {
+        val mgr = project.macroExpansionManager
+        return when (mgr.macroExpansionMode) {
+            MacroExpansionMode.Disabled -> null
+            MacroExpansionMode.Old -> getUserData(RS_EXPANSION_MACRO_CALL) as? RsMacroCall
+            is MacroExpansionMode.New -> mgr.getExpandedFrom(this)
+        }
+    }
 
 val RsExpandedElement.expandedFromRecursively: RsMacroCall?
     get() {

src/main/kotlin/org/rust/lang/core/macros/ExpandedMacroStorage.kt
@@ -13,11 +13,13 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.LocalFileSystem
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.VirtualFileManager
+import com.intellij.openapi.vfs.VirtualFileWithId
 import com.intellij.psi.PsiAnchor
 import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiManager
 import com.intellij.psi.StubBasedPsiElement
 import com.intellij.reference.SoftReference
+import gnu.trove.TIntObjectHashMap
 import org.rust.cargo.project.workspace.PackageOrigin
 import org.rust.lang.RsFileType
 import org.rust.lang.core.macros.MacroExpansionManagerImpl.Testmarks
@@ -42,17 +44,17 @@ import java.util.zip.InflaterInputStream
 
 class ExpandedMacroStorage(val project: Project) {
     // Data structures are guarded by the platform RWLock
-    private val sourceFiles: MutableMap<String, SourceFile> = hashMapOf()
-    private val expandedFileToInfo: MutableMap<String, ExpandedMacroInfo> = hashMapOf()
+    private val sourceFiles: TIntObjectHashMap<SourceFile> = TIntObjectHashMap()
+    private val expandedFileToInfo: TIntObjectHashMap<ExpandedMacroInfo> = TIntObjectHashMap()
     private val stepped: Array<MutableList<SourceFile>?> = arrayOfNulls(DEFAULT_RECURSION_LIMIT)
 
-    val isEmpty: Boolean get() = sourceFiles.isEmpty()
+    val isEmpty: Boolean get() = sourceFiles.isEmpty
 
     private fun deserialize(sfs: List<SourceFile>) {
         for (sf in sfs) {
-            sourceFiles[sf.fileUrl] = sf
+            sourceFiles.put(sf.fileId, sf)
             getOrPutStagedList(sf).add(sf)
-            expandedFileToInfo.putAll(sf.getExpansionUrls())
+            sf.forEachInfo { if (it.fileId > 0) expandedFileToInfo.put(it.fileId, it) }
         }
     }
 
@@ -65,7 +67,7 @@ class ExpandedMacroStorage(val project: Project) {
 
     fun processExpandedMacroInfos(action: (ExpandedMacroInfo) -> Unit) {
         checkReadAccessAllowed()
-        expandedFileToInfo.values.forEach(action)
+        expandedFileToInfo.forEachValue { action(it); true }
     }
 
     private fun getOrPutStagedList(sf: SourceFile): MutableList<SourceFile> {
@@ -118,7 +120,7 @@ class ExpandedMacroStorage(val project: Project) {
         }
         val info = ExpandedMacroInfo(
             sourceFile,
-            expansion?.url,
+            expansion,
             def?.bodyHash,
             call.bodyHash,
             oldInfo?.stubIndex ?: -1
@@ -127,7 +129,7 @@ class ExpandedMacroStorage(val project: Project) {
         info.bindTo(call)
 
         sourceFile.addInfo(info)
-        info.expansionFileUrl?.let { expandedFileToInfo[it] = info }
+        if (info.fileId > 0) expandedFileToInfo.put(info.fileId, info)
         info.expansionFile?.let { getOrCreateSourceFile(it) }
 
         return info
@@ -136,35 +138,36 @@ class ExpandedMacroStorage(val project: Project) {
     fun removeInvalidInfo(oldInfo: ExpandedMacroInfo, clean: Boolean) {
         checkWriteAccessAllowed()
 
-        expandedFileToInfo.remove(oldInfo.expansionFileUrl)
+        expandedFileToInfo.remove(oldInfo.fileId)
         val sf = oldInfo.sourceFile
         sf.removeInfo(oldInfo)
         if (clean && sf.isEmpty()) {
-            sourceFiles.remove(sf.fileUrl)
+            sourceFiles.remove(sf.fileId)
             stepped[sf.depth]?.remove(sf)
         }
     }
 
     fun getOrCreateSourceFile(callFile: VirtualFile): SourceFile? {
         checkWriteAccessAllowed()
-        return sourceFiles[callFile.url] ?: run {
+        val fileId = callFile.fileId
+        return sourceFiles[fileId] ?: run {
             val depth = (getInfoForExpandedFile(callFile)?.sourceFile?.depth ?: -1) + 1
             if (depth >= DEFAULT_RECURSION_LIMIT) return null
             val sf = SourceFile(this, callFile, depth)
             getOrPutStagedList(sf).add(sf)
-            sourceFiles[callFile.url] = sf
+            sourceFiles.put(fileId, sf)
             sf
         }
     }
 
     fun getSourceFile(callFile: VirtualFile): SourceFile? {
         checkReadAccessAllowed()
-        return sourceFiles[callFile.url]
+        return sourceFiles[callFile.fileId]
     }
 
     fun getInfoForExpandedFile(file: VirtualFile): ExpandedMacroInfo? {
         checkReadAccessAllowed()
-        return expandedFileToInfo[file.url]
+        return expandedFileToInfo[file.fileId]
     }
 
     fun getInfoForCall(call: RsMacroCall): ExpandedMacroInfo? {
@@ -178,7 +181,7 @@ class ExpandedMacroStorage(val project: Project) {
 
     fun unbindPsi() {
         checkReadAccessAllowed()
-        sourceFiles.values.forEach { it.unbindPsi() }
+        sourceFiles.forEachValue { it.unbindPsi(); true }
     }
 
     companion object {
@@ -217,8 +220,8 @@ class ExpandedMacroStorage(val project: Project) {
                 data.writeInt(STORAGE_VERSION)
                 data.writeInt(RsFileStub.Type.stubVersion)
 
-                data.writeInt(storage.sourceFiles.size)
-                storage.sourceFiles.values.forEach { it.writeTo(data) }
+                data.writeInt(storage.sourceFiles.size())
+                storage.sourceFiles.forEachValue { it.writeTo(data); true }
             }
         }
     }
@@ -234,7 +237,8 @@ class SourceFile(
 ) {
     private var cachedPsi: SoftReference<RsFile>? = null
 
-    val fileUrl: String = file.url
+    private val fileUrl: String get() = file.url
+    val fileId: Int get() = file.fileId
 
     val project: Project
         get() = storage.project
@@ -248,8 +252,9 @@ class SourceFile(
         get() = rootSourceFile.loadPsi()?.containingCargoTarget?.pkg?.origin == PackageOrigin.WORKSPACE
 
     @Synchronized
-    fun getExpansionUrls(): List<Pair<String, ExpandedMacroInfo>> =
-        infos.mapNotNull { info -> info.expansionFileUrl?.let { it to info } }
+    fun forEachInfo(action: (ExpandedMacroInfo) -> Unit) {
+        infos.forEach(action)
+    }
 
     @Synchronized
     fun addInfo(info: ExpandedMacroInfo) {
@@ -474,7 +479,7 @@ class SourceFile(
                 modificationStamp,
                 infos
             )
-            serInfos.mapTo(infos) { it.toExpandedMacroInfo(sf) }
+            serInfos.mapNotNullTo(infos) { it.toExpandedMacroInfo(sf) }
             return sf
         }
     }
@@ -488,12 +493,13 @@ private tailrec fun SourceFile.findRootSourceFile(): SourceFile {
 
 class ExpandedMacroInfo(
     val sourceFile: SourceFile,
-    val expansionFileUrl: String?,
+    val expansionFile: VirtualFile?,
     private val defHash: HashCode?,
     val callHash: HashCode?,
     var stubIndex: Int = -1
 ) {
-    val expansionFile: VirtualFile? by CachedVirtualFile(expansionFileUrl)
+    val expansionFileUrl: String? get() = expansionFile?.url
+    val fileId: Int get() = expansionFile?.fileId ?: -1
 
     @Volatile
     var cachedMacroCall: SoftReference<RsMacroCall>? = null
@@ -549,10 +555,11 @@ private data class SerializedExpandedMacroInfo(
     val defHash: HashCode?,
     val stubIndex: Int
 ) {
-    fun toExpandedMacroInfo(sourceFile: SourceFile): ExpandedMacroInfo {
+    fun toExpandedMacroInfo(sourceFile: SourceFile): ExpandedMacroInfo? {
+        val file = expansionFileUrl?.let { VirtualFileManager.getInstance().findFileByUrl(it) }
         return ExpandedMacroInfo(
             sourceFile,
-            expansionFileUrl,
+            file,
             callHash,
             defHash,
             stubIndex
@@ -621,3 +628,6 @@ private fun calcStubIndex(psi: StubBasedPsiElement<*>): Int {
     }
     return index
 }
+
+private val VirtualFile.fileId: Int
+    get() = (this as VirtualFileWithId).id

src/main/kotlin/org/rust/lang/core/psi/ext/PsiElement.kt
@@ -33,6 +33,15 @@ val PsiElement.ancestorPairs: Sequence<Pair<PsiElement, PsiElement>> get() {
     }
 }
 
+val PsiElement.stubParent: PsiElement
+    get() {
+        if (this is StubBasedPsiElement<*>) {
+            val stub = this.stub
+            if (stub != null) return stub.parentStub.psi
+        }
+        return parent
+    }
+
 val PsiElement.leftLeaves: Sequence<PsiElement>
     get() = generateSequence(this, PsiTreeUtil::prevLeaf).drop(1)
 

commit 4076ed6db61870c182332575c2b21eecf428b0f0

MACRO: fix getExpandedFrom for in-memory expansions
The bug is introduced in #3705.
It's hard to reproduce right now, but it affects #3640

src/main/kotlin/org/rust/lang/core/macros/MacroExpansionManager.kt
@@ -20,6 +20,7 @@ import com.intellij.openapi.progress.impl.ProgressManagerImpl
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ContentIterator
 import com.intellij.openapi.util.Disposer
+import com.intellij.openapi.util.Key
 import com.intellij.openapi.vfs.*
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
@@ -149,8 +150,17 @@ class MacroExpansionManagerImpl(
         }
     }
 
-    override fun getExpandedFrom(element: RsExpandedElement): RsMacroCall? =
-        inner?.getExpandedFrom(element)
+    override fun getExpandedFrom(element: RsExpandedElement): RsMacroCall? {
+        // For in-memory expansions
+        element.getUserData(RS_EXPANSION_MACRO_CALL)?.let { return it as RsMacroCall }
+
+        val inner = inner
+        return if (inner != null && inner.isExpansionModeNew) {
+            inner.getExpandedFrom(element)
+        } else {
+            null
+        }
+    }
 
     override fun isExpansionFile(file: VirtualFile): Boolean =
         inner?.isExpansionFile(file) == true
@@ -752,6 +762,12 @@ private fun expandMacroToMemoryFile(call: RsMacroCall): MacroExpansion? {
     }
 
     return result
+}
+
+private val RS_EXPANSION_MACRO_CALL = Key.create<RsElement>("org.rust.lang.core.psi.RS_EXPANSION_MACRO_CALL")
+
+private fun RsExpandedElement.setExpandedFrom(call: RsMacroCall) {
+    putUserData(RS_EXPANSION_MACRO_CALL, call)
 }
 
 enum class MacroExpansionScope {

src/main/kotlin/org/rust/lang/core/macros/RsExpandedElement.kt
@@ -34,23 +34,12 @@ fun RsExpandedElement.setContext(context: RsElement) {
     putUserData(RS_EXPANSION_CONTEXT, context)
 }
 
-fun RsExpandedElement.setExpandedFrom(call: RsMacroCall) {
-    putUserData(RS_EXPANSION_MACRO_CALL, call)
-}
-
 /**
  * The [RsMacroCall] that directly expanded to this element or
  * null if this element is not directly produced by a macro
  */
 val RsExpandedElement.expandedFrom: RsMacroCall?
-    get() {
-        val mgr = project.macroExpansionManager
-        return when (mgr.macroExpansionMode) {
-            MacroExpansionMode.Disabled -> null
-            MacroExpansionMode.Old -> getUserData(RS_EXPANSION_MACRO_CALL) as? RsMacroCall
-            is MacroExpansionMode.New -> mgr.getExpandedFrom(this)
-        }
-    }
+    get() = project.macroExpansionManager.getExpandedFrom(this)
 
 val RsExpandedElement.expandedFromRecursively: RsMacroCall?
     get() {
@@ -72,5 +61,4 @@ fun PsiElement.findMacroCallExpandedFrom(): RsMacroCall? {
 
 
 private val RS_EXPANSION_CONTEXT = Key.create<RsElement>("org.rust.lang.core.psi.CODE_FRAGMENT_FILE")
-private val RS_EXPANSION_MACRO_CALL = Key.create<RsElement>("org.rust.lang.core.psi.RS_EXPANSION_MACRO_CALL")
 