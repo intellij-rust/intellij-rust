a12d6c69bf1701ec24048d504a078ae62767e863
vlad20012 <beskvlad@yandex.ru>
2019-06-04 18:36:59 +0300

    ANN: fix trait members visibility (false-positive E0603)

--- src/main/kotlin/org/rust/lang/core/psi/ext/RsVisibility.kt
+++ src/main/kotlin/org/rust/lang/core/psi/ext/RsVisibility.kt
@@ -45,8 +45,10 @@ fun RsVisible.isVisibleFrom(mod: RsMod): Boolean {
     val parent = members.context ?: return true
     return when {
         // Associated items in a pub Trait are public by default
-        parent is RsImplItem && parent.traitRef != null -> parent.traitRef?.resolveToTrait()?.isPublic ?: true
-        parent is RsTraitItem && parent.isPublic -> true
+        parent is RsImplItem && parent.traitRef != null -> {
+            parent.traitRef?.resolveToTrait()?.isVisibleFrom(mod) ?: true
+        }
+        parent is RsTraitItem -> parent.isVisibleFrom(mod)
         else -> false
     }
 }
