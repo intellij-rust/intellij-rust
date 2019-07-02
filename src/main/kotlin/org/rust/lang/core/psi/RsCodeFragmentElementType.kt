e380af3c08588ed7c1d2a87075c35e10a54981bf
vlad20012 <beskvlad@gmail.com>
2019-07-02 18:36:05 +0300

    CLEAN: cleanup code fragment element types

--- src/main/kotlin/org/rust/lang/core/psi/RsCodeFragmentElementType.kt
+++ src/main/kotlin/org/rust/lang/core/psi/RsCodeFragmentElementType.kt
@@ -12,17 +12,18 @@ import com.intellij.psi.tree.IElementType
 import org.rust.lang.RsLanguage
 import org.rust.lang.core.parser.RustParser
 
+fun factory(name: String): IElementType = when (name) {
+    "EXPR_CODE_FRAGMENT" -> RsExprCodeFragmentElementType
+    else -> error("Unknown element $name")
+}
+
 abstract class RsCodeFragmentElementTypeBase(debugName: String) : ICodeFragmentElementType(debugName, RsLanguage) {
-    protected abstract val elementType: IElementType
     override fun parseContents(chameleon: ASTNode): ASTNode? {
         val project = chameleon.psi.project
         val builder = PsiBuilderFactory.getInstance().createBuilder(project, chameleon)
-        val root = RustParser().parse(elementType, builder)
+        val root = RustParser().parse(this, builder)
         return root.firstChildNode
     }
 }
 
-fun factory(name: String): IElementType = when (name) {
-    "EXPR_CODE_FRAGMENT" -> RsExprCodeFragmentElementType()
-    else -> error("Unknown element $name")
-}
+object RsExprCodeFragmentElementType : RsCodeFragmentElementTypeBase("RS_EXPR_CODE_FRAGMENT")
