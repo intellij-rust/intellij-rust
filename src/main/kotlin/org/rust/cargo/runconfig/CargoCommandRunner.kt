f6dd639c36c7bccefc4cdd2869b129a3bd1bafe9
Mikhail Chernyavsky <Mikhail.Chernyavsky@jetbrains.com>
2019-08-09 18:22:20 +0300

    RUN: Don't run `build` and `test --no-run` commands

--- src/main/kotlin/org/rust/cargo/runconfig/CargoCommandRunner.kt
+++ src/main/kotlin/org/rust/cargo/runconfig/CargoCommandRunner.kt
@@ -6,9 +6,13 @@
 package org.rust.cargo.runconfig
 
 import com.intellij.execution.configurations.RunProfile
+import com.intellij.execution.configurations.RunProfileState
 import com.intellij.execution.executors.DefaultRunExecutor
 import com.intellij.execution.runners.DefaultProgramRunner
+import com.intellij.execution.runners.ExecutionEnvironment
+import com.intellij.execution.ui.RunContentDescriptor
 import org.rust.cargo.runconfig.buildtool.CargoBuildManager.getBuildConfiguration
+import org.rust.cargo.runconfig.buildtool.CargoBuildManager.isBuildConfiguration
 import org.rust.cargo.runconfig.buildtool.CargoBuildManager.isBuildToolWindowEnabled
 import org.rust.cargo.runconfig.command.CargoCommandConfiguration
 
@@ -21,6 +25,15 @@ open class CargoCommandRunner : DefaultProgramRunner() {
         return profile.project.isBuildToolWindowEnabled ||
             cleaned.cmd.command != "test" ||
             getBuildConfiguration(profile) == null
+    }
+
+    override fun doExecute(state: RunProfileState, environment: ExecutionEnvironment): RunContentDescriptor? {
+        val configuration = environment.runProfile
+        return if (configuration is CargoCommandConfiguration && !isBuildConfiguration(configuration)) {
+            super.doExecute(state, environment)
+        } else {
+            null
+        }
     }
 
     companion object {
