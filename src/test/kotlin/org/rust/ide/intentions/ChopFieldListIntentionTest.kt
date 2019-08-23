a64d832a3279710930e8bcc89e3fe023a64a28f4
Toshiaki Kameyama <kmymtsak@gmail.com>
2019-08-23 14:40:13 +0900

    INT: add "Put fields on separate lines/one line" intentions

---
+++ src/test/kotlin/org/rust/ide/intentions/ChopFieldListIntentionTest.kt
@@ -0,0 +1,92 @@
+/*
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
+package org.rust.ide.intentions
+
+class ChopFieldListIntentionTest : RsIntentionTestBase(ChopFieldListIntention()) {
+    fun `test one parameter`() = doUnavailableTest("""
+        struct S { /*caret*/x: i32 }
+    """)
+
+    fun `test two parameter`() = doAvailableTest("""
+        struct S { /*caret*/x: i32, y: i32 }
+    """, """
+        struct S {
+            x: i32,
+            y: i32
+        }
+    """)
+
+    fun `test has all line breaks`() = doUnavailableTest("""
+        struct S {
+            /*caret*/x: i32,
+            y: i32,
+            z: i32
+        }
+    """)
+
+    fun `test has some line breaks`() = doAvailableTest("""
+        struct S { x: i32, /*caret*/y: i32,
+                   z: i32
+        }
+    """, """
+        struct S {
+            x: i32,
+            y: i32,
+            z: i32
+        }
+    """)
+
+    fun `test has some line breaks 2`() = doAvailableTest("""
+        struct S { 
+            x: i32, y: i32, z: i32/*caret*/
+        }
+    """, """
+        struct S {
+            x: i32,
+            y: i32,
+            z: i32
+        }
+    """)
+
+    fun `test has comment`() = doUnavailableTest("""
+        struct S { 
+            /*caret*/x: i32, /* comment */ 
+            y: i32,
+            z: i32
+        }
+    """)
+
+    fun `test has comment 2`() = doAvailableTest("""
+        struct S { 
+            /*caret*/x: i32, /*
+                comment
+            */y: i32,
+            z: i32
+        }
+    """, """
+        struct S {
+            x: i32, /*
+                comment
+            */
+            y: i32,
+            z: i32
+        }
+    """)
+
+    fun `test has single line comment`() = doAvailableTest("""
+        struct S {
+            /*caret*/x: i32, // comment x
+            y: i32, z: i32 // comment z
+        }
+   """, """
+        struct S {
+            x: i32,
+            // comment x
+            y: i32,
+            z: i32 // comment z
+        }
+    """)
+}
