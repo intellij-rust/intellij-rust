00f55188c96d7cc3c5a132c787aaede82785a286
Arseniy Pendryak <a.pendryak@yandex.ru>
2019-09-20 13:52:51 +0300

    Fix incompatibilities with latest 2019.3 EAPs
    (cherry picked from commit 9766d91f8dba3d2550d7cf218f1967c1c8b32b2d)

--- src/main/kotlin/org/rust/lang/core/completion/RsCommonCompletionProvider.kt
+++ src/main/kotlin/org/rust/lang/core/completion/RsCommonCompletionProvider.kt
@@ -175,7 +175,7 @@ object RsCommonCompletionProvider : CompletionProvider<CompletionParameters>() {
         }
 
         val context = RsCompletionContext(path.implLookup, expectedTy, isSimplePath = true)
-        for (elementName in CompletionUtil.sortMatching(result.prefixMatcher, keys)) {
+        for (elementName in CompletionUtilsExt.sortMatching(result.prefixMatcher, keys)) {
             val candidates = AutoImportFix.getImportCandidates(importContext, elementName, elementName) {
                 !(it.item is RsMod || it.item is RsModDeclItem || it.item.parent is RsMembers)
             }
