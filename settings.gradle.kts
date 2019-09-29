e1532b37edb1ff5590ecbf5e3b63ac51a1e63216
Arseniy Pendryak <a.pendryak@yandex.ru>
2019-09-29 13:19:58 +0300

    GRD: convert `settings.gradle` to kotlin. Step 2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,14 @@
-rootProject.name = 'intellij-rust'
+rootProject.name = "intellij-rust"
 // Special module with run, build and publish tasks
-include 'plugin'
-include 'idea',
-        'clion',
-        'debugger',
-        'toml',
-        'copyright',
-        'coverage',
-        'intelliLang',
-        'duplicates',
-        'intellij-toml'
+include("plugin")
+include(
+    "idea",
+    "clion",
+    "debugger",
+    "toml",
+    "copyright",
+    "coverage",
+    "intelliLang",
+    "duplicates",
+    "intellij-toml"
+)
