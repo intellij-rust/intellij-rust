173b8f5eaf548ef9c336b9b45583a0d4b7146ba7
vlad20012 <beskvlad@gmail.com>
2019-10-24 12:46:47 +0300

    Fix NPE in `RsGeneratedSourceFilter` on 2019.3

--- src/main/kotlin/org/rust/ide/navigation/goto/RsGeneratedSourcesFilter.kt
+++ src/main/kotlin/org/rust/ide/navigation/goto/RsGeneratedSourcesFilter.kt
@@ -8,7 +8,9 @@ package org.rust.ide.navigation.goto
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.GeneratedSourcesFilter
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiDirectory
 import com.intellij.psi.PsiElement
+import org.rust.lang.RsLanguage
 import org.rust.lang.core.macros.findNavigationTargetIfMacroExpansion
 import org.rust.lang.core.macros.macroExpansionManager
 
@@ -18,6 +20,8 @@ class RsGeneratedSourcesFilter : GeneratedSourcesFilter() {
     }
 
     override fun getOriginalElements(element: PsiElement): List<PsiElement> {
+        if (element is PsiDirectory || element.language != RsLanguage) return emptyList()
+
         val result = element.findNavigationTargetIfMacroExpansion() ?: return emptyList()
         return listOf(result)
     }
