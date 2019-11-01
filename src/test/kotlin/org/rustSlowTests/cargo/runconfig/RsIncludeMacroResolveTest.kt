0c3f5eff1197e78f741c62f7595bab0f75319c02
Arseniy Pendryak <a.pendryak@yandex.ru>
2019-11-01 15:16:59 +0300

    MACRO: don't overflow stack while including file search
    The main reason of SO Error was the following construction:
    ```rust
    //- some_name.rs
    include!(concat!(env!("OUT_DIR"), "/some_name.rs"))
    ```
    In this case, we start to process `some_name.rs` while processing `include!` macro call and fall into an endless loop
    
    (cherry picked from commit f9e67bd5e2e62ae9982b406852c8a5342ffa42f4)

--- src/test/kotlin/org/rustSlowTests/cargo/runconfig/RsIncludeMacroResolveTest.kt
+++ src/test/kotlin/org/rustSlowTests/cargo/runconfig/RsIncludeMacroResolveTest.kt
@@ -57,6 +57,130 @@ class RsIncludeMacroResolveTest : RunConfigurationTestBase() {
         }
     }
 
+    // https://github.com/intellij-rust/intellij-rust/issues/4579
+    fun `test do not overflow stack 1`() = withEnabledFetchOutDirFeature {
+        val testProject = buildProject {
+            toml("Cargo.toml", """
+                [package]
+                name = "intellij-rust-test"
+                version = "0.1.0"
+                authors = []
+            """)
+            rust("build.rs", """
+                use std::env;
+                use std::fs::File;
+                use std::io::Write;
+                use std::path::Path;
+
+                fn main() {
+                    let out_dir = env::var("OUT_DIR").unwrap();
+                    let dest_path = Path::new(&out_dir).join("main.rs");
+                    let mut f = File::create(&dest_path).unwrap();
+
+                    f.write_all(b"
+                        pub fn message() -> &'static str {
+                            \"Hello, World!\"
+                        }",
+                    ).unwrap();
+                }
+            """)
+            dir("src") {
+                rust("main.rs", """
+                    include!(concat!(env!("OUT_DIR"), "/main.rs"));
+
+                    fn main() {
+                        println!("{}", message());
+                                        //^
+                    }
+                """)
+            }
+        }
+        buildProject()
+
+        runWithInvocationEventsDispatching("Failed to resolve the reference") {
+            testProject.findElementInFile<RsPath>("src/main.rs").reference.resolve() != null
+        }
+    }
+
+    // https://github.com/intellij-rust/intellij-rust/issues/4579
+    fun `test do not overflow stack 2`() = withEnabledFetchOutDirFeature {
+        val testProject = buildProject {
+            toml("Cargo.toml", """
+                [workspace]
+                members = [
+                    "intellij-rust-test-1",
+                    "intellij-rust-test-2"
+                ]
+            """)
+            dir("intellij-rust-test-1") {
+                toml("Cargo.toml", """
+                    [package]
+                    name = "intellij-rust-test-1"
+                    version = "0.1.0"
+                    authors = []
+
+                """)
+                rust("build.rs", """
+                    use std::env;
+                    use std::fs::File;
+                    use std::io::Write;
+                    use std::path::Path;
+
+                    fn main() {
+                        let out_dir = env::var("OUT_DIR").unwrap();
+                        let dest_path = Path::new(&out_dir).join("lib.rs");
+                        let mut f = File::create(&dest_path).unwrap();
+
+                        f.write_all(b"
+                            pub fn message() -> &'static str {
+                                \"Hello, World!\"
+                            }",
+                        ).unwrap();
+                    }
+                """)
+                dir("src") {
+                    rust("lib.rs", """
+                        include!(concat!(env!("OUT_DIR"), "/lib.rs"));
+
+                        fn main() {
+                            println!("{}", message());
+                                            //^
+                        }
+                    """)
+                }
+            }
+            dir("intellij-rust-test-2") {
+                toml("Cargo.toml", """
+                    [package]
+                    name = "intellij-rust-test-2"
+                    version = "0.1.0"
+                    authors = []
+                """)
+                rust("build.rs", """
+                    use std::env;
+                    use std::fs::File;
+                    use std::path::Path;
+
+                    fn main() {
+                        let out_dir = env::var("OUT_DIR").unwrap();
+                        let dest_path = Path::new(&out_dir).join("lib.rs");
+                        let mut f = File::create(&dest_path).unwrap();
+                    }
+                """)
+                dir("src") {
+                    rust("lib.rs", """
+                        include!(concat!(env!("OUT_DIR"), "/lib.rs"));
+                    """)
+                }
+            }
+        }
+        buildProject()
+
+        runWithInvocationEventsDispatching("Failed to resolve the reference") {
+            testProject.findElementInFile<RsPath>("intellij-rust-test-1/src/lib.rs").reference.resolve() != null
+        }
+    }
+
     fun `test include in dependency`() = withEnabledFetchOutDirFeature {
         val testProject = buildProject {
             toml("Cargo.toml", """
