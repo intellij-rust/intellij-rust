97fe9e4a5133615df9af2654f75f5946651cbbb4
Konstantin Anisimov <rrevenantt@gmail.com>
2019-11-22 16:39:35 +0300

    ACT:Display impl block info in Go to Imlementations dialog

--- src/main/kotlin/org/rust/ide/navigation/goto/RsGotoTargetRendererProvider.kt
+++ src/main/kotlin/org/rust/ide/navigation/goto/RsGotoTargetRendererProvider.kt
@@ -10,12 +10,28 @@ import com.intellij.codeInsight.navigation.GotoTargetRendererProvider
 import com.intellij.ide.util.PsiElementListCellRenderer
 import com.intellij.psi.PsiElement
 import org.rust.lang.core.psi.RsImplItem
+import org.rust.lang.core.psi.ext.RsAbstractable
+import org.rust.lang.core.psi.ext.RsAbstractableOwner
+import org.rust.lang.core.psi.ext.owner
 
 class RsGotoTargetRendererProvider : GotoTargetRendererProvider {
     override fun getRenderer(element: PsiElement, gotoData: GotoTargetHandler.GotoData): PsiElementListCellRenderer<*>? {
+        val targetsInImpl = gotoData.targets.all { (it as? RsAbstractable)?.owner is RsAbstractableOwner.Impl }
+        if (!gotoData.hasDifferentNames() && targetsInImpl) {
+            return GoToImplRenderer()
+        }
         if (element is RsImplItem) return ImplRenderer()
         return null
     }
+
+    private class GoToImplRenderer : PsiElementListCellRenderer<RsAbstractable>() {
+        override fun getContainerText(element: RsAbstractable, name: String?): String? = element.presentation?.locationString
+        override fun getElementText(element: RsAbstractable): String? =
+            (element.owner as RsAbstractableOwner.Impl).impl.presentation?.presentableText
+
+        override fun getIconFlags(): Int = 0
+    }
+
 
     private class ImplRenderer : PsiElementListCellRenderer<RsImplItem>() {
         override fun getContainerText(element: RsImplItem, name: String?): String? = element.presentation?.locationString
