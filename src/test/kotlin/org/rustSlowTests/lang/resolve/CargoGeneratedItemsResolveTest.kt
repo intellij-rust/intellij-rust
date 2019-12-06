e7e4bb8bde1cc9b40915b181e55dfa5801d509e3
Arseniy Pendryak <a.pendryak@yandex.ru>
2019-12-06 11:51:07 +0300

    STUB&RES: support include without particular file name literal
    `include` macro support relies on index of include macro calls to quickly find call that includes the corresponding file.
    Previously, we use file name from latest string literal as key in such index (e.g. `"bindings.rs"` in `include!(concat!(env!("OUT_DIR"), "/bindings.rs"))`).
    Unfortunately, such solution doesn't work with calls that don't contain file name in string literal.
    For example, `include!(env!("BINDINGS"))` in `web-sys` crate.
    
    To support such cases too, all include macro calls without particular file name have special key now and are presented in index as well.
    So search of include macro call consists of two parts now:
    * search by name
    * search in all macro calls without specific file name if previous query found nothing

--- src/test/kotlin/org/rustSlowTests/lang/resolve/CargoGeneratedItemsResolveTest.kt
+++ src/test/kotlin/org/rustSlowTests/lang/resolve/CargoGeneratedItemsResolveTest.kt
@@ -430,6 +430,99 @@ class CargoGeneratedItemsResolveTest : RunConfigurationTestBase() {
         }
     }
 
+    fun `test include without file name in literal`() = withEnabledFetchOutDirFeature {
+        withEnabledEvaluateBuildScriptsFeature {
+            buildProject {
+                toml("Cargo.toml", """
+                    [package]
+                    name = "intellij-rust-test"
+                    version = "0.1.0"
+                    edition = "2018"
+                    authors = []
+                """)
+
+                dir("src") {
+                    rust("main.rs", """
+                        include!(env!("GENERATED_ENV_FILE"));
+                        fn main() {
+                            println!("{}", message());
+                                           //^
+                        }
+                    """)
+                }
+                rust("build.rs", """
+                    use std::env;
+                    use std::fs;
+                    use std::fs::File;
+                    use std::io::Write;
+                    use std::path::Path;
+
+                    fn main() {
+                        let out_dir = env::var("OUT_DIR").unwrap();
+                        let gen_dir = Path::new(&out_dir).join("gen");
+                        if !gen_dir.exists() {
+                            fs::create_dir(&gen_dir).unwrap();
+                        }
+                        let dest_path = gen_dir.join("hello.rs");
+                        generate_file(&dest_path, b"
+                            pub fn message() -> &'static str {
+                                \"Hello, World!\"
+                            }
+                        ");
+                        println!("cargo:rustc-env=GENERATED_ENV_FILE={}", dest_path.display());
+                    }
+
+                    fn generate_file<P: AsRef<Path>>(path: P, text: &[u8]) {
+                        let mut f = File::create(path).unwrap();
+                        f.write_all(text).unwrap()
+                    }
+                """)
+            }
+        }
+    }
+
+    fun `test include without file name in literal 2`() = withEnabledFetchOutDirFeature {
+        withEnabledEvaluateBuildScriptsFeature {
+            buildProject {
+                toml("Cargo.toml", """
+                    [package]
+                    name = "intellij-rust-test"
+                    version = "0.1.0"
+                    edition = "2018"
+                    authors = []
+                """)
+
+                dir("src") {
+                    rust("main.rs", """
+                        include!(concat!(env!("OUT_DIR"), "/hello", ".rs"));
+                        fn main() {
+                            println!("{}", message());
+                                           //^
+                        }
+                    """)
+                }
+                rust("build.rs", """
+                    use std::env;
+                    use std::fs::File;
+                    use std::io::Write;
+                    use std::path::Path;
+
+                    fn main() {
+                        let out_dir = env::var("OUT_DIR").unwrap();
+                        let dest_path = Path::new(&out_dir).join("hello.rs");
+                        let mut f = File::create(&dest_path).unwrap();
+
+                        f.write_all(b"
+                            pub fn message() -> &'static str {
+                                \"Hello, World!\"
+                            }",
+                        ).unwrap();
+                    }
+                """)
+            }
+        }
+    }
+
     private fun withEnabledFetchOutDirFeature(action: () -> Unit) =
         runWithEnabledFeature(RsExperiments.FETCH_OUT_DIR, action)
 
