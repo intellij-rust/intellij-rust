c2243154f9fff73afc81010588157d4268597722
Arseniy Pendryak <a.pendryak@yandex.ru>
2019-12-23 10:14:37 +0300

    T: fix formatting

--- src/test/kotlin/org/rust/ide/intentions/AddStructFieldsPatIntentionTest.kt
+++ src/test/kotlin/org/rust/ide/intentions/AddStructFieldsPatIntentionTest.kt
@@ -6,357 +6,310 @@
 package org.rust.ide.intentions
 
 class AddStructFieldsPatIntentionTest : RsIntentionTestBase(AddStructFieldsPatIntention()) {
-    fun `test simple case match`() = doAvailableTest(
-        """
-            struct Foo {
-                a: i32,
-                b: i32,
-                c: i32,
-                d: i32,
+    fun `test simple case match`() = doAvailableTest("""
+        struct Foo {
+            a: i32,
+            b: i32,
+            c: i32,
+            d: i32,
+        }
+        fn f(foo: Foo) {
+            match foo {
+                Foo { a, b, ./*caret*/. } => {}
             }
-            fn f(foo: Foo) {
-                match foo {
-                    Foo { a, b, ./*caret*/. } => {}
-                }
+        }
+    """, """
+        struct Foo {
+            a: i32,
+            b: i32,
+            c: i32,
+            d: i32,
+        }
+        fn f(foo: Foo) {
+            match foo {
+                Foo { a, b, c, d /*caret*/} => {}
             }
-        """,
-        """
-            struct Foo {
-                a: i32,
-                b: i32,
-                c: i32,
-                d: i32,
-            }
-            fn f(foo: Foo) {
-                match foo {
-                    Foo { a, b, c, d /*caret*/} => {}
-                }
-            }
-        """
-    )
+        }
+    """)
 
-    fun `test no fields match`() = doAvailableTest(
-        """
-            struct Foo {
-                a: i32,
-                b: i32,
-                c: i32,
-                d: i32,
+    fun `test no fields match`() = doAvailableTest("""
+        struct Foo {
+            a: i32,
+            b: i32,
+            c: i32,
+            d: i32,
+        }
+        fn f(foo: Foo) {
+            match foo {
+                Foo { ./*caret*/. } => {}
             }
-            fn f(foo: Foo) {
-                match foo {
-                    Foo { ./*caret*/. } => {}
-                }
+        }
+    """, """
+        struct Foo {
+            a: i32,
+            b: i32,
+            c: i32,
+            d: i32,
+        }
+        fn f(foo: Foo) {
+            match foo {
+                Foo { a, b, c, d /*caret*/} => {}
             }
-        """,
-        """
-            struct Foo {
-                a: i32,
-                b: i32,
-                c: i32,
-                d: i32,
-            }
-            fn f(foo: Foo) {
-                match foo {
-                    Foo { a, b, c, d /*caret*/} => {}
-                }
-            }
-        """
-    )
+        }
+    """)
 
-    fun `test simple let expression`() = doAvailableTest(
-        """
-            struct Foo {
-                a: i32,
-                b: i32,
-                c: i32,
-                d: i32,
-            }
-            fn f(foo: Foo) {
-                let Foo { a, b, ./*caret*/. } = foo;
-            }
-        """,
-        """
-            struct Foo {
-                a: i32,
-                b: i32,
-                c: i32,
-                d: i32,
-            }
-            fn f(foo: Foo) {
-                let Foo { a, b, c, d /*caret*/} = foo;
-            }
-        """
-    )
+    fun `test simple let expression`() = doAvailableTest("""
+        struct Foo {
+            a: i32,
+            b: i32,
+            c: i32,
+            d: i32,
+        }
+        fn f(foo: Foo) {
+            let Foo { a, b, ./*caret*/. } = foo;
+        }
+    """, """
+        struct Foo {
+            a: i32,
+            b: i32,
+            c: i32,
+            d: i32,
+        }
+        fn f(foo: Foo) {
+            let Foo { a, b, c, d /*caret*/} = foo;
+        }
+    """)
 
-    fun `test no fields let expression`() = doAvailableTest(
-        """
-            struct Foo {
-                a: i32,
-                b: i32,
-                c: i32,
-                d: i32,
-            }
-            fn f(foo: Foo) {
-                let Foo { ./*caret*/. } = foo;
-            }
-        """,
-        """
-            struct Foo {
-                a: i32,
-                b: i32,
-                c: i32,
-                d: i32,
-            }
-            fn f(foo: Foo) {
-                let Foo { a, b, c, d /*caret*/} = foo;
-            }
-        """
-    )
+    fun `test no fields let expression`() = doAvailableTest("""
+        struct Foo {
+            a: i32,
+            b: i32,
+            c: i32,
+            d: i32,
+        }
+        fn f(foo: Foo) {
+            let Foo { ./*caret*/. } = foo;
+        }
+    """, """
+        struct Foo {
+            a: i32,
+            b: i32,
+            c: i32,
+            d: i32,
+        }
+        fn f(foo: Foo) {
+            let Foo { a, b, c, d /*caret*/} = foo;
+        }
+    """)
 
-    fun `test match one field missing`() = doAvailableTest(
-        """
-            struct Foo {
-                a: i32,
-                b: i32,
-                c: i32,
-                d: i32,
+    fun `test match one field missing`() = doAvailableTest("""
+        struct Foo {
+            a: i32,
+            b: i32,
+            c: i32,
+            d: i32,
+        }
+        fn f(foo: Foo) {
+            match foo {
+                Foo { a, b, c, ./*caret*/. } => {}
             }
-            fn f(foo: Foo) {
-                match foo {
-                    Foo { a, b, c, ./*caret*/. } => {}
-                }
+        }
+    """, """
+        struct Foo {
+            a: i32,
+            b: i32,
+            c: i32,
+            d: i32,
+        }
+        fn f(foo: Foo) {
+            match foo {
+                Foo { a, b, c, d /*caret*/} => {}
             }
-        """,
-        """
-            struct Foo {
-                a: i32,
-                b: i32,
-                c: i32,
-                d: i32,
-            }
-            fn f(foo: Foo) {
-                match foo {
-                    Foo { a, b, c, d /*caret*/} => {}
-                }
-            }
-        """
-    )
+        }
+    """)
 
-    fun `test let one field missing`() = doAvailableTest(
-        """
-            struct Foo {
-                a: i32,
-                b: i32,
-                c: i32,
-                d: i32,
-            }
-            fn f(foo: Foo) {
-                let Foo { a, b, c, ./*caret*/. } = foo;
-            }
-        """,
-        """
-            struct Foo {
-                a: i32,
-                b: i32,
-                c: i32,
-                d: i32,
-            }
-            fn f(foo: Foo) {
-                let Foo { a, b, c, d /*caret*/} = foo;
-            }
-        """
-    )
+    fun `test let one field missing`() = doAvailableTest("""
+        struct Foo {
+            a: i32,
+            b: i32,
+            c: i32,
+            d: i32,
+        }
+        fn f(foo: Foo) {
+            let Foo { a, b, c, ./*caret*/. } = foo;
+        }
+    """, """
+        struct Foo {
+            a: i32,
+            b: i32,
+            c: i32,
+            d: i32,
+        }
+        fn f(foo: Foo) {
+            let Foo { a, b, c, d /*caret*/} = foo;
+        }
+    """)
 
-    fun `test filling order with existing 2 last fields in descending order`() = doAvailableTest(
-        """
-            struct Foo {
-                a: i32,
-                b: i32,
-                c: i32,
-                d: i32,
-            }
-            fn f(foo: Foo) {
-                let Foo { d, c, ./*caret*/. } = foo;
-            }
-        """,
-        """
-            struct Foo {
-                a: i32,
-                b: i32,
-                c: i32,
-                d: i32,
-            }
-            fn f(foo: Foo) {
-                let Foo { a, b, d, c /*caret*/} = foo;
-            }
-        """
-    )
+    fun `test filling order with existing 2 last fields in descending order`() = doAvailableTest("""
+        struct Foo {
+            a: i32,
+            b: i32,
+            c: i32,
+            d: i32,
+        }
+        fn f(foo: Foo) {
+            let Foo { d, c, ./*caret*/. } = foo;
+        }
+    """, """
+        struct Foo {
+            a: i32,
+            b: i32,
+            c: i32,
+            d: i32,
+        }
+        fn f(foo: Foo) {
+            let Foo { a, b, d, c /*caret*/} = foo;
+        }
+    """)
 
-    fun `test filling order with the second field existing`() = doAvailableTest(
-        """
-            struct Foo {
-                a: i32,
-                b: i32,
-                c: i32,
-                d: i32,
-            }
-            fn f(foo: Foo) {
-                let Foo { b, ./*caret*/. } = foo;
-            }
-        """,
-        """
-            struct Foo {
-                a: i32,
-                b: i32,
-                c: i32,
-                d: i32,
-            }
-            fn f(foo: Foo) {
-                let Foo { a, b, c, d /*caret*/} = foo;
-            }
-        """
-    )
+    fun `test filling order with the second field existing`() = doAvailableTest("""
+        struct Foo {
+            a: i32,
+            b: i32,
+            c: i32,
+            d: i32,
+        }
+        fn f(foo: Foo) {
+            let Foo { b, ./*caret*/. } = foo;
+        }
+    """, """
+        struct Foo {
+            a: i32,
+            b: i32,
+            c: i32,
+            d: i32,
+        }
+        fn f(foo: Foo) {
+            let Foo { a, b, c, d /*caret*/} = foo;
+        }
+    """)
 
-    fun `test tuple struct with dots`() = doAvailableTest(
-        """
-            struct Foo(i32, i32, i32);
-            fn f(foo: Foo) {
-                match foo {
-                    Foo (a, b, ./*caret*/.) => {}
-                }
+    fun `test tuple struct with dots`() = doAvailableTest("""
+        struct Foo(i32, i32, i32);
+        fn f(foo: Foo) {
+            match foo {
+                Foo (a, b, ./*caret*/.) => {}
             }
-        """,
-        """
-            struct Foo(i32, i32, i32);
-            fn f(foo: Foo) {
-                match foo {
-                    Foo (a, b, _0/*caret*/) => {}
-                }
+        }
+    """, """
+        struct Foo(i32, i32, i32);
+        fn f(foo: Foo) {
+            match foo {
+                Foo (a, b, _0/*caret*/) => {}
             }
-        """
-    )
+        }
+    """)
 
-    fun `test struct with no spaces`() = doAvailableTest(
-        """
-            struct Foo {
-                a: i32,
-                b: i32,
-                c: i32,
+    fun `test struct with no spaces`() = doAvailableTest("""
+        struct Foo {
+            a: i32,
+            b: i32,
+            c: i32,
+        }
+
+        fn f(foo: Foo) {
+            let Foo {a,b,./*caret*/.} = foo;
+        }
+    """, """
+        struct Foo {
+            a: i32,
+            b: i32,
+            c: i32,
+        }
+
+        fn f(foo: Foo) {
+            let Foo {a,b,/*caret*/ c } = foo;
+        }
+    """)
+
+    fun `test tuple struct with no spaces`() = doAvailableTest("""
+        struct Foo(i32, i32, i32);
+        fn f(foo: Foo) {
+            match foo {
+                Foo (a,b,./*caret*/.) => {}
             }
-
-            fn f(foo: Foo) {
-                let Foo {a,b,./*caret*/.} = foo;
+        }
+    """, """
+        struct Foo(i32, i32, i32);
+        fn f(foo: Foo) {
+            match foo {
+                Foo (a, b, _0/*caret*/) => {}
             }
-        """,
-        """
-            struct Foo {
-                a: i32,
-                b: i32,
-                c: i32,
+        }
+    """)
+
+    fun `test intention not available in range`() = doUnavailableTest("""
+        fn f() {
+            match 1 {
+                0./*caret*/.2 => {}
+                _ => {}
             }
+        }
+    """)
 
-            fn f(foo: Foo) {
-                let Foo {a,b,/*caret*/ c } = foo;
-            }
-        """
-    )
+    fun `test tuple with dots in the middle`() = doAvailableTest("""
+        struct Foo(i32, i32, i32);
 
-    fun `test tuple struct with no spaces`() = doAvailableTest(
-        """
-            struct Foo(i32, i32, i32);
-            fn f(foo: Foo) {
-                match foo {
-                    Foo (a,b,./*caret*/.) => {}
-                }
-            }
-        """,
-        """
-            struct Foo(i32, i32, i32);
-            fn f(foo: Foo) {
-                match foo {
-                    Foo (a, b, _0/*caret*/) => {}
-                }
-            }
-        """
-    )
+        fn f(foo: Foo) {
+            let Foo (a, ./*caret*/., c) = foo;
+        }
+    """, """
+        struct Foo(i32, i32, i32);
 
-    fun `test intention not available in range`() = doUnavailableTest(
-        """
-            fn f() {
-                match 1 {
-                    0./*caret*/.2 => {}
-                    _ => {}
-                }
-            }
-        """
-    )
+        fn f(foo: Foo) {
+            let Foo (a, _0/*caret*/, c) = foo;
+        }
+    """)
 
-    fun `test tuple with dots in the middle`() = doAvailableTest(
-        """
-            struct Foo(i32, i32, i32);
+    fun `test tuple with dots in the middle and trailing comma`() = doAvailableTest("""
+        struct Foo(i32, i32, i32);
 
-            fn f(foo: Foo) {
-                let Foo (a, ./*caret*/., c) = foo;
-            }
-        """,
-        """
-            struct Foo(i32, i32, i32);
+        fn f(foo: Foo) {
+            let Foo (a, ./*caret*/., c,) = foo;
+        }
+    """, """
+        struct Foo(i32, i32, i32);
 
-            fn f(foo: Foo) {
-                let Foo (a, _0/*caret*/, c) = foo;
-            }
-        """
-    )
+        fn f(foo: Foo) {
+            let Foo (a, _0/*caret*/, c,) = foo;
+        }
+    """)
 
-    fun `test tuple with dots in the middle and trailing comma`() = doAvailableTest(
-        """
-            struct Foo(i32, i32, i32);
+    fun `test tuple with dots in the middle and 4 fields`() = doAvailableTest("""
+        struct Foo(i32, i32, i32, i32);
 
-            fn f(foo: Foo) {
-                let Foo (a, ./*caret*/., c,) = foo;
-            }
-        """,
-        """
-            struct Foo(i32, i32, i32);
+        fn f(foo: Foo) {
+            let Foo (a, ./*caret*/., c) = foo;
+        }
+    """, """
+        struct Foo(i32, i32, i32, i32);
 
-            fn f(foo: Foo) {
-                let Foo (a, _0/*caret*/, c,) = foo;
-            }
-        """
-    )
+        fn f(foo: Foo) {
+            let Foo (a, _0/*caret*/, _1, c) = foo;
+        }
+    """)
 
-    fun `test tuple with dots in the middle and 4 fields`() = doAvailableTest(
-        """
-            struct Foo(i32, i32, i32, i32);
+    fun `test tuple with dots in the middle and 5 fields`() = doAvailableTest("""
+        struct Foo(i32, i32, i32, i32, i32);
 
-            fn f(foo: Foo) {
-                let Foo (a, ./*caret*/., c) = foo;
-            }
-        """,
-        """
-            struct Foo(i32, i32, i32, i32);
+        fn f(foo: Foo) {
+            let Foo (a, ./*caret*/., c) = foo;
+        }
+    """, """
+        struct Foo(i32, i32, i32, i32, i32);
 
-            fn f(foo: Foo) {
-                let Foo (a, _0/*caret*/, _1, c) = foo;
-            }
-        """
-    )
-
-    fun `test tuple with dots in the middle and 5 fields`() = doAvailableTest(
-        """
-            struct Foo(i32, i32, i32, i32, i32);
-
-            fn f(foo: Foo) {
-                let Foo (a, ./*caret*/., c) = foo;
-            }
-        """,
-        """
-            struct Foo(i32, i32, i32, i32, i32);
-
-            fn f(foo: Foo) {
-                let Foo (a, _0/*caret*/, _1, _2, c) = foo;
-            }
-        """
-    )
+        fn f(foo: Foo) {
+            let Foo (a, _0/*caret*/, _1, _2, c) = foo;
+        }
+    """)
 }
