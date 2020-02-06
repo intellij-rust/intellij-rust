c45047cfea4a480f4772e91c5314b0814626497b
Arseniy Pendryak <a.pendryak@yandex.ru>
2020-02-07 01:09:07 +0300

    Update 2020.1 EAPs

--- grazie/src/201/test/kotlin/org/rust/grazie/RsGrammarCheckingTestBase.kt
+++ grazie/src/201/test/kotlin/org/rust/grazie/RsGrammarCheckingTestBase.kt
@@ -5,8 +5,22 @@
 
 package org.rust.grazie
 
+import com.intellij.grazie.GrazieConfig
 import com.intellij.grazie.ide.GrazieInspection
+import com.intellij.testFramework.PlatformTestUtil
 import org.rust.ide.inspections.RsInspectionsTestBase
+import org.rust.lang.RsLanguage
 
 // BACKCOMPAT: 2019.3. Inline
-abstract class RsGrammarCheckingTestBase : RsInspectionsTestBase(GrazieInspection::class)
+abstract class RsGrammarCheckingTestBase : RsInspectionsTestBase(GrazieInspection::class) {
+
+    override fun setUp() {
+        super.setUp()
+        if (RsLanguage.id !in GrazieConfig.get().enabledProgrammingLanguages) {
+            GrazieConfig.update { state ->
+                state.copy(enabledProgrammingLanguages = state.enabledProgrammingLanguages + RsLanguage.id)
+            }
+            PlatformTestUtil.dispatchAllEventsInIdeEventQueue()
+        }
+    }
+}
