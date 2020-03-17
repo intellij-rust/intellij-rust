be7c38725902cdc5eba1e098b0f8de4315112ba3
imkiva <libkernelpanic@gmail.com>
2020-03-17 19:42:54 +0800

    DBG: fix: used wrong gdb if WSL is the default toolchain

--- clion/src/193/main/kotlin/org/rust/clion/debugger/RsCLionDebuggerDriverConfigurationProvider.kt
+++ clion/src/193/main/kotlin/org/rust/clion/debugger/RsCLionDebuggerDriverConfigurationProvider.kt
@@ -6,12 +6,22 @@
 package org.rust.clion.debugger
 
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.SystemInfo
+import com.jetbrains.cidr.cpp.toolchains.CPPToolSet
 import com.jetbrains.cidr.cpp.toolchains.CPPToolchains
 import com.jetbrains.cidr.execution.debugger.backend.DebuggerDriverConfiguration
 import org.rust.debugger.RsDebuggerDriverConfigurationProvider
 
 class RsCLionDebuggerDriverConfigurationProvider : RsDebuggerDriverConfigurationProvider {
     override fun getDebuggerDriverConfiguration(project: Project): DebuggerDriverConfiguration? {
-        return CPPToolchains.getInstance().defaultToolchain?.createDriverConfiguration(project)
+        val toolchains = CPPToolchains.getInstance()
+        if (!SystemInfo.isWindows) {
+            return toolchains.defaultToolchain?.createDriverConfiguration(project)
+        }
+
+        // In case that WSL is the default toolchain, we should find MinGW manually.
+        return toolchains.toolchains
+            .firstOrNull { it.toolSetKind == CPPToolSet.Kind.MINGW }
+            ?.createDriverConfiguration(project)
     }
 }
