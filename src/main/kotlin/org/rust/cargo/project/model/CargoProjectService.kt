9d75fb93aaf6f03ad74edcdce3dfa5c32e4b16d9
Arseniy Pendryak <a.pendryak@yandex.ru>
2020-03-27 11:45:11 +0300

    Do not add directory as source/excluded entries if it doesn't exist

--- src/main/kotlin/org/rust/cargo/project/model/CargoProjectService.kt
+++ src/main/kotlin/org/rust/cargo/project/model/CargoProjectService.kt
@@ -13,7 +13,6 @@ import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ContentEntry
 import com.intellij.openapi.util.UserDataHolderEx
-import com.intellij.openapi.util.io.FileUtil
 import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.util.messages.Topic
@@ -152,12 +151,12 @@ private fun discoverToolchain(project: Project) {
 }
 
 fun ContentEntry.setup(contentRoot: VirtualFile) {
-    val makeVfsUrl = { dirName: String -> FileUtil.join(contentRoot.url, dirName) }
-    CargoConstants.ProjectLayout.sources.map(makeVfsUrl).forEach {
+    val makeVfsUrl = { dirName: String -> contentRoot.findChild(dirName)?.url }
+    CargoConstants.ProjectLayout.sources.mapNotNull(makeVfsUrl).forEach {
         addSourceFolder(it, /* test = */ false)
     }
-    CargoConstants.ProjectLayout.tests.map(makeVfsUrl).forEach {
+    CargoConstants.ProjectLayout.tests.mapNotNull(makeVfsUrl).forEach {
         addSourceFolder(it, /* test = */ true)
     }
-    addExcludeFolder(makeVfsUrl(CargoConstants.ProjectLayout.target))
+    makeVfsUrl(CargoConstants.ProjectLayout.target)?.let(::addExcludeFolder)
 }
