cb7d1ae7009a65ce7173190ac174968c6d50649e
Jezza <jezzadabomb@gmail.com>
2020-03-28 20:10:46 +0100

    Partial changes from review process.

--- src/main/kotlin/org/rust/ide/intentions/ReplaceBlockCommentWithLineCommentIntention.kt
+++ src/main/kotlin/org/rust/ide/intentions/ReplaceBlockCommentWithLineCommentIntention.kt
@@ -12,50 +12,52 @@ import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiWhiteSpace
 import org.rust.lang.core.parser.RustParserDefinition.Companion.BLOCK_COMMENT
 import org.rust.lang.core.psi.RsPsiFactory
-import org.rust.lang.core.psi.ext.*
+import org.rust.lang.core.psi.ext.ancestorOrSelf
 
-class ReplaceBlockCommentWithLineCommentIntention : RsElementBaseIntentionAction<ReplaceBlockCommentWithLineCommentIntention.Context>() {
+class ReplaceBlockCommentWithLineCommentIntention : RsElementBaseIntentionAction<PsiComment>() {
 
     override fun getText(): String = familyName
     override fun getFamilyName(): String = "Replace with end of line comment"
 
-    override fun findApplicableContext(project: Project, editor: Editor, element: PsiElement): Context? {
+    override fun findApplicableContext(project: Project, editor: Editor, element: PsiElement): PsiComment? {
         val comment = element.ancestorOrSelf<PsiComment>() ?: return null
         if (BLOCK_COMMENT != comment.tokenType) {
             return null
         }
-        return Context(comment)
+        return comment
     }
 
-    override fun invoke(project: Project, editor: Editor, ctx: Context) {
-        val selected = ctx.comment
+    override fun invoke(project: Project, editor: Editor, ctx: PsiComment) {
         val factory = RsPsiFactory(project)
 
-        val indent = if (selected.prevSibling is PsiWhiteSpace) {
-            val space = selected.prevSibling.text.reversed().takeWhile {  it == ' ' || it == '\t' }
+        val indent = if (ctx.prevSibling is PsiWhiteSpace) {
+            val space = ctx.prevSibling
+                .text
+                .reversed()
+                .takeWhile { it == ' ' || it == '\t' }
+
             factory.createWhitespace("\n$space")
         } else {
             factory.createNewline()
         }
 
-        val comments = selected.text
-            .substring(2, selected.text.length - 2)
+        val comments = ctx.text
+            .substring(2, ctx.text.length - 2)
             .trim()
             .split("\n")
             .reversed()
 
         val lastIndex = comments.size - 1
-        val parent = selected.parent
+        val parent = ctx.parent
         comments.forEachIndexed { index, comment ->
             val commentText = comment.trim()
-            parent.addAfter(factory.createLineComment(commentText), selected)
-            if (index != lastIndex) parent.addAfter(indent, selected)
+            parent.addAfter(factory.createLineComment(commentText), ctx)
+
+            if (index != lastIndex) {
+                parent.addAfter(indent, ctx)
+            }
         }
 
-        selected.delete()
+        ctx.delete()
     }
-
-    data class Context(
-        val comment: PsiComment
-    )
 }
--- src/main/kotlin/org/rust/ide/intentions/ReplaceLineCommentWithBlockCommentIntention.kt
+++ src/main/kotlin/org/rust/ide/intentions/ReplaceLineCommentWithBlockCommentIntention.kt
@@ -15,72 +15,58 @@ import org.rust.lang.core.psi.RsPsiFactory
 import org.rust.lang.core.psi.ext.ancestorOrSelf
 import org.rust.lang.core.psi.ext.getNextNonWhitespaceSibling
 import org.rust.lang.core.psi.ext.getPrevNonWhitespaceSibling
-import java.lang.StringBuilder
 
-class ReplaceLineCommentWithBlockCommentIntention : RsElementBaseIntentionAction<ReplaceLineCommentWithBlockCommentIntention.Context>() {
+class ReplaceLineCommentWithBlockCommentIntention : RsElementBaseIntentionAction<PsiComment>() {
 
     override fun getText(): String = familyName
     override fun getFamilyName(): String = "Replace with block comment"
 
-    override fun findApplicableContext(project: Project, editor: Editor, element: PsiElement): Context? {
+    override fun findApplicableContext(project: Project, editor: Editor, element: PsiElement): PsiComment? {
         val comment = element.ancestorOrSelf<PsiComment>() ?: return null
         if (!comment.isEndOfLineComment()) {
             return null
         }
 
-        var firstComment = comment
-        while (true) {
-            val previous = firstComment.getPrevNonWhitespaceSibling() ?: break
-            if (!previous.isEndOfLineComment()) {
-                break
-            }
-            firstComment = previous as PsiComment
-        }
-
-        return Context(firstComment)
+        return generateSequence(comment) { it.prevComment() }.last()
     }
 
-    override fun invoke(project: Project, editor: Editor, ctx: Context) {
-        val firstComment = ctx.firstComment
-
-        val indent = (firstComment.prevSibling as? PsiWhiteSpace)
+    override fun invoke(project: Project, editor: Editor, ctx: PsiComment) {
+        val indent = (ctx.prevSibling as? PsiWhiteSpace)
             ?.text
             ?.reversed()
             ?.takeWhile { it == ' ' || it == '\t' } ?: ""
 
-        val comments = mutableListOf(firstComment)
-        var nextComment = firstComment
-        while (true) {
-            nextComment = nextComment.nextComment() ?: break
-            comments.add(nextComment)
-        }
+        val comments = generateSequence(ctx) { it.nextComment() }.toList()
 
-        val blockComment = if (comments.size == 1)
+        val blockComment = if (comments.size == 1) {
             " ${comments.first().commentText()} "
-        else
+        } else {
             comments.joinToString(separator = "\n", prefix = "\n", postfix = "\n$indent") {
                 "$indent${it.commentText()}"
             }
+        }
 
         comments.drop(1).forEach {
             (it.prevSibling as? PsiWhiteSpace)?.delete()
             it.delete()
         }
-        firstComment.replace(RsPsiFactory(project).createBlockComment(blockComment))
+
+        val newComment = RsPsiFactory(project).createBlockComment(blockComment)
+        ctx.replace(newComment)
     }
 
-    private fun PsiElement.isEndOfLineComment() = node.elementType == EOL_COMMENT
+    private fun PsiElement.isEndOfLineComment(): Boolean = node.elementType == EOL_COMMENT
 
-    private fun PsiComment.commentText() = text.substring(2)
+    private fun PsiComment.commentText(): String = text.substring(2)
         .replace("/*", "/ *")
         .replace("*/", "* /")
         .trim()
 
+    private fun PsiComment.prevComment(): PsiComment? {
+        return (getPrevNonWhitespaceSibling() as? PsiComment)?.takeIf { it.isEndOfLineComment() }
+    }
+
     private fun PsiComment.nextComment(): PsiComment? {
         return (getNextNonWhitespaceSibling() as? PsiComment)?.takeIf { it.isEndOfLineComment() }
     }
-
-    data class Context(
-        val firstComment: PsiComment
-    )
 }
