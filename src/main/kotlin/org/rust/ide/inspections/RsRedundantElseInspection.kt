664a1d605d6102f6e9e8c817691641426b98cf49
Jakub Beránek <berykubik@gmail.com>
2020-03-30 17:53:27 +0200

    INSP: add RsRedundantElseInspection

---
+++ src/main/kotlin/org/rust/ide/inspections/RsRedundantElseInspection.kt
@@ -0,0 +1,59 @@
+/*
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
+package org.rust.ide.inspections
+
+import com.intellij.openapi.util.TextRange
+import com.intellij.psi.PsiElement
+import org.rust.ide.inspections.fixes.SubstituteTextFix
+import org.rust.lang.core.psi.RsCondition
+import org.rust.lang.core.psi.RsElseBranch
+import org.rust.lang.core.psi.RsIfExpr
+import org.rust.lang.core.psi.RsVisitor
+import org.rust.lang.core.psi.ext.endOffsetInParent
+import org.rust.lang.core.psi.ext.isIrrefutable
+import org.rust.lang.core.psi.ext.leftSiblings
+import org.rust.lang.core.psi.ext.rangeWithPrevSpace
+
+/**
+ * Detects redundant `else` statements preceded by an irrefutable pattern.
+ * Quick fix: Remove `else`
+ */
+class RsRedundantElseInspection : RsLocalInspectionTool() {
+    override fun getDisplayName() = "Redundant else"
+
+    override fun buildVisitor(holder: RsProblemsHolder, isOnTheFly: Boolean) =
+        object : RsVisitor() {
+            override fun visitElseBranch(expr: RsElseBranch) {
+                if (!expr.isRedundant()) return
+
+                val elseExpr = expr.`else`
+                holder.registerProblem(
+                    expr,
+                    TextRange(elseExpr.startOffsetInParent, elseExpr.endOffsetInParent),
+                    "Redundant `else`",
+                    SubstituteTextFix.delete(
+                        "Remove `else`",
+                        expr.containingFile,
+                        expr.rangeWithPrevSpace
+                    )
+                )
+            }
+        }
+
+    private fun RsElseBranch.isRedundant(): Boolean {
+        val set = mutableSetOf<RsCondition>()
+        var candidate: PsiElement = this
+
+        while (candidate is RsElseBranch || candidate is RsIfExpr) {
+            candidate.leftSiblings.filterIsInstance<RsCondition>().forEach { set.add(it) }
+            candidate = candidate.parent
+        }
+
+        return set.any {
+            it.orPats?.patList?.all { pat -> pat?.isIrrefutable ?: false } ?: false
+        }
+    }
+}
