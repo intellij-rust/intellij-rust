10b3d201908548251c7ab62187410ba4a38086e9
vlad20012 <beskvlad@gmail.com>
2020-05-25 12:54:43 +0300

    PERF&MACRO: FileTypeOverrider for macro expansions

---
+++ src/main/kotlin/org/rust/lang/core/macros/RsFileTypeOverriderForMacroExpansionFileSystem.kt
@@ -0,0 +1,27 @@
+/*
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
+package org.rust.lang.core.macros
+
+import com.intellij.openapi.fileTypes.FileType
+import com.intellij.openapi.fileTypes.impl.FileTypeOverrider
+import com.intellij.openapi.vfs.VirtualFile
+import org.rust.lang.RsFileType
+
+/**
+ * Sets [FileType] to [RsFileType] for each file in [MacroExpansionFileSystem].
+ * Used only as an optimization: default file type detection is slow, but we
+ * know that any file in [MacroExpansionFileSystem] is [RsFileType]
+ */
+@Suppress("UnstableApiUsage")
+class RsFileTypeOverriderForMacroExpansionFileSystem : FileTypeOverrider {
+    override fun getOverriddenFileType(file: VirtualFile): FileType? {
+        return if (!file.isDirectory && file.fileSystem is MacroExpansionFileSystem) {
+            RsFileType
+        } else {
+            null
+        }
+    }
+}
