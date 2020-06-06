e0dd9af1b0075a5a81c4af248e48b720ea9bdde9
Jakub Beránek <berykubik@gmail.com>
2020-06-06 21:21:13 +0200

    FIX: add unsafe now also works inside loops and conditions

--- src/main/kotlin/org/rust/ide/annotator/fixes/AddUnsafeFix.kt
+++ src/main/kotlin/org/rust/ide/annotator/fixes/AddUnsafeFix.kt
@@ -10,20 +10,26 @@ import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
+import org.rust.lang.core.psi.RsBlock
 import org.rust.lang.core.psi.RsBlockExpr
 import org.rust.lang.core.psi.RsFunction
 import org.rust.lang.core.psi.RsPsiFactory
+import org.rust.lang.core.psi.ext.ancestorOrSelf
 
-class AddUnsafeFix(expr: PsiElement) : LocalQuickFixAndIntentionActionOnPsiElement(expr) {
-    private val _text = "Add unsafe to ${if (expr is RsBlockExpr) "block" else "function" }"
+class AddUnsafeFix(expr: RsBlock) : LocalQuickFixAndIntentionActionOnPsiElement(expr) {
+    private val _text = "Add unsafe to ${if (expr.parent is RsBlockExpr) "block" else "function"}"
     override fun getFamilyName() = text
     override fun getText() = _text
 
     override fun invoke(project: Project, file: PsiFile, editor: Editor?, expr: PsiElement, endElement: PsiElement) {
         val unsafe = RsPsiFactory(project).createUnsafeKeyword()
-        when (expr) {
-            is RsBlockExpr -> expr.addBefore(unsafe, expr.block)
-            is RsFunction -> expr.addBefore(unsafe, expr.fn)
+
+        if (expr.parent is RsBlockExpr) {
+            expr.parent.addBefore(unsafe, expr)
+        } else {
+            expr.ancestorOrSelf<RsFunction>()?.let {
+                it.addBefore(unsafe, it.fn)
+            }
         }
     }
 }
