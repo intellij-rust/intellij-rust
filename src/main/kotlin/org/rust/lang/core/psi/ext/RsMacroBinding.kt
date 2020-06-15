f757db2a5ad653441ffc98dbc89876e634b209b4
vlad20012 <beskvlad@gmail.com>
2020-06-15 22:05:31 +0300

    Fix exception during find usages in macro 2.0
    java.lang.IllegalStateException: Macro binding outside of a macro
    
    ```
    macro foobar($i/*caret*/:tt) {
        $i
    }
    ```

--- src/main/kotlin/org/rust/lang/core/psi/ext/RsMacroBinding.kt
+++ src/main/kotlin/org/rust/lang/core/psi/ext/RsMacroBinding.kt
@@ -8,7 +8,6 @@ package org.rust.lang.core.psi.ext
 import com.intellij.lang.ASTNode
 import com.intellij.psi.PsiElement
 import com.intellij.psi.search.SearchScope
-import org.rust.lang.core.psi.RsMacro
 import org.rust.lang.core.psi.RsMacroBinding
 import org.rust.lang.core.psi.RsPsiImplUtil.localOrMacroSearchScope
 
@@ -17,7 +16,7 @@ abstract class RsMacroBindingImplMixin(node: ASTNode) : RsNamedElementImpl(node)
     override fun getNameIdentifier(): PsiElement? = metaVarIdentifier
 
     override fun getUseScope(): SearchScope {
-        val owner = contextStrict<RsMacro>() ?: error("Macro binding outside of a macro")
+        val owner = contextStrict<RsMacroDefinitionBase>() ?: error("Macro binding outside of a macro")
         return localOrMacroSearchScope(owner)
     }
 }
