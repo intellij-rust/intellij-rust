61457ae6e45774b4ef452e36b0ca57754ec4d351
vlad20012 <beskvlad@gmail.com>
2020-06-17 16:42:46 +0300

    Fix macro expansion infinite progress on 2020.2

--- src/main/kotlin/org/rust/openapiext/DelayedBackgroundableProcessIndicator.kt
+++ src/main/kotlin/org/rust/openapiext/DelayedBackgroundableProcessIndicator.kt
@@ -6,35 +6,37 @@
 package org.rust.openapiext
 
 import com.intellij.openapi.application.invokeLater
-import com.intellij.openapi.progress.PerformInBackgroundOption
 import com.intellij.openapi.progress.Task
 import com.intellij.openapi.progress.TaskInfo
 import com.intellij.openapi.progress.impl.BackgroundableProcessIndicator
+import com.intellij.openapi.progress.util.ProgressWindow
+import com.intellij.openapi.wm.IdeFrame
+import com.intellij.openapi.wm.ex.StatusBarEx
+import com.intellij.openapi.wm.ex.WindowManagerEx
 import com.intellij.util.ui.TimerUtil
 
 /**
- * [BackgroundableProcessIndicator] has a method [BackgroundableProcessIndicator.setDelayInMillis] to postpone
- * progress bar displaying in UI, but it doesn't work (!) for background progress bar and works for modal windows
- * only. This class provides a hack to make it work. First, we pretend that want to show a modal window
- * ([PerformInBackgroundOption.DEAF] constructor parameter). Then, we override [prepareShowDialog] with nothing
- * and because of this the modal window will not actually be shown. Then, we run the timer, which in `delay`
- * milliseconds will invoke [background] that will show the "background" progress bar in the status bar.
+ * Like [BackgroundableProcessIndicator], but allows to specify delay to postpone progress bar displaying in UI
  */
-class DelayedBackgroundableProcessIndicator(task: Task.Backgroundable, delay: Int) :
-    BackgroundableProcessIndicator(
-        task.project,
-        task,
-        // "Show in a modal window" (the window won't really be shown, see prepareShowDialog)
-        PerformInBackgroundOption.DEAF
-    ) {
+class DelayedBackgroundableProcessIndicator(val task: Task.Backgroundable, delay: Int) :
+    ProgressWindow(task.isCancellable, true, task.project, null, task.cancelText) {
+
+    private var statusBar: StatusBarEx? = null
+    private var didInitializeOnEdt = false
+    private var isDisposed = false
 
     @Volatile
     private var isFinishCalled = false
 
     init {
+        setOwnerTask(task)
+        initializeStatusBar()
+    }
+
+    init {
         val timer = TimerUtil.createNamedTimer("DelayedBackgroundableProcessIndicator timer", delay) {
             invokeLater(modalityState) {
-                if (isRunning && !isFinishCalled) {
+                if (isRunning && !isFinishCalled && !isDisposed && !myBackgrounded) {
                     background()
                 }
             }
@@ -43,12 +45,44 @@ class DelayedBackgroundableProcessIndicator(task: Task.Backgroundable, delay: In
         timer.start()
     }
 
+    private fun initializeStatusBar() {
+        if (isDisposed || didInitializeOnEdt) return
+        didInitializeOnEdt = true
+        title = task.title
+        if (statusBar == null) {
+            val nonDefaultProject = if (task.project == null || task.project.isDisposed || task.project.isDefault) null else task.project
+            val frame: IdeFrame? = WindowManagerEx.getInstanceEx().findFrameHelper(nonDefaultProject)
+            statusBar = if (frame != null) frame.statusBar as StatusBarEx? else null
+        }
+    }
+
+    override fun background() {
+        if (isDisposed) return
+        assert(didInitializeOnEdt) { "Call to background action before showing dialog" }
+        task.processSentToBackground()
+        doBackground(statusBar)
+        super.background()
+    }
+
+    private fun doBackground(statusBar: StatusBarEx?) {
+        statusBar?.addProgress(this, task)
+    }
+
     override fun prepareShowDialog() {
+        // Don't show the modal window
+    }
+
+    override fun showDialog() {
         // Don't show the modal window
     }
 
     override fun finish(task: TaskInfo) {
         isFinishCalled = true
         super.finish(task)
+    }
+
+    override fun dispose() {
+        super.dispose()
+        isDisposed = true
     }
 }
