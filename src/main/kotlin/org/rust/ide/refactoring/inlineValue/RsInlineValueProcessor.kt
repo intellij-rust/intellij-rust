b70d3b3bc4e015e8596aff552156af4e73993378
Jakub Beránek <jakub.beranek@vsb.cz>
2020-08-31 15:32:28 +0200

    REF: handle struct field shorthand init in RsInlineValue

--- src/main/kotlin/org/rust/ide/refactoring/inlineValue/RsInlineValueProcessor.kt
+++ src/main/kotlin/org/rust/ide/refactoring/inlineValue/RsInlineValueProcessor.kt
@@ -13,6 +13,8 @@ import com.intellij.refactoring.BaseRefactoringProcessor
 import com.intellij.usageView.UsageInfo
 import com.intellij.usageView.UsageViewDescriptor
 import org.rust.ide.refactoring.RsInlineUsageViewDescriptor
+import org.rust.lang.core.psi.RsPsiFactory
+import org.rust.lang.core.psi.RsStructLiteralField
 import org.rust.lang.core.resolve.ref.RsReference
 
 class RsInlineValueProcessor(
@@ -33,9 +35,20 @@ class RsInlineValueProcessor(
     }
 
     override fun performRefactoring(usages: Array<out UsageInfo>) {
+        val factory = RsPsiFactory(project)
         usages.asIterable().forEach loop@{
             val reference = it.reference as? RsReference ?: return@loop
-            reference.element.replace(context.expr)
+            when (val element = reference.element) {
+                is RsStructLiteralField -> {
+                    if (element.colon == null) {
+                        element.addAfter(factory.createColon(), element.referenceNameElement)
+                    }
+                    if (element.expr == null) {
+                        element.addAfter(context.expr, element.colon)
+                    }
+                }
+                else -> element.replace(context.expr)
+            }
         }
         if (mode is InlineValueMode.InlineAllAndRemoveOriginal) {
             context.delete()
