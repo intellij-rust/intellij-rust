db1c83fdda0b8f554678c5888377be2c2565ace3
Arseniy Pendryak <a.pendryak@yandex.ru>
2020-09-14 23:09:02 +0300

    #5915: properly update debugger after NativeDebug plugin update

--- debugger/src/test/kotlin/org/rust/debugger/RsDebuggerToolchainServiceTest.kt
+++ debugger/src/test/kotlin/org/rust/debugger/RsDebuggerToolchainServiceTest.kt
@@ -27,14 +27,35 @@ class RsDebuggerToolchainServiceTest : RsTestBase() {
         super.tearDown()
     }
 
-    fun `test lldb loading`() {
+    fun `test lldb loading and update`() {
         val toolchainService = RsDebuggerToolchainService.getInstance()
         assertEquals(LLDBStatus.NeedToDownload, toolchainService.getLLDBStatus())
 
-        val result = toolchainService.downloadDebugger()
+        downloadDebugger()
 
+        val version = toolchainService.loadLLDBVersions()
+        version[RsDebuggerToolchainService.LLDB_FRONTEND_PROPERTY_NAME] = "lldbfrontend-1234567890-mac-x64"
+        toolchainService.saveLLDBVersions(version)
+
+        assertEquals(LLDBStatus.NeedToUpdate, toolchainService.getLLDBStatus(lldbDir?.absolutePath))
+
+        downloadDebugger()
+    }
+
+    private fun downloadDebugger() {
+        val toolchainService = RsDebuggerToolchainService.getInstance()
+        val result = toolchainService.downloadDebugger()
         check(result is RsDebuggerToolchainService.DownloadResult.Ok) { "Failed to load debugger" }
         lldbDir = result.lldbDir
-        check(toolchainService.getLLDBStatus(result.lldbDir.absolutePath) is LLDBStatus.Binaries)
+        val lldbStatus = toolchainService.getLLDBStatus(result.lldbDir.absolutePath)
+        check(lldbStatus is LLDBStatus.Binaries) {
+            "Unexpected lldb status after downloading: $lldbStatus"
+        }
+
+        fun checkFileExist(file: File) {
+            check(file.exists()) { "Failed to find `${file.absoluteFile}`" }
+        }
+        checkFileExist(lldbStatus.frontendFile)
+        checkFileExist(lldbStatus.frameworkFile)
     }
 }
