0e108d5645d739cef91d25962d3c043a554d3f44
Arseniy Pendryak <a.pendryak@yandex.ru>
2020-09-17 13:22:52 +0300

    #6124: introduce AddRemainingArmsIntention and AddWildcardArmIntention
    These intention do the same as `AddRemainingArmsFix` and `AddWildcardArmFix` quick fixes of `RsNonExhaustiveMatchInspection` respectively.
    They are needed to provide the same actions inside `match` expression not only on `match` keyword
    
    (cherry picked from commit e8938e4d7201bfcba318490b053e17be863065ad)

--- src/main/kotlin/org/rust/ide/inspections/checkMatch/RsNonExhaustiveMatchInspection.kt
+++ src/main/kotlin/org/rust/ide/inspections/checkMatch/RsNonExhaustiveMatchInspection.kt
@@ -7,13 +7,9 @@ package org.rust.ide.inspections.checkMatch
 
 import org.rust.ide.inspections.RsLocalInspectionTool
 import org.rust.ide.inspections.RsProblemsHolder
-import org.rust.ide.utils.checkMatch.*
+import org.rust.ide.utils.checkMatch.checkExhaustive
 import org.rust.lang.core.psi.RsMatchExpr
 import org.rust.lang.core.psi.RsVisitor
-import org.rust.lang.core.psi.ext.arms
-import org.rust.lang.core.types.infer.containsTyOfClass
-import org.rust.lang.core.types.ty.TyUnknown
-import org.rust.lang.core.types.type
 import org.rust.lang.utils.RsDiagnostic
 import org.rust.lang.utils.addToHolder
 
@@ -22,32 +18,8 @@ class RsNonExhaustiveMatchInspection : RsLocalInspectionTool() {
 
     override fun buildVisitor(holder: RsProblemsHolder, isOnTheFly: Boolean) = object : RsVisitor() {
         override fun visitMatchExpr(matchExpr: RsMatchExpr) {
-            val exprType = matchExpr.expr?.type ?: return
-            if (exprType.containsTyOfClass(TyUnknown::class.java)) return
-            try {
-                checkExhaustive(matchExpr, holder)
-            } catch (todo: NotImplementedError) {
-            } catch (e: CheckMatchException) {
-            }
-        }
-    }
-
-    private fun checkExhaustive(match: RsMatchExpr, holder: RsProblemsHolder) {
-        val matchedExprType = match.expr?.type ?: return
-
-        val matrix = match.arms
-            .filter { it.matchArmGuard == null }
-            .calculateMatrix()
-            .takeIf { it.isWellTyped() }
-            ?: return
-
-        val wild = Pattern.wild(matchedExprType)
-        val useful = isUseful(matrix, listOf(wild), true, match.crateRoot, isTopLevel = true)
-
-        /** If `_` pattern is useful, the match is not exhaustive */
-        if (useful is Usefulness.UsefulWithWitness) {
-            val patterns = useful.witnesses.mapNotNull { it.patterns.singleOrNull() }
-            RsDiagnostic.NonExhaustiveMatch(match, patterns).addToHolder(holder)
+            val patterns = matchExpr.checkExhaustive() ?: return
+            RsDiagnostic.NonExhaustiveMatch(matchExpr, patterns).addToHolder(holder)
         }
     }
 }
---
+++ src/main/kotlin/org/rust/ide/intentions/AddRemainingArmsIntention.kt
@@ -0,0 +1,51 @@
+/*
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
+package org.rust.ide.intentions
+
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiElement
+import org.rust.ide.inspections.fixes.AddRemainingArmsFix
+import org.rust.ide.utils.checkMatch.Pattern
+import org.rust.ide.utils.checkMatch.checkExhaustive
+import org.rust.lang.core.psi.RsMatchBody
+import org.rust.lang.core.psi.RsMatchExpr
+
+open class AddRemainingArmsIntention : RsElementBaseIntentionAction<AddRemainingArmsIntention.Context>() {
+
+    override fun getText(): String = AddRemainingArmsFix.NAME
+    override fun getFamilyName(): String = text
+
+    override fun findApplicableContext(project: Project, editor: Editor, element: PsiElement): Context? {
+        val matchExpr = when (val parent = element.context) {
+            is RsMatchExpr -> parent
+            is RsMatchBody -> parent.context as? RsMatchExpr
+            else -> null
+        } ?: return null
+
+        val textRange = matchExpr.match.textRange
+        val caretOffset = editor.caretModel.offset
+        // `RsNonExhaustiveMatchInspection` register its quick fixes only for `match` keyword.
+        // At the same time, platform shows these quick fixes even when caret is located just after the keyword,
+        // i.e. `match/*caret*/`.
+        // So disable this intention for such range not to provide two the same actions to a user
+        if (caretOffset >= textRange.startOffset && caretOffset <= textRange.endOffset) return null
+
+        val patterns = matchExpr.checkExhaustive() ?: return null
+        return Context(matchExpr, patterns)
+    }
+
+    override fun invoke(project: Project, editor: Editor, ctx: Context) {
+        val (matchExpr, patterns) = ctx
+        createQuickFix(matchExpr, patterns).invoke(project, matchExpr.containingFile, matchExpr, matchExpr)
+    }
+
+    protected open fun createQuickFix(matchExpr: RsMatchExpr, patterns: List<Pattern>): AddRemainingArmsFix {
+        return AddRemainingArmsFix(matchExpr, patterns)
+    }
+
+    data class Context(val matchExpr: RsMatchExpr, val patterns: List<Pattern>)
+}
