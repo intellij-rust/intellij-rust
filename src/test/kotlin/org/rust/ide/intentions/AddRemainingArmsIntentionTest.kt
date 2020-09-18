075e371576ba9107bc3e3fdc417bebfc820d853e
Arseniy Pendryak <a.pendryak@yandex.ru>
2020-09-18 11:32:38 +0300

    #6124: check AddRemainingArmsIntention and AddWildcardArmIntention

---
+++ src/test/kotlin/org/rust/ide/intentions/AddRemainingArmsIntentionTest.kt
@@ -0,0 +1,79 @@
+/*
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
+package org.rust.ide.intentions
+
+/**
+ * More tests for base functionality can be found in [org.rust.ide.inspections.match.RsNonExhaustiveMatchInspectionTest]
+ */
+class AddRemainingArmsIntentionTest : RsIntentionTestBase(AddRemainingArmsIntention::class) {
+
+    fun `test empty match`() = doAvailableTest("""
+        enum E { A, B, C }
+        fn main() {
+            let a = E::A;
+            match a {
+                /*caret*/
+            }
+        }
+    """, """
+        enum E { A, B, C }
+        fn main() {
+            let a = E::A;
+            match a {
+                E::A => {}
+                E::B => {}
+                E::C => {}
+            }
+        }
+    """)
+
+    fun `test empty non-exhaustive match`() = doAvailableTest("""
+        fn main() {
+            let a = true;
+            match a/*caret*/ {
+                true => {}
+            }
+        }
+    """, """
+        fn main() {
+            let a = true;
+            match a {
+                true => {}
+                false => {}
+            }
+        }
+    """)
+
+    fun `test do not duplicate inspection quick fixes 1`() = doUnavailableTest("""
+        enum E { A, B, C }
+        fn main() {
+            let a = E::A;
+            /*caret*/match a {
+
+            }
+        }
+    """)
+
+    fun `test do not duplicate inspection quick fixes 2`() = doUnavailableTest("""
+        enum E { A, B, C }
+        fn main() {
+            let a = E::A;
+            match/*caret*/ a {
+
+            }
+        }
+    """)
+
+    fun `test do not suggest from nested code`() = doUnavailableTest("""
+        enum E { A, B, C }
+        fn main() {
+            let a = E::A;
+            match a {
+                E::A => { /*caret*/ }
+            }
+        }
+    """)
+}
---
+++ src/test/kotlin/org/rust/ide/intentions/AddWildcardArmIntentionTest.kt
@@ -0,0 +1,77 @@
+/*
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
+package org.rust.ide.intentions
+
+/**
+ * More tests for base functionality can be found in [org.rust.ide.inspections.match.RsNonExhaustiveMatchInspectionTest]
+ */
+class AddWildcardArmIntentionTest : RsIntentionTestBase(AddWildcardArmIntention::class) {
+
+    fun `test empty match`() = doAvailableTest("""
+        enum E { A, B, C }
+        fn main() {
+            let a = E::A;
+            match a {
+                /*caret*/
+            }
+        }
+    """, """
+        enum E { A, B, C }
+        fn main() {
+            let a = E::A;
+            match a {
+                _ => {}
+            }
+        }
+    """)
+
+    fun `test empty non-exhaustive match`() = doAvailableTest("""
+        fn main() {
+            let a = true;
+            match a/*caret*/ {
+                true => {}
+            }
+        }
+    """, """
+        fn main() {
+            let a = true;
+            match a {
+                true => {}
+                _ => {}
+            }
+        }
+    """)
+
+    fun `test do not duplicate inspection quick fixes 1`() = doUnavailableTest("""
+        enum E { A, B, C }
+        fn main() {
+            let a = E::A;
+            /*caret*/match a {
+
+            }
+        }
+    """)
+
+    fun `test do not duplicate inspection quick fixes 2`() = doUnavailableTest("""
+        enum E { A, B, C }
+        fn main() {
+            let a = E::A;
+            match/*caret*/ a {
+
+            }
+        }
+    """)
+
+    fun `test do not suggest from nested code`() = doUnavailableTest("""
+        enum E { A, B, C }
+        fn main() {
+            let a = E::A;
+            match a {
+                E::A => { /*caret*/ }
+            }
+        }
+    """)
+}
