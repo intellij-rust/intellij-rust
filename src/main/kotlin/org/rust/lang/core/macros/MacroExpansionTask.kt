intellij-rust/intellij-rust

commit d4f93abae2a1bbae235bb0e756859755d003a69e

PERF&MACRO: remove empty SourceFile's (new engine)

src/main/kotlin/org/rust/lang/core/macros/MacroExpansionTask.kt
@@ -319,6 +319,18 @@ object InvalidationPipeline {
     }
 }
 
+class RemoveSourceFileIfEmptyPipeline(private val sf: SourceFile) : Pipeline.Stage1ResolveAndExpand,
+                                                                    Pipeline.Stage2WriteToFs,
+                                                                    Pipeline.Stage3SaveToStorage {
+
+    override fun expand(project: Project, expander: MacroExpander): Pipeline.Stage2WriteToFs = this
+    override fun writeExpansionToFs(fs: MacroExpansionVfsBatch): Pipeline.Stage3SaveToStorage = this
+    override fun save(storage: ExpandedMacroStorage) {
+        checkWriteAccessAllowed()
+        storage.removeSourceFileIfEmpty(sf)
+    }
+}
+
 object ExpansionPipeline {
     class Stage1(
         val call: RsMacroCall,

src/main/kotlin/org/rust/lang/core/macros/ExpandedMacroStorage.kt
@@ -172,7 +172,14 @@ class ExpandedMacroStorage(val project: Project) {
         expandedFileToInfo.remove(oldInfo.fileId)
         val sf = oldInfo.sourceFile
         sf.removeInfo(oldInfo)
-        if (clean && sf.isEmpty()) {
+        if (clean) {
+            removeSourceFileIfEmpty(sf)
+        }
+    }
+
+    fun removeSourceFileIfEmpty(sf: SourceFile) {
+        checkWriteAccessAllowed()
+        if (sf.isEmpty()) {
             sourceFiles.remove(sf.fileId)
             stepped[sf.depth]?.remove(sf)
         }
@@ -477,16 +484,17 @@ class SourceFile(
             FileBasedIndexScanRunnableCollector.getInstance(project).shouldCollect(file) ||
                 project.macroExpansionManager.isExpansionFile(file))
 
-        if (!isIndexedFile) {
+        val stages1 = if (!isIndexedFile) {
             // The file is now outside of the project, so we should not access it.
             // All infos of this file should be invalidated
-            return syncAndMapInfos { info ->
+            syncAndMapInfos { info ->
                 info.markInvalid()
                 info.makeInvalidationPipeline()
             }
+        } else {
+            matchRefs(MakePipeline(), calls)
         }
-
-        return matchRefs(MakePipeline(), calls)
+        return stages1.takeIf { it.isNotEmpty() } ?: listOf(RemoveSourceFileIfEmptyPipeline(this))
     }
 
     private inner class MakePipeline : RefMatcher<List<Pipeline.Stage1ResolveAndExpand>> {

commit 00127688ba81e19e1605432a02dad0022a1a47e4

PERF&MACRO: Take heavy process latch only if there are macros to expand
The regression was introduced in d4f93abae2.

This can slightly speed up code highlighting after `cargo refresh`.
It's mostly invisible to users, I guess.

src/main/kotlin/org/rust/lang/core/macros/MacroExpansionTask.kt
@@ -216,7 +216,7 @@ abstract class MacroExpansionTaskBase(
                 doneStages.addAndGet(if (result is EmptyPipeline) Pipeline.STAGES else 2)
 
                 // Enter heavy process mode only if at least one macros is not up-to-date
-                if (result !is EmptyPipeline) {
+                if (result !is EmptyPipeline && result !is RemoveSourceFileIfEmptyPipeline) {
                     heavyProcessRequested = true
                 }
                 result