c51a23d4ac3ce705ddf80a86cee715500efd8de1
Arseniy Pendryak <a.pendryak@yandex.ru>
2020-10-09 13:26:36 +0300

    T: fix pretty-printer tests restrictions

--- pretty_printers_tests/tests/nonzero.rs
+++ pretty_printers_tests/tests/nonzero.rs
@@ -1,6 +1,6 @@
 // min-version: 1.34.0
 // https://github.com/intellij-rust/intellij-rust/issues/6200
-// max-version: 1.48.0
+// max-version: 1.47.0
 
 // === LLDB TESTS ==================================================================================
 
