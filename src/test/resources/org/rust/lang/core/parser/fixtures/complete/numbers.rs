db6a055340e8f6c188bb054adfb1949837bcd2ac
vlad20012 <beskvlad@gmail.com>
2020-10-14 10:48:52 +0300

    T: move numeric literal parsing tests to a separate file
    (cherry picked from commit 27bbf5ecc275f2953e3f611774ad27ca315e142f)

---
+++ src/test/resources/org/rust/lang/core/parser/fixtures/complete/numbers.rs
@@ -0,0 +1,23 @@
+fn main() {
+    // Float literals
+    let _ = 1.0;
+    let _ = 1f32;
+    let _ = 1f64;
+    let _ = 1.0f64;
+    let _ = 1.0e92;
+    let _ = 1.0e92f32;
+    let _ = 1.;
+    let _ = 10e_6;
+    let _ = 1f34;
+    let _ = 1.0i98;
+    let _ = 0.0.0;
+
+    // Integer literals
+    let _ = 1234567890;
+    let _ = 1234567890i32;
+    let _ = 1_________;
+    let _ = 1_________i32;
+    let _ = 0x1234567890abcdef;
+    let _ = 0o1234567;
+    let _ = 0b10101011101010000111;
+}
