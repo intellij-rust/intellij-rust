f26ccc4d23e6e5e6233ef681203c4204c9067531
vlad20012 <beskvlad@gmail.com>
2020-10-21 23:13:56 +0300

    T: add parser tests for incomplete paths

--- src/test/kotlin/org/rust/lang/core/parser/RsPartialParsingTestCase.kt
+++ src/test/kotlin/org/rust/lang/core/parser/RsPartialParsingTestCase.kt
@@ -30,6 +30,7 @@ class RsPartialParsingTestCase : RsParsingTestCaseBase("partial") {
     fun `test macros`() = doTest(true)
     fun `test exprs`() = doTest(true)
     fun `test bounds`() = doTest(true)
+    fun `test paths`() = doTest(true)
 
     override fun checkResult(targetDataName: String, file: PsiFile) {
         check(hasError(file)) {
