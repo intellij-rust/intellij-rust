68769bc9202358bfc9c1c10f3a55dd45b97e3b22
ortem <ortem00@gmail.com>
2020-11-05 13:31:26 +0300

    DBG: Update BTreeMap/BTreeSet GDB pretty-printers
    * Fixes #5722
    * Handle zero-sized keys or values https://github.com/rust-lang/rust/pull/77788
    * Drop rust-1.33.0 BTreeMap/BTreeSet support
    
    (cherry picked from commit 04a7930086f4c280d20753dfff38a9ca3b6dc055)

--- pretty_printers_tests/tests/btree.rs
+++ pretty_printers_tests/tests/btree.rs
@@ -1,5 +1,4 @@
-// min-version: 1.33.0
-// max-version: 1.43.0
+// min-version: 1.34.0
 
 // === GDB TESTS ===================================================================================
 
@@ -11,6 +10,11 @@
 // gdb-command: print btree_map
 // gdbg-check:$2 = size=15 = {[0] = 0, [1] = 1, [2] = 2, [3] = 3, [4] = 4, [5] = 5, [6] = 6, [7] = 7, [8] = 8, [9] = 9, [10] = 10, [11] = 11, [12] = 12, [13] = 13, [14] = 14}
 
+// gdb-command: print zst_btree_map
+// gdbg-check:$3 = size=1 = {[()] = ()}
+
+// gdb-command: print zst_btree_set
+// gdbg-check:$4 = size=1 = {()}
 
 use std::collections::{BTreeMap, BTreeSet};
 
@@ -24,5 +28,12 @@ fn main() {
     for i in 0..15 {
         btree_map.insert(i, i);
     }
+
+    let mut zst_btree_map: BTreeMap<(), ()> = BTreeMap::new();
+    zst_btree_map.insert((), ());
+
+    let mut zst_btree_set: BTreeSet<()> = BTreeSet::new();
+    zst_btree_set.insert(());
+
     print!(""); // #break
 }
