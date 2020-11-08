7fab7adb06ae72319dbd11ff7bc4d89a893e1260
vlad20012 <beskvlad@gmail.com>
2020-11-08 17:53:32 +0300

    GRAM: allow language injections in string literals inside macro calls

--- src/main/kotlin/org/rust/lang/core/parser/RustParserDefinition.kt
+++ src/main/kotlin/org/rust/lang/core/parser/RustParserDefinition.kt
@@ -94,6 +94,6 @@ class RustParserDefinition : ParserDefinition {
         /**
          * Should be increased after any change of parser rules
          */
-        const val PARSER_VERSION: Int = LEXER_VERSION + 19
+        const val PARSER_VERSION: Int = LEXER_VERSION + 20
     }
 }
