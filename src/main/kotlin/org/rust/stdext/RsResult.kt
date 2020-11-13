8b844a1679481466ad7816c770727a7c09d94461
vlad20012 <beskvlad@gmail.com>
2020-11-13 12:31:09 +0300

    Introduce `RsResult` - our equivalent of Rust's Result in Kotlin
    It's called `RsResult` instead of `Result` because there is
    `kotlin.Result` in the Kotlin stdlib and it is imported by default
    
    Our equivalent to Rust's `result?` (or `try!(result)`) is
    `result.unwrapOrElse { return Err(it) }`

---
+++ src/main/kotlin/org/rust/stdext/RsResult.kt
@@ -0,0 +1,34 @@
+/*
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
+package org.rust.stdext
+
+sealed class RsResult<T, E> {
+    data class Ok<T, E>(val ok: T) : RsResult<T, E>()
+    data class Err<T, E>(val err: E) : RsResult<T, E>()
+
+    val isOk: Boolean get() = this is Ok
+    val isErr: Boolean get() = this is Err
+
+    fun ok(): T? = when (this) {
+        is Ok -> ok
+        is Err -> null
+    }
+
+    inline fun <U> map(mapper: (T) -> U): RsResult<U, E> = when (this) {
+        is Ok -> Ok(mapper(ok))
+        is Err -> Err(err)
+    }
+
+    inline fun <U> andThen(action: (T) -> RsResult<U, E>): RsResult<U, E> = when (this) {
+        is Ok -> action(ok)
+        is Err -> Err(err)
+    }
+
+    inline fun unwrapOrElse(op: (E) -> T): T = when (this) {
+        is Ok -> ok
+        is Err -> op(err)
+    }
+}
