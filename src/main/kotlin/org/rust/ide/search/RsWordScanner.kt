70ce118fea0d443dedd7446c820ace7461c04eb4
vlad20012 <beskvlad@gmail.com>
2020-11-18 19:46:42 +0300

    Support `Find Usages` action for Cargo features

--- src/main/kotlin/org/rust/ide/search/RsWordScanner.kt
+++ src/main/kotlin/org/rust/ide/search/RsWordScanner.kt
@@ -9,14 +9,24 @@ import com.intellij.lang.cacheBuilder.DefaultWordsScanner
 import com.intellij.psi.tree.TokenSet
 import org.rust.lang.core.lexer.RsLexer
 import org.rust.lang.core.parser.RustParserDefinition
+import org.rust.lang.core.psi.RS_ALL_STRING_LITERALS
 import org.rust.lang.core.psi.RS_COMMENTS
-import org.rust.lang.core.psi.RsElementTypes.*
+import org.rust.lang.core.psi.RsElementTypes.IDENTIFIER
 
 class RsWordScanner : DefaultWordsScanner(
     RsLexer(),
     TokenSet.create(IDENTIFIER),
     RS_COMMENTS,
-    TokenSet.create(STRING_LITERAL)
+    RS_ALL_STRING_LITERALS
 ) {
-    override fun getVersion(): Int = RustParserDefinition.LEXER_VERSION
+    init {
+        // This actually means that it's possible to do language injections into Rust string literals
+        setMayHaveFileRefsInLiterals(true)
+    }
+
+    override fun getVersion(): Int = RustParserDefinition.LEXER_VERSION + VERSION
+
+    companion object {
+        private const val VERSION = 1
+    }
 }
