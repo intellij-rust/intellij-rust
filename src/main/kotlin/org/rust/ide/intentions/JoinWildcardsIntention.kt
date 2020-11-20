81aa89cb895dcfa0ebb67ed899378961e31a56e7
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2020-11-20 02:45:59 +0100

    Also join wildcards in tuples and slices

--- src/main/kotlin/org/rust/ide/intentions/JoinWildcardsIntention.kt
+++ src/main/kotlin/org/rust/ide/intentions/JoinWildcardsIntention.kt
@@ -8,8 +8,7 @@ package org.rust.ide.intentions
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
-import org.rust.lang.core.psi.RsPatTupleStruct
-import org.rust.lang.core.psi.RsPatWild
+import org.rust.lang.core.psi.*
 import org.rust.lang.core.psi.ext.ancestorStrict
 import org.rust.lang.core.psi.ext.endOffset
 import org.rust.lang.core.psi.ext.startOffset
@@ -19,8 +18,14 @@ class JoinWildcardsIntention : RsElementBaseIntentionAction<List<RsPatWild>>() {
     override fun getText(): String = familyName
 
     override fun findApplicableContext(project: Project, editor: Editor, element: PsiElement): List<RsPatWild>? {
-        val structPat = element.ancestorStrict<RsPatTupleStruct>() ?: return null
-        val patList = structPat.patList
+        val patList = when (val pat = element.ancestorStrict<RsPat>() ?: return null) {
+            is RsPatTup -> pat.patList
+            is RsPatTupleStruct -> pat.patList
+            is RsPatSlice -> pat.patList
+            // note that RsOrPat also has (and allows) multiple _ wildcards.
+            // it does, however, not support `..`, so we leave it out for this inspection.
+            else -> return null
+        }
         val wildcards = patList.takeLastWhile { it is RsPatWild }.map { it as RsPatWild }
 
         // There is more than one wildcard, thus it is useful to join to `..`
@@ -30,9 +35,9 @@ class JoinWildcardsIntention : RsElementBaseIntentionAction<List<RsPatWild>>() {
         } else null
     }
 
-    override fun invoke(project: Project, editor: Editor, wildcards: List<RsPatWild>) {
-        val startOffset = wildcards.first().startOffset
-        val endOffset = wildcards.last().endOffset
+    override fun invoke(project: Project, editor: Editor, ctx: List<RsPatWild>) {
+        val startOffset = ctx.first().startOffset
+        val endOffset = ctx.last().endOffset
         editor.document.replaceString(startOffset, endOffset, "..")
     }
 }
