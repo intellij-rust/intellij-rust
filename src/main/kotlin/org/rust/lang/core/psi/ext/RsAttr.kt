0b1fd232f60527cac3afe02ff2c45ce5d0004ffd
vlad20012 <beskvlad@gmail.com>
2020-11-29 20:37:06 +0300

    Fix `RsAttr.owner`

--- src/main/kotlin/org/rust/lang/core/psi/ext/RsAttr.kt
+++ src/main/kotlin/org/rust/lang/core/psi/ext/RsAttr.kt
@@ -5,9 +5,7 @@
 
 package org.rust.lang.core.psi.ext
 
-import org.rust.lang.core.psi.RsInnerAttr
-import org.rust.lang.core.psi.RsMetaItem
-import org.rust.lang.core.psi.RsOuterAttr
+import org.rust.lang.core.psi.*
 
 interface RsAttr : RsElement {
     val metaItem: RsMetaItem
@@ -15,8 +13,15 @@ interface RsAttr : RsElement {
 
 val RsAttr.owner: RsDocAndAttributeOwner?
     get() = when (this) {
-        is RsInnerAttr -> parent?.parent as? RsDocAndAttributeOwner
         is RsOuterAttr -> parent as? RsDocAndAttributeOwner
+        is RsInnerAttr -> when (val parent = parent) {
+            is RsMembers -> parent.parent as? RsDocAndAttributeOwner
+            is RsBlock -> when (val parentParent = parent.parent) {
+                is RsFunction -> parentParent
+                else -> parent as? RsDocAndAttributeOwner
+            }
+            else -> parent as? RsDocAndAttributeOwner
+        }
         // Throw exception so that any problems with this property aren't silent and are instead easily findable
         else -> error("Unsupported attribute type: $this")
     }
