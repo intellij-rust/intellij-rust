5f96e7cba3ca08b5ee2135e131e1545f5b63e690
Toshiaki Kameyama <kmymtsak@gmail.com>
2021-01-13 06:14:25 +0900

    Up/Down mover: move `match arm` in closure

--- src/test/kotlin/org/rust/ide/actions/mover/RsMatchArmUpDownMoverTest.kt
+++ src/test/kotlin/org/rust/ide/actions/mover/RsMatchArmUpDownMoverTest.kt
@@ -99,4 +99,190 @@ class RsMatchArmUpDownMoverTest : RsStatementUpDownMoverTestBase() {
             };
         }
     """)
+
+    fun `test inside closure`() = moveUp("""
+        fn main() {
+            let xs: Vec<_> = vec![1,2,3].iter().map(|x| {
+                match x {
+                    1 => 10,
+                    2 => 20,/*caret*/
+                    _ => 30
+                }
+            }).collect();
+        }
+    """, """
+        fn main() {
+            let xs: Vec<_> = vec![1,2,3].iter().map(|x| {
+                match x {
+                    2 => 20,/*caret*/
+                    1 => 10,
+                    _ => 30
+                }
+            }).collect();
+        }
+    """)
+
+    fun `test move to last`() = moveDown("""
+        fn main() {
+            match x {
+                1 => 10,
+                2 => 20,/*caret*/
+                _ => 30,
+            };
+        }
+    """, """
+        fn main() {
+            match x {
+                1 => 10,
+                _ => 30,
+                2 => 20,/*caret*/
+            };
+        }
+    """)
+
+    fun `test move to last 2`() = moveDown("""
+        fn main() {
+            match x {
+                1 => 10,
+                2 => 20,/*caret*/
+                _ => 30
+            };
+        }
+    """, """
+        fn main() {
+            match x {
+                1 => 10,
+                _ => 30,
+                2 => 20,/*caret*/
+            };
+        }
+    """)
+
+    fun `test move to last 3`() = moveDown("""
+        fn main() {
+            match x {
+                1 => 10,
+                2 => 20,/*caret*/
+                _ => {
+                    30;
+                }
+            };
+        }
+    """, """
+        fn main() {
+            match x {
+                1 => 10,
+                _ => {
+                    30;
+                }
+                2 => 20,/*caret*/
+            };
+        }
+    """)
+
+    fun `test move from last`() = moveUp("""
+        fn main() {
+            match x {
+                1 => 10,
+                2 => 20,
+                _ => 30,/*caret*/
+            };
+        }
+    """, """
+        fn main() {
+            match x {
+                1 => 10,
+                _ => 30,/*caret*/
+                2 => 20,
+            };
+        }
+    """)
+
+    fun `test move from last 2`() = moveUp("""
+        fn main() {
+            match x {
+                1 => 10,
+                2 => 20,
+                _ => 30/*caret*/
+            };
+        }
+    """, """
+        fn main() {
+            match x {
+                1 => 10,
+                _ => 30/*caret*/,
+                2 => 20,
+            };
+        }
+    """)
+
+    fun `test move from last 3`() = moveUp("""
+        fn main() {
+            match x {
+                1 => 10,
+                2 => 20,
+                _ => {/*caret*/
+                    30;
+                }
+            };
+        }
+    """, """
+        fn main() {
+            match x {
+                1 => 10,
+                _ => {/*caret*/
+                    30;
+                }
+                2 => 20,
+            };
+        }
+    """)
+
+    fun `test move from last 4`() = moveUp("""
+        fn foo() {
+            match x {
+                1 => 10,
+                2 => {
+                    20;
+                    30;
+                }
+                _ => 40,/*caret*/
+            };
+        }
+    """, """
+        fn foo() {
+            match x {
+                1 => 10,
+                _ => 40,/*caret*/
+                2 => {
+                    20;
+                    30;
+                }
+            };
+        }
+    """)
+
+    fun `test move first down`() = moveDown("""
+        fn foo() {
+            match x {
+                1 => 10,/*caret*/
+                2 => {
+                    20;
+                    30;
+                }
+                _ => 40,
+            };
+        }
+    """, """
+        fn foo() {
+            match x {
+                2 => {
+                    20;
+                    30;
+                }
+                1 => 10,/*caret*/
+                _ => 40,
+            };
+        }
+    """)
 }
