5e4dac9b9a5285d7d1b4a3318dbadac30b97c001
vlad20012 <beskvlad@gmail.com>
2021-03-08 17:04:41 +0300

    GRAM&PSI: put macro argument subtree delimiters inside the nodes

--- src/main/kotlin/org/rust/lang/core/parser/RustParserDefinition.kt
+++ src/main/kotlin/org/rust/lang/core/parser/RustParserDefinition.kt
@@ -92,6 +92,6 @@ class RustParserDefinition : ParserDefinition {
         /**
          * Should be increased after any change of parser rules
          */
-        const val PARSER_VERSION: Int = LEXER_VERSION + 23
+        const val PARSER_VERSION: Int = LEXER_VERSION + 24
     }
 }
