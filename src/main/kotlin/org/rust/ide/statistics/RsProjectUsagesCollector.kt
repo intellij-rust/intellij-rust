76aef7c5181d76928bf59193fd114238e448a39d
Arseniy Pendryak <a.pendryak@yandex.ru>
2021-03-24 18:53:50 +0300

    Add initial version of state usage statistics collectors

---
+++ src/main/kotlin/org/rust/ide/statistics/RsProjectUsagesCollector.kt
@@ -0,0 +1,82 @@
+/*
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
+package org.rust.ide.statistics
+
+import com.intellij.internal.statistic.beans.MetricEvent
+import com.intellij.internal.statistic.eventLog.EventLogGroup
+import com.intellij.internal.statistic.eventLog.events.EventFields
+import com.intellij.internal.statistic.service.fus.collectors.ProjectUsagesCollector
+import com.intellij.openapi.project.Project
+import org.rust.cargo.project.model.cargoProjects
+import org.rust.cargo.project.workspace.PackageOrigin
+
+@Suppress("UnstableApiUsage")
+class RsProjectUsagesCollector : ProjectUsagesCollector() {
+    override fun getGroup(): EventLogGroup = GROUP
+
+    override fun getMetrics(project: Project): Set<MetricEvent> {
+        val metrics = mutableSetOf<MetricEvent>()
+        val cargoProjects = project.cargoProjects.allProjects
+        metrics += CARGO_PROJECTS_EVENT.metric(cargoProjects.size)
+
+        val workspaceInfo = PackagesInfo()
+        val dependenciesInfo = PackagesInfo()
+        for (cargoProject in cargoProjects) {
+            for (pkg in cargoProject.workspace?.packages.orEmpty()) {
+                val info = when (pkg.origin) {
+                    PackageOrigin.WORKSPACE -> workspaceInfo
+                    PackageOrigin.DEPENDENCY -> dependenciesInfo
+                    else -> continue
+                }
+                info.packageCount += 1
+                for (target in pkg.targets) {
+                    when  {
+                        target.kind.isCustomBuild -> info.buildScriptCount += 1
+                        target.kind.isProcMacro -> info.procMacroLibCount += 1
+                    }
+                }
+            }
+        }
+
+        metrics += PACKAGES.metric(workspaceInfo.packageCount, dependenciesInfo.packageCount)
+        metrics += COMPILE_TIME_TARGETS.metric(
+            BUILD_SCRIPT_WORKSPACE.with(workspaceInfo.buildScriptCount),
+            BUILD_SCRIPT_DEPENDENCY.with(dependenciesInfo.buildScriptCount),
+            PROC_MACRO_WORKSPACE.with(workspaceInfo.procMacroLibCount),
+            PROC_MACRO_DEPENDENCY.with(dependenciesInfo.procMacroLibCount)
+        )
+        return metrics
+    }
+
+    private data class PackagesInfo(
+        var packageCount: Int = 0,
+        var buildScriptCount: Int = 0,
+        var procMacroLibCount: Int = 0
+    )
+
+    companion object {
+        private val GROUP = EventLogGroup("rust.project", 1)
+
+        private val CARGO_PROJECTS_EVENT = GROUP.registerEvent("cargo_projects", EventFields.Count)
+
+        private val PACKAGES = GROUP.registerEvent("packages",
+            EventFields.Int("workspace"),
+            EventFields.Int("dependency")
+        )
+
+        private val PROC_MACRO_WORKSPACE = EventFields.Int("proc_macro_workspace")
+        private val PROC_MACRO_DEPENDENCY = EventFields.Int("proc_macro_dependency")
+        private val BUILD_SCRIPT_WORKSPACE = EventFields.Int("build_script_workspace")
+        private val BUILD_SCRIPT_DEPENDENCY = EventFields.Int("build_script_dependency")
+
+        private val COMPILE_TIME_TARGETS = GROUP.registerVarargEvent("compile_time_targets",
+            BUILD_SCRIPT_WORKSPACE,
+            BUILD_SCRIPT_DEPENDENCY,
+            PROC_MACRO_WORKSPACE,
+            PROC_MACRO_DEPENDENCY,
+        )
+    }
+}
