c9b50c210ec432cf49cecc7c007d20b0f57fb46e
0x7CA <12113428+0x7CA@users.noreply.github.com>
2021-04-07 00:16:46 +0200

    INT: Add GenerateDocStubIntention
    Adds Editor Intention to generate documentation stub based on parameters and return

---
+++ src/main/kotlin/org/rust/ide/intentions/GenerateDocstringStubIntention.kt
@@ -0,0 +1,91 @@
+/*
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
+package org.rust.ide.intentions
+
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiDocumentManager
+import com.intellij.psi.PsiElement
+import com.intellij.util.text.CharArrayUtil
+import org.rust.lang.core.psi.*
+import org.rust.lang.core.psi.ext.*
+import org.rust.lang.core.psi.impl.RsFunctionImpl
+import org.rust.lang.core.types.ty.Ty
+
+class GenerateDocStubIntention : RsElementBaseIntentionAction<GenerateDocStubIntention.Context>(){
+    override fun getText() = "Generate documentation stub"
+    override fun getFamilyName() = text
+    data class Context(
+        val func: RsElement,
+        val params: List<RsValueParameter>,
+        val returnType: Ty
+    )
+
+    override fun findApplicableContext(project: Project, editor: Editor, element: PsiElement): Context? {
+        val targetFunc = element.ancestorOrSelf<RsGenericDeclaration>() ?: return null
+        if (targetFunc !is RsFunctionImpl) return null
+        if (targetFunc.name != element.text) return null
+        if (targetFunc.text.startsWith("///")) return null
+        val params = targetFunc.valueParameters
+        if (params.isEmpty()){
+            return null
+        }
+        val returnType = targetFunc.returnType
+        return Context(targetFunc, params, returnType)
+    }
+
+    override fun invoke(project: Project, editor: Editor, ctx: Context) {
+        val (targetFunc, params, returnType) = ctx
+        val document = editor.document
+        var commentStartOffset: Int = targetFunc.textRange.startOffset
+        val lineStartOffset = document.getLineStartOffset(document.getLineNumber(commentStartOffset))
+        if (lineStartOffset > 0 && lineStartOffset < commentStartOffset) {
+            val nonWhiteSpaceOffset = CharArrayUtil.shiftBackward(document.charsSequence, commentStartOffset - 1, " \t")
+            commentStartOffset = Math.max(nonWhiteSpaceOffset, lineStartOffset)
+        }
+        val buffer = StringBuilder()
+        var commentBodyRelativeOffset = 0
+        val indentOffset = targetFunc.startOffset - lineStartOffset
+        val indentation = " ".repeat(indentOffset)
+        buffer.append("$indentation/// \n")
+        commentBodyRelativeOffset += buffer.length
+
+        document.insertString(commentStartOffset, buffer)
+        val docManager = PsiDocumentManager.getInstance(project)
+        docManager.commitDocument(document)
+        val stub = generateDocumentStub(indentation, params, returnType)
+        if (stub.isNotEmpty()) {
+            val insertionOffset = commentStartOffset + commentBodyRelativeOffset
+            document.insertString(insertionOffset, stub)
+            docManager.commitDocument(document)
+        }
+        editor.caretModel.moveToOffset(targetFunc.startOffset + buffer.length - indentOffset - 1)
+    }
+}
+
+
+private fun generateDocumentStub(indentation: String, params: List<RsValueParameter>, returnType: Ty): String {
+    val builder = StringBuilder()
+    builder.append("$indentation/// \n")
+    builder.append("$indentation/// # Arguments \n")
+    builder.append("$indentation/// \n")
+    var paramName : String?
+    for (param in params){
+        paramName = param.patText
+        builder.append("$indentation/// * `$paramName`: \n")
+    }
+    builder.append("$indentation/// \n")
+    if (returnType != null) {
+        builder.append("$indentation/// returns: $returnType \n")
+    }
+    builder.append("$indentation/// \n")
+    builder.append("$indentation/// # Examples \n")
+    builder.append("$indentation/// \n")
+    builder.append("$indentation/// ```\n")
+    builder.append("$indentation/// \n")
+    builder.append("$indentation/// ```\n")
+    return builder.toString()
+}
