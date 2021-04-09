b89da638088f55259b864d13829233f7db7963fe
Jakub Beránek <berykubik@gmail.com>
2021-04-09 20:49:38 +0200

    FIX: do not import default type arguments in ChangeSignature refactoring

--- src/main/kotlin/org/rust/ide/refactoring/changeSignature/impl.kt
+++ src/main/kotlin/org/rust/ide/refactoring/changeSignature/impl.kt
@@ -108,7 +108,8 @@ private fun changeReturnType(factory: RsPsiFactory, function: RsFunction, config
         if (config.returnType !is TyUnit) {
             val ret = factory.createRetType(config.returnTypeReference.text)
             function.addAfter(ret, function.valueParameterList) as RsRetType
-            RsImportHelper.importTypeReferencesFromTy(function, config.returnType, useAliases = true)
+            RsImportHelper.importTypeReferencesFromTy(function, config.returnType,
+                useAliases = true, skipUnchangedDefaultTypeArguments = true)
         }
     }
 }
@@ -126,7 +127,8 @@ private fun changeArguments(
     }
     for (parameter in config.parameters) {
         val defaultValue = parameter.defaultValue.item ?: continue
-        RsImportHelper.importTypeReferencesFromElements(arguments, setOf(defaultValue), useAliases = true)
+        RsImportHelper.importTypeReferencesFromElements(arguments, setOf(defaultValue),
+            useAliases = true, skipUnchangedDefaultTypeArguments = true)
     }
     val argumentsCopy = arguments.copy() as RsValueArgumentList
     val argumentsList = argumentsCopy.exprList
@@ -244,7 +246,8 @@ private fun PsiElement.collectSurroundingWhiteSpaceAndComments(): List<PsiElemen
 
 private fun importParameterTypes(descriptors: List<Parameter>, context: RsElement) {
     for (descriptor in descriptors) {
-        RsImportHelper.importTypeReferencesFromElements(context, setOf(descriptor.typeReference), useAliases = true)
+        RsImportHelper.importTypeReferencesFromElements(context, setOf(descriptor.typeReference),
+            useAliases = true, skipUnchangedDefaultTypeArguments = true)
     }
 }
 
