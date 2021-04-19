f371e7c4b6d6c3e6a6a4e45732b5c60faea1f177
Jakub Beránek <berykubik@gmail.com>
2021-04-19 22:19:14 +0200

    TOML: add inspection that offers an update to a newer dependency version

---
+++ toml/src/main/kotlin/org/rust/toml/inspections/CrateVersionInspection.kt
@@ -0,0 +1,41 @@
+/*
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
+package org.rust.toml.inspections
+
+import com.intellij.codeInspection.ProblemsHolder
+import org.rust.toml.crates.local.CargoRegistryCrate
+import org.rust.toml.crates.local.CratesLocalIndexException
+import org.rust.toml.crates.local.CratesLocalIndexService
+import org.toml.lang.psi.TomlValue
+import org.toml.lang.psi.TomlVisitor
+
+abstract class CrateVersionInspection : CargoTomlInspectionToolBase() {
+    override val requiresLocalCrateIndex: Boolean = true
+
+    abstract fun handleCrateVersion(
+        dependency: DependencyCrate,
+        crate: CargoRegistryCrate,
+        versionElement: TomlValue,
+        holder: ProblemsHolder
+    )
+
+    override fun buildCargoTomlVisitor(holder: ProblemsHolder): TomlVisitor {
+        return object : CargoDependencyCrateVisitor() {
+            override fun visitDependency(dependency: DependencyCrate) {
+                if (dependency.isForeign()) return
+
+                val crate = try {
+                    CratesLocalIndexService.getInstance().getCrate(dependency.crateName) ?: return
+                } catch (e: CratesLocalIndexException) {
+                    return
+                }
+
+                val versionElement = dependency.properties["version"] ?: return
+                handleCrateVersion(dependency, crate, versionElement, holder)
+            }
+        }
+    }
+}
