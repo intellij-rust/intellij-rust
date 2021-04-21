ec4ff1cc38f0b8837fa2e290602cf3acd9d075ab
Zhenchi <zhongzc_arch@outlook.com>
2021-04-21 17:54:59 +0800

    more test cases
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>

--- src/main/kotlin/org/rust/lang/core/resolve2/FacadeBuildDefMap.kt
+++ src/main/kotlin/org/rust/lang/core/resolve2/FacadeBuildDefMap.kt
@@ -93,7 +93,7 @@ private fun buildDefMapContainingExplicitItems(
         defMap.importExternCrateMacros(it.usePath.single())
     }
     val modCollectorContext = ModCollectorContext(defMap, crateRootData, context)
-    collectFileAndCalculateHash(crateRoot, crateRootData, crateRootData.macroIndex, modCollectorContext)
+    collectFileAndCalculateHash(crateRoot, crateRootData, crateRootData.macroIndex, modCollectorContext, includeMacroParent = null)
 
     sortImports(context.imports)
     return defMap
