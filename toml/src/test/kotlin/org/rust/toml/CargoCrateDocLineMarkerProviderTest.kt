223477a0680c65cf0d3c335881515bd45fc28cb4
Arseniy Pendryak <a.pendryak@yandex.ru>
2021-05-17 22:14:48 +0300

    T: check urls for crate documentation line markers
    These changes provide test infrastructure (including `MockBrowserLauncher`) to check documentation url that the plugin uses to open in browser.
    Required for #7224

--- toml/src/test/kotlin/org/rust/toml/CargoCrateDocLineMarkerProviderTest.kt
+++ toml/src/test/kotlin/org/rust/toml/CargoCrateDocLineMarkerProviderTest.kt
@@ -5,69 +5,96 @@
 
 package org.rust.toml
 
+import com.intellij.codeInsight.daemon.LineMarkerInfo
+import com.intellij.codeInsight.daemon.impl.DaemonCodeAnalyzerImpl
+import com.intellij.psi.PsiElement
+import org.intellij.lang.annotations.Language
 import org.rust.ProjectDescriptor
 import org.rust.WithStdlibAndDependencyRustProjectDescriptor
+import org.rust.ide.MockBrowserLauncher
+import org.rust.ide.lineMarkers.invokeNavigationHandler
 
 @ProjectDescriptor(WithStdlibAndDependencyRustProjectDescriptor::class)
 class CargoCrateDocLineMarkerProviderTest : CargoTomlLineMarkerProviderTestBase() {
-    fun `test standard version`() = doTestByText("""
+    fun `test standard version`() = doTest("""
         [dependencies]
         base64 = "0.8.0"  # - Open documentation for `base64@^0.8.0`
 
         [target.'cfg(unix)'.dependencies]
         base64 = "0.8.0"  # - Open documentation for `base64@^0.8.0`
-    """)
+    """, "https://docs.rs/base64/^0.8.0", "https://docs.rs/base64/^0.8.0")
 
-    fun `test platform specific`() = doTestByText("""
+    fun `test platform specific`() = doTest("""
         [target.'cfg(unix)'.dependencies]
         base64 = "0.8.0"  # - Open documentation for `base64@^0.8.0`
-    """)
+    """, "https://docs.rs/base64/^0.8.0")
 
-    fun `test standard detailed`() = doTestByText("""
+    fun `test standard detailed`() = doTest("""
         [dependencies]
         jquery = { version = "1.0.2", optional = true }  # - Open documentation for `jquery@^1.0.2`
-    """)
+    """, "https://docs.rs/jquery/^1.0.2")
 
-    fun `test standard dev`() = doTestByText("""
+    fun `test standard dev`() = doTest("""
         [dev-dependencies]
         libc = "0.2.33"  # - Open documentation for `libc@^0.2.33`
-    """)
+    """, "https://docs.rs/libc/^0.2.33")
 
-    fun `test standard build`() = doTestByText("""
+    fun `test standard build`() = doTest("""
         [build-dependencies]
         libc = "0.2.33"  # - Open documentation for `libc@^0.2.33`
-    """)
+    """, "https://docs.rs/libc/^0.2.33")
 
-    fun `test specific crate`() = doTestByText("""
+    fun `test specific crate`() = doTest("""
         [dependencies.clap]  # - Open documentation for `clap@^2.27.1`
         version = "2.27.1"
-    """)
+    """, "https://docs.rs/clap/^2.27.1")
 
-    fun `test no link to doc`() = doTestByText("""
+    fun `test no link to doc`() = doTest("""
         [dependencies]
         hello_utils = { path = "hello_utils" }
     """)
 
-    fun `test renamed dependencies`() = doTestByText("""
+    fun `test renamed dependencies`() = doTest("""
         [dependencies]
         config_rs = { package = "config", version = "0.9" }  # - Open documentation for `config@^0.9`
-    """)
+    """, "https://docs.rs/config/^0.9")
 
-    fun `test unescape string literals`() = doTestByText("""
+    fun `test unescape string literals`() = doTest("""
         [dependencies]
         serde = '1.0.104'  # - Open documentation for `serde@^1.0.104`
         serde_json = { version = '''1.0.104''' }  # - Open documentation for `serde_json@^1.0.104`
         serde_yaml_rs = { package = 'serde_yaml', version = '''0.8.11''' }  # - Open documentation for `serde_yaml@^0.8.11`
         serde_derive_rs = { package = "serde\u005Fderive", version ="1\u002E0\u002E104" }  # - Open documentation for `serde_derive@^1.0.104`
-    """)
+    """, "https://docs.rs/serde/^1.0.104",
+        "https://docs.rs/serde_json/^1.0.104",
+        "https://docs.rs/serde_yaml/^0.8.11",
+        "https://docs.rs/serde_derive/^1.0.104"
+    )
 
-    fun `test empty version`() = doTestByText("""
+    fun `test empty version`() = doTest("""
         [dependencies]
         base64 = ""  # - Open documentation for `base64@*`
-    """)
+    """, "https://docs.rs/base64/*")
 
-    fun `test exact version`() = doTestByText("""
+    fun `test exact version`() = doTest("""
         [dependencies]
         base64 = "=0.8.0"  # - Open documentation for `base64@=0.8.0`
-    """)
+    """, "https://docs.rs/base64/=0.8.0")
+
+    private fun doTest(@Language("Toml") source: String, vararg expectedUrls: String) {
+        doTestByText(source)
+
+        val launcher = MockBrowserLauncher()
+        launcher.replaceService(testRootDisposable)
+
+        @Suppress("UNCHECKED_CAST")
+        val markers = DaemonCodeAnalyzerImpl.getLineMarkers(myFixture.editor.document, project) as List<LineMarkerInfo<PsiElement>>
+
+        val actualUrls = mutableListOf<String>()
+        for (marker in markers) {
+            marker.invokeNavigationHandler(marker.element)
+            actualUrls += launcher.lastUrl!!
+        }
+        assertEquals(expectedUrls.toList(), actualUrls)
+    }
 }
