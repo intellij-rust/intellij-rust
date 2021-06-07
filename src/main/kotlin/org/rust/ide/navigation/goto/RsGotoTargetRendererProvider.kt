398cb6c03089cd22570d55cfe98093f9e4cefc1d
Arseniy Pendryak <a.pendryak@yandex.ru>
2021-06-07 11:02:51 +0300

    ACT: fix rendering items for `Goto Implementation` action on 2021.2
    Also, use simple renderer for `Goto Implementation` action and line markers provided by `RsImplsLineMarkerProvider`

--- src/main/kotlin/org/rust/ide/navigation/goto/RsGotoTargetRendererProvider.kt
+++ src/main/kotlin/org/rust/ide/navigation/goto/RsGotoTargetRendererProvider.kt
@@ -3,6 +3,9 @@
  * found in the LICENSE file.
  */
 
+// BACKCOMPAT: 2021.1
+@file:Suppress("DEPRECATION")
+
 package org.rust.ide.navigation.goto
 
 import com.intellij.codeInsight.navigation.GotoTargetHandler
@@ -11,31 +14,16 @@ import com.intellij.ide.util.PsiElementListCellRenderer
 import com.intellij.psi.PsiElement
 import org.rust.lang.core.psi.RsImplItem
 import org.rust.lang.core.psi.ext.RsAbstractable
-import org.rust.lang.core.psi.ext.RsAbstractableOwner
-import org.rust.lang.core.psi.ext.owner
 
 class RsGotoTargetRendererProvider : GotoTargetRendererProvider {
-    override fun getRenderer(element: PsiElement, gotoData: GotoTargetHandler.GotoData): PsiElementListCellRenderer<*>? {
-        val targetsInImpl = gotoData.targets.all { (it as? RsAbstractable)?.owner is RsAbstractableOwner.Impl }
-        if (!gotoData.hasDifferentNames() && targetsInImpl) {
-            return GoToImplRenderer()
+    override fun getRenderer(
+        element: PsiElement,
+        gotoData: GotoTargetHandler.GotoData
+    ): PsiElementListCellRenderer<*>? {
+        return if (element is RsImplItem || element is RsAbstractable && !gotoData.hasDifferentNames()) {
+            RsGoToImplRenderer()
+        } else {
+            null
         }
-        if (element is RsImplItem) return ImplRenderer()
-        return null
-    }
-
-    private class GoToImplRenderer : PsiElementListCellRenderer<RsAbstractable>() {
-        override fun getContainerText(element: RsAbstractable, name: String?): String? = element.presentation?.locationString
-        override fun getElementText(element: RsAbstractable): String? =
-            (element.owner as RsAbstractableOwner.Impl).impl.presentation?.presentableText
-
-        override fun getIconFlags(): Int = 0
-    }
-
-
-    private class ImplRenderer : PsiElementListCellRenderer<RsImplItem>() {
-        override fun getContainerText(element: RsImplItem, name: String?): String? = element.presentation?.locationString
-        override fun getElementText(element: RsImplItem): String? = element.presentation?.presentableText
-        override fun getIconFlags(): Int = 0
     }
 }
