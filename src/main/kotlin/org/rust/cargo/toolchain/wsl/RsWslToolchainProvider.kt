cfdad5403e17156717c85c911429e0857ec46b96
Arseniy Pendryak <a.pendryak@yandex.ru>
2021-06-16 15:46:25 +0300

    Move `WSL_TOOLCHAIN` const into `RsExperiments`
    Also, check this feature in `RsWslToolchainProvider`

--- src/main/kotlin/org/rust/cargo/toolchain/wsl/RsWslToolchainProvider.kt
+++ src/main/kotlin/org/rust/cargo/toolchain/wsl/RsWslToolchainProvider.kt
@@ -8,10 +8,13 @@ package org.rust.cargo.toolchain.wsl
 import com.intellij.execution.wsl.WslPath
 import org.rust.cargo.toolchain.RsToolchainBase
 import org.rust.cargo.toolchain.RsToolchainProvider
+import org.rust.ide.experiments.RsExperiments
+import org.rust.openapiext.isFeatureEnabled
 import java.nio.file.Path
 
 class RsWslToolchainProvider : RsToolchainProvider {
     override fun getToolchain(homePath: Path): RsToolchainBase? {
+        if (!isFeatureEnabled(RsExperiments.WSL_TOOLCHAIN)) return null
         val wslPath = WslPath.parseWindowsUncPath(homePath.toString()) ?: return null
         return RsWslToolchain(wslPath)
     }
--- src/main/kotlin/org/rust/cargo/toolchain/wsl/Utils.kt
+++ src/main/kotlin/org/rust/cargo/toolchain/wsl/Utils.kt
@@ -8,8 +8,6 @@ package org.rust.cargo.toolchain.wsl
 import com.intellij.execution.wsl.WSLDistribution
 import java.nio.file.Path
 
-const val WSL_TOOLCHAIN: String = "org.rust.wsl"
-
 fun WSLDistribution.expandUserHome(path: String): String {
     if (!path.startsWith("~/")) return path
     val userHome = userHome ?: return path
