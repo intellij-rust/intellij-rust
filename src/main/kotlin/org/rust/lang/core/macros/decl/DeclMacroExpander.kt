e7392d31ca730640896ccdd773c81497ffb319e6
vlad20012 <beskvlad@gmail.com>
2021-06-20 09:37:06 +0300

    MACRO: support `pat_param` macro_rules matcher, fix `pat` matcher

--- src/main/kotlin/org/rust/lang/core/macros/decl/DeclMacroExpander.kt
+++ src/main/kotlin/org/rust/lang/core/macros/decl/DeclMacroExpander.kt
@@ -284,7 +284,7 @@ class DeclMacroExpander(val project: Project): MacroExpander<RsDeclMacroData, De
     }
 
     companion object {
-        const val EXPANDER_VERSION = 14
+        const val EXPANDER_VERSION = 15
         private val USELESS_PARENS_EXPRS = tokenSetOf(
             LIT_EXPR, MACRO_EXPR, PATH_EXPR, PAREN_EXPR, TUPLE_EXPR, ARRAY_EXPR, UNIT_EXPR
         )
