39937edaf987e716fad26be1fd7cb394da3f7381
Arseniy Pendryak <a.pendryak@yandex.ru>
2021-06-30 23:04:11 +0300

    Show confirmation dialog on some "dangerous" actions
    Such actions can invoke tools like cargo/rustc that can be overridden by arbitrary binaries that can execute arbitrary code

--- src/main/kotlin/org/rust/ide/notifications/Utils.kt
+++ src/main/kotlin/org/rust/ide/notifications/Utils.kt
@@ -5,12 +5,16 @@
 
 package org.rust.ide.notifications
 
+import com.intellij.ide.IdeBundle
+import com.intellij.ide.impl.confirmLoadingUntrustedProject
+import com.intellij.ide.impl.isTrusted
 import com.intellij.notification.NotificationListener
 import com.intellij.notification.NotificationType
 import com.intellij.notification.Notifications
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.WindowManager
+import org.rust.RsBundle
 
 fun Project.showBalloon(content: String, type: NotificationType, action: AnAction? = null) {
     showBalloon("", content, type, action)
@@ -38,4 +42,15 @@ fun showBalloonWithoutProject(content: String, type: NotificationType) {
 fun Project.setStatusBarText(text: String) {
     val statusBar = WindowManager.getInstance().getStatusBar(this)
     statusBar?.info = text
+}
+
+@Suppress("UnstableApiUsage")
+fun Project.confirmLoadingUntrustedProject(): Boolean {
+    return isTrusted() || confirmLoadingUntrustedProject(
+        this,
+        title = IdeBundle.message("untrusted.project.dialog.title", RsBundle.message("cargo"), 1),
+        message = IdeBundle.message("untrusted.project.dialog.text", RsBundle.message("cargo"), 1),
+        trustButtonText = IdeBundle.message("untrusted.project.dialog.trust.button"),
+        distrustButtonText = IdeBundle.message("untrusted.project.dialog.distrust.button")
+    )
 }
