c5ea2d996ac52d337bd86f6cef68dc1d6c0611b6
Jakub Beránek <berykubik@gmail.com>
2021-07-09 00:26:28 +0200

    INSP: propagate unused import annotation range

--- src/main/kotlin/org/rust/ide/inspections/lints/RsUnusedImportInspection.kt
+++ src/main/kotlin/org/rust/ide/inspections/lints/RsUnusedImportInspection.kt
@@ -18,31 +18,72 @@ class RsUnusedImportInspection : RsLintInspection() {
     override fun getLint(element: PsiElement): RsLint = RsLint.UnusedImports
 
     override fun buildVisitor(holder: RsProblemsHolder, isOnTheFly: Boolean) = object : RsVisitor() {
-        override fun visitUseSpeck(o: RsUseSpeck) {
+        override fun visitUseItem(item: RsUseItem) {
             if (!holder.project.isNewResolveEnabled) return
-            val item = o.ancestorStrict<RsUseItem>() ?: return
             if (item.isReexport) return
-            if (o.path?.resolveStatus != PathResolveStatus.RESOLVED) return
 
             // Do not check uses if there is a child mod inside the current mod
-            val parentMod = o.containingMod
+            val parentMod = item.containingMod
             val hasChildMods = parentMod.expandedItemsExceptImplsAndUses.any {
                 it is RsModItem || it is RsModDeclItem
             }
-            if (o.parentOfType<RsFunction>() == null && hasChildMods) return
+            if (item.parentOfType<RsFunction>() == null && hasChildMods) return
 
-            checkUseSpeck(o, holder)
+            val owner = item.parent as? RsItemsOwner ?: return
+            val usage = owner.pathUsage
+
+            val speck = item.useSpeck ?: return
+
+            val unusedSpeckSet = HashSet<RsUseSpeck>()
+            storeUseSpeckUsage(speck, usage, unusedSpeckSet)
+            markUnusedSpecks(speck, unusedSpeckSet, holder)
         }
     }
 
-    private fun checkUseSpeck(useSpeck: RsUseSpeck, holder: RsProblemsHolder) {
-        if (!useSpeck.isUsed()) {
-            val element = getHighlightElement(useSpeck)
-            holder.registerLintProblem(
-                element,
-                "Unused import: `${useSpeck.text}`"
-            )
+    /**
+     * Traverse all use specks recursively and store information about their usage.
+     */
+    private fun storeUseSpeckUsage(
+        useSpeck: RsUseSpeck,
+        usage: PathUsageMap,
+        unusedSpeckSet: MutableSet<RsUseSpeck>
+    ) {
+        val group = useSpeck.useGroup
+        val isUsed = if (group == null) {
+            isUseSpeckUsed(useSpeck, usage)
+        } else {
+            group.useSpeckList.forEach {
+                storeUseSpeckUsage(it, usage, unusedSpeckSet)
+            }
+            group.useSpeckList.any { it !in unusedSpeckSet }
         }
+        if (!isUsed) {
+            unusedSpeckSet.add(useSpeck)
+        }
+    }
+
+    private fun markUnusedSpecks(
+        useSpeck: RsUseSpeck,
+        unusedSpeckSet: Set<RsUseSpeck>,
+        holder: RsProblemsHolder
+    ) {
+        val used = useSpeck !in unusedSpeckSet
+
+        if (!used) {
+            markAsUnused(useSpeck, holder)
+        } else {
+            useSpeck.useGroup?.useSpeckList?.forEach {
+                markUnusedSpecks(it, unusedSpeckSet, holder)
+            }
+        }
+    }
+
+    private fun markAsUnused(useSpeck: RsUseSpeck, holder: RsProblemsHolder) {
+        val element = getHighlightElement(useSpeck)
+        holder.registerLintProblem(
+            element,
+            "Unused import: `${useSpeck.text}`"
+        )
     }
 }
 
@@ -58,16 +99,8 @@ private fun getHighlightElement(useSpeck: RsUseSpeck): PsiElement {
  * A usage can be either a path that uses the import of the use speck or a method call/associated item available through
  * a trait that is imported by this use speck.
  */
-private fun RsUseSpeck.isUsed(): Boolean {
-    val owner = this.parentOfType<RsUseItem>()?.parent as? RsItemsOwner ?: return true
-    val usage = owner.pathUsage
-    return isUseSpeckUsed(this, usage)
-}
-
 private fun isUseSpeckUsed(useSpeck: RsUseSpeck, usage: PathUsageMap): Boolean {
-    // Use speck with an empty group is always unused
-    val useGroup = useSpeck.useGroup
-    if (useGroup != null && useGroup.useSpeckList.isEmpty()) return false
+    if (useSpeck.path?.resolveStatus != PathResolveStatus.RESOLVED) return true
 
     val items = if (useSpeck.isStarImport) {
         val module = useSpeck.path?.reference?.resolve() as? RsMod ?: return true
