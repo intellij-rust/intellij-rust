598272d9eb1a59ee04608e0e00fa2ca38df25e36
mchernyavsky <chernyavsky.mikhail@gmail.com>
2021-08-30 09:22:32 +0300

    ANN: Annotate Const Generics Defaults as experimental

--- src/main/kotlin/org/rust/ide/annotator/RsErrorAnnotator.kt
+++ src/main/kotlin/org/rust/ide/annotator/RsErrorAnnotator.kt
@@ -1340,6 +1340,11 @@ private fun checkDuplicates(
 }
 
 private fun checkConstGenerics(holder: RsAnnotationHolder, constParameter: RsConstParameter) {
+    val default = constParameter.blockExpr
+    if (default != null) {
+        CONST_GENERICS_DEFAULTS.check(holder, default, "const generics defaults")
+    }
+
     val constGenericAvailability = CONST_GENERICS.availability(constParameter)
     if (constGenericAvailability == AVAILABLE) return
 
