8f3ae0e7c6f0b7aa258cf81d0a2ee477097036da
Aleksei Trifonov <avrong@avrong.me>
2021-09-02 14:06:11 +0300

    Use intellij-rust fork for wasm-pack template

--- src/main/kotlin/org/rust/ide/newProject/RsProjectTemplate.kt
+++ src/main/kotlin/org/rust/ide/newProject/RsProjectTemplate.kt
@@ -21,7 +21,7 @@ open class RsCustomTemplate(
     name: String, val url: String
 ) : RsProjectTemplate(name, false, RsIcons.CARGO_GENERATE) {
     object ProcMacroTemplate : RsCustomTemplate("Procedural Macro", "https://github.com/intellij-rust/rust-procmacro-quickstart-template")
-    object WasmPackTemplate : RsCustomTemplate("WebAssembly Lib", "https://github.com/rustwasm/wasm-pack-template")
+    object WasmPackTemplate : RsCustomTemplate("WebAssembly Lib", "https://github.com/intellij-rust/wasm-pack-template")
 
     val shortLink: String
         get() = url.substringAfter("//")
