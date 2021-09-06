5d70b692662e42b0ca4448d0dfd407fe394d0dd7
Arseniy Pendryak <a.pendryak@yandex.ru>
2021-09-06 14:06:54 +0300

    Sort lint objects

--- src/main/kotlin/org/rust/ide/inspections/lints/RsLint.kt
+++ src/main/kotlin/org/rust/ide/inspections/lints/RsLint.kt
@@ -18,19 +18,24 @@ enum class RsLint(
     private val groupIds: List<String> = emptyList(),
     private val defaultLevel: RsLintLevel = WARN
 ) {
+    // Rustc lints
+    // warnings
     NonSnakeCase("non_snake_case", listOf("bad_style", "nonstandard_style")),
     NonCamelCaseTypes("non_camel_case_types", listOf("bad_style", "nonstandard_style")),
     NonUpperCaseGlobals("non_upper_case_globals", listOf("bad_style", "nonstandard_style")),
     Deprecated("deprecated"),
     UnusedVariables("unused_variables", listOf("unused")),
     UnusedImports("unused_imports", listOf("unused")),
-    WhileTrue("while_true"),
-    NeedlessLifetimes("clippy::needless_lifetimes", listOf("clippy::complexity", "clippy::all", "clippy")),
     UnreachablePattern("unreachable_patterns", listOf("unused")),
+    WhileTrue("while_true"),
     UnreachableCode("unreachable_code"),
     BareTraitObjects("bare_trait_objects", listOf("rust_2018_idioms")),
     NonShorthandFieldPatterns("non_shorthand_field_patterns"),
-    UnknownCrateTypes("unknown_crate_types", defaultLevel = DENY);
+    // errors
+    UnknownCrateTypes("unknown_crate_types", defaultLevel = DENY),
+
+    // CLippy lints
+    NeedlessLifetimes("clippy::needless_lifetimes", listOf("clippy::complexity", "clippy::all", "clippy"));
 
     /**
      * Returns the level of the lint for the given PSI element.
