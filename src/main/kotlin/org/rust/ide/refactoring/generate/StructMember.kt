a18b43fec60b4371309ab3b78d8e364833f55327
Jakub Beránek <berykubik@gmail.com>
2021-09-08 18:15:43 +0200

    REF: support qualified paths in Generate Constructor refactoring

--- src/main/kotlin/org/rust/ide/refactoring/generate/StructMember.kt
+++ src/main/kotlin/org/rust/ide/refactoring/generate/StructMember.kt
@@ -5,14 +5,14 @@
 
 package org.rust.ide.refactoring.generate
 
-import org.rust.lang.core.psi.RsNamedFieldDecl
-import org.rust.lang.core.psi.RsStructItem
-import org.rust.lang.core.psi.RsTupleFieldDecl
+import org.rust.ide.presentation.PsiRenderingOptions
+import org.rust.ide.presentation.TypeSubstitutingPsiRenderer
+import org.rust.ide.presentation.renderTypeReference
+import org.rust.lang.core.psi.*
 import org.rust.lang.core.psi.ext.RsFieldDecl
 import org.rust.lang.core.psi.ext.isTupleStruct
 import org.rust.lang.core.psi.ext.namedFields
 import org.rust.lang.core.psi.ext.positionalFields
-import org.rust.lang.core.psi.substAndGetText
 import org.rust.lang.core.types.Substitution
 
 data class StructMember(
@@ -32,22 +32,31 @@ data class StructMember(
             }
         }
 
-        private fun fromTupleList(tupleFieldList: List<RsTupleFieldDecl>, substitution: Substitution): List<StructMember> {
+        private fun fromTupleList(
+            tupleFieldList: List<RsTupleFieldDecl>,
+            substitution: Substitution
+        ): List<StructMember> {
             return tupleFieldList.mapIndexed { index, tupleField ->
-                val typeName = tupleField.typeReference.substAndGetText(substitution)
+                val typeName = tupleField.typeReference.render(substitution)
                 StructMember("field$index", "()", typeName, tupleField)
             }
         }
 
-        private fun fromFieldList(fieldDeclList: List<RsNamedFieldDecl>, substitution: Substitution): List<StructMember> {
+        private fun fromFieldList(
+            fieldDeclList: List<RsNamedFieldDecl>,
+            substitution: Substitution
+        ): List<StructMember> {
             return fieldDeclList.map {
                 StructMember(
                     it.identifier.text ?: "()",
                     it.identifier.text + ":()",
-                    it.typeReference?.substAndGetText(substitution) ?: "()",
+                    it.typeReference?.render(substitution) ?: "()",
                     it
                 )
             }
         }
     }
 }
+
+private fun RsTypeReference.render(substitution: Substitution): String =
+    TypeSubstitutingPsiRenderer(PsiRenderingOptions(shortPaths = false), substitution).renderTypeReference(this)
