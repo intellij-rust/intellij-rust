a0e0991331b08a6a55ee2ad9936ed058a434171a
mchernyavsky <chernyavsky.mikhail@gmail.com>
2021-09-28 04:38:45 +0300

    GRAM: Parse `if let guard` syntax

--- src/main/kotlin/org/rust/lang/core/parser/RustParserDefinition.kt
+++ src/main/kotlin/org/rust/lang/core/parser/RustParserDefinition.kt
@@ -104,6 +104,6 @@ class RustParserDefinition : ParserDefinition {
         /**
          * Should be increased after any change of parser rules
          */
-        const val PARSER_VERSION: Int = LEXER_VERSION + 34
+        const val PARSER_VERSION: Int = LEXER_VERSION + 35
     }
 }
