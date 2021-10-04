70f5c8ded60f7dbab661dd03be0c640657e5f7fa
Arseniy Pendryak <a.pendryak@yandex.ru>
2021-10-04 18:56:38 +0300

    GRD: update gradle plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,11 +49,11 @@ val compileNativeCodeTaskName = "compileNativeCode"
 
 plugins {
     idea
-    kotlin("jvm") version "1.5.20"
-    id("org.jetbrains.intellij") version "1.1.3"
+    kotlin("jvm") version "1.5.31"
+    id("org.jetbrains.intellij") version "1.2.1"
     id("org.jetbrains.grammarkit") version "2021.1.3"
     id("net.saliman.properties") version "1.5.1"
-    id("org.gradle.test-retry") version "1.2.0"
+    id("org.gradle.test-retry") version "1.3.1"
 }
 
 idea {
@@ -125,8 +125,6 @@ allprojects {
         buildSearchableOptions { enabled = false }
 
         test {
-            // Drop when `org.jetbrains.intellij` plugin version will be at least 1.2.0
-            systemProperty("idea.force.use.core.classloader", "true")
             testLogging {
                 showStandardStreams = prop("showStandardStreams").toBoolean()
                 afterSuite(
