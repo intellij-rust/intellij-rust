6880e76b11ca25bab9cb2ce0735ee761ba569225
Arseniy Pendryak <a.pendryak@yandex.ru>
2021-11-08 11:35:05 +0300

    #8057: fix search scopes for generated code
    In #8012 we started using `IndexableSetContributor` to provide locations that should be indexed. But this solution changes the scope of generated files. As a result, any code that uses index search with `GlobalSearchScope.allScope(project)` or `RsWithMacrosProjectScope` scopes doesn't process code from generated files. For example, it breaks `impl` block search during type inference
    
    These changes use `AdditionalLibraryRootsProvider` to create a fake library from generated code. It restores the previous search scope for generated code and makes index search work as before

--- src/main/kotlin/org/rust/lang/core/indexing/RsIndexableSetContributor.kt
+++ src/main/kotlin/org/rust/lang/core/indexing/RsIndexableSetContributor.kt
@@ -8,7 +8,6 @@ package org.rust.lang.core.indexing
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.util.indexing.IndexableSetContributor
-import org.rust.cargo.project.model.cargoProjects
 import org.rust.lang.core.macros.macroExpansionManagerIfCreated
 
 class RsIndexableSetContributor : IndexableSetContributor() {
@@ -17,9 +16,6 @@ class RsIndexableSetContributor : IndexableSetContributor() {
     override fun getAdditionalProjectRootsToIndex(project: Project): Set<VirtualFile> {
         val additionalProjectRootsToIndex = hashSetOf<VirtualFile>()
 
-        project.cargoProjects.allProjects.forEach { cargoProject ->
-            cargoProject.workspace?.packages.orEmpty().mapNotNullTo(additionalProjectRootsToIndex) { it.outDir }
-        }
         project.macroExpansionManagerIfCreated?.indexableDirectory?.let {
             additionalProjectRootsToIndex += it
         }
