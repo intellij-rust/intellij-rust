007e4820f323e7b465a4efee56bdb2d72b94cf91
Arseniy Pendryak <a.pendryak@yandex.ru>
2021-11-11 10:53:25 +0300

    CARGO: prevent some runtime errors in future IDE versions
    Project trusted API will be changed in coming IDE versions (2021.3.1 and 2021.2.4).
    Two main changes:
    - IDE will ask if users trust project on the first project opening for every project so we don't need to do it in the plugin
    - some corresponding API will be changed so we shouldn't call it at all to avoid runtime errors
    
    These changes introduce `isOldTrustedProjectApiAvailable` property to indicate if old API is available and adjust `CargoProjectOpenProcessor` implementation

--- src/main/kotlin/org/rust/cargo/project/CargoProjectOpenProcessor.kt
+++ src/main/kotlin/org/rust/cargo/project/CargoProjectOpenProcessor.kt
@@ -18,6 +18,7 @@ import org.rust.RsBundle
 import org.rust.cargo.CargoConstants
 import org.rust.cargo.icons.CargoIcons
 import org.rust.cargo.project.model.guessAndSetupRustProject
+import org.rust.ide.security.isOldTrustedProjectApiAvailable
 import javax.swing.Icon
 
 class CargoProjectOpenProcessor : ProjectOpenProcessor() {
@@ -33,11 +34,20 @@ class CargoProjectOpenProcessor : ProjectOpenProcessor() {
     override fun doOpenProject(virtualFile: VirtualFile, projectToClose: Project?, forceNewFrame: Boolean): Project? {
         val basedir = if (virtualFile.isDirectory) virtualFile else virtualFile.parent
 
-        val choice = confirmOpeningUntrustedProject(basedir, listOf(RsBundle.message("cargo")))
-        if (choice == OpenUntrustedProjectChoice.CANCEL) return null
+        var choice: OpenUntrustedProjectChoice? = null
+        // Starting with 2021.3.1 and 2021.2.4 IDE always ask users
+        // if they trust project before opening, i.e. we don't need to check it manually.
+        // Moreover, the corresponding API was changed, so we should avoid using old API
+        // not to produce runtime errors
+        if (isOldTrustedProjectApiAvailable) {
+            choice = confirmOpeningUntrustedProject(basedir, listOf(RsBundle.message("cargo")))
+            if (choice == OpenUntrustedProjectChoice.CANCEL) return null
+        }
 
         return PlatformProjectOpenProcessor.getInstance().doOpenProject(basedir, projectToClose, forceNewFrame)?.also {
-            it.setTrusted(choice == OpenUntrustedProjectChoice.IMPORT)
+            if (isOldTrustedProjectApiAvailable) {
+                it.setTrusted(choice == OpenUntrustedProjectChoice.IMPORT)
+            }
             StartupManager.getInstance(it).runWhenProjectIsInitialized { guessAndSetupRustProject(it) }
         }
     }
