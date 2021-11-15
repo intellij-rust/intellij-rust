64f3c8b1a8f3020366c8020e3f35740c39c5a5ff
Mikhail Chernyavsky <mikhail.chernyavsky@jetbrains.com>
2021-11-15 10:15:43 +0300

    ACT: Handle generic arguments in `Add missing type arguments` and `Remove redundant type arguments` quick fixes

--- src/main/kotlin/org/rust/ide/inspections/fixes/AddGenericArguments.kt
+++ src/main/kotlin/org/rust/ide/inspections/fixes/AddGenericArguments.kt
@@ -10,6 +10,7 @@ import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
+import com.intellij.psi.SmartPsiElementPointer
 import org.rust.ide.inspections.getTypeArgumentsAndDeclaration
 import org.rust.ide.utils.template.buildAndRunTemplate
 import org.rust.lang.core.psi.RsElementTypes.COMMA
@@ -17,16 +18,15 @@ import org.rust.lang.core.psi.RsElementTypes.LT
 import org.rust.lang.core.psi.RsPath
 import org.rust.lang.core.psi.RsPsiFactory
 import org.rust.lang.core.psi.RsTypeArgumentList
-import org.rust.lang.core.psi.RsTypeReference
-import org.rust.lang.core.psi.ext.RsElement
-import org.rust.lang.core.psi.ext.elementType
-import org.rust.lang.core.psi.ext.getNextNonCommentSibling
-import org.rust.lang.core.psi.ext.requiredGenericParameters
+import org.rust.lang.core.psi.ext.*
 import org.rust.openapiext.createSmartPointer
 
-class AddTypeArguments(element: RsElement) : LocalQuickFixAndIntentionActionOnPsiElement(element) {
-    override fun getText(): String = "Add missing type arguments"
-    override fun getFamilyName() = text
+class AddGenericArguments(
+    private val declaration: SmartPsiElementPointer<RsGenericDeclaration>,
+    element: RsElement
+) : LocalQuickFixAndIntentionActionOnPsiElement(element) {
+    override fun getText(): String = "Add missing $argumentsName"
+    override fun getFamilyName() = "Add missing generic arguments"
 
     override fun invoke(
         project: Project,
@@ -36,29 +36,40 @@ class AddTypeArguments(element: RsElement) : LocalQuickFixAndIntentionActionOnPs
         endElement: PsiElement
     ) {
         val element = startElement as? RsElement ?: return
-        val inserted = insertTypeArgumentsIfNeeded(element) ?: return
+        val inserted = insertGenericArgumentsIfNeeded(element) ?: return
         editor?.buildAndRunTemplate(element, inserted.map { it.createSmartPointer() })
     }
+
+    private val argumentsName: String
+        get() {
+            val element = declaration.element ?: return "generic arguments"
+            return when {
+                element.typeParameters.isNotEmpty() && element.constParameters.isNotEmpty() -> "generic arguments"
+                element.typeParameters.isNotEmpty() -> "type arguments"
+                element.constParameters.isNotEmpty() -> "const arguments"
+                else -> "generic arguments"
+            }
+        }
 }
 
 /**
- * Inserts type arguments if they are needed and returns a list of inserted type arguments.
+ * Inserts type arguments if they are needed and returns a list of inserted generic arguments.
  */
-fun insertTypeArgumentsIfNeeded(pathOrMethodCall: RsElement): List<RsTypeReference>? {
+fun insertGenericArgumentsIfNeeded(pathOrMethodCall: RsElement): List<RsElement>? {
     val (typeArguments, declaration) = getTypeArgumentsAndDeclaration(pathOrMethodCall) ?: return null
 
     val requiredParameters = declaration.requiredGenericParameters
     if (requiredParameters.isEmpty()) return null
 
-    val argumentCount = typeArguments?.typeReferenceList?.size ?: 0
+    val argumentCount = (typeArguments?.typeReferenceList?.size ?: 0) + (typeArguments?.exprList?.size ?: 0)
     if (argumentCount >= requiredParameters.size) return null
 
     val factory = RsPsiFactory(pathOrMethodCall.project)
-    val missingTypes = requiredParameters.drop(argumentCount).map { it.name ?: "_" }
+    val missingParams = requiredParameters.drop(argumentCount).map { it.name ?: "_" }
 
     val replaced = if (typeArguments != null) {
         var anchor = with(typeArguments) {
-            typeReferenceList.lastOrNull() ?: lifetimeList.lastOrNull() ?: lt
+            (lifetimeList + typeReferenceList + exprList).maxByOrNull { it.startOffset } ?: lt
         }
         val nextSibling = anchor.getNextNonCommentSibling()
         val addCommaAfter = nextSibling?.isComma == true
@@ -66,7 +77,7 @@ fun insertTypeArgumentsIfNeeded(pathOrMethodCall: RsElement): List<RsTypeReferen
             anchor = nextSibling
         }
 
-        for (type in missingTypes) {
+        for (type in missingParams) {
             if (anchor.elementType != LT && !anchor.isComma) {
                 anchor = typeArguments.addAfter(factory.createComma(), anchor)
             }
@@ -79,13 +90,13 @@ fun insertTypeArgumentsIfNeeded(pathOrMethodCall: RsElement): List<RsTypeReferen
 
         typeArguments
     } else {
-        val newArgumentList = factory.createTypeArgumentList(missingTypes)
+        val newArgumentList = factory.createTypeArgumentList(missingParams)
 
         // this can only happen for type references (base types/trait refs)
         if (pathOrMethodCall !is RsPath) return null
         pathOrMethodCall.addAfter(newArgumentList, pathOrMethodCall.identifier) as RsTypeArgumentList
     }
-    return replaced.typeReferenceList.drop(argumentCount)
+    return (replaced.typeReferenceList + replaced.exprList).sortedBy { it.startOffset }.drop(argumentCount)
 }
 
 private val PsiElement.isComma: Boolean
