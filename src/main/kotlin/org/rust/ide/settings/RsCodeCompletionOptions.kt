7e044f38534af8aa78d777493b0084e354f9ea93
Arseniy Pendryak <a.pendryak@yandex.ru>
2021-11-29 12:57:08 +0300

    UI: fix Rust code completion setting panel layout
    Also, simplify `RsCodeCompletionConfigurable` and `RsAutoImportOptions` using `ConfigurableBuilder` API

--- src/main/kotlin/org/rust/ide/settings/RsCodeCompletionOptions.kt
+++ src/main/kotlin/org/rust/ide/settings/RsCodeCompletionOptions.kt
@@ -6,29 +6,10 @@
 package org.rust.ide.settings
 
 import com.intellij.application.options.CodeCompletionOptionsCustomSection
-import com.intellij.ui.IdeBorderFactory
-import com.intellij.ui.components.JBCheckBox
-import com.intellij.ui.layout.panel
-import org.rust.openapiext.CheckboxDelegate
-import javax.swing.JPanel
+import com.intellij.openapi.options.ConfigurableBuilder
 
-class RsCodeCompletionConfigurable : CodeCompletionOptionsCustomSection {
-    private val suggestOutOfScopeItemsCheckbox: JBCheckBox = JBCheckBox("Suggest out of scope items")
-    private var suggestOutOfScopeItems: Boolean by CheckboxDelegate(suggestOutOfScopeItemsCheckbox)
-
-    override fun createComponent(): JPanel = panel {
-        row { suggestOutOfScopeItemsCheckbox() }
-    }.apply { border = IdeBorderFactory.createTitledBorder("Rust") }
-
-    override fun isModified(): Boolean {
-        return suggestOutOfScopeItems != RsCodeInsightSettings.getInstance().suggestOutOfScopeItems
-    }
-
-    override fun apply() {
-        RsCodeInsightSettings.getInstance().suggestOutOfScopeItems = suggestOutOfScopeItems
-    }
-
-    override fun reset() {
-        suggestOutOfScopeItems = RsCodeInsightSettings.getInstance().suggestOutOfScopeItems
+class RsCodeCompletionConfigurable : ConfigurableBuilder("Rust"), CodeCompletionOptionsCustomSection {
+    init {
+        checkBox("Suggest out of scope items", RsCodeInsightSettings.getInstance()::suggestOutOfScopeItems)
     }
 }
