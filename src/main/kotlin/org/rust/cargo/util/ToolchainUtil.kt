cbb91b6f9ca66dc43142c6663dbb86e542e92840
Jayson Reis <santosdosreis@gmail.com>
2021-12-09 12:42:11 +0100

    fix: Use a last resort to try and find toolchains when their binaries are links

--- src/main/kotlin/org/rust/cargo/util/ToolchainUtil.kt
+++ src/main/kotlin/org/rust/cargo/util/ToolchainUtil.kt
@@ -5,6 +5,7 @@
 
 package org.rust.cargo.util
 
+import com.intellij.execution.wsl.WslDistributionManager
 import com.intellij.openapi.util.SystemInfo
 import org.rust.stdext.isExecutable
 import java.nio.file.Path
@@ -12,6 +13,6 @@ import java.nio.file.Path
 fun Path.hasExecutable(toolName: String): Boolean = pathToExecutable(toolName).isExecutable()
 
 fun Path.pathToExecutable(toolName: String): Path {
-    val exeName = if (SystemInfo.isWindows) "$toolName.exe" else toolName
+    val exeName = if (SystemInfo.isWindows && !WslDistributionManager.isWslPath(this.toString())) "$toolName.exe" else toolName
     return resolve(exeName).toAbsolutePath()
 }
