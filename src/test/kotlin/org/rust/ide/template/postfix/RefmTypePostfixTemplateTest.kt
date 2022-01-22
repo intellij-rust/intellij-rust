134374c73fe58a2557f69aa761fe309ae4829900
Jakub Beránek <berykubik@gmail.com>
2022-01-22 10:26:00 +0100

    COMP: enable ref and refm postfix completions for type references

---
+++ src/test/kotlin/org/rust/ide/template/postfix/RefmTypePostfixTemplateTest.kt
@@ -0,0 +1,14 @@
+/*
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
+package org.rust.ide.template.postfix
+
+class RefmTypePostfixTemplateTest : RsPostfixTemplateTest(RefmTypePostfixTemplate(RsPostfixTemplateProvider())) {
+    fun `test type reference`() = doTest("""
+        fn foo(a: str.refm/*caret*/) {}
+    """, """
+        fn foo(a: &mut str/*caret*/) {}
+    """)
+}
