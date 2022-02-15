81d6e2cffeb4dfabd955267d4bab40456fa01aa9
Dmitry Murzin <diralik@yandex.ru>
2022-02-15 15:35:48 +0300

    REF: Fix inline function dialog

--- src/main/kotlin/org/rust/ide/refactoring/inlineFunction/ui.kt
+++ src/main/kotlin/org/rust/ide/refactoring/inlineFunction/ui.kt
@@ -15,23 +15,24 @@ import org.rust.lang.core.resolve.ref.RsReference
 class RsInlineFunctionDialog(
     private val function: RsFunction,
     private val refElement: RsReference?,
+    /** If true, we can't inline other usages */
     private val allowInlineThisOnly: Boolean,
     project: Project = function.project,
-    occurrencesNumber: Int = initOccurrencesNumber(function)
+    private val occurrencesNumber: Int = initOccurrencesNumber(function)
 ) : RsInlineDialog(function, refElement, project, occurrencesNumber) {
     init {
         init()
     }
 
+    override fun canInlineThisOnly(): Boolean = allowInlineThisOnly
+
+    override fun allowInlineAll(): Boolean = true
+
     public override fun doAction() {
-        val inlineThisOnly = allowInlineThisOnly && isInlineThisOnly
-        invokeRefactoring(RsInlineFunctionProcessor(
-            project,
-            function,
-            refElement,
-            inlineThisOnly,
-            !inlineThisOnly && !isKeepTheDeclaration
-        ))
+        val inlineThisOnly = allowInlineThisOnly || isInlineThisOnly
+        val removeDefinition = myRbInlineAll.isSelected && function.isWritable
+        val processor = RsInlineFunctionProcessor(project, function, refElement, inlineThisOnly, removeDefinition)
+        invokeRefactoring(processor)
     }
 
     override fun getBorderTitle(): String =
@@ -46,23 +47,22 @@ class RsInlineFunctionDialog(
     }
 
     override fun getInlineAllText(): String {
-        val text =
-            if (function.isWritable && !allowInlineThisOnly) {
-                "all.invocations.and.remove.the.method"
-            } else {
-                "all.invocations.in.project"
-            }
+        val text = if (function.isWritable) {
+            "all.invocations.and.remove.the.method"
+        } else {
+            "all.invocations.in.project"
+        }
         return RefactoringBundle.message(text)
     }
 
     override fun getInlineThisText(): String =
         RefactoringBundle.message("this.invocation.only.and.keep.the.method")
 
-    override fun getKeepTheDeclarationText(): String =
-        if (function.isWritable) {
-            "Inline all references and keep the method"
+    override fun getKeepTheDeclarationText(): String? =
+        if (function.isWritable && (occurrencesNumber > 1 || !myInvokedOnReference)) {
+            "Inline all and keep the method"
         } else {
-            super.getKeepTheDeclarationText()
+            null
         }
 
     override fun getHelpId(): String =
