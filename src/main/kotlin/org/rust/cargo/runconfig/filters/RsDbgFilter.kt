dcd45dcaa726b6228f092f685333b9374444c5ec
Arseniy Pendryak <a.pendryak@yandex.ru>
2022-03-27 12:24:32 +0300

    RUN: provide file links in terminal for dbg! macro output

---
+++ src/main/kotlin/org/rust/cargo/runconfig/filters/RsDbgFilter.kt
@@ -0,0 +1,17 @@
+/*
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
+package org.rust.cargo.runconfig.filters
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+
+/**
+ * Detects source code locations in dbg! macro output and adds links to them
+ */
+class RsDbgFilter(
+    project: Project,
+    cargoProjectDir: VirtualFile
+) : RegexpFileLinkFilter(project, cargoProjectDir, "\\s*\\[$FILE_POSITION_RE].*")
