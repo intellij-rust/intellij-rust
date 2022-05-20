bae77808dfaf9a3c1f71716d6dee5e9566dfb6c4
vlad20012 <beskvlad@gmail.com>
2022-05-20 13:20:59 +0300

    T: simplify RsGotoDeclarationTest

--- src/test/kotlin/org/rust/ide/navigation/goto/RsGotoDeclarationTest.kt
+++ src/test/kotlin/org/rust/ide/navigation/goto/RsGotoDeclarationTest.kt
@@ -13,125 +13,76 @@ import org.rust.WithStdlibRustProjectDescriptor
 
 class RsGotoDeclarationTest : RsTestBase() {
     fun `test struct declaration`() = doTest("""
-        struct S;
-        type T = /*caret*/S;
-    """, """
-        struct /*caret*/S;
-        type T = S;
+        struct /*caret_after*/S;
+        type T = /*caret_before*/S;
     """)
 
     fun `test defined with a macro`() = doTest("""
         macro_rules! foo { ($ i:item) => { $ i } }
-        foo! { struct S; }
-        type T = /*caret*/S;
-    """, """
-        macro_rules! foo { ($ i:item) => { $ i } }
-        foo! { struct /*caret*/S; }
-        type T = S;
+        foo! { struct /*caret_after*/S; }
+        type T = /*caret_before*/S;
     """)
 
     fun `test defined with a macro with doc comment`() = doTest("""
         macro_rules! foo { ($ i:item) => { $ i } }
         /// docs
-        foo! { struct S; }
-        type T = /*caret*/S;
-    """, """
-        macro_rules! foo { ($ i:item) => { $ i } }
-        /// docs
-        foo! { struct /*caret*/S; }
-        type T = S;
+        foo! { struct /*caret_after*/S; }
+        type T = /*caret_before*/S;
     """)
 
     fun `test defined with nested macros`() = doTest("""
         macro_rules! foo { ($ i:item) => { $ i } }
-        foo! { foo! { struct S; } }
-        type T = /*caret*/S;
-    """, """
-        macro_rules! foo { ($ i:item) => { $ i } }
-        foo! { foo! { struct /*caret*/S; } }
-        type T = S;
+        foo! { foo! { struct /*caret_after*/S; } }
+        type T = /*caret_before*/S;
     """)
 
     fun `test defined with a macro indirectly`() = doTest("""
         macro_rules! foo { ($ i:item) => { $ i } }
-        foo! { mod a { struct S; } }
+        foo! { mod a { struct /*caret_after*/S; } }
         use a::S;
-        type T = /*caret*/S;
-    """, """
-        macro_rules! foo { ($ i:item) => { $ i } }
-        foo! { mod a { struct /*caret*/S; } }
-        use a::S;
-        type T = S;
+        type T = /*caret_before*/S;
     """)
 
     fun `test defined with a macro with struct inside macro definition`() = doTest("""
         macro_rules! foo { () => { struct S; } }
-        foo!();
-        type T = /*caret*/S;
-    """, """
-        macro_rules! foo { () => { struct S; } }
-        /*caret*/foo!();
-        type T = S;
+        /*caret_after*/foo!();
+        type T = /*caret_before*/S;
     """)
 
     @ProjectDescriptor(WithStdlibRustProjectDescriptor::class)
     fun `test resolve path to derive meta item`() = doTest("""
-        #[derive(Default)]
+        #[derive(/*caret_after*/Default)]
         struct S;
-        fn main() { S::/*caret*/default(); }
-    """, """
-        #[derive(/*caret*/Default)]
-        struct S;
-        fn main() { S::default(); }
+        fn main() { S::/*caret_before*/default(); }
     """)
 
     @ProjectDescriptor(WithStdlibRustProjectDescriptor::class)
     fun `test resolve aliased path to derive meta item`() = doTest("""
-        #[derive(Default)]
+        #[derive(/*caret_after*/Default)]
         struct S;
         type T = S;
-        fn main() { T::/*caret*/default(); }
-    """, """
-        #[derive(/*caret*/Default)]
-        struct S;
-        type T = S;
-        fn main() { T::default(); }
+        fn main() { T::/*caret_before*/default(); }
     """)
 
     @ProjectDescriptor(WithStdlibRustProjectDescriptor::class)
     fun `test resolve method to derive meta item`() = doTest("""
-        #[derive(Clone)]
+        #[derive(/*caret_after*/Clone)]
         struct S;
-        fn main() { S./*caret*/clone(); }
-    """, """
-        #[derive(/*caret*/Clone)]
-        struct S;
-        fn main() { S.clone(); }
+        fn main() { S./*caret_before*/clone(); }
     """)
 
     @ProjectDescriptor(WithStdlibRustProjectDescriptor::class)
     fun `test resolve operator to derive meta item`() = doTest("""
-        #[derive(PartialEq)]
+        #[derive(/*caret_after*/PartialEq)]
         struct S;
-        fn main() { S /*caret*/== S; }
-    """, """
-        #[derive(/*caret*/PartialEq)]
-        struct S;
-        fn main() { S == S; }
+        fn main() { S /*caret_before*/== S; }
     """)
 
     fun `test self parameter`() = doTest("""
         struct S;
         impl S {
-            fn foo(&mut self) {
-                /*caret*/self;
-            }
-        }
-    """, """
-        struct S;
-        impl S {
-            fn foo(&mut /*caret*/self) {
-                self;
+            fn foo(&mut /*caret_after*/self) {
+                /*caret_before*/self;
             }
         }
     """)
@@ -139,25 +90,17 @@ class RsGotoDeclarationTest : RsTestBase() {
     fun `test Self type in impl`() = doTest("""
         struct S;
         /// docs
-        impl S {
-            fn foo() -> Self/*caret*/ { unimplemented!() }
-        }
-    """, """
-        struct S;
-        /// docs
-        impl /*caret*/S {
-            fn foo() -> Self { unimplemented!() }
+        impl /*caret_after*/S {
+            fn foo() -> Self/*caret_before*/ { unimplemented!() }
         }
     """)
 
     fun `test associated type binding`() = doTest("""
-        trait Foo { type Item; }
-        type T = dyn Foo</*caret*/Item = i32>;
-    """, """
-        trait Foo { type /*caret*/Item; }
-        type T = dyn Foo<Item = i32>;
+        trait Foo { type /*caret_after*/Item; }
+        type T = dyn Foo</*caret_before*/Item = i32>;
     """)
 
-    private fun doTest(@Language("Rust") before: String, @Language("Rust") after: String) =
-        checkEditorAction(before, after, IdeActions.ACTION_GOTO_DECLARATION)
+    private fun doTest(@Language("Rust") code: String) = checkCaretMove(code) {
+        myFixture.performEditorAction(IdeActions.ACTION_GOTO_DECLARATION)
+    }
 }
