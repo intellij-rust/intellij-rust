cf27eeaa7a9fd4d87768a929b5b94528a4987ce2
Dmitry Murzin <diralik@yandex.ru>
2022-06-14 21:00:24 +0300

    Enable unused imports inspection if proc macros are enabled

--- src/test/kotlin/org/rust/ide/refactoring/move/RsMoveTopLevelItemsTest.kt
+++ src/test/kotlin/org/rust/ide/refactoring/move/RsMoveTopLevelItemsTest.kt
@@ -424,10 +424,10 @@ class RsMoveTopLevelItemsTest : RsMoveTopLevelItemsTestBase() {
     //- lib.rs
         mod mod1 {
             // comment 1
-            #[attr1]
+            #[rustfmt::attr1]
             fn foo1/*caret*/() {}
             /// comment 2
-            #[attr2]
+            #[rustfmt::attr2]
             struct Foo2/*caret*/ {}
             fn bar() {
                 foo1();
@@ -448,11 +448,11 @@ class RsMoveTopLevelItemsTest : RsMoveTopLevelItemsTestBase() {
         }
         mod mod2 {
             // comment 1
-            #[attr1]
+            #[rustfmt::attr1]
             pub fn foo1() {}
 
             /// comment 2
-            #[attr2]
+            #[rustfmt::attr2]
             pub struct Foo2 {}
         }
     """)
