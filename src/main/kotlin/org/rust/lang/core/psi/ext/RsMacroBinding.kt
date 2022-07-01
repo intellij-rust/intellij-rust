aaccbd4093239a1cbaaef9066a6ca28670a7aa73
vlad20012 <beskvlad@gmail.com>
2022-07-01 21:05:31 +0200

    REF&MACRO: fix macro metavar renaming

--- src/main/kotlin/org/rust/lang/core/psi/ext/RsMacroBinding.kt
+++ src/main/kotlin/org/rust/lang/core/psi/ext/RsMacroBinding.kt
@@ -9,11 +9,17 @@ import com.intellij.lang.ASTNode
 import com.intellij.psi.PsiElement
 import com.intellij.psi.search.SearchScope
 import org.rust.lang.core.psi.RsMacroBinding
+import org.rust.lang.core.psi.RsPsiFactory
 import org.rust.lang.core.psi.RsPsiImplUtil.localOrMacroSearchScope
 
 abstract class RsMacroBindingImplMixin(node: ASTNode) : RsNamedElementImpl(node), RsMacroBinding {
 
     override fun getNameIdentifier(): PsiElement? = metaVarIdentifier
+
+    override fun setName(name: String): PsiElement? {
+        nameIdentifier?.replace(RsPsiFactory(project).createMetavarIdentifier(name))
+        return this
+    }
 
     override fun getUseScope(): SearchScope {
         val owner = contextStrict<RsMacroDefinitionBase>() ?: error("Macro binding outside of a macro")
