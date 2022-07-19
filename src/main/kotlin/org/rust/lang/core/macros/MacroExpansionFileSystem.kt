intellij-rust/intellij-rust

commit 74d85146ac4a80f086471cbb75e21429704f4f8f

PERF&MACRO: edit single macro without entering the dumb mode

src/main/kotlin/org/rust/lang/core/macros/MacroExpansionFileSystem.kt
@@ -13,6 +13,7 @@ import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.VirtualFileManager
 import com.intellij.openapi.vfs.newvfs.NewVirtualFileSystem
 import com.intellij.openapi.vfs.newvfs.VfsImplUtil
+import com.intellij.openapi.vfs.newvfs.impl.FakeVirtualFile
 import com.intellij.util.ArrayUtil
 import com.intellij.util.PathUtilRt
 import org.rust.lang.core.macros.MacroExpansionFileSystem.Companion.readFSItem
@@ -100,8 +101,20 @@ class MacroExpansionFileSystem : NewVirtualFileSystem() {
         PathUtilRt.isValidFileName(name, PathUtilRt.Platform.UNIX, false, null)
 
     @Throws(IOException::class)
-    override fun createChildDirectory(requestor: Any?, parent: VirtualFile, dir: String): VirtualFile =
-        throw UnsupportedOperationException()
+    override fun createChildDirectory(requestor: Any?, parent: VirtualFile, dir: String): VirtualFile {
+        if (requestor != TrustedRequestor) {
+            throw UnsupportedOperationException()
+        }
+        val parentFsDir = convert(parent) ?: throw FileNotFoundException("${parent.path} (No such file or directory)")
+        if (parentFsDir !is FSDir) throw IOException("${parent.path} is not a directory")
+        val existingDir = parentFsDir.findChild(dir)
+        if (existingDir == null) {
+            parentFsDir.addChildDir(dir, bump = true)
+        } else if (existingDir !is FSDir) {
+            throw IOException("Directory already contains a file named $dir")
+        }
+        return FakeVirtualFile(parent, dir)
+    }
 
     @Throws(IOException::class)
     override fun createChildFile(requestor: Any?, parent: VirtualFile, file: String): VirtualFile =
@@ -116,12 +129,33 @@ class MacroExpansionFileSystem : NewVirtualFileSystem() {
         throw UnsupportedOperationException()
 
     @Throws(IOException::class)
-    override fun moveFile(requestor: Any?, file: VirtualFile, newParent: VirtualFile): Unit =
-        throw UnsupportedOperationException()
+    override fun moveFile(requestor: Any?, file: VirtualFile, newParent: VirtualFile) {
+        if (requestor != TrustedRequestor) {
+            throw UnsupportedOperationException()
+        }
+        val fsItem = convert(file) ?: throw FileNotFoundException("${file.path} (No such file or directory)")
+        val newParentFsDir = convert(newParent) ?: throw FileNotFoundException("${newParent.path} (No such file or directory)")
+        if (newParentFsDir !is FSDir) throw IOException("${newParent.path} is not a directory")
+        if (newParentFsDir.findChild(file.name) != null) {
+            throw IOException("Directory already contains a file named $file.name")
+        }
+        val oldParentFsDir = fsItem.parent ?: throw IOException("Can't move root (${file.path})")
+        oldParentFsDir.removeChild(fsItem.name, bump = true)
+        fsItem.parent = newParentFsDir
+        newParentFsDir.addChild(fsItem, bump = true)
+    }
 
     @Throws(IOException::class)
-    override fun renameFile(requestor: Any?, file: VirtualFile, newName: String): Unit =
-        throw UnsupportedOperationException()
+    override fun renameFile(requestor: Any?, file: VirtualFile, newName: String) {
+        if (requestor != TrustedRequestor) {
+            throw UnsupportedOperationException()
+        }
+        val fsItem = convert(file) ?: throw FileNotFoundException("${file.path} (No such file or directory)")
+        val parent = fsItem.parent ?: throw IOException("Can't rename root (${file.path})")
+        parent.removeChild(fsItem.name)
+        fsItem.name = newName
+        parent.addChild(fsItem, bump = true)
+    }
 
     private fun convert(file: VirtualFile): FSItem? {
         val parentFile = file.parent ?: return root
@@ -203,7 +237,19 @@ class MacroExpansionFileSystem : NewVirtualFileSystem() {
         modStamp: Long,
         timeStamp: Long
     ): OutputStream {
-        throw UnsupportedOperationException()
+        if (requestor != TrustedRequestor) {
+            throw UnsupportedOperationException()
+        }
+        return object : ByteArrayOutputStream() {
+            @Throws(IOException::class)
+            override fun close() {
+                super.close()
+                val fsItem = convert(file)
+                if (fsItem !is FSFile) throw IOException("The file is a directory (${file.path}")
+                fsItem.length = size()
+                setTimeStamp(file, timeStamp)
+            }
+        }
     }
 
     override fun getLength(file: VirtualFile): Long {
@@ -220,7 +266,7 @@ class MacroExpansionFileSystem : NewVirtualFileSystem() {
     }
 
     sealed class FSItem {
-        abstract val parent: FSDir?
+        abstract var parent: FSDir?
         abstract var name: String
         abstract var timestamp: Long
 
@@ -246,7 +292,12 @@ class MacroExpansionFileSystem : NewVirtualFileSystem() {
         override fun toString(): String = javaClass.simpleName + ": " + name
 
         open class FSDir(
+            @get:Synchronized
+            @set:Synchronized
             override var parent: FSDir?,
+
+            @get:Synchronized
+            @set:Synchronized
             override var name: String,
 
             @get:Synchronized
@@ -320,7 +371,12 @@ class MacroExpansionFileSystem : NewVirtualFileSystem() {
         }
 
         class FSFile(
-            override val parent: FSDir,
+            @get:Synchronized
+            @set:Synchronized
+            override var parent: FSDir?,
+
+            @get:Synchronized
+            @set:Synchronized
             override var name: String,
 
             @get:Synchronized
@@ -444,6 +500,8 @@ class MacroExpansionFileSystem : NewVirtualFileSystem() {
     class FSItemIsADirectoryException(path: String) : FSException(path)
     class FSItemAlreadyExistsException(path: String) : FSException(path)
     class IllegalPathException(path: String) : FSException(path)
+
+    object TrustedRequestor
 
     companion object {
         private const val PROTOCOL: String = "rust-macros"

src/main/kotlin/org/rust/lang/core/macros/MacroExpansionTask.kt
@@ -5,23 +5,29 @@
 
 package org.rust.lang.core.macros
 
+import com.intellij.openapi.application.invokeAndWaitIfNeeded
+import com.intellij.openapi.application.runWriteAction
+import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.progress.ProcessCanceledException
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.Task
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.SimpleModificationTracker
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiDocumentManager
 import org.rust.RsTask
 import org.rust.lang.core.crate.CratePersistentId
 import org.rust.lang.core.macros.MacroExpansionFileSystem.FSItem
+import org.rust.lang.core.macros.MacroExpansionFileSystem.TrustedRequestor
+import org.rust.lang.core.psi.RsPsiManager
 import org.rust.lang.core.resolve2.CrateDefMap
 import org.rust.lang.core.resolve2.DefCollector
 import org.rust.lang.core.resolve2.MacroIndex
 import org.rust.lang.core.resolve2.updateDefMapForAllCrates
-import org.rust.openapiext.testAssert
-import org.rust.openapiext.toThreadSafeProgressIndicator
+import org.rust.openapiext.*
 import org.rust.stdext.HashCode
 import org.rust.stdext.mapToSet
+import java.io.IOException
 
 /**
  * Overview of macro expansion process:
@@ -77,13 +83,27 @@ class MacroExpansionTask(
         MACRO_LOG.debug("Finished macro expansion task in $elapsed2 ms")
     }
 
+    private class FileCreation(
+        val crate: CratePersistentId,
+        val expansionName: String,
+        val content: String,
+        val ranges: RangeMap,
+    )
+
+    private class FileDeletion(
+        val crate: CratePersistentId,
+        val expansionName: String,
+        val file: FSItem.FSFile,
+    )
+
     private data class FileAttributes(
         val path: MacroExpansionVfsBatch.Path,
         val rangeMap: RangeMap,
     )
 
     private fun updateMacrosFiles(allDefMaps: List<CrateDefMap>) {
-        val batch = MacroExpansionVfsBatch("/$MACRO_EXPANSION_VFS_ROOT/$projectDirectoryName")
+        val contentRoot = "/$MACRO_EXPANSION_VFS_ROOT/$projectDirectoryName"
+        val batch = MacroExpansionVfsBatch(contentRoot)
         val hasStaleExpansions = deleteStaleExpansions(allDefMaps, batch)
 
         val defMaps = allDefMaps.filter {
@@ -91,8 +111,17 @@ class MacroExpansionTask(
         }
         if (!hasStaleExpansions && defMaps.isEmpty()) return
 
-        val filesToWriteAttributes = createOrDeleteNeededFiles(defMaps, batch)
-        applyBatchAndWriteAttributes(batch, filesToWriteAttributes)
+        val files = collectFilesForCreationAndDeletion(defMaps)
+
+        val singleChangeApplied = !batch.hasChanges
+            && files.first.size == 1
+            && files.second.size == 1
+            && applySingleChange(contentRoot, files.first.single(), files.second.single())
+
+        if (!singleChangeApplied) {
+            val filesToWriteAttributes = createOrDeleteNeededFiles(files, batch)
+            applyBatchAndWriteAttributes(batch, filesToWriteAttributes)
+        }
 
         modificationTracker.incModificationCount()
         for (defMap in defMaps) {
@@ -100,9 +129,10 @@ class MacroExpansionTask(
         }
     }
 
-    private fun createOrDeleteNeededFiles(defMaps: List<CrateDefMap>, batch: MacroExpansionVfsBatch): List<FileAttributes> {
+    private fun collectFilesForCreationAndDeletion(defMaps: List<CrateDefMap>): Pair<List<FileCreation>, List<FileDeletion>> {
         val expansionSharedCache = MacroExpansionSharedCache.getInstance()
-        val filesToWriteAttributes = mutableListOf<FileAttributes>()
+        val pendingFileWrites = mutableListOf<FileCreation>()
+        val pendingFileDeletions = mutableListOf<FileDeletion>()
         for (defMap in defMaps) {
             val existingFiles = collectExistingFiles(defMap.crate)
 
@@ -118,13 +148,80 @@ class MacroExpansionTask(
                 val expansion = expansionSharedCache.getExpansionIfCached(mixHash)?.ok() ?: continue
 
                 val ranges = expansion.ranges
-                val path = batch.createFile(defMap.crate, expansionName, expansion.text, implicit = true)
-                filesToWriteAttributes += FileAttributes(path, ranges)
+                pendingFileWrites += FileCreation(defMap.crate, expansionName, expansion.text, ranges)
             }
             for (fileName in filesToDelete) {
                 val file = existingFiles.getValue(fileName)
-                batch.deleteFile(file)
+                pendingFileDeletions += FileDeletion(defMap.crate, fileName, file)
             }
+        }
+        return pendingFileWrites to pendingFileDeletions
+    }
+
+    /** An optimization for single macro change (i.e. typing in a macro call) */
+    private fun applySingleChange(
+        contentRoot: String,
+        singleWrite: FileCreation,
+        singleDeletion: FileDeletion
+    ): Boolean {
+        val oldPath = "$contentRoot/${singleDeletion.crate}/${expansionNameToPath(singleDeletion.expansionName)}"
+        val newPath = "$contentRoot/${singleWrite.crate}/${expansionNameToPath(singleWrite.expansionName)}"
+        val lastSlash = newPath.lastIndexOf('/')
+        if (lastSlash == -1) error("unreachable")
+        val newParentPath = newPath.substring(0, lastSlash)
+        val newName = newPath.substring(lastSlash + 1)
+
+        return invokeAndWaitIfNeeded {
+            val root = MacroExpansionFileSystem.getInstance().findFileByPath("/")!!
+            val oldFile = MacroExpansionFileSystem.getInstance().findFileByPath(oldPath)
+                ?: return@invokeAndWaitIfNeeded false
+            val oldPsiFile = oldFile.toPsiFile(project) ?: return@invokeAndWaitIfNeeded false
+            val (nearestNewParentFile, segmentsToCreate) = root.findNearestExistingFile(newParentPath)
+
+            runWriteAction {
+                try {
+                    var newFileParent = nearestNewParentFile
+                    for (segment in segmentsToCreate) {
+                        newFileParent = newFileParent.createChildDirectory(TrustedRequestor, segment)
+                    }
+                    RsPsiManager.withIgnoredPsiEvents(oldPsiFile) {
+                        oldFile.move(TrustedRequestor, newFileParent)
+                        oldFile.rename(TrustedRequestor, newName)
+                    }
+                    oldFile.getOutputStream(TrustedRequestor).use {
+                        it.write(singleWrite.content.toByteArray())
+                    }
+                    oldFile.writeRangeMap(singleWrite.ranges)
+                } catch (e: IOException) {
+                    MACRO_LOG.error(e)
+                    return@runWriteAction false
+                }
+
+                if (isUnitTestMode && runSyncInUnitTests) {
+                    // In unit tests macro expansion task works synchronously, so we have to
+                    // commit the document synchronously too
+                    val doc = FileDocumentManager.getInstance().getCachedDocument(oldFile)
+                    if (doc != null) {
+                        PsiDocumentManager.getInstance(project).commitDocument(doc)
+                    }
+                }
+
+                true
+            }
+        }
+    }
+
+    private fun createOrDeleteNeededFiles(
+        files: Pair<List<FileCreation>, List<FileDeletion>>,
+        batch: MacroExpansionVfsBatch
+    ): List<FileAttributes> {
+        val filesToWriteAttributes = mutableListOf<FileAttributes>()
+        for (fileCreation in files.first) {
+            val path = batch.createFile(fileCreation.crate, fileCreation.expansionName, fileCreation.content, implicit = true)
+            filesToWriteAttributes += FileAttributes(path, fileCreation.ranges)
+        }
+        for (fileDeletion in files.second) {
+            batch.deleteFile(fileDeletion.file)
         }
         return filesToWriteAttributes
     }

src/main/kotlin/org/rust/lang/core/macros/MacroExpansionVfsBatch.kt
@@ -5,13 +5,13 @@
 
 package org.rust.lang.core.macros
 
-import com.intellij.openapi.util.text.StringUtil
 import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.newvfs.RefreshQueue
 import org.jetbrains.annotations.TestOnly
 import org.rust.lang.core.crate.CratePersistentId
 import org.rust.lang.core.macros.MacroExpansionFileSystem.FSItem
+import org.rust.openapiext.findNearestExistingFile
 
 class MacroExpansionVfsBatch(private val contentRoot: String) {
 
@@ -75,7 +75,7 @@ class MacroExpansionVfsBatch(private val contentRoot: String) {
         val root = expansionFileSystem.findFileByPath("/") ?: return
 
         for (path in pathsToMarkDirty) {
-            markDirty(findNearestExistingFile(root, path))
+            markDirty(root.findNearestExistingFile(path).first)
         }
 
         RefreshQueue.getInstance().refresh(async, true, callback, root)
@@ -84,12 +84,4 @@ class MacroExpansionVfsBatch(private val contentRoot: String) {
     private fun markDirty(file: VirtualFile) {
         VfsUtil.markDirty(false, false, file)
     }
-}
-
-private fun findNearestExistingFile(root: VirtualFile, path: String): VirtualFile {
-    var file = root
-    for (segment in StringUtil.split(path, "/")) {
-        file = file.findChild(segment) ?: break
-    }
-    return file
 }

src/main/kotlin/org/rust/lang/core/psi/RsPsiManager.kt
@@ -18,7 +18,6 @@ import com.intellij.openapi.util.ModificationTracker
 import com.intellij.openapi.util.SimpleModificationTracker
 import com.intellij.psi.*
 import com.intellij.psi.util.PsiModificationTracker
-import com.intellij.testFramework.LightVirtualFile
 import com.intellij.util.messages.MessageBusConnection
 import com.intellij.util.messages.Topic
 import org.rust.cargo.project.model.CargoProjectsService
@@ -84,9 +83,6 @@ interface RsPsiManager {
             psi.getUserData(IGNORE_PSI_EVENTS) == true
 
         private fun setIgnorePsiEvents(psi: PsiFile, ignore: Boolean) {
-            val virtualFile = psi.virtualFile ?: return
-            check(virtualFile is LightVirtualFile)
-
             psi.putUserData(IGNORE_PSI_EVENTS, if (ignore) true else null)
         }
     }
@@ -145,8 +141,9 @@ class RsPsiManagerImpl(val project: Project) : RsPsiManager, Disposable {
 
             // if file is null, this is an event about VFS changes
             if (file == null) {
-                if (element is RsFile ||
-                    element is PsiDirectory && project.cargoProjects.findPackageForFile(element.virtualFile) != null) {
+                val isStructureModification = element is RsFile && !isIgnorePsiEvents(element)
+                    || element is PsiDirectory && project.cargoProjects.findPackageForFile(element.virtualFile) != null
+                if (isStructureModification) {
                     incRustStructureModificationCount(element as? RsFile, element as? RsFile)
                 }
             } else {

src/main/kotlin/org/rust/openapiext/utils.kt
@@ -149,6 +149,15 @@ fun VirtualFile.findFileByMaybeRelativePath(path: String): VirtualFile? =
     else
         findFileByRelativePath(path)
 
+fun VirtualFile.findNearestExistingFile(path: String): Pair<VirtualFile, List<String>> {
+    var file = this
+    val segments = StringUtil.split(path, "/")
+    segments.forEachIndexed { i, segment ->
+        file = file.findChild(segment) ?: return file to segments.subList(i, segments.size)
+    }
+    return file to emptyList()
+}
+
 val VirtualFile.pathAsPath: Path get() = Paths.get(path)
 
 fun VirtualFile.toPsiFile(project: Project): PsiFile? =

src/test/kotlin/org/rust/lang/core/macros/RsMacroExpansionCachingTest.kt
@@ -10,6 +10,8 @@ import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiFile
+import com.intellij.psi.impl.PsiManagerEx
+import com.intellij.util.io.storage.HeavyProcessLatch
 import org.intellij.lang.annotations.Language
 import org.rust.ExpandMacros
 import org.rust.TestProject
@@ -21,7 +23,6 @@ import org.rust.lang.core.psi.ext.expansion
 import org.rust.lang.core.psi.ext.stubChildrenOfType
 import org.rust.lang.core.psi.ext.stubDescendantsOfTypeOrSelf
 import org.rust.openapiext.Testmark
-import org.rust.openapiext.TestmarkPred
 
 @ExpandMacros
 class RsMacroExpansionCachingTest : RsMacroExpansionTestBase() {
@@ -53,15 +54,22 @@ class RsMacroExpansionCachingTest : RsMacroExpansionTestBase() {
             listOf(macroCall) + calls.orEmpty()
         }
 
-    private fun checkReExpandedInner(
-        testmark: TestmarkPred?,
+    private fun countDumbModeEnters(): DumbModeCounter {
+        val counter = DumbModeCounter()
+        HeavyProcessLatch.INSTANCE.addListener(testRootDisposable, counter)
+        return counter
+    }
+
+    private fun checkReExpanded(
         action: () -> Unit,
         @Language("Rust") code: String,
-        vararg names: String
+        vararg names: String,
+        allowDumbMode: Boolean = true,
     ) {
         InlineFile(code).withCaret()
         val oldStamps = myFixture.file.collectMacros().collectStamps()
-        testmark?.checkHit { action() } ?: action()
+        val dumbModeCounter = countDumbModeEnters()
+        action()
         val collectMacros = myFixture.file.collectMacros()
         val changed = collectMacros.collectStamps().entries
             .filter { oldStamps[it.key] != it.value }
@@ -69,23 +77,24 @@ class RsMacroExpansionCachingTest : RsMacroExpansionTestBase() {
         check(changed == names.toList()) {
             "Expected to re-expand ${names.asList()}, re-expanded $changed instead"
         }
+        if (!allowDumbMode) {
+            dumbModeCounter.checkDumbModeWasNotEntered()
+        }
     }
-
-    private fun checkReExpanded(action: () -> Unit, @Language("Rust") code: String, vararg names: String) =
-        checkReExpandedInner(null, action, code, *names)
-
-    private fun TestmarkPred.checkReExpanded(action: () -> Unit, @Language("Rust") code: String, vararg names: String) =
-        checkReExpandedInner(this, action, code, *names)
 
     private fun checkReExpandedTree(
         action: (p: TestProject) -> Unit,
         @Language("Rust") code: String,
-        names: List<String>
+        names: List<String>,
+        allowDumbMode: Boolean = true,
     ) {
         val p = fileTreeFromText(code).create()
         val file = p.psiFile("main.rs") as RsFile
-        checkAstNotLoaded()
+        PsiManagerEx.getInstanceEx(project).setAssertOnFileLoadingFilter({
+            it.fileSystem !is MacroExpansionFileSystem
+        }, testRootDisposable)
         val oldStamps = file.collectMacros().collectStamps()
+        val dumbModeCounter = countDumbModeEnters()
         action(p)
         assertNotNull(file.stub)
         val changed = file.collectMacros().collectStamps().entries
@@ -93,6 +102,9 @@ class RsMacroExpansionCachingTest : RsMacroExpansionTestBase() {
             .map { it.key }
         check(changed == names) {
             "Expected to re-expand $names, re-expanded $changed instead"
+        }
+        if (!allowDumbMode) {
+            dumbModeCounter.checkDumbModeWasNotEntered()
         }
     }
 
@@ -113,14 +125,14 @@ class RsMacroExpansionCachingTest : RsMacroExpansionTestBase() {
         macro_rules! bar { ($ i:ident) => { mod $ i {} } }
         foo!(a/*caret*/);
         bar!(b);
-    """, "foo")
+    """, "foo", allowDumbMode = false)
 
     fun `test edit def 1`() = checkReExpanded(type(), """
         macro_rules! foo { ($ i:ident) => { mod $ i {/*caret*/} } }
         macro_rules! bar { ($ i:ident) => { mod $ i {} } }
         foo!(a);
         bar!(b);
-    """, "foo")
+    """, "foo", allowDumbMode = false)
 
     fun `test edit def 2`() = checkReExpanded(type(), """
         macro_rules! foo { ($ i:ident) => { mod $ i {/*caret*/} } }
@@ -141,7 +153,7 @@ class RsMacroExpansionCachingTest : RsMacroExpansionTestBase() {
         macro_rules! foo { () => { mod a/*caret*/ {} } }
         macro_rules! bar { () => { foo!(); } }
         bar!();
-    """, "foo")
+    """, "foo", allowDumbMode = false)
 
     fun `test stub call 1`() = checkReExpandedTree(replaceInFile("main.rs", "aaa", "aab"), """
     //- main.rs
@@ -149,7 +161,7 @@ class RsMacroExpansionCachingTest : RsMacroExpansionTestBase() {
         macro_rules! bar { ($ i:ident) => { mod $ i {} } }
         foo!(aaa);
         bar!(bbb);
-    """, listOf("foo"))
+    """, listOf("foo"), allowDumbMode = false)
 
     fun `test stub call 2`() = checkReExpandedTree(replaceInFile("main.rs", "//", ""), """
     //- main.rs
@@ -256,4 +268,22 @@ class RsMacroExpansionCachingTest : RsMacroExpansionTestBase() {
         struct S2;
         struct S3;
     """)
+
+    private class DumbModeCounter : HeavyProcessLatch.HeavyProcessListener {
+        var count = 0
+
+        override fun processStarted(op: HeavyProcessLatch.Operation) {
+            if (op.type == HeavyProcessLatch.Type.Indexing) {
+                count++
+            }
+        }
+
+        override fun processFinished(op: HeavyProcessLatch.Operation) {}
+
+        fun checkDumbModeWasNotEntered() {
+            check(count == 0) {
+                "Expected Dumb Mode is not entered, actually Dumb Mode has been entered $count time(s)"
+            }
+        }
+    }
 }

commit 3082ea34a00cf4fe3a282b4cc769f93f53b3d310

Fix deadlock in MacroExpansionFileSystem
The bug was introduced in 74d85146ac4a80f086471cbb75e21429704f4f8f by adding @Synchronized` to `FSItem.name`

src/main/kotlin/org/rust/lang/core/macros/MacroExpansionFileSystem.kt
@@ -275,7 +275,6 @@ class MacroExpansionFileSystem : NewVirtualFileSystem() {
             timestamp = max(currentTimestamp(), timestamp + 1)
         }
 
-        @Synchronized
         fun delete() {
             parent?.removeChild(name, bump = true)
         }