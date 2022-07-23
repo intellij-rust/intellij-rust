281d31d6db7b4373a20f0713cdad72bc35189f7f
Jakub Beránek <berykubik@gmail.com>
2022-07-23 12:28:47 +0200

    Add edition2021 to known attributes of doctest injection

--- src/main/kotlin/org/rust/ide/injected/RsDoctestLanguageInjector.kt
+++ src/main/kotlin/org/rust/ide/injected/RsDoctestLanguageInjector.kt
@@ -46,8 +46,9 @@ private val RUST_LANG_ALIASES = listOf(
     "no_run",
     "test_harness",
 //    "compile_fail", // don't highlight code that is expected to contain errors
+    "edition2015",
     "edition2018",
-    "edition2015"
+    "edition2021"
 )
 
 class RsDoctestLanguageInjector : MultiHostInjector {
