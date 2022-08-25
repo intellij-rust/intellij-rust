d6ec46013e336f5536c8be5843c35f17aef2a004
Anton Fetisov <fetisov.anton.u@gmail.com>
2022-08-26 01:01:35 +0400

    TYPE: change folded regions to look more like unfolded ones
    Examples of displayed foldings:
    /* uses */   -->  uses ...;
    /* mod */    -->  mod ...;
    /* crates */ -->  extern crate ...;
    fn f() /* where */ {}   -->   fn f() where ... {}
    
    This should make folded code blocks more distinct from folded block comments (even though the latter
    are almost never used in the wild).
    
    Unfortunately, this means that folding cannot be invoked on unfolded tokens (i.e. `use`, `mod`, `where`,
    `extern crate` and `;` in the examples above). This can be mildly confusing, but at the moment I have no
    better solution.

--- src/test/resources/org/rust/ide/folding/fixtures/where.rs
+++ src/test/resources/org/rust/ide/folding/fixtures/where.rs
@@ -1,8 +1,20 @@
+fn bar() where <fold text='{...}'>{}</fold>
+
 fn foo<X, Y><fold text='(...)'>(x: X, y: Y)</fold> -> ()
-<fold text='/* where */'>where
+where <fold text='...'>
     X: Clone,
     Y: Clone,</fold>
 <fold text='{...}'>{
 
 }</fold>
 
+fn foobar<X, Y><fold text='(...)'>(x: X, y: Y)</fold> -> ()
+where<fold text='...'>
+    X: Clone,
+    Y: Clone,</fold>
+<fold text='{...}'>{
+
+}</fold>
+
+fn foobar<X, Y><fold text='(...)'>(x: X, y: Y)</fold> -> ()
+where
