cb29acfbfe10d8456e426b6bea9909ae2fd54685
Dmitry Murzin <diralik@yandex.ru>
2022-10-17 23:33:33 +0300

    Disable intention preview for some intentions

--- src/main/kotlin/org/rust/ide/annotator/fixes/AddFeatureAttributeFix.kt
+++ src/main/kotlin/org/rust/ide/annotator/fixes/AddFeatureAttributeFix.kt
@@ -5,6 +5,7 @@
 
 package org.rust.ide.annotator.fixes
 
+import com.intellij.codeInsight.intention.FileModifier
 import com.intellij.codeInspection.LocalQuickFixAndIntentionActionOnPsiElement
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
@@ -12,7 +13,10 @@ import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import org.rust.lang.core.psi.RsInnerAttr
 import org.rust.lang.core.psi.RsPsiFactory
-import org.rust.lang.core.psi.ext.*
+import org.rust.lang.core.psi.ext.RsElement
+import org.rust.lang.core.psi.ext.ancestorOrSelf
+import org.rust.lang.core.psi.ext.childrenOfType
+import org.rust.lang.core.psi.ext.name
 
 class AddFeatureAttributeFix(
     private val featureName: String,
@@ -21,6 +25,9 @@ class AddFeatureAttributeFix(
 
     override fun getFamilyName(): String = "Add feature attribute"
     override fun getText(): String = "Add `$featureName` feature"
+
+    // TODO: Add intention preview
+    override fun getFileModifierForPreview(target: PsiFile): FileModifier? = null
 
     override fun invoke(project: Project, file: PsiFile, editor: Editor?, startElement: PsiElement, endElement: PsiElement) {
         addFeatureAttribute(project, startElement, featureName)
