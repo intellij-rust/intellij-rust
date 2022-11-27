f8b09ccaca6adfe58f7864c48bd4fbe6b23899e2
Mikhail Chernyavsky <mikhail.chernyavsky@jetbrains.com>
2022-11-27 19:07:25 +0100

    GRAM: Parse half-open range patterns
    (cherry picked from commit cb178fe3815b650f6ed9fceafed3bb3108257da9)

--- src/main/kotlin/org/rust/lang/core/parser/RustParserDefinition.kt
+++ src/main/kotlin/org/rust/lang/core/parser/RustParserDefinition.kt
@@ -111,6 +111,6 @@ class RustParserDefinition : ParserDefinition {
         /**
          * Should be increased after any change of parser rules
          */
-        const val PARSER_VERSION: Int = LEXER_VERSION + 43
+        const val PARSER_VERSION: Int = LEXER_VERSION + 44
     }
 }
