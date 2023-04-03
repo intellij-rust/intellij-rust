4bdd6899c765b953c648b08eb41ec891d307938e
Louis Vignier <louis.vignier@jetbrains.com>
2023-04-03 14:44:00 +0200

    SSR: Add `org.rust.ssr` experimental feature

--- src/test/kotlin/org/rust/ide/ssr/RsSSRTestBase.kt
+++ src/test/kotlin/org/rust/ide/ssr/RsSSRTestBase.kt
@@ -9,16 +9,20 @@ import com.intellij.structuralsearch.inspection.SSBasedInspection
 import com.intellij.structuralsearch.inspection.StructuralSearchProfileActionProvider
 import com.intellij.structuralsearch.plugin.ui.SearchConfiguration
 import org.intellij.lang.annotations.Language
+import org.rust.ide.experiments.RsExperiments
 import org.rust.ide.inspections.RsInspectionsTestBase
 import org.rust.lang.RsFileType
+import org.rust.openapiext.runWithEnabledFeatures
 
 abstract class RsSSRTestBase : RsInspectionsTestBase(SSBasedInspection::class) {
     protected fun doTest(@Language("Rust") code: String, pattern: String) {
-        val configuration = SearchConfiguration().apply {
-            matchOptions.setFileType(RsFileType)
-            matchOptions.fillSearchCriteria(pattern.trimIndent())
+        runWithEnabledFeatures(RsExperiments.SSR) {
+            val configuration = SearchConfiguration().apply {
+                matchOptions.setFileType(RsFileType)
+                matchOptions.fillSearchCriteria(pattern.trimIndent())
+            }
+            StructuralSearchProfileActionProvider.createNewInspection(configuration, project)
+            checkWarnings(code)
         }
-        StructuralSearchProfileActionProvider.createNewInspection(configuration, project)
-        checkWarnings(code)
     }
 }
