6b2e5677eed9082cb20581a03b6f54641cd01f2b
Mikołaj Komar <mikokomar@op.pl>
2023-05-03 22:47:16 +0200

    PR feedback: Remove unnecessary import

--- src/main/kotlin/org/rust/cargo/toolchain/impl/CargoMetadata.kt
+++ src/main/kotlin/org/rust/cargo/toolchain/impl/CargoMetadata.kt
@@ -5,7 +5,6 @@
 
 package org.rust.cargo.toolchain.impl
 
-import ch.epfl.scala.bsp4j.*
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.diagnostic.logger
