53b3c9d31a010d41fe3b4705ec670afc72cc4112
Mikołaj Komar <mikokomar@op.pl>
2023-05-16 18:34:59 +0200

    Remove unnecessary comments

--- src/main/kotlin/org/rust/bsp/BspBuildTask.kt
+++ src/main/kotlin/org/rust/bsp/BspBuildTask.kt
@@ -1,8 +1,3 @@
-/*
- * Use of this source code is governed by the MIT license that can be
- * found in the LICENSE file.
- */
-
 @file:Suppress("UnstableApiUsage")
 
 package org.rust.bsp
--- src/main/kotlin/org/rust/bsp/service/BspProjectViewService.kt
+++ src/main/kotlin/org/rust/bsp/service/BspProjectViewService.kt
@@ -1,8 +1,3 @@
-/*
- * Use of this source code is governed by the MIT license that can be
- * found in the LICENSE file.
- */
-
 package org.rust.bsp.service
 
 import ch.epfl.scala.bsp4j.BuildTargetIdentifier
