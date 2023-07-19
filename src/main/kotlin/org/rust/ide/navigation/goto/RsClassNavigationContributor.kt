b7ae51db98d8d8617920e92cee97e84733aa696c
Mikhail Chernyavsky <mikhail.chernyavsky@jetbrains.com>
2023-07-19 13:18:37 +0200

    UI: Rename `Classes` to `Types` in Search Everywhere

--- src/main/kotlin/org/rust/ide/navigation/goto/RsClassNavigationContributor.kt
+++ src/main/kotlin/org/rust/ide/navigation/goto/RsClassNavigationContributor.kt
@@ -5,9 +5,15 @@
 
 package org.rust.ide.navigation.goto
 
+import com.intellij.lang.Language
+import org.rust.RsBundle
+import org.rust.lang.RsLanguage
 import org.rust.lang.core.psi.ext.RsNamedElement
 import org.rust.lang.core.stubs.index.RsGotoClassIndex
 
 class RsClassNavigationContributor
-    : RsNavigationContributorBase<RsNamedElement>(RsGotoClassIndex.KEY, RsNamedElement::class.java)
-
+    : RsNavigationContributorBase<RsNamedElement>(RsGotoClassIndex.KEY, RsNamedElement::class.java) {
+    override fun getElementKind(): String = RsBundle.message("go.to.class.kind.text")
+    override fun getElementKindsPluralized(): List<String> = listOf(RsBundle.message("go.to.class.kind.text.pluralized"))
+    override fun getElementLanguage(): Language = RsLanguage
+}
