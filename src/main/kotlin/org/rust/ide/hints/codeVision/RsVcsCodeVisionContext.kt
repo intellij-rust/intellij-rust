b3414e37ee96a323a4614e8682f956f169085b52
vlad20012 <beskvlad@gmail.com>
2023-07-27 17:06:53 +0200

    Show VCS Code Vision hints for more items
    Now an author should be shown for type alias, const/static,
     outer mod declaration (`mod foo`)
     and for a trait alias (unstable Rust feature)

--- src/main/kotlin/org/rust/ide/hints/codeVision/RsVcsCodeVisionContext.kt
+++ src/main/kotlin/org/rust/ide/hints/codeVision/RsVcsCodeVisionContext.kt
@@ -10,8 +10,7 @@ import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.util.registry.Registry
 import com.intellij.psi.PsiElement
 import org.rust.ide.statistics.RsCodeVisionUsageCollector.Companion.logAuthorClicked
-import org.rust.lang.core.psi.RsFunction
-import org.rust.lang.core.psi.RsModItem
+import org.rust.lang.core.psi.*
 import org.rust.lang.core.psi.ext.RsMacroDefinitionBase
 import org.rust.lang.core.psi.ext.RsStructOrEnumItemElement
 import org.rust.lang.core.psi.ext.RsTraitOrImpl
@@ -27,11 +26,16 @@ class RsVcsCodeVisionContext : VcsCodeVisionLanguageContext {
             is RsFunction,
             is RsStructOrEnumItemElement,
             is RsTraitOrImpl,
+            is RsTypeAlias,
+            is RsConstant,
             is RsMacroDefinitionBase,
-            is RsModItem -> true
+            is RsModItem,
+            is RsModDeclItem,
+            is RsTraitAlias -> true
             else -> false
         }
     }
+
     override fun handleClick(mouseEvent: MouseEvent, editor: Editor, element: PsiElement) {
         logAuthorClicked(element)
     }
