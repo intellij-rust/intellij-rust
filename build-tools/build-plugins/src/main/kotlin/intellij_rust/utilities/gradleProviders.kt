bd6e31945c8edbcf6f4228169051c1e814f35636
Adam <897017+aSemy@users.noreply.github.com>
2023-09-06 11:48:44 +0200

    update docs & add errors in Gradle providers utils

--- build-tools/build-plugins/src/main/kotlin/intellij_rust/utilities/gradleProviders.kt
+++ build-tools/build-plugins/src/main/kotlin/intellij_rust/utilities/gradleProviders.kt
@@ -7,10 +7,15 @@ package intellij_rust.utilities
 
 import org.gradle.api.provider.Provider
 
-/** Invert the value of this provider */
+/** Invert the [Boolean] value of this provider. */
 operator fun Provider<Boolean>.not(): Provider<Boolean> =
     map { !it }
 
-fun Provider<String>.toInt(): Provider<Int> = map { it.toInt() }
+/** Convert the contained [String] value to an [Int], failing if the value is not convertable. */
+fun Provider<String>.toInt(): Provider<Int> = map {
+    it.toIntOrNull() ?: error("Tried to convert Provider<String> to an Int, but '$it' is not an Int")
+}
 
-fun Provider<String>.toBoolean(): Provider<Boolean> = map { it.toBoolean() }.orElse(false)
+/** Convert the contained [String] value to a [Boolean], using [default] if the provider has no value. */
+fun Provider<String>.toBoolean(default: Boolean = false): Provider<Boolean> =
+    map { it.toBoolean() }.orElse(default)
