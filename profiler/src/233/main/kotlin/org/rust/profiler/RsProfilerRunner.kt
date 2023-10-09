a2f67dc032cde583cafb04257be0b590418f73a2
Mikhail Chernyavsky <mikhail.chernyavsky@jetbrains.com>
2023-10-09 15:41:00 +0200

    RUST-11458: Disable profiler on non-WSL Windows
    (cherry picked from commit 1f2a6e9ce7f96e7bad1cbe8d614bef6b4f9a86a6)

--- profiler/src/233/main/kotlin/org/rust/profiler/RsProfilerRunner.kt
+++ profiler/src/233/main/kotlin/org/rust/profiler/RsProfilerRunner.kt
@@ -5,17 +5,30 @@
 
 package org.rust.profiler
 
+import com.intellij.execution.configurations.RunConfiguration
 import com.intellij.execution.configurations.RunProfile
+import com.intellij.openapi.util.SystemInfo
 import com.intellij.profiler.clion.ProfilerExecutor
 import com.intellij.profiler.clion.ProfilerRunChecker
 import org.rust.RsBundle
+import org.rust.cargo.project.settings.toolchain
 import org.rust.cargo.runconfig.RsExecutableRunner
+import org.rust.cargo.toolchain.wsl.RsWslToolchain
 
 class RsProfilerRunner : RsExecutableRunner(ProfilerExecutor.EXECUTOR_ID, RsBundle.message("dialog.title.unable.to.run.profiler")) {
     override fun getRunnerId(): String = RUNNER_ID
 
-    override fun canRun(executorId: String, profile: RunProfile): Boolean =
-        ProfilerRunChecker.canRun() && super.canRun(executorId, profile)
+    override fun canRun(executorId: String, profile: RunProfile): Boolean {
+        if (!ProfilerRunChecker.canRun()) return false
+
+        if (SystemInfo.isWindows) {
+            val toolchain = (profile as? RunConfiguration)?.project?.toolchain
+            if (toolchain !is RsWslToolchain) return false
+        }
+
+        return super.canRun(executorId, profile)
+    }
+
 
     companion object {
         const val RUNNER_ID: String = "RsProfilerRunner"
--- profiler/src/233/main/kotlin/org/rust/profiler/legacy/RsProfilerRunnerLegacy.kt
+++ profiler/src/233/main/kotlin/org/rust/profiler/legacy/RsProfilerRunnerLegacy.kt
@@ -5,13 +5,17 @@
 
 package org.rust.profiler.legacy
 
+import com.intellij.execution.configurations.RunConfiguration
 import com.intellij.execution.configurations.RunProfile
 import com.intellij.openapi.util.NlsContexts
+import com.intellij.openapi.util.SystemInfo
 import com.intellij.profiler.clion.ProfilerExecutor
 import com.intellij.profiler.clion.ProfilerRunChecker
 import org.rust.RsBundle
+import org.rust.cargo.project.settings.toolchain
 import org.rust.cargo.runconfig.buildtool.CargoBuildManager.isBuildToolWindowAvailable
 import org.rust.cargo.runconfig.legacy.RsAsyncRunner
+import org.rust.cargo.toolchain.wsl.RsWslToolchain
 
 @NlsContexts.DialogTitle
 private val ERROR_MESSAGE_TITLE: String = RsBundle.message("dialog.title.unable.to.run.profiler")
@@ -22,8 +26,16 @@ private val ERROR_MESSAGE_TITLE: String = RsBundle.message("dialog.title.unable.
 class RsProfilerRunnerLegacy : RsAsyncRunner(ProfilerExecutor.EXECUTOR_ID, ERROR_MESSAGE_TITLE) {
     override fun getRunnerId(): String = RUNNER_ID
 
-    override fun canRun(executorId: String, profile: RunProfile): Boolean =
-        ProfilerRunChecker.canRun() && super.canRun(executorId, profile)
+    override fun canRun(executorId: String, profile: RunProfile): Boolean {
+        if (!ProfilerRunChecker.canRun()) return false
+
+        if (SystemInfo.isWindows) {
+            val toolchain = (profile as? RunConfiguration)?.project?.toolchain
+            if (toolchain !is RsWslToolchain) return false
+        }
+
+        return super.canRun(executorId, profile)
+    }
 
     companion object {
         const val RUNNER_ID: String = "RsProfilerRunnerLegacy"
